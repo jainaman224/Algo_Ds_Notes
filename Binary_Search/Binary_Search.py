8d9245ae63574b6f7428e0c292652c7c4bd30940
jainaman224 <jainaman224@gmail.com>
2016-06-28 17:21:36 +0530

    added ternary search and updated binary search

--- Binary_Search/Binary_Search.py
+++ Binary_Search/Binary_Search.py
@@ -4,14 +4,14 @@ def Binary_Search(array, desired):
     right = len(array) - 1
 
     while left <= right:
-        # return positon if found
-        middle = left + (right - left) / 2;
+        # Return positon if found
+        middle = left + int((right - left) / 2);
 
-        if(array[middle] == desired):
+        if array[middle] == desired:
             return middle
-        elif(desired < array[middle]):
+        elif desired < array[middle]:
             right = middle - 1
-        elif(desired > array[middle]):
+        elif desired > array[middle]:
             left = middle + 1
 
     return -1
@@ -19,17 +19,20 @@ def Binary_Search(array, desired):
 array = [1, 2, 3, 4, 5, 6, 7]
 
 # Element to be searched is 4
-if(Binary_Search(array,4) != -1):
+if Binary_Search(array, 4) != -1:
     print("Found")
 else:
     print("Not Found")
 
 # Element to be searched is 9
-if(Binary_Search(array,9) != -1):
+if Binary_Search(array, 9) != -1:
     print("Found")
 else:
     print("Not Found")
 
-# Output
-# Found
-# Not Found
+''' Output
+
+Found
+Not Found
+
+'''
---
+++ Ternary_Search/Ternary_Search.py
@@ -0,0 +1,43 @@
+# Function for Binary Search
+def Ternary_Search(array, left, right, desired):
+    while left <= right:
+        # Return positon if found
+        mid1 = left + int((right - left) / 3);
+        mid2 = mid1 + int((right - left) / 3);
+
+        if array[mid1] == desired:
+            return mid1
+
+        if array[mid2] == desired:
+            return mid2
+
+        if array[mid1] > desired:
+            return Ternary_Search(array, left, mid1 - 1, desired)
+
+        if array[mid2] < desired:
+            return Ternary_Search(array, mid2 + 1, right, desired)
+
+        return Ternary_Search(array, mid1 + 1, mid2 - 1, desired)
+
+    return -1
+
+array = [1, 2, 3, 4, 5, 6, 7]
+
+# Element to be searched is 4
+if(Ternary_Search(array, 0, 6, 4) != -1):
+    print("Found")
+else:
+    print("Not Found")
+
+# Element to be searched is 9
+if(Ternary_Search(array, 0, 6, 9) != -1):
+    print("Found")
+else:
+    print("Not Found")
+
+''' Output
+
+Found
+Not Found
+
+'''
