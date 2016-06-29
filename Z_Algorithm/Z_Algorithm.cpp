3c34f96fc5c0ef5fd9d09b739b6c4d0b76c02d92
Aman Jain <jainaman224@gmail.com>
2016-06-29 19:56:28 +0530

    Added Z algo (#29)

---
+++ Z_Algorithm/Z_Algorithm.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+
+using namespace std;
+
+void getZarr(string str, int Z[])
+{
+    int n = str.length(), Left = 0, Right = 0, k;
+
+    for(int i = 1; i < n; i++)
+    {
+        if(i > Right)
+        {
+            Left = Right = i;
+
+            while(Right < n && str[Right - Left] == str[Right])
+                Right++;
+
+            Z[i] = Right - Left;
+            Right--;
+        }
+        else
+        {
+            k = i - Left;
+
+            if(Z[k] < Right - i + 1)
+                Z[i] = Z[k];
+            else
+            {
+                Left = i;
+
+                while(Right < n && str[Right - Left] == str[Right])
+                    Right++;
+
+                Z[i] = Right - Left;
+                Right--;
+            }
+        }
+    }
+}
+
+
+void search(string text, string pattern)
+{
+    string concat = pattern + "$" + text;
+    int size = concat.length();
+    int *Z = new int[size];
+
+    getZarr(concat, Z);
+
+    for(int i = 0; i < size; i++)
+        if(Z[i] == pattern.length())
+            cout << "Pattern found at " <<  i - pattern.length() - 1 << endl;
+
+    delete Z;
+}
+
+
+int main()
+{
+    string text = "namanchamanbomanamansanam";
+    string pattern = "aman";
+    search(text, pattern);
+    return 0;
+}
+
+/* Output
+
+Pattern found at 1
+Pattern found at 7
+Pattern found at 16
+
+*/
