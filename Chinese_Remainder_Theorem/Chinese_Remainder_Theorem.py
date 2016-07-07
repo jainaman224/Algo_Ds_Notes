5b515e3d07078529f3c7816db072f37289fa09cc
Aman Jain <jainaman224@gmail.com>
2016-07-07 18:00:48 +0530

    Added Chinese Remainder theorem (#48)

---
+++ Chinese_Remainder_Theorem/Chinese_Remainder_Theorem.py
@@ -0,0 +1,36 @@
+# Calculating inverse using extended Euclidean algorithm iterative method
+def inverse(a, m):
+    m0 = m; x0 = 0; x1 = 1
+
+    if m == 1:
+        return 0
+
+    while a > 1:
+        q = int(a / m); t = m
+        m = a % m; a = t
+        t = x0
+        x0 = x1 - q * x0; x1 = t
+
+    if x1 < 0:
+        x1 += m0
+
+    return x1
+
+def findMinimumDividend(divisor, rem):
+    product = 1
+
+    for i in range(len(divisor)):
+        product *= divisor[i]
+
+    result = 0
+
+    for i in range(len(divisor)):
+        partialProduct = int(product / divisor[i])
+        result += remainder[i] * inverse(partialProduct, divisor[i]) * partialProduct;
+
+    return result % product
+
+divisor = [3, 4, 5, 7, 11]
+remainder = [2, 3, 1, 4, 5]
+
+print("Minimum value of dividend is " + str(findMinimumDividend(divisor, remainder)))
