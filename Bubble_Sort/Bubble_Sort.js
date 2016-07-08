20543e9859da7fc72130ffbbb2eca456219b5f9c
Aman Jain <jainaman224@gmail.com>
2016-07-08 16:47:46 +0530

    More bubble sort programs (#57)

---
+++ Bubble_Sort/Bubble_Sort.js
@@ -0,0 +1,30 @@
+function Bubble_Sort(array)
+{
+    var temp;
+
+    for(var i = 0; i < array.length; i++)
+    {
+        for(var j = 0; j < array.length - i; j++)
+        {
+            if(array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
+
+
+array = [2, 4, 3, 1, 6, 8, 4];
+
+Bubble_Sort(array);
+console.log(array);
+
+
+/* Output
+
+[ 1, 2, 3, 4, 4, 6, 8 ]
+
+*/
