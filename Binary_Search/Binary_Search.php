35f7b9061a2feae083ee38703721201d97841c6a
nj4710 <nj4710@users.noreply.github.com>
2016-07-08 16:50:35 +0530

    Create Binary_Search.php (#64)

---
+++ Binary_Search/Binary_Search.php
@@ -0,0 +1,27 @@
+<?php
+$list = array(3, 4, 25, 45, 78);
+$element_to_be_found = 25;
+
+function binary_search($element_to_be_found, $list, $left, $right)
+{
+    if ($left > $right)
+        return -1;
+ 
+    $mid = ($right + $left) / 2;
+    
+    if($list[$mid] == $element_to_be_found)
+        return $mid;
+    else if($list[$mid] > $element_to_be_found)
+        return binary_search($element_to_be_found,$list,$left,$mid-1);
+    else if($list[$mid] < $element_to_be_found)
+        return binary_search($element_to_be_found,$list,$mid+1,$right);
+}
+
+$ans = binary_search($element_to_be_found, $list, 0, count($list) - 1);
+
+if($ans == -1)
+    echo "Element not found";
+else
+    echo "Element found at index $ans";
+
+?>
