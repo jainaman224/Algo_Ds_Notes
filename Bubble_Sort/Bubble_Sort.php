5044b44ba72e6a6189bca06881d8db79a4838eb1
AnkitaKhurana <ankita0096@gmail.com>
2016-07-10 17:25:06 +0530

    Bubble sort in php

---
+++ Bubble_Sort/Bubble_Sort.php
@@ -0,0 +1,26 @@
+<?php
+ 
+ $arr = array(3, 1 ,6 , 9 , 3 );
+ //Bubble sort function 
+ function Bubble_Sort($arr, $size)
+ {
+ 	for( $i = 0; $i < $size - 1; $i++)
+    {
+        for( $j = 0; $j < $size - $i - 1; $j++)
+        {
+            // Do swapp$ing
+            if($arr[$j] > $arr[$j + 1])
+            {
+                $temp = $arr[$j];
+                $arr[$j] = $arr[$j + 1];
+                $arr[$j + 1] = $temp;
+            }
+        }
+    }
+
+   for ( $k=0 ; $k<$size;$k++)   
+         echo $arr[$k];
+}
+ //Bubble sort function calling 
+ Bubble_Sort($arr, 5);
+ ?>
