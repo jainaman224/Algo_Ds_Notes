21d3c616b29f8fdd4a22ad672d7fda98c992223c
Ayushi Negi <ayushin78@users.noreply.github.com>
2016-07-10 21:00:12 +0530

    Update Binary_Search.php (#94)

--- Binary_Search/Binary_Search.php
+++ Binary_Search/Binary_Search.php
@@ -1,3 +1,5 @@
+<?php
+
 $list = array(3, 4, 25, 45, 78);
 $element_to_be_found = 25;
 
@@ -9,7 +11,7 @@ function binary_search($element_to_be_found, $list, $left, $right)
     $mid = ($right + $left) / 2;
 
     if($list[$mid] == $element_to_be_found)
-        return $mid;
+        return $mid + 1;
     else if($list[$mid] > $element_to_be_found)
         return binary_search($element_to_be_found,$list,$left,$mid-1);
     else if($list[$mid] < $element_to_be_found)
@@ -21,4 +23,13 @@ $ans = binary_search($element_to_be_found, $list, 0, count($list) - 1);
 if($ans == -1)
     echo "Element not found";
 else
-    echo "Element found at index $ans";
+    echo "Element found at position $ans";
+    
+/*
+
+Output : Element found at position 3
+    
+*/
+
+?>
+    
