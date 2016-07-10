53b7e142ec3ce7dc3c7e1a4051d32ae369c4999c
Kavya Sharma <sharma.kavya1996@gmail.com>
2016-07-10 21:03:44 +0530

    Update Dynamic_Programming_Rod_Cutting.java

--- Dynamic_Programming_Rod_Cutting/Dynamic_Programming_Rod_Cutting.java
+++ Dynamic_Programming_Rod_Cutting/Dynamic_Programming_Rod_Cutting.java
@@ -8,7 +8,7 @@ class RodCutting
 {   //a fxn for calculating max of two nos.
 	static int max(int a,int b)
      { 
-     	 return(a>b)?a:b;
+     	 return(a > b) ? a : b ;
      	 
      }
      
@@ -19,11 +19,11 @@ class RodCutting
          val[0]=0;
          
          for(i=1;i<=n;i++)
-         { int max_value=Integer.MIN_VALUE;
-           for(j=0;j<i;j++)
-            max_value=max(max_value,cost[j]+val[i-j-1]);
-           val[i]=max_value;
-         }
+            { int max_value=Integer.MIN_VALUE;
+              for(j=0;j<i;j++)
+                max_value=max(max_value,cost[j]+val[i-j-1]);
+              val[i]=max_value;
+            }
          
          return val[n];
      }
@@ -31,12 +31,17 @@ class RodCutting
      // driver fxn
      
      public static void main(String args[])
-     { int []arr=new int []{3,5,8,9,10,17,17,20};
+     { int []arr=new int [] {3,5,8,9,10,17,17,20};
        int size=arr.length;
        System.out.println("Maximum value is " + CutRod(arr,size));
      }
 }   
 
+
 /*OUTPUT
 Maximum value is 24 */
-     
+
+
+
+
+  
