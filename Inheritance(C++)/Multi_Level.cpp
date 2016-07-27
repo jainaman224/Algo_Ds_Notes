2227bddd2f9fab9edec321b24c4d6d71021dcebb
jainaman224 <jainaman224@gmail.com>
2016-07-27 13:01:36 +0530

    improved code

--- Inheritance(C++)/Multi_Level.cpp
+++ Inheritance(C++)/Multi_Level.cpp
@@ -1,50 +1,62 @@
-#include<iostream>
+#include <iostream>
+
+using namespace std;
+
 class Father
-       {
-	protected:
-	int age;
-	public:
-	void getage()
-	{
-	age = 75;
-	}
-		};
+{
+    protected:
+        int age;
+
+    public:
+        void getage()
+        {
+            age = 75;
+        }
+};
+
 class Son : public Father // Son class inherited from Father
-       {
-	protected:
-	int S_age;
-	public:
-	void getage_S()
-	{
-	S_age = 45;
-	}
-		};
+{
+    protected:
+        int S_age;
+
+    public:
+        void getage_S()
+        {
+            S_age = 45;
+        }
+};
+
 class GrandSon : public Son  // Grandson class inherited from Son
-       {
-	protected:
-	int GS_age;
-	public:
-	void getage_GS()
-	{
-	GS_age = 15;
-	}
-		};
+{
+    protected:
+        int GS_age;
+
+    public:
+        void getage_GS()
+        {
+            GS_age = 15;
+        }
+};
 
 int main()
 {
 	GrandSon obj;
+
 	obj.getage();
 	obj.getage_S();
 	obj.getage_GS();
-	cout << "Father's age :" << age;
-        cout << "\nSon's age :" << S_age;
-	cout << "\nGrandson's age :" << GS_age; 
-getch();
+
+	cout << "Father's age :" << obj.age;
+    cout << "\nSon's age :" << obj.S_age;
+	cout << "\nGrandson's age :" << obj.GS_age;
+    
+    return 0;
 }
 
-/*
+/* Output
+
 Father's age : 75
 Son's age : 45
 Grandson's age : 15
-*/
 
+*/
--- Inheritance(C++)/multiple.cpp
+++ Inheritance(C++)/multiple.cpp
@@ -1,48 +1,60 @@
-#include<iostream>
- 
+#include <iostream>
+
+using namespace std;
+
 class lectures
 {
     protected:
-       int rollno,marks1,marks2;
+        int rollno, marks1, marks2;
+
     public:
-                void get()
-              {
-              	rollno = 007;
-		marks1 = 75;
-		marks2 = 50;	    
-              }
+        void get()
+        {
+            rollno = 007;
+            marks1 = 75;
+		    marks2 = 50;
+        }
 };
+
 class sports
 {
     protected:
-       int sportsmarks;                   
+       int sportsmarks;
+
     public:
-                void getsportsmarks()
-              {
-                 sportsmarks = 100;
-              }
+        void getsportsmarks()
+        {
+            sportsmarks = 100;
+        }
 };
-class result:public lectures,public sports    //Result class inherited from lectures and sports
+
+class result : public lectures, public sports    // Result class inherited from lectures and sports
 {
-    int total,average;
+    private:
+        int total, average;
+
     public:
-    void display()
-              {
-                 total = (marks1+marks2+sportsmarks);
-                 average = total/3.0;
-                 cout << "\nRoll No : " << rollno
-		 cout << "\nTotal: " << total;
-                 cout << "\nAverage : " << average;
-              }
+        void display()
+        {
+            total = (marks1+marks2+sportsmarks);
+            average = total/3.0;
+            cout << "\nRoll No : " << rollno;
+            cout << "\nTotal: " << total;
+            cout << "\nAverage : " << average;
+        }
 };
-void main()
+
+int main()
 {
    result obj;
+
    obj.get();
    obj.getsportsmarks();
    obj.display();
-   getch();
+
+   return 0;
 }
+
 
 /* OUTPUT
 
