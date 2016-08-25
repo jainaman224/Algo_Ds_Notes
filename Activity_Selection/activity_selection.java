38a35fe6af233cbd8f5616be4f870a75c6c6eff4
nj4710 <nj4710@gmail.com>
2016-08-25 21:53:08 +0530

    minor changes in activity selection

--- Activity_Selection/activity_selection.java
+++ Activity_Selection/activity_selection.java
@@ -4,14 +4,14 @@ import java.io.*;
 
 class activity_selection
 {
-	static void activities(int s[],int f[],int n)
+	static void activities(int start[],int finish[],int n)
 	{
 		int i,j;
-		i=0;
+		i = 0;
 		System.out.print(i + " ");
-		for(j=1;j<n;j++)
+		for(j = 1 ; j < n ; j++)
 		{
-			if(s[j] >= f[i])
+			if (start[j] >= finish[i])
 			{
 				System.out.print(j+" ");
 				i = j;
@@ -20,9 +20,10 @@ class activity_selection
 	}
 	public static void main(String args[])
 	{
-		int s[] = {1, 3, 1, 5, 8, 6};
-		int f[] = {2, 6, 6, 7, 10, 8};
-		int n = s.length;
-		activities(s,f,n);	
+		int start[] = {1, 3, 1, 5, 8, 6};
+		int finish[] = {2, 6, 6, 7, 10, 8};
+		int n = start.length;
+		activities(start, finish, n);	
 	}
 }
+//Output: 0 1 4
