78c72f5fb11a274d93ea26e7037f2d7a82a8a657
jainaman224 <jainaman224@gmail.com>
2016-08-25 22:20:39 +0530

    inheriteance-java

--- Inheritance(Java)/Multi_Level.java
+++ Inheritance(Java)/Multi_Level.java
@@ -17,13 +17,44 @@ class Son extends Father
 {
     int S_age;
 
-    public void setage()
+    public void setage_S()
     {
         S_age = 45;
     }
 
-    public int getage()
+    public int getage_S()
     {
+        return S_age;
+    }
+}
 
+class GrandSon extends Son
+{
+    int GS_age;
+
+    public void setage_GS()
+    {
+        GS_age = 15;
+    }
+
+    public int getage_GS()
+    {
+        return GS_age;
+    }
+}
+
+class Multi_Level
+{
+    public static void main(String args[])
+    {
+        GrandSon obj = new GrandSon();
+
+        obj.setage();
+    	obj.setage_S();
+    	obj.setage_GS();
+
+        System.out.println("Father's age : " + obj.getage());
+        System.out.println("Son's age : " + obj.getage_S());
+    	System.out.println("Grandson's age : " + obj.getage_GS());
     }
 }
