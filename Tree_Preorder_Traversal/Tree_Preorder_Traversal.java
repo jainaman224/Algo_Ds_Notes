9334c144a9c140f5aef542d95d0b4e6ae104122a
varunpurohit76 <varunpurohit76@gmail.com>
2016-10-19 23:59:51 +0530

    tree traversals

---
+++ Tree_Preorder_Traversal/Tree_Preorder_Traversal.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Node
+{
+    int key;
+    Node left, right;
+ 
+    public Node(int item)
+    {
+        key = item;
+        left = right = null;
+    }
+}
+ 
+class BinaryTree
+{
+    Node root;
+ 
+    BinaryTree()
+    {
+        root = null;
+    }
+ 
+    void printPreorder(Node node)
+    {
+        if (node == null)
+            return;
+ 
+        System.out.print(node.key + " ");
+        printPreorder(node.left);
+        printPreorder(node.right);
+    }
+ 
+    void printPreorder()
+    {     
+    	printPreorder(root);  
+    }
+ 
+    public static void main(String[] args)
+    {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+ 
+        System.out.println("Preorder traversal of binary tree is ");
+        tree.printPreorder();
+    }
+}
