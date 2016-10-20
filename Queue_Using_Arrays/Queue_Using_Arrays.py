5912fbb7c5134ce04785fade66114fea4b3e3eaf
Pranay Berry <pranayberry@gmail.com>
2016-10-21 01:47:43 +0530

    Added inputs and removed input from user

--- Queue_Using_Arrays/Queue_Using_Arrays.py
+++ Queue_Using_Arrays/Queue_Using_Arrays.py
@@ -1,31 +1,36 @@
-size = -1
+size = 4
 front = -1
 rear = -1
-
+"""
+Uncomment to input size from user and validate it
 while (1):
 	size = int(raw_input("Enter max input\n> "))
 	if size > 0:
 		break
-
+"""
 queue = [0] * size
 
+#Function to check for empty queue
 def isEmpty():
 	global front, rear
 	if (front == -1 or front > rear):
 		return True
 
-def enqueue():
+#Fuction to insert an element in queue
+def enqueue(element):
 	global front, rear
 	if rear == size - 1:
 		print "Queue is Full"
 	else:
 		if front == -1:
 			front = 0
-		element = int(raw_input("Enter the element to insert\n> "))
+		#Uncomment to get input from user
+		#element = int(raw_input("Enter the element to insert\n> "))
 		rear = rear + 1
 		queue[rear] = element
 		print "Item inserted successfully"
 
+#Fuction to delete an element from the queue
 def dequeue():
 	global front, rear
 	if isEmpty():
@@ -35,6 +40,7 @@ def dequeue():
 		front += 1
 		print "Item deleted: " + str(element)
 
+#Fuction to display elements from queue
 def display():
 	global front, rear
 	if isEmpty():
@@ -46,6 +52,8 @@ def display():
 			temp += 1
 		print
 
+"""
+Code to get choice form the user
 while(1):
 	print "---Queue Operations---"
 	print "1. Insert an element in queue."
@@ -62,3 +70,26 @@ while(1):
 		display()
 	else:
 		break
+"""
+
+enqueue(23)
+enqueue(45)
+enqueue(65)
+enqueue(89)
+enqueue(90)
+dequeue()
+dequeue()
+
+
+"""
+Output 
+Item inserted successfully
+Item inserted successfully
+Item inserted successfully
+Item inserted successfully
+Queue is Full
+Item deleted: 23
+Item deleted: 45
+"""
+
+
