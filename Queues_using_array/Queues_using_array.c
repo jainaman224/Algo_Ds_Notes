77998eb7b4fc8b4881b59fd6d89938fe9de7c8f4
akshit04 <akshit.kharbanda04@gmail.com>
2016-10-22 02:39:21 +0530

    Queues_using_array modified

--- Queues_using_array/Queues_using_array.c
+++ Queues_using_array/Queues_using_array.c
@@ -1,5 +1,4 @@
 #include<stdio.h>
-#include<conio.h>
 #define SIZE 10
 
 void enQueue(int);
@@ -7,59 +6,78 @@ void deQueue();
 void display();
 void QueueSize();
 void empty();
+void inital_queue(int);
 
 int queue[SIZE], front = -1, rear = -1, count=0;
 
 void main()
 {
- int value, choice;
- clrscr();
- while(1){
-  printf("\n\n***** MENU *****\n");
-  printf("1. Insertion\n2. Deletion\n3. Display\n4. Empty\n5. Exit");
-  printf("\nEnter your choice: ");
-  scanf("%d",&choice);
-  switch(choice){
-   case 1: printf("Enter the value to be insert: ");
-   scanf("%d",&value);
-   enQueue(value);
-   break;
-   case 2: deQueue();
-   break;
-   case 3: display();
-   break;
-   case 4: empty();
-   break;
-   case 5: exit(0);
-   default: printf("\nWrong selection!!! Try again!!!");
- }
+    int value, choice;
+    inital_queue(1);
+    inital_queue(2);
+    inital_queue(3);
+    inital_queue(4);
+    printf("INITIAL STATUS OF QUEUE\n");
+    display();
+    while(1)
+    {
+        printf("\n\n***** MENU *****\n");
+        printf("1. Insertion\n2. Deletion\n3. Display\n4. Empty\n5. Exit");
+        printf("\nEnter your choice: ");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:
+            value=9;
+            enQueue(value);
+            break;
+            case 2: deQueue();
+            break;
+            case 3: display();
+            break;
+            case 4: empty();
+            break;
+            case 5: exit(0);
+            default: printf("\nWrong selection!!! Try again!!!");
+        }
+    }
 }
-}
-void enQueue(int value)
+
+void inital_queue(int item)
 {
- if(rear == SIZE-1)
-  printf("\nQueue is Full!!! Insertion is not possible!!!");
-else{
+
   if(front == -1)
    front = 0;
- rear++;
- queue[rear] = value;
- printf("\nInsertion success!!!");
+ queue[++rear] = item;
+ count++;
 }
-count++;
+
+void enQueue(int value)
+{
+    if(rear == SIZE-1)
+      printf("\nQueue is Full!!! Insertion is not possible!!!");
+    else
+    {
+     queue[++rear] = value;
+     printf("\nInsertion success!!!");
+    }
+    count++;
+    display();
 }
 
 void deQueue()
 {
- if(front == rear)
-  printf("\nQueue is Empty!!! Deletion is not possible!!!");
-else{
-  printf("\nDeleted : %d", queue[front]);
-  front++;
-  if(front == rear)
-   front = rear = -1;
-count--;
-}
+    if(front == rear)
+      printf("\nQueue is Empty!!! Deletion is not possible!!!");
+    else
+    {
+      printf("\nDeleted : %d", queue[front]);
+      front++;
+      if(front == rear)
+         front = rear = -1;
+     count--;
+    }
+    display();
 }
 
 void QueueSize()
@@ -69,21 +87,49 @@ void QueueSize()
 
 void display()
 {
- if(rear == -1)
-  printf("\nQueue is Empty!!!");
-else
-{
-  int i;
-  printf("\nQueue elements are:\n");
-  for(i=front; i<=rear; i++)
-   printf("%d\t",queue[i]);
-}
+    if(rear == -1)
+      printf("\nQueue is Empty!!!");
+    else
+    {
+      int i;
+      printf("\nQueue elements are:\n");
+      for(i=front; i<=rear; i++)
+         printf("%d ",queue[i]);
+    }
 }
 
 void empty()
 {
-        if(count==0)
-                printf("\nQueue is empty\n");
-        else
-                printf("\nQueue is not empty\n");
+    if(count==0)
+        printf("\nQueue is empty\n");
+    else
+        printf("\nQueue is not empty\n");
 }
+
+
+/*
+INITIAL STATUS OF QUEUE :
+1 2 3 4
+
+if choice=1
+output : 
+Queue elements are:
+1 2 3 4 9
+
+if choice=2
+output :
+Queue elements are:
+2 3 4
+
+if choice=3
+output :
+Queue elements are:
+1 2 3 4
+
+if choice=4
+output :
+Queue is not empty
+
+if choice=5
+exit
+*/
