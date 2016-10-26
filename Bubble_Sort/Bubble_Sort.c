9e913bc3ad9c15a148334941440432898e729e99
Deepanshu Rekhi <drekhi12@gmail.com>
2016-10-26 22:11:39 +0530

    Bubble_Sort.c
    Efficient Bubble Sort in C

---
+++ Bubble_Sort/Bubble_Sort.c
@@ -0,0 +1,46 @@
+
+// Function for bubble sort
+void Bubble_Sort(int array[], int size)
+{
+    int temp;
+	int flag,i,j;
+    for( i = 0; i < size - 1; i++)
+    {	flag=0;
+        for( j = 0; j < size - i - 1; j++)
+        {
+            // Do swapping
+            if(array[j] > array[j + 1])
+            {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                flag++;
+            }
+        }
+        if(flag==0)
+        break;
+    }
+
+}
+
+// Function to print elements of array
+void Print_Array(int array[], int size)
+{
+	int i;
+    for( i = 0; i < size; i++)
+        printf("%d ",array[i]);
+
+    printf("\n");
+}
+
+// Driver Function
+int main()
+{
+    int array[] = {2, 4, 3, 1, 6, 8, 4};
+    Bubble_Sort(array, 7);
+    Print_Array(array, 7);
+    return 0;
+}
+
+// Output
+// 1 2 3 4 4 6 8
