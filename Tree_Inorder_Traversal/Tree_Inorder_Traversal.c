cc93d4cd8095e2e6994b21f2608018a10fde266c
gargi-gupta <gargi15029@iiitd.ac.in>
2016-10-27 00:16:38 +0530

    Update Tree_Inorder_Traversal.c

--- Tree_Inorder_Traversal/Tree_Inorder_Traversal.c
+++ Tree_Inorder_Traversal/Tree_Inorder_Traversal.c
@@ -1,41 +1,77 @@
-#include<stdio.h>
+#include <stdio.h>
+ 
+    #define MAX 50
+    int queue_array[MAX];
+    int rear = - 1;
+    int front = - 1;
 
-typedef struct node
-{
-	int data;
-	struct node *lptr,*rptr;
-}node;
 
-node *newnode(int data)
+insert()
 {
-	node *newn;
-	newn=(node*)malloc(sizeof(node));
-	newn->data=data;
-	newn->lptr=NULL;
-	newn->rptr=NULL;
-	return newn;
-}
+    int add_item;
+    if (rear == MAX - 1)
+    printf("Overflow \n");
+    else
+    {
+        if (front == - 1)
+        front = 0;
+        printf("Insert in queue : ");
+        scanf("%d", &add_item);
+        rear = rear + 1;
+        queue_array[rear] = add_item;
+        //printf("sdfghj");
+    }
+} 
+ 
+delete()
+{
+    if (front == - 1 || front > rear)
+    {
+        printf("Underflow \n");          return ;
+    }
+    else
+    {
+        printf("Element deleted: %d\n", queue_array[front]);
+        front = front + 1;
+        //printf("zxcvb");
+    }
+} 
 
-/*Function for INORDER Transversal*/
-void inorder(node *root)
-{
-	if(root!=NULL)
-	{
-		inorder(root->lptr);
-		printf("%d ",root->data);
-		inorder(root->rptr);
-	}
-}
 
-void main()
+display()
 {
-	node *root;
-	root=newnode(1);
-	root->lptr=newnode(2);
-	root->rptr=newnode(3);
-	root->lptr->lptr=newnode(4);
-	root->lptr->rptr=newnode(5);
-	
-	printf("In Order Transversal: ");
-	inorder(root);
-}
+    int i;
+    if (front == - 1)
+        printf("Empty queue \n");
+    else
+    {
+        printf("Queue is : \n");
+        for (i = front; i <= rear; i++)
+            printf("%d ", queue_array[i]);
+        printf("\n");
+        //printf("qwerty");
+    }
+} 
+
+main()
+{
+    int choice;
+    while (1)
+    {
+        printf("1.Insert\n");
+        printf("2.Delete\n");
+        printf("3.Display\n");
+        printf("4.Quit \n");
+        printf("Enter choice : ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+            case 1: insert();  break;
+            case 2: delete();  break;
+            case 3: display(); break;
+            case 4: exit(1);
+            default:
+            printf("Invalid choice \n");
+        } 
+    }
+} 
