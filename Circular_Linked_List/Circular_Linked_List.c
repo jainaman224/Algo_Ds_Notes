4379d47246b41f207be94c138ff8bff2ec436ab0
SilentFlame <csvinay.d@gmail.com>
2016-10-27 03:04:17 +0530

    Some algorithms in c

---
+++ Circular_Linked_List/Circular_Linked_List.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct circ buff;
+struct circ{
+	int val;
+	buff *prev;
+	buff *next;
+};
+buff *head;
+buff *tail;
+
+buff *insert(buff *head,int x){
+	buff *tmp;
+	tmp=(buff*)malloc(sizeof(buff));
+	tmp->val=x;
+	tmp->next=tmp;
+	tmp->prev=tmp;
+
+	if(head==NULL){
+		tail=tmp;
+		return tmp;
+	}
+	else{
+		tmp->prev=tail;
+		tmp->next=head;
+		tail->next=tmp;
+		head->prev=tmp;
+		tail=tmp;
+	}
+	return head;
+}
+
+buff *print(buff *head){
+	buff *start=head;
+	while(head!=tail){
+		printf("%d ",head->val);
+	head=head->next;
+	}
+	printf("%d\n",head->val);
+	return start;
+}
+
+int main(){
+	int x,i,j; 
+	tail=head=NULL;
+	
+	//char c;
+//	scanf("%c",&c);
+//	if(c=='i'){
+while(1){
+	scanf("%d",&x);
+	head=insert(head,x);
+	head=print(head);
+	printf("tyr-> %d\n",tail->next->val); //tail ke next ka element.
+	printf("try2-> %d\n",head->prev->val);//head ke prev ka element.
+}
+
+return 0;
+}
---
+++ Merge_Sort/Merge_Sort.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+int merge(int lf[], int rt[], int a[], int m, int n)
+{
+	int i,j,k;
+	i=0;
+	j=0;
+	k=0;
+	while(i<m && j<n)
+	{
+		if(lf[i]<rt[j])
+			a[k++]=lf[i++];
+
+		else
+			a[k++]=rt[j++];
+	}
+	while(i<m)
+		a[k++]=lf[i++];
+
+	while(j<n)
+		a[k++]=rt[j++];
+}
+
+int divide(int a[], int n)
+{
+	int i,lf[n/2],rt[n-n/2];
+	if(n<=1)
+		return;
+
+		for(i=0;i<n/2;i++)
+					lf[i]=a[i];
+		
+		for(i=n/2;i<n;i++)
+			rt[i-n/2]=a[i];
+
+			divide(lf, n/2);
+			divide(rt, n-n/2);
+			merge(lf, rt, a, n/2, n-n/2);
+
+}
+
+
+int main()
+{
+	int n,i,a[1000];
+
+		scanf("%d",&n);
+
+		for(i=0;i<n;i++)
+		scanf("%d",&a[i]);
+			
+		divide(a,n);
+
+
+		for(i=0;i<n;i++)
+		printf("%d ",a[i]);
+
+		printf("\n");
+return 0;
+}
