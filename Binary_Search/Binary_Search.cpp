fabf7f5acf9233d28fe53cd53ae54fa98e7b6182
Aldo Culquicondor <alculquicondor@gmail.com>
2016-10-26 20:46:46 -0500

    functions for searching elements greater than or equal (lower bound) and strictly greater than (upper bound) of a given number in the array

--- Binary_Search/Binary_Search.cpp
+++ Binary_Search/Binary_Search.cpp
@@ -22,10 +22,47 @@ int Binary_Search(int array[], int size, int desired)
     return -1;
 }
 
+
+// Function for lower bound:
+// finds the position of the lowest number greater than or equal to desired
+int Lower_Bound(int array[], int size, int desired)
+{
+    int left = -1, right = size, middle;
+    while (right - left > 1)
+    {
+        middle = left + (right - left) / 2;
+
+        if (array[middle] < desired)
+            left = middle;
+        else
+            right = middle;
+    }
+    return right;
+}
+
+
+// Function for upper bound:
+// finds the position of the lowest number strictly greater than desired
+int Upper_Bound(int array[], int size, int desired)
+{
+    int left = -1, right = size, middle;
+    while (right - left > 1)
+    {
+        middle = left + (right - left) / 2;
+
+        if (array[middle] <= desired)
+            left = middle;
+        else
+            right = middle;
+    }
+    return right;
+}
+
+
 // Driver Function
 int main()
 {
-    int array[] = {1, 2, 3, 4, 5, 6, 7};
+    int array[] = {1, 2, 3, 4, 5, 5, 7};
 
     // Element 4 to be searched
     if(Binary_Search(array, 7, 4) != -1)
@@ -39,6 +76,18 @@ int main()
     else
         cout << "Not Found" << endl;
 
+    // Element greater than or equal than 5
+    cout << Lower_Bound(array, 7, 5) << endl;
+
+    // Element greater than or equal than 6
+    cout << Lower_Bound(array, 7, 6) << endl;
+
+    // Element greater than 5
+    cout << Upper_Bound(array, 7, 5) << endl;
+
+    // Element greater than 0
+    cout << Upper_Bound(array, 7, 0) << endl;
+
     return 0;
 }
 
@@ -46,5 +95,9 @@ int main()
 
 Found
 Not Found
+4
+6
+6
+0
 
 */
