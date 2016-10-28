176746245269cff71a03fe0f18312f74c9b7b0e7
Neha Jha <nj4710@users.noreply.github.com>
2016-10-28 17:03:05 +0530

    Create Counting_Sort.c (#333)

---
+++ Counting_Sort/Counting_Sort.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+
+// Function that sort the given input
+void counting_sort(int input[], int n)
+{
+    int output[n]; // The output will have sorted input array
+    int max = input[0];
+    int min = input[0];
+
+    for(int i = 1; i < n; i++)
+    {
+        if(input[i] > max)
+            max = input[i]; // Maximum value in array
+        else if(input[i] < min)
+            min = input[i]; // Minimum value in array
+    }
+
+    int k = max - min + 1; // Size of count array
+
+    int count_array[k]; // Create a count_array to store count of each individual input value
+    for(int i=0; i<k; i++)
+        count_array[i]=0;
+
+    for(int i = 0; i < n; i++)
+        count_array[input[i] - min]++; // Store count of each individual input value
+
+    /* Change count_array so that count_array now contains actual
+     position of input values in output array */
+    for(int i = 1; i < k; i++)
+        count_array[i] += count_array[i - 1];
+
+    // Populate output array using count_array and input array
+    for(int i = 0; i < n; i++)
+    {
+        output[count_array[input[i] - min] - 1] = input[i];
+        count_array[input[i] - min]--;
+    }
+
+    for(int i = 0; i < n; i++)
+        input[i] = output[i]; // Copy the output array to input, so that input now contains sorted values
+
+}
+
+
+// Driver program to test above function
+int main()
+{
+    int n = 9, i;
+
+    int input[] = {1, 5, 2, 7, 3, 4, 4, 1, 5};
+
+    counting_sort(input, n);
+
+    printf("Sorted Array : ");
+    for(i = 0; i < n; i++)
+        printf("%d ", input[i]);
+
+    return 0;
+}
+
+
+/* Output
+Sorted Array : 1  1  2  3  4  4  5  5  7
+*/
