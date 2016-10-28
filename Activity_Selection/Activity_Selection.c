f24322d0617a8fadbe067a3e9df1e408b8d2dcc1
Neha Jha <nj4710@users.noreply.github.com>
2016-10-28 16:25:23 +0530

    Create Activity_Selection.c

---
+++ Activity_Selection/Activity_Selection.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void print_activities(int start[], int finish[], int n)
+{
+    printf("%s", "Following activities are selected");
+    int i = 0;
+    printf("\n%d  ", i);
+    for (int j = 1; j < n; j++)
+    {
+        if (start[j] >= finish[i])
+        {
+            printf("%d  ", j);
+            i = j;
+        }
+    }
+}
+
+int main()
+{
+    int start[] = {1, 3, 1, 5, 8, 6};
+    int finish[] = {2, 6, 6, 7, 10, 8};
+    int n = sizeof(start) / sizeof(start[0]);
+    print_activities(start, finish, n);
+    return 0;
+}
+
+// Output
+// Following activities are selected
+// 0  1  4
