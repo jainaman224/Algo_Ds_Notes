24197a32eb2464e6243dc24af9afe2d9b604762c
Ayushi Negi <ayushin78@users.noreply.github.com>
2016-10-31 22:29:29 +0530

    updated Sounting_Sort.c

--- Counting_Sort/Counting_Sort.c
+++ Counting_Sort/Counting_Sort.c
@@ -7,7 +7,8 @@ void counting_sort(int input[], int n)
     int max = input[0];
     int min = input[0];
 
-    for(int i = 1; i < n; i++)
+	int i;
+    for(i = 1; i < n; i++)
     {
         if(input[i] > max)
             max = input[i]; // Maximum value in array
@@ -18,25 +19,25 @@ void counting_sort(int input[], int n)
     int k = max - min + 1; // Size of count array
 
     int count_array[k]; // Create a count_array to store count of each individual input value
-    for(int i=0; i<k; i++)
+    for(i=0; i<k; i++)
         count_array[i]=0;
 
-    for(int i = 0; i < n; i++)
+    for(i = 0; i < n; i++)
         count_array[input[i] - min]++; // Store count of each individual input value
 
     /* Change count_array so that count_array now contains actual
      position of input values in output array */
-    for(int i = 1; i < k; i++)
+    for(i = 1; i < k; i++)
         count_array[i] += count_array[i - 1];
 
     // Populate output array using count_array and input array
-    for(int i = 0; i < n; i++)
+    for(i = 0; i < n; i++)
     {
         output[count_array[input[i] - min] - 1] = input[i];
         count_array[input[i] - min]--;
     }
 
-    for(int i = 0; i < n; i++)
+    for(i = 0; i < n; i++)
         input[i] = output[i]; // Copy the output array to input, so that input now contains sorted values
 
 }
