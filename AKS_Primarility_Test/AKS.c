064d7ec4acd72338218867b2cce3b91330fcf419
Ankit Dhankhar <ankit97dhankhar@gmail.com>
2017-04-02 23:36:33 +0530

    Addition of cpp code of AKS primality test (#410)
    * Add files via upload
    
    * Rename AKS_Primality_Test.cpp to AKS.cpp
    
    * Update INDEX.md
    
    * Update INDEX.md
    
    * Add files via upload
    
    * Add files via upload
    
    * Update INDEX.md
    
    * Update AKS.c
    
    * Update AKS.c
    
    * Update AKS.c
    
    * Update AKS.java
    
    * Update AKS.java

--- AKS_Primarility_Test/AKS.c
+++ AKS_Primarility_Test/AKS.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+/*
 
+Check if n is a perfect power: if n = ab for integers a > 1 and b > 1, output composite.
+
+
+Find the smallest r such that ordr(n) > (log2 n)2. (if r and n are not coprime, then skip this r)
+
+
+For all 2 ≤ a ≤ min(r, n−1), check that a does not divide n: If a|n for some 2 ≤ a ≤ min(r, n−1), output composite.
+
+
+If n ≤ r, output prime.
+
+
+For a = 2 to sqrt(phi (r))*log(2)(n)  
+	if (X+a)n≠ Xn+a (mod Xr − 1,n), output composite;
+
+
+Output prime.
+
+*/
 int isprm(long long n)
 {
 	long long i = 5, w = 2;
@@ -19,8 +39,8 @@ int isprm(long long n)
 
 int main()
 {
-	int i;
-	for (i = 1; i <= 100000000; i++)
+	// Prints all prime less than 100000000
+	for (int i = 1; i <= 100000000; i++)
 	{
 		if(isprm(i) == 1)
 			printf("%d\n",i);
