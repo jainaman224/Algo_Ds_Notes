ea4e1a8e27aaf70b55c5b1a28a0f7a573a3a6d26
Manjeet Kumar <kumar.manjeet13@gmail.com>
2017-07-12 20:07:53 +0530

    spiral 2D array
    Code for creating(or printing) 2D array in spiral order(inward spiral)

--- main.cpp
+++ main.cpp
@@ -1,95 +1,43 @@
 #include <iostream>
-#include <string>
-#include <stack>
-
 
 using namespace std;
 
-bool isOperand(char c)
-{
-    if(c>='0' && c<='9') return true;
-    if(c>='a' && c<='z') return true;
-    if(c>='A' && c<='Z') return true;
-    return false;
-}
-
-bool isOperator(char c)
-{
-    if(c=='+' || c=='-' || c=='*' || c=='/' || c=='$') return true;
-    return false;
-}
-
-bool isRightAssociative(char c)
-{
-    if(c=='$') return true;
-    return false;
-}
-
-int getOpWeight(char op)
-{
-    int weight = -1;
-    switch(op)
-    {
-        case '+': weight = 1;
-                    break;
-        case '-': weight = 1;
-                    break;
-        case '*': weight = 2;
-                    break;
-        case '/': weight = 2;
-                    break;
-        case '$': weight = 3;
-                    break;
-    }
-    return weight;
-}
-
-bool hasHigherPrecedance(char c, char d)
-{
-    if(getOpWeight(c) == getOpWeight(d)){
-        if(isRightAssociative(c)) return false;
-        else return true;
-    }
-    return getOpWeight(c)>getOpWeight(d) ? true: false;
-}
-
-string postfix(string str)
-{
-    stack<char> S;
-    string result = "";
-    for(int i=0;i<str.length();i++){
-        if(str[i]==' ' || str[i]==',') continue;
-        else if(isOperand(str[i])){
-            result += str[i];
-        }
-        else if(isOperator(str[i])){
-            while(!S.empty() && (hasHigherPrecedance(S.top(),str[i]))){
-                result += S.top(); S.pop();
-            }
-            S.push(str[i]);
-        }
-        else if(str[i]=='('){
-            S.push(str[i]);
-        }
-        else if(str[i]==')'){
-            while(!S.empty() && S.top()!='('){
-                    result += S.top(); S.pop();
-            }
-            S.pop();
-        }
-
-    }
-    while(!S.empty()){
-        result += S.top(); S.pop();
-    }
-    return result;
-}
-
 int main()
 {
-    string exp;
-    cout << "Enter the infix expression: ";
-    getline(cin,exp);
-    cout << endl << "Postfix expression: " << postfix(exp);
+    int a[100][100];
+    int N;
+    cout << "Enter the size of the chakravyuha: ";
+    cin >> N;
+    int top=0,bottom=N-1,left=0,right=N-1;
+//for keeping tabs on which direction we are moving
+    int k=1;
+    while(top <= bottom && left <= right){
+        for(int i = left; i <= right; i++){
+            a[top][i] = k;
+            k++;
+        }
+        top++;
+        for(int i = top; i <= bottom; i++){
+            a[i][right] = k;
+            k++;
+        }
+        right--;
+        for(int i = right; i >= left; i--){
+            a[bottom][i] = k;
+            k++;
+        }
+        bottom--;
+        for(int i = bottom; i >= top; i--){
+            a[i][left] = k;
+            k++;
+        }
+        left++;
+    }
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            cout << a[i][j] << "\t";
+        }
+        cout << endl;
+    }
     return 0;
 }
