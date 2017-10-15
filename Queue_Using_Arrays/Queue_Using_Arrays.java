57a3711e3fe9bb5378483ab6ab1ef430f1394207
sprea <spriha27@gmail.com>
2017-10-15 23:11:47 +0530

    Added code in Java for Queue_Using_Arrays

---
+++ Queue_Using_Arrays/Queue_Using_Arrays.java
@@ -0,0 +1,78 @@
+public class Queue_Using_Arrays {
+	int[] arr = new int[5];
+	int front = -1;
+	int rear = -1;
+	
+	//Constructors
+	Queue_Using_Arrays() {
+		arr = new int[10];
+		front = -1;
+		rear=-1;
+		}
+	Queue_Using_Arrays(int size) {
+		arr = new int[size];
+		front = -1;
+		rear=-1;
+	}
+	
+	//Displaying the queue
+	public void print() {
+		for(int i=front; i<=rear;i++){
+			System.out.println(arr[i]+"\t");
+		}
+	}
+	
+	//Checking whether the queue is full
+	public boolean IsFull(){
+		if (rear==arr.length-1){
+			return true;
+		}
+		else return false;
+	}
+	
+	//Checking whether the queue is empty
+	public boolean IsEmpty(){
+		if (rear==-1 && front == -1){
+			return true;
+		}
+		else return false;
+	}
+	
+	//Enqueue operation
+	public void enqueue(int item) {
+		if(IsEmpty()){
+			front++;
+			rear++;
+			arr[rear]=item;
+		}
+		else if(!IsFull()){
+			rear++;
+			arr[rear]=item;
+		}
+		else{
+			System.out.println("Can't enqueue");
+		}
+	}
+	
+	//Returns size of queue
+	public int getSize() {
+		return  arr.length;
+	}
+	
+	//Dequeue operation
+	public void dequeue() {
+		if(front!=rear && !IsEmpty()){
+			int temp = arr[front];
+			front++;
+			System.out.println(temp);
+		}
+		else if( front == rear && !IsEmpty()){
+			int temp = arr[front];
+			front = -1;
+			rear = -1;
+			System.out.println(temp);
+		}
+		else
+			System.out.println("Can't dequeue.");
+	}
+}
