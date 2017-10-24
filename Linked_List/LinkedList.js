c859ea9435869d9314d8ff130870d9ff3106a7ce
nj4710 <nj4710@gmail.com>
2017-10-24 23:35:47 +0530

    remove method for linked list

--- Linked_List/LinkedList.js
+++ Linked_List/LinkedList.js
@@ -33,6 +33,30 @@ Linked_List.prototype.get = function(num) {
 	}
 	return current;
 }
+Linked_List.prototype.remove = function(pos) {
+	var current = this.head,
+	length = this.length,
+	count = 0,
+	prev = null;
+	if(pos > length) {
+		return "node doesn't exist!";
+	}
+	if(pos === 0) {
+		this.head = current.head;
+		this.length--;
+		return this.head;
+	}
+	while(count < pos-1) {
+		prev = current;
+		current = current.next;
+		count++;
+	}
+	prev.next = current.next;
+	current = null;
+	this.length--;
+	return this.head;
+
+}
 var ll = new Linked_List();
 ll.add(2);
 ll.add(3);
@@ -40,3 +64,5 @@ ll.add(4);
 ll.add(5);
 ll.add(6);
 //2->3->4->5->6
+ll.remove(2);
+//2->4->5->6
