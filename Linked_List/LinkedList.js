aca9290181f44108252785c8449f8e42061024b5
Neha Jha <nj4710@users.noreply.github.com>
2018-08-01 11:08:25 +0530

    remove method for linked list (#427)

--- Linked_List/LinkedList.js
+++ Linked_List/LinkedList.js
@@ -39,6 +39,31 @@ Linked_List.prototype.get = function(index) {
 	}
 	return current;
 }
+
+Linked_List.prototype.remove = function(pos) {
+	var current = this.head,
+	length = this.length,
+	count = 0,
+	prev = null;
+	if(pos > length) {
+		return "node doesn't exist!";
+	}
+	if(pos === 0) {
+		this.head = current.head;
+		this.length--;
+		return this.head;
+	}
+	while(count < pos-1) {
+		prev = current;
+		current = current.next;
+		count++;
+	}
+	prev.next = current.next;
+	current = null;
+	this.length--;
+	return this.head;
+}
+
 // For Printing Linkedlist
 Linked_List.prototype.toString = function () {
     var currentNode =  this.head
@@ -49,11 +74,15 @@ Linked_List.prototype.toString = function () {
     }
     return str
 }
+
 var ll = new Linked_List();
 ll.add(2);
 ll.add(3);
 ll.add(4);
 ll.add(5);
 ll.add(6);
+//2->3->4->5->6
+ll.remove(2);
+//2->4->5->6
 ll.toString();
-//👉 2 👉 3 👉 4 👉 5 👉 6
+//👉 2 👉 4 👉 5 👉 6
