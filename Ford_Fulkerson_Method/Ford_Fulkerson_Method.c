95c7077eb4919697471442f3f41c5b901a3ef03d
Aman Jain <jainaman224@gmail.com>
2018-08-08 22:17:14 +0530

    Update and rename ford_fulkerson.c to Ford_Fulkerson_Method.c

--- Ford_Fulkerson_Method/Ford_Fulkerson_Method.c
+++ Ford_Fulkerson_Method/Ford_Fulkerson_Method.c
@@ -1,10 +1,10 @@
-//https://github.com/jainaman224/Algo_Ds_Notes/issues/161
 #include <stdio.h>
 #define WHITE 0
 #define GRAY 1
 #define BLACK 2
 #define MAX_NODES 1000
 #define oo 1000000000
+
 int n;  // number of nodes
 int e;  // number of edges
 int capacity[MAX_NODES][MAX_NODES]; // capacity matrix
@@ -13,10 +13,11 @@ int color[MAX_NODES]; // needed for breadth-first search
 int pred[MAX_NODES];  // array to store augmenting path
 
 int min (int x, int y) {
-    return x<y ? x : y;  // returns minimum of x and y
+    return x < y ? x : y;  // returns minimum of x and y
 }
-int head,tail;
-int q[MAX_NODES+2];
+
+int head, tail;
+int q[MAX_NODES + 2];
 
 void enqueue (int x) {
     q[tail] = x;
@@ -33,19 +34,19 @@ int dequeue () {
 
 int bfs (int start, int target) {
     int u,v;
-    for (u=0; u<n; u++) {
+    for (u = 0; u < n; u++) {
 	color[u] = WHITE;
     }   
     head = tail = 0;
     enqueue(start);
     pred[start] = -1;
-    while (head!=tail) {
+    while (head != tail) {
 	u = dequeue();
         // Search all adjacent white nodes v. If the capacity
         // from u to v in the residual network is positive,
         // enqueue v.
-	for (v=0; v<n; v++) {
-	    if (color[v]==WHITE && capacity[u][v]-flow[u][v]>0) {
+	for (v = 0; v < n; v++) {
+	    if (color[v] == WHITE && capacity[u][v] - flow[u][v] > 0) {
 		enqueue(v);
 		pred[v] = u;
 	    }
@@ -53,28 +54,28 @@ int bfs (int start, int target) {
     }
     // If the color of the target node is black now,
     // it means that we reached it.
-    return color[target]==BLACK;
+    return color[target] == BLACK;
 }
 
 int max_flow (int source, int sink) {
-    int i,j,u;
+    int i, j, u;
     // Initialize empty flow.
     int max_flow = 0;
-    for (i=0; i<n; i++) {
-	for (j=0; j<n; j++) {
+    for (i = 0; i < n; i++) {
+	for (j = 0; j < n; j++) {
 	    flow[i][j] = 0;
 	}
     }
     // While there exists an augmenting path,
     // increment the flow along this path.
-    while (bfs(source,sink)) {
+    while (bfs(source, sink)) {
         // Determine the amount by which we can increment the flow.
 	int increment = oo;
-	for (u=n-1; pred[u]>=0; u=pred[u]) {
-	    increment = min(increment,capacity[pred[u]][u]-flow[pred[u]][u]);
+	for (u = n - 1; pred[u] >= 0; u = pred[u]) {
+	    increment = min(increment, capacity[pred[u]][u] - flow[pred[u]][u]);
 	}
         // Now increment the flow.
-	for (u=n-1; pred[u]>=0; u=pred[u]) {
+	for (u = n - 1; pred[u] >= 0; u = pred[u]) {
 	    flow[pred[u]][u] += increment;
 	    flow[u][pred[u]] -= increment;
 	}
@@ -87,19 +88,19 @@ int max_flow (int source, int sink) {
 //Reading the input file and the main program
 
 void read_input_file() {
-    int a,b,c,i,j;
-    FILE* input = fopen("mf.in","r");
+    int a, b, c, i, j;
+    FILE* input = fopen("mf.in", "r");
     // read number of nodes and edges
-    fscanf(input,"%d %d",&n,&e);
+    fscanf(input, "%d %d", &n, &e);
     // initialize empty capacity matrix 
-    for (i=0; i<n; i++) {
-	for (j=0; j<n; j++) {
+    for (i = 0; i < n; i++) {
+	for (j = 0; j < n; j++) {
 	    capacity[i][j] = 0;
 	}
     }
     // read edge capacities
     for (i=0; i<e; i++) {
-	fscanf(input,"%d %d %d",&a,&b,&c);
+	fscanf(input, "%d %d %d", &a, &b, &c);
 	capacity[a][b] = c;
     }
     fclose(input);
@@ -107,7 +108,7 @@ void read_input_file() {
 
 int main () {
     read_input_file();
-    printf("%d\n",max_flow(0,n-1));
+    printf("%d\n", max_flow(0, n - 1));
     return 0;
 }
 
