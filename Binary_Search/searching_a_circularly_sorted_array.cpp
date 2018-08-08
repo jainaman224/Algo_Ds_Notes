1abedeb88e2a33156281df5d3817d08177ac14ea
Aman Jain <jainaman224@gmail.com>
2018-08-08 22:09:22 +0530

    Update searching_a_circularly_sorted_array.cpp

--- Binary_Search/searching_a_circularly_sorted_array.cpp
+++ Binary_Search/searching_a_circularly_sorted_array.cpp
@@ -2,25 +2,25 @@
 
 using namespace std;
 
-int searchCircular(int a[],int len,int num)
+int searchCircular(int arr[], int len, int num)
 {
     int low = 0;
-    int high = len-1;
+    int high = len - 1;
     while(low <= high){
-        int mid = (low + (high-low)/2);
-        if(a[mid] == num) return mid;
-        else if(a[mid] <= a[high]){
-            if(num > a[mid] && num <= a[high]){
+        int mid = (low + (high - low) / 2);
+        if(arr[mid] == num) return mid;
+        else if(arr[mid] <= arr[high]){
+            if(num > arr[mid] && num <= arr[high]){
                 low = mid + 1;
             }
             else
                 high = mid - 1;
         }
-        else if(a[mid] > a[low]){
-            if(num < a[mid] && num >= a[low])
+        else if(arr[mid] > arr[low]){
+            if(num < arr[mid] && num >= arr[low])
                 high = mid - 1;
             else
-                low = mid+1;
+                low = mid + 1;
         }
     }
     return -1;
@@ -28,7 +28,7 @@ int searchCircular(int a[],int len,int num)
 
 int main()
 {
-    int a[] = {8,9,10,11,12,2,3,4,5,6,7}; //array must contain distinct elements for this to work
-    cout << endl << searchCircular(a,11,9);
+    int arr[] = {8, 9, 10, 11, 12, 2, 3, 4, 5, 6, 7}; // array must contain distinct elements for this to work
+    cout << endl << searchCircular(arr, 11, 9);
     return 0;
 }
