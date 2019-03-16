fb575bf3aaa9a25d9fc72052d542230fd93db99f
Abhinav Masters <mastersabhinav@gmail.com>
2019-03-17 00:55:56 +0530

    Removed unused statements.

--- Knapsack_Unbounded/Knapsack_Unbounded.cpp
+++ Knapsack_Unbounded/Knapsack_Unbounded.cpp
@@ -1,16 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ld long double
-#define ulint unsigned long long int
 #define lint long long int
-#define mp make_pair
-#define pb push_back
-#define f(x) for(lint i=0;i<x;i++)
-#define fj(x) for(lint j=0;j<x;j++)
-#define mod 1000000007
-#define F first
-#define S second
-#define faster ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 
 lint UnboundedKnapsack(lint Capacity,lint n, lint weight[],lint value[]){
     lint maxProfit[Capacity+1];
