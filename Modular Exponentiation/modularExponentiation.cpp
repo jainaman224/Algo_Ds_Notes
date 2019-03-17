b6a36fe2449c2af778afd3c8a9c77927c16ab3b0
Abhinav Masters <mastersabhinav@gmail.com>
2019-03-17 13:32:08 +0530

    Added comments.

--- Modular Exponentiation/modularExponentiation.cpp
+++ Modular Exponentiation/modularExponentiation.cpp
@@ -12,18 +12,18 @@ using namespace std;
 #define faster ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 
 lint modex(lint base,lint exponent,lint mod){
-    if(base == 0){
+    if(base == 0){                                  // 0^x = 0.
         return 0;		
     }
     if(exponent == 0){
-        return 1;
+        return 1;                                   // x^0 = 1.
     }
     if(exponent%2 == 0){
-        lint ans = modex(base,exponent/2,mod);
-        return (ans*ans)%mod;
+        lint ans = modex(base,exponent/2,mod);      // as, a^b = (a^(b/2))*(a^(b/2)), we use recursion to effectively reduce the no.
+        return (ans*ans)%mod;                       // of operations required for computing.
     }
-    return ((base%mod)*(modex(base,exponent-1,mod)))%mod;
-}
+    return ((base%mod)*(modex(base,exponent-1,mod)))%mod;   // when exponent is odd, we reduce it by 1 to make it even, which again uses
+}                                                           // recursion for computation.
 
 int main(){
     lint base = 12;
