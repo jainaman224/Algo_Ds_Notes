dbfdaaecffd3c386367cb48f6cdbfbc82742ab3f
divs30 <40079593+divs30@users.noreply.github.com>
2019-03-19 02:27:59 +0530

    corrected modular exponentiation code
    corrected modular exponentiation code

--- modular_exponentiation/iterative_modular exponentiation.c
+++ modular_exponentiation/iterative_modular exponentiation.c
@@ -1,24 +1,25 @@
 #include<stdio.h>
 
 //calculation of modular exponentiation using iterative approach
-long long power(long long base, long long exp) {
+long long power(long long base, long long exp,long long p) {
     long long holder = 1;
+    base=base%p;
     while (exp > 0) {
         if (exp & 1)
-            holder = holder * base;
-        base = base * base;
+            holder = (holder * base)%p;
+        base = (base * base)%p;
         exp >>= 1;
     }
     return holder;
 }
 int main()
 {
-	long long base,exponent ;
+	long long base,exponent ,p;
     
-	scanf("%lli %lli",&base,&exponent);
+	scanf("%lli %lli %lli",&base,&exponent,&p);
 	//taking input for base and exponent ;
 
-	printf("The result of given expression is id %lli",power(base,exponent));
+	printf("The result of given expression is id %lli",power(base,exponent,p));
 	
 	return 0;
 }
