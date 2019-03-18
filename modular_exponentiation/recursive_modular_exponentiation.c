70fcf9405897ea9b1baf916b7c542c9e14dffe52
divs30 <40079593+divs30@users.noreply.github.com>
2019-03-19 02:44:20 +0530

    corrected modular exponentiation code
    corrected modular exponentiation code

--- modular_exponentiation/recursive_modular_exponentiation.c
+++ modular_exponentiation/recursive_modular_exponentiation.c
@@ -1,23 +1,33 @@
 #include<stdio.h>
 
 //recursive mechanism 
-long long int power(long int base,long int exponent)
+long long power(long int base,long long exponent,long long p)
 {
+	long long holder;
+	 if(base==0)
+	 	return 0;
 	 if ( exponent == 0)
         return 1;
-    long long holder = power(base,  exponent / 2);//r is the placeholder
-    if (exponent % 2)
-        return holder * holder * base;
-    else
-        return holder * holder;
-	
+        
+    if(exponent%2==0)
+    {
+    	holder=power(base,exponent/2,p);
+    	holder=(holder*holder)%p;
+	}
+	else
+	{
+		holder=base%p;
+		holder=(holder*power(base,exponent-1,p)%p)%p;
+	}
+	return (holder+p)%p;
+        
 }
 
 int main()
 {
-	long int base,exponent;
-	scanf("%ld %ld",&base,&exponent);
-	printf("The value of given expression is %ld",power(base,exponent));	
+	long long base,exponent,p;
+	scanf("%lli %lli %lli",&base,&exponent,&p);
+	printf("The value of given expression is %lli",power(base,exponent,p));	
 	return 0;
 	
 }
@@ -25,6 +35,6 @@ int main()
 /*
 output:
 base : 2
-exponent:10
-output :The result of given expression is 1024
+exponent:4
+output :The result of given expression is 1
 */
