bd61256f96a578095e4884d4dbb5d698748ddf85
Kanchan <kthareja12@gmail.com>
2019-03-20 11:33:40 +0530

    fixed changes

--- Edit_Distance_Algorithm/Edit_Distance.js
+++ Edit_Distance_Algorithm/Edit_Distance.js
@@ -1,5 +1,6 @@
 
-function Edit_Distance(a, b, m , n) {
+
+function Edit_Distance(first_str, second_str, m , n) {
 
     // If first string is empty, the only option is to insert all characters of second string into first 
     if( m == 0) 
@@ -7,17 +8,17 @@ function Edit_Distance(a, b, m , n) {
 
     // If second string is empty, remove all characters of first string.
     if (n == 0) 
-    return m 
+        return m; 
 
     // If last characters of two strings are same, nothing much to do.
-    if( a[m-1] == b[n-1] )
-    return Edit_Distance(a, b, m-1, n-1) 
+    if( first_str[m-1] == second_str[n-1] )
+        return Edit_Distance(first_str, second_str, m-1, n-1);
 
     // If last characters are not same, consider all three operations on last character of first string, recursively 
     // compute minimum cost for all three operations and take minimum of three values. 
-    return 1 + Math.min(Edit_Distance(a, b, m, n-1),       // Insert 
-            Edit_Distance(a, b, m-1, n),          // Remove 
-            Edit_Distance(a, b, m-1, n-1)         // Replace 
+    return 1 + Math.min(Edit_Distance(first_str, second_str, m, n-1),       // Insert 
+            Edit_Distance(first_str, second_str, m-1, n),          // Remove 
+            Edit_Distance(first_str, second_str, m-1, n-1)         // Replace 
             );
 
 } 
@@ -30,3 +31,4 @@ console.log(Edit_Distance(a, b, a.length(), b.length()))
 
 // OUTPUT 
 //   6
+
