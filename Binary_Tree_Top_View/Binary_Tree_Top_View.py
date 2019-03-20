eb3d77ddd5dcfecb26cc5fe38b8051cdc40d85b3
Harshit Singh <harshit.singh1101@gmail.com>
2019-03-21 01:42:24 +0530

    Update Binary_Tree_Top_View.py

--- Binary_Tree_Top_View/Binary_Tree_Top_View.py
+++ Binary_Tree_Top_View/Binary_Tree_Top_View.py
@@ -2,10 +2,10 @@ class newNode:
 
 # Construct to create a newNode
 def __init__(self, key):
-self.data = key
-self.left = None
-self.right = None
-self.hd = 0
+  self.data = key
+  self.left = None
+  self.right = None
+  self.head = 0
 
 # function should print the topView
 # of the binary tree
@@ -14,9 +14,9 @@ def topview(root) :
 if(root == None) :
   return
 q = []
-m = dict()
-hd = 0
-root.hd = hd
+mp = dict()
+head = 0
+root.hd = head
 
 # push node and horizontal
 # distance to queue
@@ -24,35 +24,33 @@ q.append(root)
 
 while(len(q)) :
   root = q[0]
-  hd = root.hd
+  head = root.head
 
   # count function returns 1 if the
   # container contains an element
   # whose key is equivalent to hd,
   # or returns zero otherwise.
-  if hd not in m:
+  if head not in mp:
     m[hd] = root.data
     if(root.left) :
-      root.left.hd = hd – 1
+      root.left.head = head – 1
       q.append(root.left)
 
     if(root.right):
-      root.right.hd = hd + 1
+      root.right.hd = head + 1
       q.append(root.right)
 
   q.pop(0)
-for i in sorted (m):
-  print(m[i], end = “”)
+for i in sorted (mp):
+  print(mp[i], end = “”)
 
 # Driver Code
 if __name__ == ‘__main__’:
-
-
-root = newNode(1)
-root.left = newNode(2)
-root.right = newNode(3)
-root.left.right = newNode(4)
-root.left.right.right = newNode(5)
-root.left.right.right.right = newNode(6)
-print(“Following are nodes in top”,“view of Binary Tree”)
-topview(root)
+  root = newNode(1)
+  root.left = newNode(2)
+  root.right = newNode(3)
+  root.left.right = newNode(4)
+  root.left.right.right = newNode(5)
+  root.left.right.right.right = newNode(6)
+  print(“Following are nodes in top”,“view of Binary Tree”)
+  topview(root)
