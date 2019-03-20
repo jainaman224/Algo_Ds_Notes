1c54274a96f66c37668b5a20cc012adc5b1e4590
Harshit Singh <harshit.singh1101@gmail.com>
2019-03-21 01:43:06 +0530

    Update Binary_Tree_Top_View.py

--- Binary_Tree_Top_View/Binary_Tree_Top_View.py
+++ Binary_Tree_Top_View/Binary_Tree_Top_View.py
@@ -10,39 +10,38 @@ def __init__(self, key):
 # function should print the topView
 # of the binary tree
 def topview(root) :
+  if(root == None) :
+    return
+  q = []
+  mp = dict()
+  head = 0
+  root.hd = head
 
-if(root == None) :
-  return
-q = []
-mp = dict()
-head = 0
-root.hd = head
+  # push node and horizontal
+  # distance to queue
+  q.append(root)
 
-# push node and horizontal
-# distance to queue
-q.append(root)
+  while(len(q)) :
+    root = q[0]
+    head = root.head
 
-while(len(q)) :
-  root = q[0]
-  head = root.head
+    # count function returns 1 if the
+    # container contains an element
+    # whose key is equivalent to hd,
+    # or returns zero otherwise.
+    if head not in mp:
+      m[hd] = root.data
+      if(root.left) :
+        root.left.head = head – 1
+        q.append(root.left)
 
-  # count function returns 1 if the
-  # container contains an element
-  # whose key is equivalent to hd,
-  # or returns zero otherwise.
-  if head not in mp:
-    m[hd] = root.data
-    if(root.left) :
-      root.left.head = head – 1
-      q.append(root.left)
+      if(root.right):
+        root.right.hd = head + 1
+        q.append(root.right)
 
-    if(root.right):
-      root.right.hd = head + 1
-      q.append(root.right)
-
-  q.pop(0)
-for i in sorted (mp):
-  print(mp[i], end = “”)
+    q.pop(0)
+  for i in sorted (mp):
+    print(mp[i], end = “”)
 
 # Driver Code
 if __name__ == ‘__main__’:
