acf208912a88f81be29d43c032c318b7dc6fd1bc
Kanchan <kthareja12@gmail.com>
2019-03-20 11:36:03 +0530

    fixed changes

--- Edit_Distance_Algorithm/Edit_Distance.py
+++ Edit_Distance_Algorithm/Edit_Distance.py
@@ -1,5 +1,6 @@
 
-def Edit_Distance(a, b, m , n): 
+
+def Edit_Distance(first_str, second_str, m , n): 
 
 	# If first string is empty, the only option is to insert all characters of second string into first 
 	if m == 0: 
@@ -10,14 +11,14 @@ def Edit_Distance(a, b, m , n):
 		return m 
 
 	# If last characters of two strings are same, nothing much to do.
-	if a[m-1]==b[n-1]: 
-		return Edit_Distance(a,b,m-1,n-1) 
+	if first_str[m-1] == second_str[n-1]: 
+		return Edit_Distance(first_str,second_str,m-1,n-1) 
 
 	# If last characters are not same, consider all three operations on last character of first string, recursively 
 	# compute minimum cost for all three operations and take minimum of three values. 
-	return 1 + min(Edit_Distance(a, b, m, n-1), # Insert 
-				Edit_Distance(a, b, m-1, n), # Remove 
-				Edit_Distance(a, b, m-1, n-1) # Replace 
+	return 1 + min(Edit_Distance(first_str, second_str, m, n-1), # Insert 
+				Edit_Distance(first_str, second_str, m-1, n), # Remove 
+				Edit_Distance(first_str, second_str, m-1, n-1) # Replace 
 				) 
 
 a = "kanchan"
@@ -27,3 +28,5 @@ print Edit_Distance(a, b, len(a), len(b))
 
 # OUTPUT :
 #   6
+
+
