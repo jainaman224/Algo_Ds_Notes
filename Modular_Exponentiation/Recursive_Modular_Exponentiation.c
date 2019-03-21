67637533eb76f4ae4cf405a50589f46b88f8a6a1
Aman Jain <jainaman224@gmail.com>
2019-03-21 21:25:49 +0530

    Update and rename modular_exponentiation/recursive_modular_exponentiation.c to Modular_Exponentiation/Recursive_Modular_Exponentiation.c

--- Modular_Exponentiation/Recursive_Modular_Exponentiation.c
+++ Modular_Exponentiation/Recursive_Modular_Exponentiation.c
@@ -4,15 +4,15 @@
 long long power(long int base,long long exponent,long long p)
 {
 	long long holder;
-	 if(base==0)
-	 	return 0;
-	 if ( exponent == 0)
-        return 1;
+	if(base==0)
+		return 0;
+	if ( exponent == 0)
+        	return 1;
         
-    if(exponent%2==0)
-    {
-    	holder=power(base,exponent/2,p);
-    	holder=(holder*holder)%p;
+	if(exponent%2==0)
+	{
+    		holder=power(base,exponent/2,p);
+    		holder=(holder*holder)%p;
 	}
 	else
 	{
@@ -20,7 +20,6 @@ long long power(long int base,long long exponent,long long p)
 		holder=(holder*power(base,exponent-1,p)%p)%p;
 	}
 	return (holder+p)%p;
-        
 }
 
 int main()
@@ -29,7 +28,6 @@ int main()
 	scanf("%lli %lli %lli",&base,&exponent,&p);
 	printf("The value of given expression is %lli",power(base,exponent,p));	
 	return 0;
-	
 }
 
 /*
