3b86694780952dc40a8b118f9d8d9d7181e293d1
Kanchan <kthareja12@gmail.com>
2019-03-21 13:03:07 +0530

    completed changes

--- Edit_Distance_Algorithm/Edit_Distance.java
+++ Edit_Distance_Algorithm/Edit_Distance.java
@@ -8,22 +8,22 @@ class Edit_distance
 		else return diagnol; 
 	} 
 
-	static int Edit_Distance(String first_str , String second_str , int m , int n) 
+	static int Edit_Distance(String first_str , String second_str , int size1 , int size2) 
 	{ 
 		// If first string is empty, the only option is to insert all characters of second string into first 
-		if (m == 0) return n; 
+		if (size1 == 0) return size2; 
 	
 		// If second string is empty, remove all characters of first string 
-		if (n == 0) return m; 
+		if (size2 == 0) return size1; 
 	
 	 
-		if (first_str.charAt(m-1) == second_str.charAt(n-1)) 
-		return Edit_Distance(first_str, second_str, m-1, n-1); 
+		if (first_str.charAt(size1-1) == second_str.charAt(size2-1)) 
+			return Edit_Distance(first_str, second_str, size1-1, size2-1); 
 	
 		// If last characters are not same, consider all three operations on last character of first string, recursively 
 		// compute minimum cost for all three operations and take minimum of three values. 
-		return 1 + min ( Edit_Distance(first_str, second_str, m, n-1), Edit_Distance(first_str, second_str, m-1, n), 
-				Edit_Distance(first_str, second_str, m-1, n-1)); 
+		return 1 + min ( Edit_Distance(first_str, second_str, size1, size2-1), Edit_Distance(first_str, second_str, size1-1, size2), 
+				Edit_Distance(first_str, second_str, size1-1, size2-1)); 
 	} 
 
 	public static void main(String args[]) 
