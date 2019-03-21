83fe10c870f635fa46c3724487833d9c3f63a102
Aman Jain <jainaman224@gmail.com>
2019-03-21 21:27:06 +0530

    Update and rename modular_exponentiation/iterative_modular exponentiation.c to Modular_Exponentiation/Iterative_Modular_Exponentiation.c

--- Modular_Exponentiation/Iterative_Modular_Exponentiation.c
+++ Modular_Exponentiation/Iterative_Modular_Exponentiation.c
@@ -2,16 +2,17 @@
 
 //calculation of modular exponentiation using iterative approach
 long long power(long long base, long long exp,long long p) {
-    long long holder = 1;
-    base=base%p;
-    while (exp > 0) {
-        if (exp & 1)
-            holder = (holder * base)%p;
-        base = (base * base)%p;
-        exp >>= 1;
-    }
-    return holder;
+	long long holder = 1;
+	base=base%p;
+	while (exp > 0) {
+		if (exp & 1)
+			holder = (holder * base)%p;
+		base = (base * base)%p;
+		exp >>= 1;
+	}
+	return holder;
 }
+
 int main()
 {
 	long long base,exponent ,p;
@@ -23,12 +24,10 @@ int main()
 	
 	return 0;
 }
+
 /*
 output:
 base : 2
 exponent:4
 output :The result of given expression is1
-
-
-
 */
