f24f72bb75cf398b5c33ff6cf8acdf99b4c2ad76
somya-kapoor <42409030+somya-kapoor@users.noreply.github.com>
2019-03-22 23:25:48 +0530

    Pascal's_Triangle.c

---
+++ Pascal's_Triangle/Pascal's_Triangle.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+   int f;
+   for(f = 1; n > 1; n--)
+      f *= n;
+   return f;
+}
+
+int main() 
+{
+   int n, i, j;
+   printf("Enter the numbers of rows ");
+   scanf("%d",&n);
+   for(i = 0; i < n; i++) 
+   {
+      for(j = 0; j < n-i; j++)
+         printf("  ");
+      for(j = 0; j <= i; j++)
+         printf(" %3d",factorial(i)/(factorial(j)*factorial(i-j)) );
+      printf("\n");
+   }
+   return 0;
+}
+//INPUT:Enter the numbers of rows 6
+//OUTPUT:
+//               1
+//             1   1
+//           1   2   1
+//         1   3   3   1
+//       1   4   6   4   1
+//     1   5  10  10   5   1
