34dbcd93209b4b0f3763cf1e2f7824c284f46376
somya-kapoor <42409030+somya-kapoor@users.noreply.github.com>
2019-03-22 22:51:34 +0530

    Update Pigeonhole_Sort.c

--- Pigeonhole_Sort/Pigeonhole_Sort.c
+++ Pigeonhole_Sort/Pigeonhole_Sort.c
@@ -6,12 +6,12 @@ void pigeonholesort(int arr[], int size)
    int min = arr[0], max = arr[0]; 
    // Finding min value and max values in the array arr[]
    for (int i = 1; i < size; i++) 
-     { 
+   { 
         if (arr[i] < min) 
             min = arr[i]; 
         if (arr[i] > max) 
             max = arr[i]; 
-      }
+    }
     int range;
     range = max - min + 1;
     //Creating the holes
@@ -19,18 +19,18 @@ void pigeonholesort(int arr[], int size)
     for(int i=0;i<range;i++)
           holes[i]=0;
     //Filling the holes
-    for (int j=0; j < size;j++)
-        holes[arr[j] - min]++;
-    // Put the elements back into the array in order.
-    // k is the index of sorted array
-    int k=0;
-    for (int count = 0; count < range; count++)
-        while (holes[count]-- > 0)
+    for (int j=0; j < size;j++) while (holes[count]-- > 0)
           {
                    arr[k] = count + min;
                    k=k+1;
           }
 }
+        holes[arr[j] - min]++;
+    // Put the elements back into the array in order.
+    // k is the index of sorted array
+    int k=0;
+    for (int count = 0; count < range; count++)
+       
  
 int main()
 { 
@@ -50,7 +50,8 @@ int main()
                         
    return 0;
 }
-/INPUT:Enter the size of the array 5
+
+//INPUT:Enter the size of the array 5
 //Enter the elements -10
 //5
 //0
