a221c95bd64c84b282b44250f3aeeee64e41a0ed
Dishant Sethi <31399916+dishantsethi@users.noreply.github.com>
2019-03-25 15:05:49 +0530

    update

--- Array-Rotation/Array-rotation.py
+++ Array-Rotation/Array-rotation.py
@@ -1,18 +1,21 @@
 //Program to rotate array Arr[] of size n by d Elements
-def leftRotationOneByOne(arr,n):
+def leftRotation(arr,n):
     temp = arr[0]
     for i in range(n-1):
         arr[i] = arr[i+1]
     arr[n-1] = temp
 
-def leftRotation(arr,d,n):
+def Rotate(arr,d,n):
     for i in range(d):
-        leftRotationOneByOne(arr,n)
-
-def printArray(arr,n):
+        leftRotation(arr,n)
     for i in range(n):
         print(arr[i],end=" ")
 
 arr = [1,2,3,4,5,6,7]
-leftRotation(arr,2,7)
-printArray(arr,7) 
+Rotate(arr,2,7)
+
+/*
+Input array: {1, 2, 3, 4, 5, 6, 7 }
+Expected Output
+3 4 5 6 7 1 2 
+*/
