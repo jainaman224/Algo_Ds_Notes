80aa4eea70da8e96feb54752bba6ad6718f81100
Divya Shahi <40079593+divs30@users.noreply.github.com>
2019-03-25 21:52:01 +0530

    improved the code
    improved the code

--- kadane's.c
+++ kadane's.c
@@ -1,81 +1,65 @@
 #include<stdio.h>
 
-int max(int ans,int sum)
+int max(int ans,int sum)		//function to calculate maximum of two
 {
 	return (ans>sum?ans:sum);
-	
 }
 
-int max_sum_negative_array(int a[],int n)
-{//since in  an array containing only negative elments only no non neagative element maximum sum will be the smallest number
-	int ans=a[0],i;
-	
-	for(i=0;i<n;i++)
-	{
-		if(a[i]>ans)
-			ans=a[i];
-	}		
-	return ans;
-	
-	
-}
-	
-int max_sum(int a[],int n)
+int max_sum(int a[],int size)		//function to calculate max sum	
 {
 	int ans=0;
 	int sum=0,i;
-	for(i=0;i<n;i++)
+	for(i=0;i<size;i++)
 	{
 		if(sum+a[i]>0)
 			sum+=a[i];
 		else
 			sum=0;
-		ans=max(ans,sum);
-		
+		ans=max(ans,sum);	
 	}
-	
 	return ans;
 }
+
 int main()
 {
-	int n,i,k,flag=0;
-	scanf("%d",&n);
-	int a[n];
-	for(i=0;i<n;i++)
+	int size,i,k,max;
+	scanf("%d",&size);
+	int a[size];
+	for(i=0;i<size;i++)
 		scanf("%d",&a[i]);
-	for(i=0;i<n;i++)
+	max=a[0];
+	for(i=0;i<size;i++)
 	{
+		if(a[i]>max)		// putting maximum value in 
+			max=a[i];
 		if(a[i]>=0)
 		{
-			flag=1;
 			break;
 		}	
 	}
 	
-	
-	if(flag==1)
+	if(max>=0)			//if maximum value is positive value than call max_sum(int *,int ) function
 	{
-		k=max_sum(a,n);
+		k=max_sum(a,size);
 		printf("Maximum sum of sub-array %d",k);	
 	}
 	else
 	{
-			k=max_sum_negative_array(a,n);
-		printf("Maximum sum of sub-array %d",k);
-		
+		printf("%d",max);	//if max element is negative than max  itself is the maximum sum
 	}
 		
 	return 0;
 }
 
 
-/*1.input :3
-	 -1 -2 -3
-   output :-1
-  2.input :3
-	 -1 2 3
-   output :5
-  3input :3
-	 -1 0 -3
-   output :0
-  */
+/*
+output:
+5
+-8 -7 -2 -13 -74
+-2
+*/
+/*output
+5
+-1 5 -3 10 -11
+Maximum sum of sub-array 12
+*/
