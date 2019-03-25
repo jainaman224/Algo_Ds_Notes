3572f82f46435c9a5c2972a6dd233857bb27babe
kaustubh-pandey <kaustubh.p16@iiits.in>
2019-03-26 01:10:57 +0530

    Added Square Root Decomposition in C++

---
+++ Square_Root_Decomposition/Square_Root_Decomposition.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void update(int index,int value,int *blocks,int a[],int n){
+	blocks[(int)(index/sqrt(n))] += value-a[index];
+	a[index]=value;
+}
+//assuming 0 based indexing
+int query(int low,int high,int *blocks,int a[],int n){
+	int block_size=sqrt(n);
+	int sum=0;
+
+	while(low % block_size!=0 && low<high){
+		sum+=a[low];
+		low++;
+	}
+	while(low+block_size<=high){
+		sum+=blocks[low/block_size];
+		low+=block_size;
+	}
+	while(low<=high){
+		sum+=a[low];
+		low++;
+	}
+
+	return sum;
+
+}
+//divide the array in blocks of size sqrt(n), sum of elements in each block is stored in blocks
+int *formBlocks(int a[],int n){
+	int block_size=sqrt(n);
+	int index=-1;
+	int *blocks = new int[(int)(n/block_size)+1];
+
+	for(int i=0;i<n;i++){
+		if(i%block_size==0){
+			index++;
+		}
+		blocks[index]+=a[i];
+	}
+	return blocks;
+}
+int main(){
+	/* Applying square root decomposition to perform range queries
+	for finding the sum of numbers in a given range 
+	Indexing : 0 based in array
+	*/
+	int n = 10; //number of elements 
+	int a[]={2,3,4,6,8,9,1,-1,0,3};  //elements
+	int *blocks=formBlocks(a,n);
+	//queries
+	cout<<query(0,2,blocks,a,n)<<endl; //find sum from index 0 to index 2
+	cout<<query(3,7,blocks,a,n)<<endl; //find sum from index 3 to index 7
+	cout<<query(1,4,blocks,a,n)<<endl; //find sum from index 1 to index 4
+	//update
+	update(3,1,blocks,a,n);				//update the value at index 3 with value of 1
+	//query
+	cout<<query(1,4,blocks,a,n)<<endl;	//find sum from index 1 to index 4 
+	return 0;
+}
+
---
+++ Square_Root_Decomposition/Square_Root_Decomposition.py
@@ -0,0 +1,48 @@
+from math import sqrt
+
+def update(index,value,blocks,a):
+	blocks[int(index/sqrt(len(a)))] += value-a[index]
+	a[index]=value
+#assuming 0 based indexing
+def query(low,high,blocks,a):
+	block_size=int(sqrt(len(a)))
+	total=0
+	while(low % block_size!=0 and low<high):
+		total+=a[low]
+		low+=1
+	
+	while(low+block_size<=high):
+		total+=blocks[low//block_size]
+		low+=block_size
+	
+	while(low<=high):
+		total+=a[low]
+		low+=1
+	return total
+#divide the list in blocks of size sqrt(n), sum of elements in each block is stored in blocks
+def formBlocks(a):
+	block_size=int(sqrt(len(a)))
+	index=-1
+	blocks=[0]*(len(a)//block_size+1)
+	for i in range(len(a)):
+		if(i%block_size==0):
+			index+=1
+		blocks[index]+=a[i]
+	return blocks
+
+'''
+Applying square root decomposition to perform range queries
+	for finding the sum of numbers in a given range 
+	Indexing : 0 based in list
+'''
+a=[2,3,4,6,8,9,1,-1,0,3]
+blocks=formBlocks(a)
+#queries
+print(query(0,2,blocks,a))	#find sum from index 0 to index 2
+print(query(3,7,blocks,a))	#find sum from index 3 to index 7
+print(query(1,4,blocks,a))	#find sum from index 1 to index 4
+#update 
+update(3,1,blocks,a)		#update the value at index 3 with value of 1
+#verify if the update was success
+print(query(1,4,blocks,a))
+
