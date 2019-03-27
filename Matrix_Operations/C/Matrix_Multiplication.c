314cd99989e2ed9fe7895b67fdff8cf2fd297e89
Ayan Raj <ayanujju351@gmail.com>
2019-03-27 15:21:07 +0530

    Matrix_Multiplication.c

---
+++ Matrix_Operations/C/Matrix_Multiplication.c
@@ -0,0 +1,65 @@
+/* C program to multiply two square matrices */
+
+#include <stdio.h>  
+
+#define N 4  
+
+/* This function multiplies  
+   mat1[][] and mat2[][], and  
+   stores the result in res[][] */
+   
+void multiply(int mat1[][N],  
+              int mat2[][N],  
+              int res[][N]) 
+{ 
+    int i, j, k; 
+    for (i = 0; i < N; i++) 
+    { 
+        for (j = 0; j < N; j++) 
+        { 
+            res[i][j] = 0; 
+            for (k = 0; k < N; k++) 
+                res[i][j] += mat1[i][k] *  
+                             mat2[k][j]; 
+        } 
+    } 
+} 
+
+// Main Function 
+int main() 
+{ 
+    int i, j; 
+    int res[N][N]; // for storing result 
+    int mat1[N][N] = {{1, 1, 1, 1}, 
+                      {2, 2, 2, 2}, 
+                      {3, 3, 3, 3}, 
+                      {4, 4, 4, 4}}; 
+
+    int mat2[N][N] = {{1, 1, 1, 1}, 
+                      {2, 2, 2, 2}, 
+                      {3, 3, 3, 3}, 
+                      {4, 4, 4, 4}}; 
+
+    multiply(mat1, mat2, res); 
+    
+    printf("Result matrix is \n");
+    for (i = 0; i < N; i++) 
+    { 
+        for (j = 0; j < N; j++)
+        printf("%d ",res[i][j]);
+        printf("\n");
+    } 
+
+    return 0; 
+} 
+
+
+/*
+OUTPUT:
+Result matrix is
+10 10 10 10
+20 20 20 20
+30 30 30 30
+40 40 40 40 
+*/
+
