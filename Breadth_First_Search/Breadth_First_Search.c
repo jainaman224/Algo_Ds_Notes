6ced5a0561b6e5d0738e6168ef21eba8c73fe1d1
susiejojo <dipanwita.g@research.iiit.ac.in>
2019-03-28 10:55:43 +0530

    BFS Code in C

--- Breadth_First_Search/Breadth_First_Search.c
+++ Breadth_First_Search/Breadth_First_Search.c
@@ -3,86 +3,113 @@
 //uses 1-based indexing for vertices
 #include<stdio.h>
 #include<stdlib.h>
-int queue[1000];
-int front=-1;
-int rear=-1;
+
+//inititation of data structures
+int queue[1000];//the queue array
 int graph[1000][1000];//the graph 2-D array
-int state[1000]; //stores 1 for visited nodes(black), 0 for unvisited nodes(white)
-int n; 
+int state[1000]; //array that stores 1 for visited nodes(black), 0 for unvisited nodes(white)
+
+int n; //no of vertices in the graph 
+
+//queue pointers:
+int front = -1;
+int rear = -1;
+
+//function declarations
 int dequeue(int n);
 int front1();
 int enqueue(int e);
 int isempty();
-int printq(int n);
 void graph_initiator();
 void BFS(int node);
-//BFS functions
-void BFS_run()//runs BFS on the whole graph starting from vertex 0
+
+
+//BFS FUNCTIONS:
+
+/*Runs BFS on every vertex of the graph that hasn't been visited(marked black or 1) yet */
+void BFS_run()	
 {
-	for(int i=0; i<n; i++) 
+	for (int i = 0; i < n; i++) // initialising state of each vertex as 0(white)
 		state[i] = 0;
 	int v;
-	for (int i=0;i<n;i++)
+	for (int i = 0; i < n; i++)
 	{
-	if (state[i]!=1)
-	BFS(i);
+		if (state[i] != 1)
+		    BFS(i);
 	}
 }
  
-void BFS(int node)//main function for BFS
+/*BFS first enqueues each unvisited vertex to the queue,marks it as visited and stores its value in val.
+ Then it dequeues the vertex at the front of the queue itself from the queue.
+ For the row of val in the adjacency matrix every column that is marked 1 has an edge from the val vertex to that vertex. 
+ Hence they form the neighbors of val.Checks if the neighbours have already been visited or not. 
+ If not, then it enqueues the neighbour and marks it as visited. Else, it moves on to the next neighbour.
+ */
+void BFS(int node)
 {
 	enqueue(node);
-	state[node]=1;
+	state[node] = 1;
 	while (!isempty())
 	{
-	int val=front1();
-	printf("%d ",val+1);
-	dequeue(n);
-	for (int i=0;i<n;i++)
-	{
-		if (graph[val][i]==1){ // checks for neighbours of node to perform BFS on
-		if (state[graph[val][i]]!=1)
+		int val = front1();
+		printf("%d ",val + 1);
+		dequeue(n);
+		for (int i = 0; i < n; i++)
 		{
-			enqueue(graph[val][i]);
-			state[graph[val][i]]=1;
-		}
-	}
-		else
-			continue;
-	} 
+			if (graph[val][i] == 1)// checks for existence of edge between two nodes
+			{ 
+				if (state[graph[val][i]] != 1) // checks for neighbours of node to perform BFS on
+				{
+					enqueue(graph[val][i]);
+					state[graph[val][i]] = 1;
+				}
+			}
+			else
+				continue;
+		} 
 	}
 }
-//UTILITY FUNCTIONS FOR QUEUE
+
+//FUNCTIONS FOR QUEUE
+
+/* Function to enqueue, i.e insert new element at the rear of the queue array, adds 1 to the rear pointer 
+after each insertion. Handles isempty case by setting front =0
+*/
 int enqueue(int e)
 {
 
-	if(front == -1) 
+	if (front == -1) 
 		front = 0;
-	rear = rear+1;
+	rear = rear + 1;
 	queue[rear] = e ;
 	
 }
+/*Function to delete elements from the front of the queue and return them.
+ Returns queue underflow while trying to dequeue from an empty queue */
 int dequeue(int n)
 {
 	int elmnt;
-	if(front == -1 || front > rear)
+	if (front == -1 || front > rear)
 	{
 		printf("Queue Underflow\n");
 		exit(1);
 	}
 	
 	elmnt= queue[front];
-	front+=1;
+	front += 1;
 	return elmnt;
 }
-int front1()//returns queue[front]
+
+/*Returns the element at front of the queue*/
+int front1()
 {
 	if (isempty()){
 		printf("Empty queue!!\n");}
 	else
-	return queue[front];
+		return queue[front];
 }
 
+/*Returns 1 if queue is empty,else 0*/
 int isempty()
 {
 	
@@ -91,13 +118,9 @@ int isempty()
 	else
 		return 0;
 }
-int printq(int n)
-{
-	int size=(rear-front);
-	for (int i=front;i<=size+1;i++)
-		printf("%d\n",queue[i]);
-}
-void graph_initiator()//initiates the graph
+
+/*Function to initiate the graph from edges input by user*/
+void graph_initiator()
 {
 	n=0;
 	printf("Enter number of vertices : \n");
@@ -105,18 +128,21 @@ void graph_initiator()//initiates the graph
 	int e;
 	printf("Enter number of edges:\n");
 	scanf("%d",&e);
-	printf("Enter start and finish vertex for each edge:\n");
+
 	//inputs the edges from start to finish nodes
- 	for (int i=0;i<n;i++)
+	printf("Enter start and finish vertex for each edge:\n");
+ 	for (int i = 0; i < n; i++)//initialise whole graph matrix with 0s
  	{
- 		for (int j=0;j<n;j++)
+ 		for (int j = 0;j < n; j++)
  			graph[i][j]=0;
  	}
-	for(int i=1; i<=e; i++)
+
+	for (int i = 1; i <= e; i++)
 	{
 		int origin,destin;
 		scanf("%d %d",&origin,&destin);
-		graph[origin-1][destin-1] = 1;
+		//sets 1 for every column in the row of the origin where an edge to destination exists
+		graph[origin - 1][destin - 1] = 1;
 	}
 }
 int main()
