a7bd58b2fc1c93fab522110af702990b5cc9ae7a
Kunal Kushwaha <kunalkushwaha453@gmail.com>
2019-03-29 19:44:45 +0530

    Dynamic Queue Single File created

--- Dynamic_Queue/Dynamic_Queue.java
+++ Dynamic_Queue/Dynamic_Queue.java
@@ -1,4 +1,9 @@
-public class Queue_Circluar {
+/*
+    author: kunal-kushwaha
+    date: 29-03-2019
+*/
+
+class Queue_Circluar {
 
     protected int[] data;
     private static int DEFAULT_SIZE = 10;
@@ -66,3 +71,57 @@ public class Queue_Circluar {
     }
 
 }
+
+class Queue extends Queue_Circluar{
+
+    public Queue(){
+        super();
+    }
+
+    public Queue(int size){
+        super(size);
+    }
+
+    @Override
+    public boolean insert(int item) {
+
+        if(isFull()){
+            System.out.println("RESIZING");
+            int[] temp = new int[data.length * 2];
+
+            for (int i = 0; i < data.length; i++) {
+                temp[i] = data[(front + i) % data.length];
+            }
+
+            front = 0;
+            end = data.length;
+            data = temp;
+        }
+        return super.insert(item);
+    }
+}
+
+public class Dynamic_Queue {
+
+    public static void main(String[] args) throws Exception{
+
+        Queue queue = new Queue();
+
+        for (int i = 0; i < 50; i++) {
+            queue.insert(i);
+        }
+
+        System.out.println("Queue contains : ");
+        queue.display();
+
+    }
+}
+
+/*
+    OUTPUT :
+    RESIZING
+    RESIZING
+    RESIZING
+    Queue contains :
+    0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 END
+*/
