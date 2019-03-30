02469086b8279174594e97922ae8432a49c7a2e2
singh-shreya6 <singh.shreya8@gmail.com>
2019-03-30 13:42:42 +0530

    Fixes in Activity Selection in C, C++, Go

--- Activity_Selection/Activity_Selection.c
+++ Activity_Selection/Activity_Selection.c
@@ -1,14 +1,22 @@
+/*
+The problem statement for Activity Selection is that "Given a set of n activities with their start and finish times, we need to select maximum number of non-conflicting activities that can be performed by a single person, given that the person can handle only one activity
+at a time." The Activity Selection problem follows a Greedy approach.
+*/
+
 #include <stdio.h>
 
-void print_activities(int start[], int finish[], int n)
+// Function to compute the activities to be chosen
+void ActivitySelection(int start[], int finish[], int n)
 {
-    printf("Following activities are selected");
+    printf("Following activities are selected: ");
+    // Select the first activity
     int i = 0;
     printf("\n%d  ", i);
     int j;
-    for (j = 1; j < n; j++)
+    // if start time of current activity j is greater than or equal to previous activity chosen, select activity j
+    for (j = 1; j < n; j++) 
     {
-        if (start[j] >= finish[i])
+        if (start[j] >= finish[i]) 
         {
             printf("%d  ", j);
             i = j;
@@ -16,15 +24,28 @@ void print_activities(int start[], int finish[], int n)
     }
 }
 
-int main()
+// Driver function
+int main() 
 {
-    int start[] = {1, 3, 1, 5, 8, 6};
-    int finish[] = {2, 6, 6, 7, 10, 8};
+    // starting time of activities
+    int start[] = {1, 3, 1, 5, 6, 8};
+    // sorted finish time of activities
+    int finish[] = {2, 6, 6, 7, 8, 10};
+    // computing size of array
     int n = sizeof(start) / sizeof(start[0]);
-    print_activities(start, finish, n);
+    // call to Activity Selection function
+    ActivitySelection(start, finish, n);
     return 0;
 }
 
-// Output
-// Following activities are selected
-// 0  1  4
+/*
+Input:
+Start
+1 3 1 5 6 8
+Finish
+2 6 6 7 8 10
+
+Output:
+Following activities are selected
+ 0 1 4 5
+*/
--- Activity_Selection/Activity_Selection.cpp
+++ Activity_Selection/Activity_Selection.cpp
@@ -1,31 +1,53 @@
-#include <iostream>
+/*
+The problem statement for Activity Selection is that "Given a set of n
+activities with their start and finish times, we need to select maximum number
+of non-conflicting activities that can be performed by a single person, given
+that the person can handle only one activity at a time." The Activity Selection
+problem follows a Greedy approach.
+*/
 
+#include <iostream>
 using namespace std;
 
-void print_activities(int start[], int finish[], int n)
+// Function to compute the activities to be chosen
+void Activity_Selection(int start[], int finish[], int n) 
 {
-    cout << "Following activities are selected";
+    cout << "Following activities are selected" << endl;
+    // Select the first activity
     int i = 0;
-    cout << "\n" << i << "\t";
-    for (int j = 1; j < n; j++)
+    cout<<i<<" ";
+    // if start time of current activity j is greater than or equal to previous
+    // activity chosen, select activity j
+    for (int j = 1; j < n; j++) 
     {
-        if (start[j] >= finish[i])
-        {
-            cout << j << "\t";
-            i = j;
-        }
+	if (start[j] >= finish[i]) 
+	{
+	    cout << j << " ";
+	    i = j;
+	}
     }
 }
 
-int main()
+// Driver Function
+int main() 
 {
-    int start[] = {1, 3, 1, 5, 8, 6};
-    int finish[] = {2, 6, 6, 7, 10, 8};
+    // The array of n elements where start[i] denotes starting time of ith activity
+    int start[] = {1, 3, 1, 5, 6, 8};
+    // The array of n elements where finish[i] denotes finishing time of ith activity
+    int finish[] = {2, 6, 6, 7, 8, 10};
     int n = sizeof(start) / sizeof(start[0]);
-    print_activities(start, finish, n);
+    // Call to activities function
+    Activity_Selection(start, finish, n);
     return 0;
 }
 
-// Output
-// Following activities are selected
-// 0	1	4
+/*
+Input:
+Start
+1 3 1 5 6 8
+Finish
+2 6 6 7 8 10
+Output:
+Following activities are selected
+ 0 1 4 5
+*/
