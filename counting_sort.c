94ce74dd6fbc81ecb2f3b653197ec41d37697c89
Ayush Mishra <32820478+shadow989@users.noreply.github.com>
2019-03-30 11:17:26 +0530

    update in counting sort
    applying proper indentation,input output etc

--- counting_sort.c
+++ counting_sort.c
@@ -1,60 +1,71 @@
 //Implementation of counting sort in c language
 
-#include <conio.h>
 #include <stdio.h>
 
 int main() 
 {
-	// initializing the variables of use
-    int *a ,*c,i,max=0,j,n;
+    // initializing the variables of use
+    int *arr ,*sort,i,max=0,j,size;
 
     printf("Enter the size of array:");
-    scanf("%d",&n);
+    scanf("%d",&size);
 
-    //making dynamic array for actual array and resulting array of size n
+    //making dynamic array for actual array and resulting array of size variable named size
   	
-    a = (int*)malloc(n*sizeof(int));//actual array
+    arr = (int*)malloc(size*sizeof(int));//actual array
 
-    c = (int*)malloc(n*sizeof(int));//resulting array
+    sort = (int*)malloc(size*sizeof(int));//resulting array
 
     printf("Enter array elements:\n");
     //taking array elements as input
-    for (i=1;i<=n;i++)
+    for (i=1;i<=size;i++)
     {
-        scanf("%d",&a[i]);
-        if(max<a[i])
-        max=a[i];
+        scanf("%d",&arr[i]);
+        if(max<arr[i])
+        max=arr[i];
     }
     //making dynamic intermediate array which will be used for hashing
-    int *b;
-    b = (int*)malloc(max*sizeof(int));
+    int *temp;
+    temp = (int*)malloc(max*sizeof(int));
     //initializing temporary array with all values as zero
     for (i = 0; i <= max; i++)
     {
-        b[i] = 0;
+        temp[i] = 0;
     }
 
-    for (j = 1; j <= n; j++)
+    for (j = 1; j <= size; j++)
     {
-        b[a[j]] = b[a[j]] + 1;
+        temp[arr[j]] = temp[arr[j]] + 1;
     }
+	
     for (i = 1; i <= max; i++)
     {
-        b[i] = b[i] + b[i-1];
+        temp[i] = temp[i] + temp[i-1];
     }
-    //storing sorted elements in c array in order
-    for (j = n; j >= 1; j--)
+	
+    //storing sorted elements in sort array in order
+    for (j = size; j >= 1; j--)
 
     {
-        c[b[a[j]]] = a[j];
-        b[a[j]] = b[a[j]] - 1;
+        sort[temp[arr[j]]] = arr[j];
+        temp[arr[j]] = temp[arr[j]] - 1;
     }
+    //printing the sorted array
     printf("The sorted array is:\n");
-    for(i=1;i<=n;i++)
+    for(i=1;i<=size;i++)
     {
-        printf("%d\t",c[i]);
+        printf("%d\t",sort[i]);
     }
-    //to hold the result in turboc++ it takes a single charater and program ends
-    getch();
+    
 }
+
+//Input and output pattern
+//Input will be in two lines first size of input array and second array elements
+//size = 5;  size of input array
+//5 4 2 8 1  input array elements
+
+
+//Output
+// 1  2  4  5  8
+
 
