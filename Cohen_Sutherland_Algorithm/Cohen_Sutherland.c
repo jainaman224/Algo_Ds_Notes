39562b0a515154723159b611811237598d747343
Manbir <marwahmanbirsingh@gmail.com>
2019-03-31 15:33:35 +0530

    Updated Cohen_Sutherland.c

--- Cohen_Sutherland_Algorithm/Cohen_Sutherland.c
+++ Cohen_Sutherland_Algorithm/Cohen_Sutherland.c
@@ -1,26 +1,31 @@
+// Cohen-Sutherland Line Clipping Algorithm in C
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
 #include<graphics.h>
 #include<dos.h>
- 
+
+//Creating a structure for coordinates
 typedef struct coordinate
 {
     int x,y;
     char code[4];
-}PT;
- 
+}Point;
+
+// A function to draw the clipping window
 void drawwindow();
-void drawline(PT p1,PT p2);
-PT setcode(PT p);
-int visibility(PT p1,PT p2);
-PT resetendpt(PT p1,PT p2);
+//A function to draw the line to be clipped
+void drawline(Point p1, Point p2);
+Point setcode(Point p);
+int visibility(Point p1, Point p2);
+Point resetendpt(Point p1, Point p2);
  
 void main()
 {
     int gd=DETECT,v,gm;
-    PT p1,p2,p3,p4,ptemp;
-    
+    Point p1, p2, p3, p4, ptemp;
+
+    // Taking input for line coordinates    
     printf("\nEnter x1 and y1\n");
     scanf("%d %d",&p1.x,&p1.y);
     printf("\nEnter x2 and y2\n");
@@ -42,20 +47,23 @@ void main()
     
     switch(v)
     {
-    case 0: drawwindow();
+        case 0: drawwindow();
             delay(500);
             drawline(p1,p2);
             break;
-    case 1: drawwindow();
+
+        case 1: drawwindow();
             delay(500);
             break;
-    case 2: p3=resetendpt(p1,p2);
+
+        case 2: p3=resetendpt(p1,p2);
             p4=resetendpt(p2,p1);
             drawwindow();
             delay(500);
             drawline(p3,p4);
             break;
     }    
+
     delay(5000);
     closegraph();
 }
@@ -69,31 +77,36 @@ void drawwindow()
     line(150,350,150,100);
 }
  
-void drawline(PT p1,PT p2)
+void drawline(Point p1, Point p2)
 {
     line(p1.x,p1.y,p2.x,p2.y);
 }
 
 //Setting the 4 bit code
-PT setcode(PT p)
+Point setcode(Point p)
 {
-    PT ptemp;    
+    Point ptemp;    
+
     if(p.y<100)
-        ptemp.code[0]='1';  //Top
+        ptemp.code[0]='1';    //Top
     else
         ptemp.code[0]='0';    
+
     if(p.y>350)
-        ptemp.code[1]='1';  //Bottom
+        ptemp.code[1]='1';    //Bottom
     else
-        ptemp.code[1]='0';        
+        ptemp.code[1]='0';    
+
     if(p.x>450)
-        ptemp.code[2]='1';  //Right
+        ptemp.code[2]='1';    //Right
     else
-        ptemp.code[2]='0';        
+        ptemp.code[2]='0';    
+
     if(p.x<150)
-        ptemp.code[3]='1';  //Left
+        ptemp.code[3]='1';    //Left
     else
         ptemp.code[3]='0';    
+
     ptemp.x=p.x;
     ptemp.y=p.y;
     
@@ -101,29 +114,34 @@ PT setcode(PT p)
 }
 
 //Checking for visibility of the line
-int visibility(PT p1,PT p2)
+int visibility(Point p1, Point p2)
 {
-    int i,flag=0;    
+    int i,flag=0;   
+
     for(i=0;i<4;i++)
     {
         if((p1.code[i]!='0') || (p2.code[i]!='0'))
             flag=1;
-    }    
+    }  
+
     if(flag==0)
         return(0);    
     for(i=0;i<4;i++)
     {
         if((p1.code[i]==p2.code[i]) && (p1.code[i]=='1'))
             flag='0';
-    }    
+    }  
+
     if(flag==0)
         return(1);    
     return(2);
 }
- 
-PT resetendpt(PT p1,PT p2)
+
+// Reject the part of the line that is outside the clipping window
+// and display the reest that is within the window
+Point resetendpt(Point p1, Point p2)
 {
-    PT temp;
+    Point temp;
     int x,y,i;
     float m,k;
     
@@ -171,7 +189,9 @@ PT resetendpt(PT p1,PT p2)
 // 200
 // 200
 
-// Output before line clipping
+// Output before line clipping:
+// https://drive.google.com/file/d/1im2UXnXnRY40ynvx0S-wJZe1S3VkmujL/view?usp=sharing
 
-// Output after line clipping
+// Output after line clipping:
+// https://drive.google.com/file/d/1IcQmUDupNMYwKcInY33IefuGn0Snei63/view?usp=sharing
 
