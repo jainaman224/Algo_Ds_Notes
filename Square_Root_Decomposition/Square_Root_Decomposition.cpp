78137b5bf4526f11cbb39e08d5f9d342bbd2453b
Kaustubh Pandey <kaustubh.p16@iiits.in>
2019-03-31 13:38:51 +0530

    Changed variable name

--- Square_Root_Decomposition/Square_Root_Decomposition.cpp
+++ Square_Root_Decomposition/Square_Root_Decomposition.cpp
@@ -1,18 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void update(int index,int value,int *blocks,int a[],int n){
-	blocks[(int)(index/sqrt(n))] += value-a[index];
-	a[index]=value;
+void update(int index,int value,int *blocks,int input_arr[],int n){
+	blocks[(int)(index/sqrt(n))] += value-input_arr[index];
+	input_arr[index]=value;
 }
 
 //assuming 0 based indexing
-int query(int low,int high,int *blocks,int a[],int n){
+int query(int low,int high,int *blocks,int input_arr[],int n){
 	int block_size=sqrt(n);
 	int sum=0;
 
 	while(low % block_size!=0 && low<high){
-		sum+=a[low];
+		sum+=input_arr[low];
 		low++;
 	}
 	while(low+block_size<=high){
@@ -20,7 +20,7 @@ int query(int low,int high,int *blocks,int a[],int n){
 		low+=block_size;
 	}
 	while(low<=high){
-		sum+=a[low];
+		sum+=input_arr[low];
 		low++;
 	}
 
@@ -29,7 +29,7 @@ int query(int low,int high,int *blocks,int a[],int n){
 }
 
 //divide the array in blocks of size sqrt(n), sum of elements in each block is stored in blocks
-int *formBlocks(int a[],int n){
+int *formBlocks(int input_arr[],int n){
 	int block_size=sqrt(n);
 	int index=-1;
 	int *blocks = new int[(int)(n/block_size)+1];
@@ -38,7 +38,7 @@ int *formBlocks(int a[],int n){
 		if(i%block_size==0){
 			index++;
 		}
-		blocks[index]+=a[i];
+		blocks[index]+=input_arr[i];
 	}
 	return blocks;
 }
@@ -49,26 +49,26 @@ int main(){
 	Indexing : 0 based in array
 	*/
 	//input : array of integers
-	int a[]={2,3,4,6,8,9,1,-1,0,3};  //elements
-	int n = sizeof(a)/sizeof(a[0]); //number of elements 
-	int *blocks=formBlocks(a,n);
+	int input_arr[]={2,3,4,6,8,9,1,-1,0,3};  //elements
+	int n = sizeof(input_arr)/sizeof(input_arr[0]); //number of elements 
+	int *blocks=formBlocks(input_arr,n);
 	
 	//queries
 	//input queries with left and right indices to find the sum in that range
-	cout<<query(0,2,blocks,a,n)<<endl; //find sum from index 0 to index 2
+	cout<<query(0,2,blocks,input_arr,n)<<endl; //find sum from index 0 to index 2
 	//expected output : 9
 	
-	cout<<query(3,7,blocks,a,n)<<endl; //find sum from index 3 to index 7
+	cout<<query(3,7,blocks,input_arr,n)<<endl; //find sum from index 3 to index 7
 	//expected output : 23
 	
-	cout<<query(1,4,blocks,a,n)<<endl; //find sum from index 1 to index 4
+	cout<<query(1,4,blocks,input_arr,n)<<endl; //find sum from index 1 to index 4
 	//expected output : 21
 	
 	//update
-	update(3,1,blocks,a,n);				//update the value at index 3 with value of 1
+	update(3,1,blocks,input_arr,n);				//update the value at index 3 with value of 1
 	
 	//query
-	cout<<query(1,4,blocks,a,n)<<endl;	//find sum from index 1 to index 4 
+	cout<<query(1,4,blocks,input_arr,n)<<endl;	//find sum from index 1 to index 4 
 	//expected output : 18
 	return 0;
 }
