1cbd0bb0ea8a9c45de22296cba9511d5f9f36602
Kaustubh Pandey <kaustubh.p16@iiits.in>
2019-03-31 13:41:28 +0530

    Changed variable name

--- Square_Root_Decomposition/Square_Root_Decomposition.py
+++ Square_Root_Decomposition/Square_Root_Decomposition.py
@@ -1,15 +1,15 @@
 from math import sqrt
 
-def update(index,value,blocks,a):
-	blocks[int(index/sqrt(len(a)))] += value-a[index]
-	a[index]=value
+def update(index,value,blocks,input_arr):
+	blocks[int(index/sqrt(len(input_arr)))] += value-input_arr[index]
+	input_arr[index]=value
 
 #assuming 0 based indexing
-def query(low,high,blocks,a):
-	block_size=int(sqrt(len(a)))
+def query(low,high,blocks,input_arr):
+	block_size=int(sqrt(len(input_arr)))
 	total=0
 	while(low % block_size!=0 and low<high):
-		total+=a[low]
+		total+=input_arr[low]
 		low+=1
 	
 	while(low+block_size<=high):
@@ -17,19 +17,19 @@ def query(low,high,blocks,a):
 		low+=block_size
 	
 	while(low<=high):
-		total+=a[low]
+		total+=input_arr[low]
 		low+=1
 	return total
 
 #divide the list in blocks of size sqrt(n), sum of elements in each block is stored in blocks
-def formBlocks(a):
-	block_size=int(sqrt(len(a)))
+def formBlocks(input_arr):
+	block_size=int(sqrt(len(input_arr)))
 	index=-1
-	blocks=[0]*(len(a)//block_size+1)
-	for i in range(len(a)):
+	blocks=[0]*(len(input_arr)//block_size+1)
+	for i in range(len(input_arr)):
 		if(i%block_size==0):
 			index+=1
-		blocks[index]+=a[i]
+		blocks[index]+=input_arr[i]
 	return blocks
 
 '''
@@ -37,25 +37,25 @@ Applying square root decomposition to perform range queries
 	for finding the sum of numbers in a given range 
 	Indexing : 0 based in list
 '''
-a=[2,3,4,6,8,9,1,-1,0,3]
-blocks=formBlocks(a)
+input_arr=[2,3,4,6,8,9,1,-1,0,3]
+blocks=formBlocks(input_arr)
 #queries
 '''
 input queries with left and right indices to find the sum in that range
 '''
-print(query(0,2,blocks,a))	#find sum from index 0 to index 2
+print(query(0,2,blocks,input_arr))	#find sum from index 0 to index 2
 #expected output : 9
 
-print(query(3,7,blocks,a))	#find sum from index 3 to index 7
+print(query(3,7,blocks,input_arr))	#find sum from index 3 to index 7
 #expected output : 23
 
-print(query(1,4,blocks,a))	#find sum from index 1 to index 4
+print(query(1,4,blocks,input_arr))	#find sum from index 1 to index 4
 #expected output : 21
 
 #update 
-update(3,1,blocks,a)		#update the value at index 3 with value of 1
+update(3,1,blocks,input_arr)		#update the value at index 3 with value of 1
 
 #verify if the update was success
-print(query(1,4,blocks,a))
+print(query(1,4,blocks,input_arr))
 #expected output : 18
 
