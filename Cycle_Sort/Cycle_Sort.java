c46697d0e6db96137cc50dc1192c26b8aff8e783
dldisha <dldisha929@gmail.com>
2019-03-31 17:29:10 +0530

    updating

---
+++ Cycle_Sort/Cycle_Sort.java
@@ -0,0 +1,202 @@
+/* 
+Cycle Sort
+Cycle sort is a comparison based sorting algorithm which forces array to be factored into 
+the number of cycles where each of them can be rotated to produce a sorted array.
+It is an in-place and unstable sorting algorithm. 
+Best and Average case time complexity:O(n^2)
+
+
+Algorithm
+An element a is given, index of a can be calculated by counting the number of elements that 
+are smaller than a.
+1. If the element is found to be at its correct position, simply leave it as it is.
+2. Otherwise, find the correct position of a by counting the total number of elements that are 
+less than a. where it must be present in the sorted array. The other element b which is replaced is 
+to be moved to its correct position. This process continues until we got an element at the 
+original position of a. */
+
+import java.io.*; 
+import java.util.*; 
+
+public class Cycle_Sort   
+{  
+    static void sort(int a[], int n)  
+    {  
+         int writes = 0,start,element,pos,temp,i;
+         // Find position where we put the element.
+        for (start = 0; start <= n - 2; start++) 
+        {   int writes = 0,start,element,pos,temp,i;
+         // Find position where we put the element.
+        for (start = 0; start <= n - 2; start++) 
+        {  
+            element = a[start];  
+            pos = start; 
+        // Find position where we put the element. We basically 
+        // count all smaller elements on right side of element.  
+        for (i = start + 1; i<n; i++)  
+            if (a[i] <element)  
+                pos++;  
+        // If element is already in correct position     
+        if (pos == start)  
+            continue; 
+        // ignore all duplicate elements      
+        while (element == a[pos])  
+            pos += 1;  
+        // put the element to it's right position
+        if (pos != start) {  
+            temp = element;  
+            element = a[pos];  
+            a[pos] = temp;    
+            writes++;  
+        } 
+        // Rotate rest of the cycle  
+        while (pos != start) {  
+            pos = start; 
+            // Find position where we put the element  
+            for (i = start + 1; i<n; i++)  
+                if (a[i] <element)  
+                    pos += 1;  
+            while (element == a[pos])  
+                pos += 1; 
+            // put the element to it's right position     
+            if (element != a[pos]) {  
+                temp = element;  
+                element = a[pos];  
+                a[pos] = temp;    
+                writes++;  
+                }  
+            }  
+        } 
+            element = a[start];  
+            pos = start; 
+        // Find position where we put the element. We basically 
+        // count all smaller elements on right side of element.  
+        for (i = start + 1; i<n; i++)  
+            if (a[i] <element)  
+                pos++;  
+        // If element is already in correct position     
+        if (pos == start)  
+            continue; 
+        // ignore all duplicate elements      
+        while (element == a[pos])  
+            pos += 1;  
+        // put the element to it's right position
+        if (pos != start) {  
+            temp = element;  
+            element = a[pos];  
+            a[pos] = temp;    
+            writes++;  
+        } 
+        // Rotate rest of the cycle  
+        while (pos != start) {  
+            pos = start; 
+            // Find position where we put the element  
+            for (i = start + 1; i<n; i++)  
+                if (a[i] <element)  
+                    pos += 1;  
+            while (element == a[pos])  
+                pos += 1; 
+            // put the element to it's right position     
+            if (element != a[pos]) {  
+                temp = element;  
+                element = a[pos];  
+                a[pos] = temp;    
+                writes++;  
+                }  
+            }  
+        } int writes = 0,start,element,pos,temp,i;
+         // Find position where we put the element.
+        for (start = 0; start <= n - 2; start++) 
+        {   int writes = 0,start,element,pos,temp,i;
+         // Find position where we put the element.
+        for (start = 0; start <= n - 2; start++) 
+        {  
+            element = a[start];  
+            pos = start; 
+        // Find position where we put the element. We basically 
+        // count all smaller elements on right side of element.  
+        for (i = start + 1; i<n; i++)  
+            if (a[i] <element)  
+                pos++;  
+        // If element is already in correct position     
+        if (pos == start)  
+            continue; 
+        // ignore all duplicate elements      
+        while (element == a[pos])  
+            pos += 1;  
+        // put the element to it's right position
+        if (pos != start) {  
+            temp = element;  
+            element = a[pos];  
+            a[pos] = temp;    
+            writes++;  
+        } 
+        // Rotate rest of the cycle  
+        while (pos != start) {  
+            pos = start; 
+            // Find position where we put the element  
+            for (i = start + 1; i<n; i++)  
+                if (a[i] <element)  
+                    pos += 1;  
+            while (element == a[pos])  
+                pos += 1; 
+            // put the element to it's right position     
+            if (element != a[pos]) {  
+                temp = element;  
+                element = a[pos];  
+                a[pos] = temp;    
+                writes++;  
+                }  
+            }  
+        } 
+            element = a[start];  
+            pos = start; 
+        // Find position where we put the element. We basically 
+        // count all smaller elements on right side of element.  
+        for (i = start + 1; i<n; i++)  
+            if (a[i] <element)  
+                pos++;  
+        // If element is already in correct position     
+        if (pos == start)  
+            continue; 
+        // ignore all duplicate elements      
+        while (element == a[pos])  
+            pos += 1;  
+        // put the element to it's right position
+        if (pos != start) {  
+            temp = element;  
+            element = a[pos];  
+            a[pos] = temp;    
+            writes++;  
+        } 
+        // Rotate rest of the cycle  
+        while (pos != start) {  
+            pos = start; 
+            // Find position where we put the element  
+            for (i = start + 1; i<n; i++)  
+                if (a[i] <element)  
+                    pos += 1;  
+            while (element == a[pos])  
+                pos += 1; 
+            // put the element to it's right position     
+            if (element != a[pos]) {  
+                temp = element;  
+                element = a[pos];  
+                a[pos] = temp;    
+                writes++;  
+                }  
+            }  
+        }  
+    }     
+
+    public static void main(String[] args)
+    {
+        int a[] = { 12, 2, 1, 9, -4, 2, 4, 4};
+        int n = a.length,i;
+        sort(a, n);
+        System.out.println("After sort, array : ");
+        for (i = 0; i < n; i++)
+        System.out.println(a[i]);
+    }
+}
+
---
+++ Tim_Sort/Tim_Sort.c
@@ -0,0 +1,118 @@
+//GSSoC
+//TimSort
+
+#include<stdio.h>  
+
+int minimum(int a, int b)  
+{  
+    if(a<b)  
+        return a;   
+    else   
+        return b;   
+}  
+ 
+//MERGE SORT FUNCTION :   
+void merge(int array[], int left, int mid, int right)  
+{  
+    int leftLength = mid - left + 1, rightLength = right - mid;  
+    int Left[leftLength], Right[rightLength];  
+    int i,j,k;  
+    for (i = 0; i < leftLength; i++)  
+        Left[i] = array[left + i];  
+    for (i = 0; i < rightLength; i++)  
+        Right[i] = array[mid + 1 + i];  
+       
+    i = 0;  
+    j = 0;  
+    k = left;  
+       
+    while (i < leftLength && j < rightLength)  
+    {  
+        if (Left[i] <= Right[j])  
+        {  
+            array[k] = Left[i];  
+            i++;  
+        }  
+        else  
+        {  
+            array[k] = Right[j];  
+            j++;  
+        }  
+        k++;  
+    }  
+    while (i < leftLength)  
+    {  
+        array[k] = Left[i];  
+        k++;  
+        i++;  
+    }  
+       
+    while (j < rightLength)  
+    {  
+        array[k] = Right[j];  
+        k++;  
+        j++;  
+    }  
+}  
+
+// INSERTION SORT FUNCTION :
+void insertionSort(int array[], int left, int right)  
+{  
+    int temp, i, j;   
+    for (i = left + 1; i <= right; i++)  
+    {  
+        temp = array[i];  
+        j = i - 1;  
+        while (array[j] > temp && j >= left)  
+        {  
+            array[j+1] = array[j];  
+            j--;  
+        }  
+        array[j+1] = temp;  
+    }  
+} 
+      
+int main()  
+{  
+    int array[] = {50, 39, 89, 10, 8};  
+    int n = sizeof(array)/sizeof(array[0]);  
+    printf("Unsorted Array : \n");  
+    for (int i = 0; i < n; i++)  
+        printf("%d  ", array[i]);  
+    printf("\n");  
+       
+
+    int size,left,mid,right,Limit=32;  
+    for (int i = 0; i < n; i+=Limit)  
+        insertionSort(array, i, minimum((i+31), (n-1)));  
+    for (size = Limit; size < n; size = 2*size)  
+    {  
+        for (left = 0; left < n; left += 2*size)  
+        {  
+            mid = left + size - 1;  
+            right = minimum((left + 2*size - 1), (n-1));  
+       
+            merge(array, left, mid, right);  
+        }  
+    } 
+
+    printf("Sorted Array : \n");  
+    for (int i = 0; i < n; i++)  
+        printf("%d  ", array[i]);  
+    printf("\n");  
+    return 0;  
+}  
+
+/*
+OUTPUT
+----------------
+Unsorted Array :
+50 39 89 10 8 
+Sorted Array :
+8 10 39 50 89
+----------------
+*/
+
+// Happy Coding! 
+ 
+
