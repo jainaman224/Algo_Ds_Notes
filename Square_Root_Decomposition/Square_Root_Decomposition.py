1f991049cc1166dbab04adf0a9537b251dfc6a68
Kaustubh Pandey <kaustubh.p16@iiits.in>
2019-03-31 13:35:44 +0530

    Update Square_Root_Decomposition.py

--- Square_Root_Decomposition/Square_Root_Decomposition.py
+++ Square_Root_Decomposition/Square_Root_Decomposition.py
@@ -3,6 +3,7 @@ from math import sqrt
 def update(index,value,blocks,a):
 	blocks[int(index/sqrt(len(a)))] += value-a[index]
 	a[index]=value
+
 #assuming 0 based indexing
 def query(low,high,blocks,a):
 	block_size=int(sqrt(len(a)))
@@ -19,6 +20,7 @@ def query(low,high,blocks,a):
 		total+=a[low]
 		low+=1
 	return total
+
 #divide the list in blocks of size sqrt(n), sum of elements in each block is stored in blocks
 def formBlocks(a):
 	block_size=int(sqrt(len(a)))
@@ -38,6 +40,9 @@ Applying square root decomposition to perform range queries
 a=[2,3,4,6,8,9,1,-1,0,3]
 blocks=formBlocks(a)
 #queries
+'''
+input queries with left and right indices to find the sum in that range
+'''
 print(query(0,2,blocks,a))	#find sum from index 0 to index 2
 #expected output : 9
 
