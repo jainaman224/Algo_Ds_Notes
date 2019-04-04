6908999ce6b8d10428e85c1a0e9088a00c2e1712
prashant-mahanta <prashantkumar.m16@iiits.in>
2019-04-04 23:45:56 +0530

    Fixed reviews

--- Longest_Repeating_Subsequence/Longest_Repeating_Subsequence.cpp
+++ Longest_Repeating_Subsequence/Longest_Repeating_Subsequence.cpp
@@ -2,20 +2,23 @@
 
 using namespace std;
 
-int LongestRepeatingSubsequence(string str){
+int LongestRepeatingSubsequence(string str){ 
     int length;
     length = str.length();
+
     //Initialize all dp elements with 0
     vector<vector<int>> dp(length+1, vector<int>(length+1, 0));
 
-    for(int i=1; i<=length; i++){
-        for(int j=1; j<=length; j++){
-            //If two characters are same(different position)  
-            //then dp of present state is dp of previous state plus 1
+    for (int i=1; i<=length; i++) {
+        for (int j=1; j<=length; j++) {
+
+            // If two characters are same(different position)  
+            // then dp of present state is dp of previous state plus 1
             if(i != j && str[i-1] == str[j-1])
                 dp[i][j] = dp[i-1][j-1] + 1;
             else{
-                //else maximum of just vertically above or horizontally left element
+
+                // else maximum of just vertically above or horizontally left element
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1]);              
             }
         }
@@ -26,7 +29,7 @@ int LongestRepeatingSubsequence(string str){
 }
 
 int main()
-{    
+{     
     string str = "AlgoDsNoteDs";
     int length = LongestRepeatingSubsequence(str);
     cout << "String: " << str << "\n";
--- Longest_Repeating_Subsequence/Longest_Repeating_Subsequence.py
+++ Longest_Repeating_Subsequence/Longest_Repeating_Subsequence.py
@@ -1,16 +1,19 @@
 
 def LongestRepeatingSubsequence(string):
     length = len(string)
+    
     # Initialize dp with zero
     dp = [[0]*(length+1) for i in range(length+1)]
 
     for i in range(1,length+1):
         for j in range(1,length+1):
+
             # If two characters are same(different position)
             # then dp of present state is dp of previous state plus 1
             if i != j and string[i-1] == string[j-1]:
                 dp[i][j] = dp[i-1][j-1] + 1
             else:
+
                 # Else maximum of just vertically above or horizontally left element
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1])
 
