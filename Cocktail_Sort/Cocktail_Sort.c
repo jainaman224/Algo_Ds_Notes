e4ada0e24d0b4fba9d29d6a0fc7b8ea205c7c285
somya-kapoor <42409030+somya-kapoor@users.noreply.github.com>
2019-04-04 20:05:48 +0530

    Cocktail_Sort.c

--- Cocktail_Sort/Cocktail_Sort.c
+++ Cocktail_Sort/Cocktail_Sort.c
@@ -1,22 +1,30 @@
 //Cocktail sort is a variation of bubble sort that is both a stable sorting
 //algorithm and a comparison sort. The algorithm differs from a bubble sort in 
 //that it sorts in both directions on each pass through the list. 
-#include <stdio.h>  
+#include <stdio.h>
+
+void swap (int *a, int *b) //swapping function
+{ 
+   int temp;
+   temp = *a;
+   *a = *b;
+   *b = temp;
+}   
    
 int main()  
 {  
-    int n;
+    int num;
     printf("Enter the number of elements ");
-    scanf("%d",&n);
-    int a[n];
+    scanf("%d",&num);
+    int arr[num];
     int i;
     printf("Enter the elements ");
-    for (i = 0 ; i < n ; i++ )  
-        scanf("%d",&a[i]);
-    int temp;
+    for (i = 0 ; i < num ; i++ )  
+        scanf("%d",&arr[i]);
+    
     int swapped = 1;  
     int begin = 0;  
-    int end = n - 1;  
+    int end = num - 1;  
    
     while (swapped)
     {  
@@ -25,11 +33,9 @@ int main()
         for (i = begin ; i < end ; ++i) 
         {    
             // Move the bigger element to the right most position as in bubble sort
-            if (a[i] > a[i + 1])
+            if (arr[i] > arr[i + 1])
             {  
-                temp = a[i];  
-                a[i] = a[i+1];  
-                a[i+1] = temp;                  
+                swap (&arr[i],&arr[i+1]);                  
                 swapped = 1;  
             }  
         } 
@@ -40,11 +46,9 @@ int main()
         // moving the smaller elements to left part of the array which is modification over bubble sort
         for (i = end - 1; i >= begin; --i)
         {  
-            if (a[i] > a[i + 1])   
+            if (arr[i] > arr[i + 1])   
             {
-                temp = a[i];  
-                a[i] = a[i+1];  
-                a[i+1] = temp;  
+                swap (&arr[i],&arr[i+1]); 
                 swapped = 1;  
             }  
         }  
@@ -52,11 +56,11 @@ int main()
     } 
     
     printf("Sorted array :\n");  
-    for (i = 0; i < n; i++)  
-        printf("%d ", a[i]);  
+    for (i = 0; i < num; i++)  
+        printf("%d ", arr[i]);  
     printf("\n");  
     return 0;  
-} 
+}  
 /*Enter the number of elements 5
 Enter the elements 5
 4
