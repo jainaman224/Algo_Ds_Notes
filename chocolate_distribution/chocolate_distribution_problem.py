d59a1c0d3b0e126f921ccc44f274e26444f788c6
Divya Shahi <40079593+divs30@users.noreply.github.com>
2019-04-05 17:49:45 +0530

    added spaces
    added spaces

--- chocolate_distribution/chocolate_distribution_problem.py
+++ chocolate_distribution/chocolate_distribution_problem.py
@@ -1,40 +1,40 @@
-#this chocolate function will return the minimum required difference
-def chocolate(l,no_of_packets,no_of_students):
+# this chocolate function will return the minimum required difference
+def chocolate( l ,no_of_packets, no_of_students):
     if no_of_packets < no_of_students :
         return -1
     if no_of_packets==0 or no_of_students==0 :
         return 0
     l.sort();
-    p=len(l)
-    p=p-1  #last index of list
-    last=0
-    first=0
-    i=0
-    diff=0
-    required_diff=l[p]+1#assigning maximum no.+1
+    p = len(l)
+    p = p-1  #last index of list
+    last = 0
+    first = 0
+    i = 0
+    diff = 0
+    required_diff = l[p] + 1#assigning maximum no.+1
     
-    i=0
-    while (i+no_of_students-1)<no_of_packets :
-        diff=l[i+no_of_students-1]- l[i]
-        if diff <required_diff :
+    i = 0
+    while ( i + no_of_students - 1) < no_of_packets :
+        diff=l[i + no_of_students-1] - l[i]
+        if diff < required_diff :
             required_diff=diff
-            last=i+no_of_students-1
-            first=i
-        i=i+1
+            last = i + no_of_students - 1
+            first = i
+        i = i + 1
     return required_diff
 
 
-no_of_students=int(input("enter the number of students"))
+no_of_students = int(input( "enter the number of students" ))
 
-no_of_packets=int(input("Enter the number of packets"))
+no_of_packets = int(input( "Enter the number of packets "))
 #print("enter the numbet contained by each packet repectively")
-b=0
-list_of_chocolate=[]
-for i in range(no_of_packets):
-   b=int(input("enter the no."))
+b = 0
+list_of_chocolate = []
+for i in range( no_of_packets ) :
+   b = int(input(" enter the no. "))
    list_of_chocolate.append(b)
 
-print("the minimum difference in chocolate distributed between two people is :",end=" ")
+print("the minimum difference in chocolate distributed between two people is : " ,end=" ")
 print(chocolate(list_of_chocolate,no_of_packets,no_of_students))
 
 """
