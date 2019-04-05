970ea718edab479b90b5f5c7f708e804f27f3f9c
Divya Shahi <40079593+divs30@users.noreply.github.com>
2019-04-05 13:12:44 +0530

    added space
    added space

--- chocolate_distribution/chocolate_distribution.cpp
+++ chocolate_distribution/chocolate_distribution.cpp
@@ -1,25 +1,25 @@
 #include<bits/stdc++.h>
 #include<iostream>
 
-using namespace std;
+using namespace std ;
 
-int min_diff(int a[],int num_packets,int num_children)
+int min_diff (int a[],int num_packets,int num_children)
 {
-    if(num_children==0||num_packets==0)
-        return 0;
-    if(num_children>num_packets)
-        return -1;
+    if (num_children==0||num_packets==0)
+        return 0 ;
+    if (num_children>num_packets)
+        return -1 ;
     int i,j,diff=0,required_diff,first=0,last=0;
-    required_diff=INT_MAX;//INT_MAX is a macro that specifies that an integer variable cannot store any value beyond this limit.
+    required_diff=INT_MAX; // INT_MAX is a macro that specifies that an integer variable cannot store any value beyond this limit.
     sort(a,a+num_packets);
-    for(i=0;i+num_children-1<num_packets;i++)
+    for (i=0;i+num_children-1<num_packets;i++)
     {
         diff=a[i+num_children-1]-a[i];
-        if(diff<required_diff)
+        if (diff<required_diff)
         {
-            required_diff=diff;
-            first=i;
-            last=i+num_children-1;
+            required_diff=diff ;
+            first=i ;
+            last=i+num_children-1 ;
         }
     }
     return required_diff;
@@ -27,17 +27,17 @@ int min_diff(int a[],int num_packets,int num_children)
 
 int main()
 {
-    int num_packets,num_children,i,j,k;
-    cin>>num_children;//size of group in which chocolates  needs to be distributed
-    cin>>num_packets;//size of array
-    int a[num_packets];
-    for(i=0;i<num_packets;i++)
-        cin>>a[i];//taking input for the quantity of chocolate contained in each packet
-    if(min_diff(a,num_packets,num_children)>=0)
+    int num_packets, num_children, i, j, k ;
+    cin>>num_children;// size of group in which chocolates  needs to be distributed
+    cin>>num_packets;// size of array
+    int a[num_packets] ;
+    for (i=0;i<num_packets;i++)
+        cin>>a[i];// taking input for the quantity of chocolate contained in each packet
+    if (min_diff(a,num_packets,num_children)>=0)
          cout<<"The minimum difference of chocolates distibuted between children who got maximum and minimum is "<<min_diff(a,num_packets,num_children) ;
     else
-        cout<<"As the number of students is less than number of packets hence no minimum difference is valid or is -1";
-    return 0;
+        cout<<"As the number of students is less than number of packets hence no minimum difference is valid or is -1" ;
+    return 0 ;
 }
 /*
 input :
