09dc65bc43c17d00471ff8c7608cb1d84d793249
Riya <riyad17031998@gmail.com>
2019-04-08 15:31:42 +0530

    Create Check_Travis.c

---
+++ Check_Travis.c
@@ -0,0 +1,15 @@
+    #include<stdio.h>
+
+    int main()
+    {
+       int a, b, c;
+
+       printf("Enter two numbers to add\n");
+       scanf("%d%d", &a, &b);
+
+       c = a + b;
+
+       printf("Sum of the numbers = %d\n", c);
+
+       return 0;
+    }
