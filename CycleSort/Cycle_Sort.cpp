226bdc99542722731f0cdaa2405b6729cdd04abe
dldisha <dldisha929@gmail.com>
2019-04-10 02:24:36 +0530

    fixes

--- CycleSort/Cycle_Sort.cpp
+++ CycleSort/Cycle_Sort.cpp
@@ -1,19 +1,3 @@
-/* 
-Cycle Sort
-Cycle sort is a comparison based sorting algorithm which forces array to be factored into 
-the number of cycles where each of them can be rotated to produce a sorted array.
-It is an in-place and unstable sorting algorithm. 
-Best and Average case time complexity:O(n^2)
-
-
-Algorithm
-An element a is given, index of a can be calculated by counting the number of elements that 
-are smaller than a.
-1. If the element is found to be at its correct position, simply leave it as it is.
-2. Otherwise, find the correct position of a by counting the total number of elements that are 
-less than a. where it must be present in the sorted array. The other element b which is replaced is 
-to be moved to its correct position. This process continues until we got an element at the 
-original position of a. */
 
 #include<iostream>
 using namespace std;
@@ -21,6 +5,7 @@ using namespace std;
 void sort(int a[], int n)  
 {  
     int writes = 0,start,element,pos,temp,i;
+    
     // Find position where we put the element.
     for (start = 0; start <= n - 2; start++) {  
         element = a[start];  
@@ -28,11 +13,13 @@ void sort(int a[], int n)
         for (i = start + 1; i < n; i++)  
             if (a[i] < element)  
                 pos++;  
+        
         // If element is already in correct position     
         if (pos == start)  
             continue;  
         while (element == a[pos])  
             pos += 1; 
+        
         // put the element to it's right position      
         if (pos != start) {    
             temp = element;  
@@ -40,6 +27,7 @@ void sort(int a[], int n)
             a[pos] = temp;    
             writes++;  
         } 
+        
         // Rotate rest of the cycle  
         while (pos != start) {  
             pos = start;  
@@ -48,6 +36,7 @@ void sort(int a[], int n)
                     pos += 1;  
             while (element == a[pos])  
                 pos += 1; 
+            
             // put the element to it's right position      
             if (element != a[pos]) {  
                 temp = element;  
