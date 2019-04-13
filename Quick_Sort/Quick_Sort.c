ecc15293915db10fdb11069d05503dff811827f0
Nehal Kumar Singh <41484543+geekyNehal@users.noreply.github.com>
2019-04-13 07:40:58 +0530

    Update Quick_Sort.c

--- Quick_Sort/Quick_Sort.c
+++ Quick_Sort/Quick_Sort.c
@@ -9,51 +9,51 @@ int partition (int arr[], int low, int high)
         if (arr[j] <= pivot)
         {
             i++ ;
-            swap(&arr[i], &arr[j]) ;
+            swap(&arr[i], &arr[j]);
         }
     }
-    swap(&arr[i + 1], &arr[high]) ;
-    return (i + 1) ;
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
 }
 //Swapping the value in the array 
 void swap(int *a,int *b)
 {
-   int temp= *a ;
-   *a = *b ;
-   *b = temp ;
+   int temp= *a;
+   *a = *b;
+   *b = temp;
 }
 void quickSort(int arr[], int low, int high)
 {
     if (low < high)
     {
-        int pi = partition(arr, low, high) ;
-        quickSort(arr, low, pi - 1) ;
-        quickSort(arr, pi + 1, high) ;
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
     }
 }
 
 int main()
 {
-    int t ;
-    printf("Enter Number of times you want to sort:\n") ;
-    scanf("%d", &t) ;
+    int t;
+    printf("Enter Number of times you want to sort:\n");
+    scanf("%d", &t);
 
     while(t--)
     {
-        int n, i ;
-        printf("Enter the Size of the Array:\n") ;
-        scanf("%d", &n) ;
-        int arr[n] ;
-        printf("Enter Array:\n") ;
+        int n, i;
+        printf("Enter the Size of the Array:\n");
+        scanf("%d", &n);
+        int arr[n];
+        printf("Enter Array:\n");
         for(i=0; i<n ; i++)
-            scanf("%d", &arr[i]) ;
+            scanf("%d", &arr[i]);
         quickSort(arr, 0, n-1) ;
-        printf("Sorted Array:\n") ;
+        printf("Sorted Array:\n");
         for(i=0 ; i<n ; i++)
-            printf("%d ", arr[i]) ;
-        printf("\n") ;
+            printf("%d ", arr[i]);
+        printf("\n");
     }
-    return 0 ;
+    return 0;
 }
 /*
 ----------------OUTPUT----------------
