81d323679c629e55f9b3a2c9806b7748625b872e
Neeramitra Reddy <46085683+neeru1207@users.noreply.github.com>
2019-04-13 21:38:57 +0530

    Updated Egg_Drop.py
    Added the requested changes

--- Egg_Drop/Egg_Drop.py
+++ Egg_Drop/Egg_Drop.py
@@ -1,3 +1,7 @@
+#Egg Drop problem
+
+#Function for recursive approach
+
 def eggDropRecursive(egg,floor):
     if floor==0:
         return 0
@@ -9,6 +13,9 @@ def eggDropRecursive(egg,floor):
         if sol<min:
             min=sol
     return min
+
+#Function for recursive dynamic programming approach
+
 def eggDropRecursiveDP(egg,floor,mem):
     if floor==0:
         return 0
@@ -23,6 +30,9 @@ def eggDropRecursiveDP(egg,floor,mem):
             min=sol
     mem[egg][floor]=min
     return min
+
+#Function for iterative dynamic programming approach
+
 def eggDropIterativeDP(eggs,floors,mem):
     for e in range(1,eggs+1):
         for f in range(1,floors+1):
@@ -38,10 +48,14 @@ def eggDropIterativeDP(eggs,floors,mem):
                         min = sol
                 mem[e][f] = min;
     return mem[eggs][floors]
+
+#Taking the user input for the number of eggs and floors
+
 egg=int(input("Enter the number of eggs: "))
 floor=int(input("Enter the number of floors: "))
 mem=[[0 for i in range(0,floor+2)] for j in range(0,egg+2)]
 print(eggDropIterativeDP(egg,floor,mem))
+
 #Sample Output
 #Enter the number of eggs: 5
 #Enter the number of floors: 100
