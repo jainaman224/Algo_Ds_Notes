878ebffe5a87c99216fb6d2194e6ae926fb66bfe
Nehal Kumar Singh <41484543+geekyNehal@users.noreply.github.com>
2019-04-13 07:41:50 +0530

    Update Quick_Sort.c

--- Quick_Sort/Quick_Sort.c
+++ Quick_Sort/Quick_Sort.c
@@ -3,12 +3,12 @@
 int partition (int arr[], int low, int high)
 {
     int pivot = arr[high];
-    int i = (low - 1) ;
+    int i = (low - 1);
     for (int j = low; j <= high- 1 ; j++)
     {
         if (arr[j] <= pivot)
         {
-            i++ ;
+            i++;
             swap(&arr[i], &arr[j]);
         }
     }
