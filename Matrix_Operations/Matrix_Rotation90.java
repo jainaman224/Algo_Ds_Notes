bb49c6d54c249ba291fbab32040185a5b2ac8dc1
Kanchan <kthareja12@gmail.com>
2019-04-13 16:26:34 +0530

    indentation

--- Matrix_Operations/Matrix_Rotation90.java
+++ Matrix_Operations/Matrix_Rotation90.java
@@ -4,42 +4,42 @@ class matrixRotation
 { 
     static void Rotate(int matrix[][], int N) 
     { 
-    for (int i = 0; i < N / 2; i++) 
-    { 
-        for (int j = i; j < N-i-1; j++) 
+        for (int i = 0; i < N / 2; i++) 
         { 
-        int t = matrix[i][j]; 
-          matrix[i][j] = matrix[j][N-1-i]; 
-        matrix[j][N-1-i] = matrix[N-1-i][N-1-j]; 
-          matrix[N-1-i][N-1-j] = matrix[N-1-j][i]; 
-            matrix[N-1-j][i] = t; 
-        } 
+            for (int j = i; j < N-i-1; j++) 
+            { 
+                int t = matrix[i][j]; 
+                matrix[i][j] = matrix[j][N-1-i]; 
+                matrix[j][N-1-i] = matrix[N-1-i][N-1-j]; 
+                matrix[N-1-i][N-1-j] = matrix[N-1-j][i]; 
+                matrix[N-1-j][i] = t; 
+            } 
         } 
     } 
 
     public static void main (String[] args) 
     { 
-    int N = 3; 
+        int N = 3; 
     
-    // Sample Input
-    int matrix[][] = 
-    { 
-        {1, 2, 3}, 
-        {5, 6, 7}, 
-        {9, 10, 11} 
-    }; 
+        // Sample Input
+        int matrix[][] = 
+        { 
+            {1, 2, 3}, 
+            {5, 6, 7}, 
+            {9, 10, 11} 
+        }; 
   
-    Rotate(matrix, N); 
+        Rotate(matrix, N); 
   
-    // Print rotated matrix 
-    for (int i = 0; i < N; i++) 
-    { 
-        for (int j = 0; j < N; j++) 
-            System.out.print(" " + matrix[i][j]); 
+        // Print rotated matrix 
+        for (int i = 0; i < N; i++) 
+        { 
+            for (int j = 0; j < N; j++) 
+                System.out.print(" " + matrix[i][j]); 
   
+            System.out.print("\n"); 
+        } 
         System.out.print("\n"); 
-    } 
-    System.out.print("\n"); 
     } 
 } 
 
