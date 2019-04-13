ae29ab6e54b15cc545ebdeee7c68bb5fb664cccd
Kanchan Thareja <kthareja12@gmail.com>
2019-04-13 10:14:08 +0530

    fixed indentation

--- Matrix_Operations/Matrix_Rotation90.c
+++ Matrix_Operations/Matrix_Rotation90.c
@@ -8,14 +8,14 @@ void Rotate(int matrix[][N])
     for (int x = 0; x < N / 2; x++) 
     { 
         for (int i = x; i < N-x-1; i++) 
-	    { 
-	        int t = matrix[x][i];
-	        matrix[x][i] = matrix[i][N-1-x]; 
-	        matrix[i][N-1-x] = matrix[N-1-x][N-1-i]; 
+        { 
+            int t = matrix[x][i];
+            matrix[x][i] = matrix[i][N-1-x]; 
+            matrix[i][N-1-x] = matrix[N-1-x][N-1-i]; 
 
-	        matrix[N-1-x][N-1-i] = matrix[N-1-i][x];  
-	        matrix[N-1-i][x] = t; 
-	    } 
+            matrix[N-1-x][N-1-i] = matrix[N-1-i][x];  
+            matrix[N-1-i][x] = t; 
+        } 
     } 
 } 
 
@@ -25,17 +25,17 @@ int main()
     // Sample Input 
     int matrix[N][N] = 
     { 
-	    {1, 2, 3}, 
-	    {5, 6, 7}, 
-	    {9, 10, 11}
+        {1, 2, 3}, 
+        {5, 6, 7}, 
+        {9, 10, 11}
     }; 
 
     Rotate(matrix); 
 
     for (int i = 0; i < N; i++) 
     { 
-	    for (int j = 0; j < N; j++) 
-	        printf("%2d ", mat[i][j]); 
+        for (int j = 0; j < N; j++) 
+            printf("%2d ", mat[i][j]); 
 
         printf("\n"); 
     } 
