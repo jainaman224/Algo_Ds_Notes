48b9ce935fe97693dd2f9b95004f3a2f38a9af99
Nehal Kumar Singh <41484543+geekyNehal@users.noreply.github.com>
2019-04-17 12:52:19 +0530

    Quick Sort C code improvement and resolved error #941 (#944)

--- Quick_Sort/Quick_Sort.c
+++ Quick_Sort/Quick_Sort.c
@@ -1,20 +1,26 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
 
-using namespace std;
-
+//Swapping the value in the array 
+void swap(int *a,int *b)
+{
+   int temp= *a;
+   *a = *b;
+   *b = temp;
+}
 int partition (int arr[], int low, int high)
 {
+    //Taking the last value of array as pivot.
     int pivot = arr[high];
     int i = (low - 1);
-    for (int j = low; j <= high- 1; j++)
+    for (int j = low; j <= high - 1; j++)
     {
         if (arr[j] <= pivot)
         {
             i++;
-            swap(arr[i], arr[j]);
+            swap(&arr[i], &arr[j]);
         }
     }
-    swap(arr[i + 1], arr[high]);
+    swap(&arr[i + 1], &arr[high]);
     return (i + 1);
 }
 
@@ -22,6 +28,7 @@ void quickSort(int arr[], int low, int high)
 {
     if (low < high)
     {
+        //Partitioning the array.
         int pi = partition(arr, low, high);
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
@@ -30,32 +37,41 @@ void quickSort(int arr[], int low, int high)
 
 int main()
 {
-    // freopen("in.txt","r",stdin);
-    // freopen("out.txt","w",stdout);
     int t;
+    //Testcase
+    printf("Enter Number of times you want to sort:\n");
     scanf("%d", &t);
 
     while(t--)
     {
-        int n,i;
-        scanf("%d",&n);
-        int arr[100];
-        for(i=0;i<n;i++)
-            scanf("%d",&arr[i]);
-        quickSort(arr, 0, n-1);
-        for(i=0;i<n;i++)
-            printf("%d ",arr[i]);
+        int n, i;
+        printf("Enter the Size of the Array:\n");
+        scanf("%d", &n);
+        int arr[n];
+        printf("Enter Array:\n");
+        for(i = 0; i < n; i++)
+            scanf("%d", &arr[i]);
+        quickSort(arr, 0, n-1) ;
+        printf("Sorted Array:\n");
+        for(i = 0; i < n; i++)
+            printf("%d ", arr[i]);
         printf("\n");
     }
     return 0;
 }
-// sample input :
-// 2
-// 5
-// 1 5 8 32 46
-// 10
-// 3 3 4 23 356 12 523 43 67 864
-
-// sample output :
-// 1 5 8 32 46
-// 3 3 4 12 23 43 67 356 523 864
+/*
+----------------OUTPUT----------------
+Enter Number of times you want to sort:
+1
+Enter the Size of the Array:
+6
+Enter Array:
+4
+8
+5
+9
+2
+6
+Sorted Array:
+2 4 5 6 8 9 
+*/
