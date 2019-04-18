2b62e76df5d258364618d79c7a182eb41c834d6b
somya-kapoor <42409030+somya-kapoor@users.noreply.github.com>
2019-04-18 21:54:50 +0530

    Update Egg_Drop.cpp

--- Egg_Drop/Egg_Drop.cpp
+++ Egg_Drop/Egg_Drop.cpp
@@ -3,16 +3,16 @@
 using namespace std; 
 
 // To get maximum of two integers 
-#define max( a, b) ((a > b)? a : b)
+#define max(a, b) ((a > b)? a : b)
 
 int eggDrop(int eggs, int floors)
 {
     /* A 2D table where entery eggFloor[i][j] will represent minimum 
        number of trials needed for e eggs and f floors. */
     int eggfloor[eggs][floors]; 
-    for (int e = 1; e <= eggs ; e++)
+    for (int e = 1; e <= eggs; e++)
     {
-        for (int f = 0; f <= floors ; f++) 
+        for (int f = 0; f <= floors; f++) 
         {
             if(f == 0)
             {
@@ -27,7 +27,7 @@ int eggDrop(int eggs, int floors)
                 // Fill rest of the entries in table using optimal substructure 
                 // property 
                 int min = INT_MAX;
-                for (int i = 1; i <= f ; i++)
+                for (int i = 1; i <= f; i++)
                 {
                     int sol = max(eggfloor[e-1][i-1], eggfloor[e][f-i]) + 1;
                     if(sol < min)
@@ -44,10 +44,10 @@ int eggDrop(int eggs, int floors)
 
 int main() 
 { 
-    int eggs , floors; 
-    cout << "Enter the number of eggs: " ;
+    int eggs ,floors; 
+    cout << "Enter the number of eggs: ";
     cin >> eggs;
-    cout << "Enter the number of floors: " ;
+    cout << "Enter the number of floors: ";
     cin >> floors;
 
     cout << "Minimum number of trials: " << eggDrop(eggs, floors) << endl; 
