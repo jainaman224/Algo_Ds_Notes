e7ad1f892716e0043d39ce129d1460734d50ad1b
tusharsabhani <37148864+tusharsabhani@users.noreply.github.com>
2019-04-21 21:50:09 +0530

    Added Beizer Curve for C and C++

---
+++ Bezier_Curve/Bezier_Curve.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <graphics.h>
+#include <math.h>
+#include <iostream.h>
+
+//function that takes coordinates of control points as input and then draws the curve
+void bezier (int x[4], int y[4])
+{
+    int gd = DETECT, gm;
+    int i;
+    double t;
+    initgraph (&gd, &gm, "");
+
+    for (t = 0.0; t < 1.0; t += 0.0005)
+    {
+        double xt = pow (1-t, 3) * x[0] + 3 * t * pow (1-t, 2) * x[1] + 3 * pow (t, 2) * (1-t) * x[2] + pow (t, 3) * x[3];
+        double yt = pow (1-t, 3) * y[0] + 3 * t * pow (1-t, 2) * y[1] + 3 * pow (t, 2) * (1-t) * y[2] + pow (t, 3) * y[3];
+        putpixel (xt, yt, WHITE);
+    }
+    for (i = 0; i < 4; i++)
+        putpixel (x[i], y[i], YELLOW);
+    return;
+}
+
+void main()
+{
+    // two arrays to hold the coordinates of 4 control points
+    int x[4] = {23, 49, 95, 130};
+    int y[4] = {76, 92, 112, 73};
+    bezier (x, y);
+    closegraph();
+}
+
+//Sample output - https://ibb.co/qJttpQm
+
