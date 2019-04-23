aedd5db81dbc622b8c87a83286465cf6f1e62f29
dldisha <dldisha929@gmail.com>
2019-04-23 21:41:50 +0530

    fixes

--- Travelling_Salesman_Problem/Travelling_Salesman.cpp
+++ Travelling_Salesman_Problem/Travelling_Salesman.cpp
@@ -1,96 +1,96 @@
 #include<iostream>
 using namespace std;
- 
+
 int ary[10][10], completed[10], n, cost = 0;
- 
+
 void takeInput()
 {
     int i, j;
  
-	cout << "Enter the number of villages: ";
-	cin >> n;
+    cout << "Enter the number of villages: ";
+    cin >> n;
  	
- 	//Enter distance between cities
-	cout << "\n Enter the Cost Matrix \n";
+    //Enter distance between cities
+    cout << "\n Enter the Cost Matrix \n";
  
-	for (i = 0; i < n; i++)
-	{
-		cout << "\n Enter Elements of Row " << i+1 << "\n";
+    for (i = 0; i < n; i++)
+    {
+        cout << "\n Enter Elements of Row " << i+1 << "\n";
  
-		for (j = 0; j < n; j++)
-			cin >> ary[i][j];
+        for (j = 0; j < n; j++)
+            cin >> ary[i][j];
  
-		completed[i] = 0;
-	}
+        completed[i] = 0;
+    }
  	
- 	//Display matrix of distance between cities
-	cout << "\n\n The cost list is:";
+    //Display matrix of distance between cities
+    cout << "\n\n The cost list is:";
  
-	for (i = 0; i < n; i++)
-	{
-		cout << "\n";
+    for (i = 0; i < n; i++)
+    {
+        cout << "\n";
  
-		for (j = 0; j < n; j++)
-			cout << "\t" << ary[i][j];
-	}
+        for (j = 0; j < n; j++)
+            cout << "\t" << ary[i][j];
+    }
 }
 
 //Function for TSP algo
 int least(int c)
 {
-	int i, nc = 999;
-	int min = 999, kmin;
+    int i, nc = 999;
+    int min = 999, kmin;
   
-	for (i = 0; i < n; i++)
-	{
-		if ((ary[c][i] != 0) && (completed[i] == 0))
-			if (ary[c][i] + ary[i][c] < min)
-			{
-				min = ary[i][0] + ary[c][i];
-				kmin = ary[c][i];
-				nc = i;
-			}
-	}
+    for (i = 0; i < n; i++)
+    {
+        if ((ary[c][i] != 0) && (completed[i] == 0))
+            if (ary[c][i] + ary[i][c] < min)
+            {
+                min = ary[i][0] + ary[c][i];
+                kmin = ary[c][i];
+                nc = i;
+            }
+    }
  
-	if (min != 999)
-		cost += kmin;
+    if (min != 999)
+	    cost += kmin;
  
-	return nc;
+    return nc;
 }
  
 //Function to determine minimum cost
 void mincost(int city)
 {
-	int i, ncity;
+    int i, ncity;
  
-	completed[city] = 1;
+    completed[city] = 1;
  
-	cout << city + 1 << "--->";
-	ncity = least(city);
+    cout << city + 1 << "--->";
+    ncity = least(city);
  
-	if (ncity == 999)
-	{
-		ncity = 0;
-		cout << ncity + 1;
-		cost += ary[city][ncity];
+    if (ncity == 999)
+    {
+        ncity = 0;
+        cout << ncity + 1;
+        cost += ary[city][ncity];
  
-		return;
-	}
+        return;
+    }
  
-	mincost(ncity);
+    mincost(ncity);
 }
  
 //Display results
 int main()
 {
-	takeInput();
+    takeInput();
  
-	cout << "\n\n The Path is: \n";
-	mincost(0); //passing 0 because starting vertex
+    cout << "\n\n The Path is: \n";
+    mincost(0); //passing 0 because starting vertex
  
-	cout << "\n\n Minimum cost is " << cost;
+    cout << "\n\n Minimum cost is " << cost;
  
-	return 0;
+    return 0;
 }
 
 /* Output:
