3da90c42ee9f2a884da76270ef2313d65624fb5f
Khushal Agarwal <hrithik.agarwal87@gmail.com>
2019-04-23 22:48:10 +0530

    Update Hamming_Numbers.cpp

--- Hamming_Numbers/Hamming_Numbers.cpp
+++ Hamming_Numbers/Hamming_Numbers.cpp
@@ -12,24 +12,24 @@ bool is_hamming (int n) {
     if (n%2==0 || n%3==0 || n%5==0) {
 	   for (int i = 2; i*i < n; ++i) {
 	    if (n%i == 0) {
-				if (i%2 != 0 and i%3!=0 and i%5!=0) 
+	        if (i%2 != 0 and i%3!=0 and i%5!=0) 
                     return false;
-			}
-			if (n%(n/i) == 0) {
-				if ((n/i)%2 != 0 and (n/i)%3!=0 and (n/i)%5!=0) 
+	    }
+	    if (n%(n/i) == 0) {
+		if ((n/i)%2 != 0 and (n/i)%3!=0 and (n/i)%5!=0) 
                     return false;
-			}
-		}
-		return true;		
+	    }
+        }
+    	return true;		
     }
-	return false;
+    return false;
 }
 //If hamming number is found in above function then it is printed in the main function for the given range
 int main () {
-	int n;
-	cin >> n;           //user input
-	int count = 0;
-	int i = 1;
+    int n;
+    cin >> n;           //user input
+    int count = 0;
+    int i = 1;
     bool first = true;
     while (count < n) {
         if (is_hamming(i)) {
