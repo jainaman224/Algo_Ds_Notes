b100530ab154cebfef81ad7a948b689b6b026b33
Khushal Agarwal <hrithik.agarwal87@gmail.com>
2019-04-24 19:18:54 +0530

    Update Hamming_Numbers.c

--- Hamming_Numbers/Hamming_Numbers.c
+++ Hamming_Numbers/Hamming_Numbers.c
@@ -8,14 +8,14 @@ int is_hamming (int n) {
     {
         return 1;
     }
-    if (n%2==0 || n%3==0 || n%5==0) {
-        for (int i = 2; i*i < n; ++i) {
-	    if (n%i == 0) {
-		if (i%2 != 0 && i%3!=0 && i%5!=0)       
+    if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0) {
+        for (int i = 2; i * i < n; ++i) {
+	    if (n % i == 0) {
+		if (i % 2 != 0 && i % 3 !=0 && i % 5 !=0)       
 		    return 0;
 	    }
-	    if (n%(n/i) == 0) {
-		if ((n/i)%2 != 0 && (n/i)%3!=0 && (n/i)%5!=0) 
+	    if (n % (n / i) == 0) {
+		if ((n / i) % 2 != 0 && (n / i) % 3 !=0 && (n / i) % 5 != 0) 
                     return 0;
 	    }
 	}
@@ -26,7 +26,7 @@ int is_hamming (int n) {
 //If hamming number is found in above function then it is printed in the main function for the given range
 int main () {
     int n;
-    scanf("%d",&n);           //user input
+    scanf("%d", &n);           //user input
     int count = 0;
     int i = 1;
     int first = 1;
