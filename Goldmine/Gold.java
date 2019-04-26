283df35e586b58dabf5639fdbd824e434b574d47
gourabk121 <44121563+gourabk121@users.noreply.github.com>
2019-04-27 00:34:53 +0530

    Update Gold.java

--- Goldmine/Gold.java
+++ Goldmine/Gold.java
@@ -4,7 +4,7 @@
 //Given a gold mine of a*b dimensions.
 //Each field in this mine contains a positive integer which is the amount of gold in tons. 
 //Initially the miner is at first column but can be at any row.
-//He can move only (right->,right up /,right down) that is from a given gourabk121-patch-1cell,
+//He can move only (right->,right up /,right down) that is from a given cell,
 //the miner can move to the cell diagonally up towards the right or right or diagonally down towards the right.
 //Find out maximum amount of gold he can collect.
 
