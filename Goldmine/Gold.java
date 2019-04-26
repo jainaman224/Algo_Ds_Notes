2b2fc6bae31d436a6b429372e9748fa3bbae2eef
gourabk121 <44121563+gourabk121@users.noreply.github.com>
2019-04-26 23:51:02 +0530

    Update Gold.java

--- Goldmine/Gold.java
+++ Goldmine/Gold.java
@@ -1,14 +1,12 @@
 // Girlscript Summer Of Code
 // goldmine problem  in java
 
-
 //Given a gold mine of a*b dimensions.
 //Each field in this mine contains a positive integer which is the amount of gold in tons. 
 //Initially the miner is at first column but can be at any row.
 //He can move only (right->,right up /,right down) that is from a given cell,
 //the miner can move to the cell diagonally up towards the right or right or diagonally down towards the right.
 //Find out maximum amount of gold he can collect.
-
 
 import java.util.*; 
   
@@ -37,7 +35,7 @@ class Hello
     
     public static void main(String arg[]) 
     { 
-       int m,n, i, j;
+       int m, n, i, j;
        int arr[][] = new int[10][10];
        Scanner scan = new Scanner(System.in);
        // enter row and column for array.
@@ -49,7 +47,7 @@ class Hello
        System.out.println("Enter " +(m*n)+ " Array Elements : ");
        for(i=0; i<m; i++)
        {    
-            for(j=0; j<n; j++)
+            for(j = 0; j < n; j++)
             {
                arr[i][j] = scan.nextInt();
             }
@@ -57,7 +55,6 @@ class Hello
        System.out.print(getMaxGold(arr, m, n)); 
     } 
 } 
-
 
 //Input : mat[][] = {{5,4,2},
 //                   {1,4,2},
