59bc556bf1fe378e29a4cd1bee425a8490dab30a
Shreya Singh <singh.shreya8@gmail.com>
2019-04-27 15:01:25 +0530

    Sorting Algorithms in Go (#1086)

---
+++ Selection_Sort/Selection_Sort.go
@@ -0,0 +1,47 @@
+/*
+    Selection Sort is a sorting algorithm that is based
+    on the idea of finding the minimum or maximum element
+    in an unsorted array and then putting it in its correct
+    position in a sorted array.
+*/
+
+package main
+
+import "fmt"
+
+// Function for selection sort
+func Selection_Sort(array[] int, size int) []int {
+    var min_index int
+    var temp int
+
+    // Iterate over unsorted array
+    for i := 0; i < size - 1; i++ {
+        min_index = i
+
+        // Find index of minimum element
+        for j := i + 1; j < size; j++ {
+            if array[j] < array[min_index] {
+                min_index = j
+            }
+        }
+
+        temp = array[i]
+        array[i] = array[min_index]
+        array[min_index] = temp
+    }
+    return array
+}
+
+// Driver Function
+func main() {
+    array := []int{2, 4, 3, 1, 6, 8, 4}
+    fmt.Println(Selection_Sort(array, 7))
+}
+
+/* 
+    Input:
+    [ 2 4 3 1 6 8 4 ]
+
+    Output:
+    [ 1 2 3 4 4 6 8 ]
+*/
