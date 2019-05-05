31f35bac91c08aa71e4dc7e7cd93b45cf75e67a7
somya-kapoor <42409030+somya-kapoor@users.noreply.github.com>
2019-05-06 00:25:23 +0530

    Tim_Sort

--- Tim_Sort/Tim_Sort.c
+++ Tim_Sort/Tim_Sort.c
@@ -74,8 +74,13 @@ void insertionSort(int array[], int left, int right)
       
 int main()  
 {  
-    int array[] = {50, 39, 89, 10, 8};  
-    int n = sizeof(array)/sizeof(array[0]);  
+    int n;
+    printf("Enter number of elements in your array: ");
+    scanf("%d", &n);    
+    int array[n]; 
+    printf("Enter your array: ");
+    for (int i = 0; i < n; i++)
+        scanf("%d", &array[i]);
     printf("Unsorted Array : \n");  
     for (int i = 0; i < n; i++)  
         printf("%d  ", array[i]);  
@@ -104,6 +109,13 @@ int main()
 }  
 
 /*
+INPUT:
+Enter number of elements in your array: 5
+Enter your array: 50
+39
+89
+10
+8
 OUTPUT
 ----------------
 Unsorted Array :
