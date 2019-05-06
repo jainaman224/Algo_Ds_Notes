9838276f4ada3c7febe331828446df0f13e86fe2
Shubham Mantri <smantri600@gmail.com>
2019-05-06 12:01:27 +0530

    Updated

--- Bubble_Sort/Bubble_Sort.kt
+++ Bubble_Sort/Bubble_Sort.kt
@@ -7,7 +7,7 @@ fun bubbleSort(list: MutableList<Int>):List<Int> {
     var flag: Int = 0
     for (i in 0 until (list.size - 1)) {
         for (j in 0 until (list.size - i - 1)) {
-            if(list[j] > list[j + 1]) {
+            if (list[j] > list[j + 1]) {
                 var tmp = list[j + 1]
                 list[j + 1] = list[j]
                 list[j] = tmp
