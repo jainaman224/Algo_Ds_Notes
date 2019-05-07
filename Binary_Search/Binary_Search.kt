7f82c05c048f861107eb0bb7ee1c28a8d1027e2c
Shubham Mantri <smantri600@gmail.com>
2019-05-07 19:36:39 +0530

    Update for Dynamic Input

--- Binary_Search/Binary_Search.kt
+++ Binary_Search/Binary_Search.kt
@@ -1,33 +1,44 @@
+import java.util.Scanner
+
 // Function For Binary Search
-fun binarySearch(list: List<Int>, left: Int, right: Int, num: Int):Int {
-    if (list.size >= 1) {
+fun binarySearch(array: IntArray, left: Int, right: Int, num: Int):Int {
+    if (array.size >= 1) {
         var mid = (left + (right - 1)) / 2
-        if (list[mid] == num) {
+        if (array[mid] == num) {
             return mid
         }
-        if (list[mid] > num) {
-            return binarySearch(list, 1, mid - 1, num)
+        if (array[mid] > num) {
+            return binarySearch(array, 1, mid - 1, num)
         }
-        if (list[mid] < num) {
-            return binarySearch(list, mid + 1, right, num)
+        if (array[mid] < num) {
+            return binarySearch(array, mid + 1, array.size, num)
         }
     }
- return -1
+    return -1
 }
 
 // Driver Function for Binary Search
 fun main() {
-    var list = mutableListOf(4, 6, 18, 29, 67, 89)
-    // let we want to find 67
-    var num = 67
-    var limit = list.size - 1
-    var search = binarySearch(list, 0, limit, num)
+    val scanner = Scanner(System.`in`)
+    print("Enter size of Array : ")
+    val size = scanner.nextInt()
+    val array = IntArray(size)
+    print("Enter Numbers in Array : ")
+    for (i in 0 until size)
+        array[i] = scanner.nextInt()
+    print("Enter an Number : ")
+    var num = scanner.nextInt()
+    var limit = size - 1
+    var search = binarySearch(array, 0, limit, num)
     if (search == -1) {
-        println("$num Not Found In The List")
+        print("$num Not Found In The List")
     }
     else {
-        println("$num is at position $search")
+        print("$num is at position $search")
     }
 }
 
+// Input:- Enter size of Array : 6
+//         Enter Numbers in Array : 4, 6, 18, 29, 67, 89
+//         Enter an Number : 67
 // Output:- 67 is at position 4
