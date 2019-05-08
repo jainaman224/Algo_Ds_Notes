08db424df5849985ddfa6e4d024af51c8f344766
py93 <47155755+py93@users.noreply.github.com>
2019-05-08 23:06:15 +0530

    Python implementation of Array Rotation using temp array (#1107)
    * Array Rotation in Java using temp array
    
    For the issue #750
    
    * Added sample input output
    
    * Fixed spacing
    
    * Removed extra lines and fixed spacing
    
    * Issue #750: Array Rotation using Temp Array in Python
    
    * Comments updated
    
    * Comments updated
    
    * Array Rotation using temp array (Python and Java)

--- Array_Rotation/ArrayRotationUsingTempArray.java
+++ Array_Rotation/ArrayRotationUsingTempArray.java
@@ -2,28 +2,28 @@ import java.util.Arrays;
  
 public class ArrayRotationUsingTempArray
 {
-    //function to left rotate the array by d elements 
+    // function to left rotate the array by d elements 
     private static void rotate(int[] arr, int n, int d) 
     {
-        //it brings down d to the minimum number of rotations we need to make in case d > n
+        // it brings down d to the minimum number of rotations we need to make in case d > n
          d = d % n; 
         
-        //initialize temp array of size d
+        // initialize temp array of size d
         int[] temp = new int[d];
         
-        //copy the first d elements to temp array
+        // copy the first d elements to temp array
         for (int i = 0; i < d; i++)
         {
             temp[i] = arr[i];
         }
         
-        //left moves the rest of the elements in original array by d elements
+        // left moves the rest of the elements in original array by d elements
         for (int i = 0; i < n-d; i++)
         {
             arr[i] = arr[i+d];
         }
         
-        //assigns the remaining elements from temp array to original array
+        // assigns the remaining elements from temp array to original array
         for (int i = 0; i < d; i++)
         {
             arr[i+n-d] = temp[i];
