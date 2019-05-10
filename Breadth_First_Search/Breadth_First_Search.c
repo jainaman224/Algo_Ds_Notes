b468aabfc02ad96bc0d26fb376939628339d30a2
Dipanwita Guhathakurta <42820001+susiejojo@users.noreply.github.com>
2019-05-10 17:59:17 +0530

    Update Breadth_First_Search.c

--- Breadth_First_Search/Breadth_First_Search.c
+++ Breadth_First_Search/Breadth_First_Search.c
@@ -32,11 +32,11 @@ void BFS_run()
     for (int i = 0; i < n; i++) // initialising state of each vertex as 0(white)
         state[i] = 0;
     int v;
-	for (int i = 0; i < n; i++)
-	{
+    for (int i = 0; i < n; i++)
+    {
         if (state[i] != 1)
-		    BFS(i);
-	}
+	    BFS(i);
+    }
 }
  
 /*BFS first enqueues each unvisited vertex to the queue,marks it as visited and stores its value in val.
@@ -48,26 +48,26 @@ void BFS_run()
 void BFS(int node)
 {
     enqueue(node);
-	state[node] = 1;
-	while (!isempty())
+    state[node] = 1;
+    while (!isempty())
+    {
+        int val = front1();
+	printf("%d ",val + 1);
+	dequeue(n);
+	for (int i = 0; i < n; i++)
 	{
-		int val = front1();
-		printf("%d ",val + 1);
-		dequeue(n);
-		for (int i = 0; i < n; i++)
+            if (graph[val][i] == 1) // checks for existence of edge between two nodes
+	    { 
+	        if (state[graph[val][i]] != 1) // checks for neighbours of node to perform BFS on
 		{
-		    if (graph[val][i] == 1) // checks for existence of edge between two nodes
-			{ 
-				if (state[graph[val][i]] != 1) // checks for neighbours of node to perform BFS on
-				{
-					enqueue(graph[val][i]);
-					state[graph[val][i]] = 1;
-				}
-			}
-			else
-				continue;
-		} 
-	}
+		    enqueue(graph[val][i]);
+		    state[graph[val][i]] = 1;
+		}
+	    }
+	    else
+	        continue;
+	} 
+    }
 }
 
 //FUNCTIONS FOR QUEUE
@@ -77,63 +77,61 @@ after each insertion. Handles isempty case by setting front =0
 */
 int enqueue(int e)
 {
-
-	if (front == -1) 
-		front = 0;
-	rear = rear + 1;
-	queue[rear] = e ;
-	
+    if (front == -1) 
+    front = 0;
+    rear = rear + 1;
+    queue[rear] = e ;	
 }
 /*Function to delete elements from the front of the queue and return them.
  Returns queue underflow while trying to dequeue from an empty queue */
 int dequeue(int n)
 {
-	int elmnt;
-	if (front == -1 || front > rear)
-	{
-		printf("Queue Underflow\n");
-		exit(1);
-	}
-	
-	elmnt= queue[front];
-	front += 1;
-	return elmnt;
+    int elmnt;
+    if (front == -1 || front > rear)
+    {
+        printf("Queue Underflow\n");
+	exit(1);
+    }
+    elmnt= queue[front];
+    front += 1;
+    return elmnt;
 }
 
 /*Returns the element at front of the queue*/
 int front1()
 {
-	if ( isempty() ){
-		printf( "Empty queue!!\n" );}
-	else
-		return queue[front];
+    if ( isempty() ){
+        printf( "Empty queue!!\n" );}
+    else
+	return queue[front];
 }
 
 /*Returns 1 if queue is empty,else 0*/
 int isempty()
 {
 	
-	if (front == -1 || front > rear)
-		return 1;
-	else
-		return 0;
+    if (front == -1 || front > rear)
+        return 1;
+    else
+	return 0;
 }
 
 void matrix_initiator(int origin, int destin)
 {
-	// assigns vertices and edges 
-	graph[origin - 1][ destin - 1] = 1;
+    // assigns vertices and edges 
+    graph[origin - 1][ destin - 1] = 1;
 }
 /*Function to initiate the graph from edges input by user*/
 void graph_initiator()
 {
-	n = 4;
-	int e = 6;
- 	for (int i = 0; i < n; i++) //initialise whole graph matrix with 0s
- 	{
- 		for (int j = 0;j < n; j++)
- 			graph[i][j] = 0;
- 	}
+    n = 4;
+    int e = 6;
+    for (int i = 0; i < n; i++) //initialise whole graph matrix with 0s
+    {
+        for (int j = 0;j < n; j++)
+            graph[i][j] = 0;
+    }
+	
     matrix_initiator(1 , 2);
     matrix_initiator(1 , 3);
     matrix_initiator(2 , 3);
@@ -143,13 +141,14 @@ void graph_initiator()
 }
 int main()
 {
-	graph_initiator();
-	BFS_run();
-	printf("\n");
+    graph_initiator();
+    BFS_run();
+    printf("\n");
 }
 /*OUTPUT
 1 2 3 4 
 */
+
 
 
 
