655a2aa136fa4f48ed1bc4a5d81c75e47b0ccecd
Abhinav Masters <mastersabhinav@gmail.com>
2019-05-11 00:21:29 +0530

    Modified Insertion Sort (#1239)

--- Insertion_Sort/Insertion_Sort.py
+++ Insertion_Sort/Insertion_Sort.py
@@ -3,7 +3,6 @@ def Insertion_Sort(list):
     for i in range(1, len(list)):
         temp = list[i]
         j = i - 1
-
         while j >= 0 and list[j] > temp:
             list[j + 1] = list[j]
             j -= 1
@@ -14,14 +13,20 @@ def Insertion_Sort(list):
 def Print_list(list):
     for i in range(0, len(list)):
         print(list[i],end=" ")
-
     print()
 
-list = [2, 4, 3, 1, 6, 8, 4]
-
+num = int(input())
+list = []
+for i in range(0, num):
+    list.append(int(input()))
 Insertion_Sort(list)
-
 Print_list(list)
 
-# Output
-# 1 2 3 4 4 6 8
+'''
+Input :
+num = 6
+array = [1, 6, 3, 3, 5, 2]
+
+Output :
+[1, 2, 3, 3, 5, 6]
+'''
