96eeee6a4b314a130aead0f454ece1dc625be749
prashant-mahanta <prashantkumar.m16@iiits.in>
2019-05-10 22:21:08 +0530

    Indentation fixed

--- Lucas_Theorem/Lucas_Theorem.java
+++ Lucas_Theorem/Lucas_Theorem.java
@@ -1,38 +1,39 @@
 public class Lucas_Theorem {
 
-	static int nCrModpDP (int n, int r, int p)
-	{
-		int C[] = new int[r+1];
-		C[0] = 1;
+    static int nCrModpDP (int n, int r, int p)
+    {
+        int C[] = new int[r+1];
+        C[0] = 1;
 
-		for (int i=1; i<=n; i++)
-		{
-			for(int j=Math.min(i,r); j>0; j--)
-				C[j] = (C[j] + C[j-1]) % p;
-		}
-		
-		return C[r];
-	}
+        for (int i=1; i<=n; i++)
+        {
+            for(int j=Math.min(i,r); j>0; j--)
+                C[j] = (C[j] + C[j-1]) % p;
+        }
+        
+        return C[r];
+    }
 
-	static int nCrModpLucas (int n, int r, int p)
-	{
-		if(r==0)
-			return 1;
-		int ni,ri;
+    static int nCrModpLucas (int n, int r, int p)
+    {
+        if(r==0)
+            return 1;
+        int ni,ri;
 
-		ni = n%p;
-		ri = r%p;
-		return (nCrModpLucas(n/p, r/p, p) * nCrModpDP(ni, ri, p)) % p;
+        ni = n%p;
+        ri = r%p;
+        return (nCrModpLucas(n/p, r/p, p) * nCrModpDP(ni, ri, p)) % p;
 
-	}
+    }
 
-	public static void main (String args[]) 
-	{
-		int n,r,p;
-		n = 1000;r=900;p=13;
-		System.out.println("Value of nCr % p is " + nCrModpLucas(n,r,p));
-	}
+    public static void main (String args[]) 
+    {
+        int n,r,p;
+        n = 1000;r=900;p=13;
+        System.out.println("Value of nCr % p is " + nCrModpLucas(n,r,p));
+    }
 }
 /*      INPUT - OUTPUT
      sample input=13
      output=value of nCr %p is 8  */
+     
--- Lucas_Theorem/Lucas_Theorem.py
+++ Lucas_Theorem/Lucas_Theorem.py
@@ -1,31 +1,32 @@
 def nCrModpDP(n,r,p):
-	C=[0]*(r+1)
-	C[0] = 1
-	for i in range(1,n+1):
-		for j in range(min(i,r),0,-1):
-			C[j] = (C[j] + C[j-1])%p
+    C=[0]*(r+1)
+    C[0] = 1
+    for i in range(1,n+1):
+        for j in range(min(i,r),0,-1):
+            C[j] = (C[j] + C[j-1])%p
 
-	return C[r]
+    return C[r]
 
 def nCrModpLucas(n,r,p):
-	n, r = int(n), int(r)
-	if r == 0:
-		return 1
+    n, r = int(n), int(r)
+    if r == 0:
+        return 1
 
-	ni = n%p
-	ri = r%p
-	return (nCrModpLucas(n/p, r/p, p) * nCrModpDP(ni, ri, p))%p
+    ni = n%p
+    ri = r%p
+    return (nCrModpLucas(n/p, r/p, p) * nCrModpDP(ni, ri, p))%p
 
 def main():
-	n = 1000
-	r = 900
-	p = 13
-	print("Value of nCr % p is",nCrModpLucas(n,r,p))
+    n = 1000
+    r = 900
+    p = 13
+    print("Value of nCr % p is",nCrModpLucas(n,r,p))
 
 if __name__ == '__main__':
-	main()
+    main()
 
-'''     INPUT - OUTPUT
+'''     
+	INPUT - OUTPUT
      sample input=13
      output=value of nCr %p is 8 
 '''
