19f36fb7b6e23f561b3a6605ec4d0e834bca5413
Shubham Mantri <smantri600@gmail.com>
2019-05-10 15:22:01 +0530

    Changes for Indentation

--- nCr mod P/nCr_mod_P.kt
+++ nCr mod P/nCr_mod_P.kt
@@ -7,12 +7,12 @@ fun nCr(n: Int, r: Int, mod: Int):Int {
     }
     var pascal = IntArray(r + 1)
     pascal[0] = 1
-    for (i in 1 until r+1) {
+    for (i in 1 until r + 1) {
         pascal[i] = 0
     }
 
     // We use the known formula nCr = (n-1)C(r) + (n-1)C(r-1) for computing the values.
-    for (i in 1 until n+1) {
+    for (i in 1 until n + 1) {
         var k: Int
         if (i < r) {
             k = i
@@ -21,7 +21,7 @@ fun nCr(n: Int, r: Int, mod: Int):Int {
             k = r
         }
         for (j in k downTo 1) {
-            pascal[j] = (pascal[j] + pascal[j-1]) % mod
+            pascal[j] = (pascal[j] + pascal[j - 1]) % mod
         }
     }
     return pascal[r]
