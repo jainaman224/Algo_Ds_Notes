0f464c53c344b3c525549b4573ba9152a6942f43
prashant-mahanta <prashantkumar.m16@iiits.in>
2019-05-11 21:43:34 +0530

    Fixed suggested changes

--- Lucas_Theorem/Lucas_Theorem.py
+++ Lucas_Theorem/Lucas_Theorem.py
@@ -1,7 +1,7 @@
 def nCrModpDP(n, r, p):
-    C=[0] * (r+1)
+    C=[0] * (r + 1)
     C[0] = 1
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         for j in range(min(i, r), 0, -1):
             C[j] = (C[j] + C[j - 1])%p
 
