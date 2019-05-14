98a637aeb7fe76d311c0e13dcba831f88aa63821
Shubham Mantri <smantri600@gmail.com>
2019-05-15 01:17:00 +0530

    Dynamic Input and Improvement

--- Babylonian_Method/Babylonian_Method.kt
+++ Babylonian_Method/Babylonian_Method.kt
@@ -1,19 +1,22 @@
-fun square_root(num: Float):Float {
+import java.util.Scanner
+
+fun square_root(num: Float): Float {
     var number = num
     var guess = 1F
     var accuracy = 0.000001
-    while(number - guess > accuracy) {
+    while (number - guess > accuracy) {
         number = (number + guess) / 2
         guess = num / number
     }
     return number
 }
 
-//Driver Function for Babylonian
 fun main() {
-    //let number be 90
-    var num = 90
+    // Taking Input from user.
+    val scanner = Scanner(System.`in`)
+    var num = scanner.nextInt()
     println("Square Root of $num is ${square_root(num.toFloat())}")
 }
 
-//Output: Square Root of 90 is 9.486834
+// Input :- 90
+// Output: Square Root of 90 is 9.486834
