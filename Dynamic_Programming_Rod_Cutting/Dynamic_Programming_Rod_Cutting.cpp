835dcf0fec187ebc38de06d0d645849ebac89b1f
Abhinav Masters <mastersabhinav@gmail.com>
2019-05-15 23:28:05 +0530

    Modified files for runtime input (#1275)

--- Dynamic_Programming_Rod_Cutting/Dynamic_Programming_Rod_Cutting.cpp
+++ Dynamic_Programming_Rod_Cutting/Dynamic_Programming_Rod_Cutting.cpp
@@ -7,20 +7,20 @@ using namespace std;
 // A function for calculating max of two nos.
 int max(int a, int b)
 {
-	return (a>b)?a:b;
+	return (a > b) ? a : b;
 }
 
 // Returns the max obtainable cost
 int CutRod(int cost[], int n)
 {
-	int val[n+1], i, j;
+	int val[n + 1], i, j;
     val[0] = 0;
 
-	for(i = 1; i <= n; i++)
+	for (i = 1; i <= n; i++)
     {
 		int max_value = INT_MIN;
-       	for(j = 0; j < i; j++)
-        	max_value = max(max_value, cost[j]+val[i-j-1]);
+       	for (j = 0; j < i; j++)
+        	max_value = max(max_value, cost[j] + val[i - j - 1]);
         val[i] = max_value;
     }
     return val[n];
@@ -28,15 +28,21 @@ int CutRod(int cost[], int n)
 
 // Driver function
 int main() {
-	int arr[] = {3, 5, 8, 9, 10, 17, 17, 20};
-	int size = sizeof(arr) / sizeof(arr[0]);
+	int size;
+	cin >> size;
+	int arr[size];
+	for (int i = 0; i < size; i++) {
+		cin >> arr[i];
+	}
 	cout << "Maximum value is " << CutRod(arr, size) << endl;
 	return 0;
 }
 
 
-/* OUTPUT
-
+/* 
+INPUT: 
+size = 8
+arr = {3, 5, 8, 9, 10, 17, 17, 20}
+OUTPUT:
 Maximum value is 24
-
 */
