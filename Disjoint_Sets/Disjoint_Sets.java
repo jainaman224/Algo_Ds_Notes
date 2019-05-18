f6022887003d5cd727c182967d7a6c8e3cf58444
Dibya Gautam <dibya.gautam7@gmail.com>
2019-05-18 12:24:51 +0530

    Update Disjoint_Sets.java

--- Disjoint_Sets/Disjoint_Sets.java
+++ Disjoint_Sets/Disjoint_Sets.java
@@ -1,66 +1,83 @@
-public class Disjoint_Sets {
-    //declare global arrays to store the parent of each element and size of each set
-    static int[] parent;
-    static int[] size;
-    
-    //function that initially defines the parent of all element as itself.
-    static void makeSet(int n) {
-        for (int i=0;i<n+1;i++) {
-            parent[i]=i;
-            size[i]=1;
-        }
-        return;
-    }
-    
-    
-    //recursive function to find the parent of an element.
-    static int find(int a) {
-        if (parent[a]==a) return a;
-        else return (find(parent[a]));      
-    }
-    
-    
-    //function to merge two elements such that both of them belong to the same set, i.e. they share a common parent
-    static void union(int a, int b) {
-        int parent_a=find(a);
-        int parent_b=find(b);
-        
-        if (parent_a!=parent_b) {
-            if (size[parent_a]<size[parent_b]){
-                //swap a and b
-                int temp=a;
-                a=b;
-                b=temp;
-            }
-            parent[parent_b]=parent_a;
-            size[a]+=size[b];
-    
-        }
-    }
-    
-    
-    public static void main(String[] args) {
-                
-        int n=6;
-        parent=new int[n+1];
-        size=new int[n+1];
-        makeSet(n); //make set with n nodes (1,2,...,n)
-        
-        // create graphs 1--3--4 and 2--5--6
-        union(1,3);
-        union(3,4);
-        
-        union(2,5);
-        union(5,6); 
-        
-         //check if 1 and 4 belong to the same set (expected output: 1  1)
-        System.out.println(find(1)+ "  " + find(4));
-        
-        //check if 2 and 6 belong to the same set (expected output: 2  2)
-        System.out.println(find(2)+ "  " + find(6));
-        
-        //check if 1 and 2 belong to the same set (expected output: 1  2)
-        System.out.println(find(1)+ "  " + find(2));
-    }
+import java.util.Scanner;
 
+
+public class Disjoint_Sets {
+	static int[] parent;
+	static int[] size;
+	
+	// function that initially defines the parent of all element as itself.
+	static void makeSet(int n) {
+		for (int i = 0; i < n+1; i++) {
+			parent[i] = i;
+			size[i] = 1;
+		}
+		return;
+	}
+	
+	// recursive function to find the parent of an element.
+	static int find(int a) {
+		if (parent[a] == a) return a;
+		else return find(parent[a]);		
+	}
+	
+	// function to merge two elements such that both of them belong to the same set
+	static void union(int a, int b) {
+		int parent_a = find(a);
+		int parent_b = find(b);
+		
+		if (parent_a != parent_b) {
+			if (size[parent_a] < size[parent_b]) {
+				//swap a and b
+				int temp = a;
+				a = b;
+				b = temp;
+			}
+			parent[parent_b] = parent_a;
+			size[a] += size[b];
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter number of nodes and number of edges");
+		int n = input.nextInt();
+		int e=input.nextInt();
+		parent = new int[n+1];
+		size = new int[n+1];
+		System.out.println("Enter e edges");
+		
+		makeSet(n);		// make set with n nodes (1,2,...,n)
+		for (int i = 0; i < e; i++) {
+			int a=input.nextInt();
+			int b=input.nextInt();
+			union(a, b);
+		}
+		for (int i = 1; i < n+1; i++) {
+			System.out.println(find(i));
+		}
+	}
 }
+
+/* Sample Input/Output
+ 
+	Input:
+	6 4
+	1 3
+	3 4
+	2 5
+	5 6
+	
+	Output:
+	1
+	2
+	1
+	1
+	2
+	2
+	
+	Explanation:
+	First line of input is n and e respectively.
+	e line contain edges
+	
+	ith line of output corresponds to the set that i belongs to. (0 <= i <= n)
+*/
