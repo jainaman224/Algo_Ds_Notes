2bb5eb92a3b784416f5786f53ebbd64a6a397b8d
somya-kapoor <42409030+somya-kapoor@users.noreply.github.com>
2019-05-28 03:36:16 +0530

    Centered_Square_Number (#1397)
    Centered_Square_Number

---
+++ Centered_Square_Number/Centered_Square_Number.c
@@ -0,0 +1,30 @@
+/*
+Centered Square Number is a centered figurate number that gives the number
+of dots in a square with a dot in the center and all other dots surrounding 
+the center dot in successive square layers.
+
+Nth Centered square number can be calculated by using formula n^2 + (n-1)^2.
+*/
+
+#include<stdio.h> 
+
+int centeredSquare(int num) 
+{ 
+    // Using formula  
+    return num * num + ((num - 1) * (num - 1)); 
+} 
+   
+int main() 
+{ 
+    int num;
+    scanf("%d", &num);
+    printf("%d", centeredSquare(num)); 
+    return 0; 
+}
+
+/*
+Input:
+6
+output:
+61
+*/
