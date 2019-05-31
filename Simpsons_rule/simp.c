489de17a67f60ac20ec8f29011848c40b2bec564
gourabk121 <44121563+gourabk121@users.noreply.github.com>
2019-05-31 20:03:21 +0530

    Update simp.c

--- Simpsons_rule/simp.c
+++ Simpsons_rule/simp.c
@@ -1,37 +1,40 @@
-
-// Girlscript Summer Of Code
 // Simpson's rule  in c
 //this is used to find integration 
+//It is used when it is very difficult to solve the given integral mathematically.
+//This rule gives approximation easily without actually knowing the integration rules.
 
- #include<stdio.h>
+#include<stdio.h>
 #include<conio.h>
 #include<math.h>
 
- float f(float x);
+float f(float x);
 float f(float x) {
     return (x);
 }
+
 int main() {
     int a, b, n, i;
     float s1 = 0, s2 = 0, sum, h;
     printf("Enter the value of upper limit = ");
-    scanf("%d", & b);
+    scanf("%d", &b);
     printf("Enter the value of lower limit = ");
-    scanf("%d", & a);
+    scanf("%d", &a);
     printf("Enter the number of intervals = ");
-    scanf("%d", & n);
+    scanf("%d", &n);
     h = fabs(b - a) / n;
     if (n % 2 == 0) {
         for (i = 1; i <= n - 1; i++) {
             if (i % 2 == 0) {
                 s1 = s1 + f(a + i * h);
-            } else {
+            } 
+            else {
                 s2 = s2 + f(a + i * h);
             }
         }
         sum = (h / 3) * (f(a) + f(b) + 4 * s2 + 2 * s1);
         printf("the value is=%f", sum);
-    } else {
+    } 
+    else {
         printf("Not appliciable! Interval must be even number");
     }
     getch();
