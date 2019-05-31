1652b49adb0c946e041ac91d45f3b0a18b697bd6
Khushal Agarwal <hrithik.agarwal87@gmail.com>
2019-05-31 23:50:52 +0530

    Update Hamming_Numbers.js

--- Hamming_Numbers/Hamming_Numbers.js
+++ Hamming_Numbers/Hamming_Numbers.js
@@ -1,4 +1,4 @@
-//Function for finding hamming numbers
+// Function for finding hamming numbers
 function Hamming(n) {
     var succession = [1];
     var length = succession.length;
@@ -12,7 +12,7 @@ function Hamming(n) {
     }
     return succession;
 }
-//Function to check if a number is hamming number and if true return it.
+// Function to check if a number is hamming number and if true return it.
 function isHammingNumber(num) {
     while (num % 5 === 0) num /= 5;
     while (num % 3 === 0) num /= 3;
@@ -20,8 +20,8 @@ function isHammingNumber(num) {
 
     return num == 1;
 }
-//the value 20 in (Hamming(20)) can be changed to get more hamming numbers.
+// the value 20 in (Hamming(20)) can be changed to get more hamming numbers.
 console.log(Hamming(20));
 
-//OUTPUT
-//[ 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 18, 20, 24, 25, 27, 30, 32, 36 ]
+// OUTPUT
+// [ 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, 16, 18, 20, 24, 25, 27, 30, 32, 36 ]
