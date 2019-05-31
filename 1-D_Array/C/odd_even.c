fcfa2b28b20787752f2343cc6568c0eedeeed766
gourabk121 <44121563+gourabk121@users.noreply.github.com>
2019-05-31 23:51:32 +0530

    Create odd_even.c

---
+++ 1-D_Array/C/odd_even.c
@@ -0,0 +1,36 @@
+//program to find odd and even numbers from a array
+
+#include <stdio.h>
+
+void main()
+{
+    int a[20],i,n;
+    printf("Enter the array size ");
+    scanf("%d",&n);
+    printf("Enter the array elements\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    printf("Even numbers are\n");
+    for(i=0;i<n;i++){
+        if(a[i]%2==0){
+            printf("%d\t",a[i]);
+        }
+    }
+    printf("\nOdd numbers are\n");
+    for(i=0;i<n;i++){
+        if(a[i]%2!=0){
+            printf("%d\t",a[i]);
+        }
+    }
+}
+
+//output
+//Enter the array size 5
+//Enter the array elements
+//2 5 15 6 9
+//Even numbers are
+//2 6
+//Odd numbers are
+//5 15 9
+
