78b3dbb4c2dc318cc84f87cefa93a313650c43c2
MastersAbh <mastersabhinav@gmail.com>
2019-05-31 20:10:25 +0530

    Added Count of set bits till N

---
+++ Set_Bits_Till_N/Set_Bits_Till_N.cpp
@@ -0,0 +1,40 @@
+/*
+TOTAL SET BITS FROM 1 to N
+
+Total set bits in a number n is defined as number of 1s in 
+binary representation of the number. The task is to calculate
+total number of set bits in all numbers till N.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int count(int n) {
+    int bit = 0;
+    while(n > 0) {
+        bit += n & 1;
+        n >>= 1;
+    }
+    return bit;
+}
+
+int setBits(int n) {
+    int bit = 0;
+    for (int i = 0; i < n; i++) {
+        bit = bit + count(i);
+    }
+    return bit;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << "The total set bits are " << setBits(n);
+}
+
+/*
+INPUT :
+n = 12
+OUTPUT :
+The total set bits are 20
+*/
