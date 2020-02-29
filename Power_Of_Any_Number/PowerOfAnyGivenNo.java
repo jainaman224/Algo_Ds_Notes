98437b46f93edc1b48d504f8c8ac3a9745dce9c5
Chanpreet Singh <34856246+chanpreet1999@users.noreply.github.com>
2020-03-01 02:02:11 +0530

    Parial fulfillment  of issue #1599

---
+++ Power_Of_Any_Number/PowerOfAnyGivenNo.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class PowerOfAnyGivenNo {
+
+	/*
+	 * Problem Statement
+	 * Program to find if a No. is a power of any Given No. in Java
+	 *	
+	 *	Approach
+	 *	comparing the log's value with the floor value of log 
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);	//input
+		System.out.print("Enter a no. : ");
+		int number = sc.nextInt();
+		
+		System.out.print("Check for which no. ? ");
+		int base = sc.nextInt();
+		
+		if( isPowerOfGivenNo(number, base) )
+			System.out.println("Yes "+number +" is a power of "+base);
+		else
+			System.out.println("No "+number +" is not a power of "+base);
+		sc.close();
+	}
+
+	private static boolean isPowerOfGivenNo(int number, int base) {
+		//explicitly check for 1 because log(1) = 0
+		if(base == 1){
+			if(number == 1)
+				return true;
+			else	
+				return false;
+		}
+		//converting to positive	
+		number = Math.abs(number);
+		
+		float logValue = (float) (Math.log(number)/Math.log(base));
+		return logValue == Math.floor(logValue);
+	}
+
+}
+
+/*
+ * Input :
+ 	Enter a no. : 14641
+	Check for which no. ? 11
+ 
+ * Output :
+	Yes 14641 is a power of 11
+ * */
