6e242f4aa1460491548445b619cadc3a8a0a2749
Chanpreet Singh <34856246+chanpreet1999@users.noreply.github.com>
2020-03-01 02:11:19 +0530

    Count Frequency Of Elemets In Java Fixex #1601
    Fixes #1601

---
+++ Freq_Of_Elements_Of_Array/CountFrequencyOfArrayElements.java
@@ -0,0 +1,60 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+
+/*Problem Statement
+ * Given an array of elements count the frequency of every element in the array.
+ */
+
+public class CountFrequencyOfArrayElements {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//input size of array
+		int n = sc.nextInt();
+		
+		//input array
+		int a[] = new int[n];
+		for(int i = 0; i < n; i++)
+			a[i] = sc.nextInt();
+		
+		countFreq(a, n);
+		sc.close();
+	}
+
+	private static void countFreq(int[] a, int n) {
+		HashMap<Integer, Integer> hm = new HashMap<>(); // using hashmap to store the freq
+		
+		for(int i = 0; i < n; i++)
+			{
+				if( hm.containsKey(a[i]) )
+					hm.put( a[i], hm.get(a[i]) + 1);
+				else
+					hm.put(a[i], 1);
+			}
+			
+		//display the frequencies	
+		System.out.println("Element \t Frequency");
+		hm.forEach(
+				(k,v) ->
+				{
+					System.out.println(k+" \t\t "+v);
+				}
+				);
+	}
+	
+}
+
+/*
+ * Input : 
+  5
+  2 1 1 6 1
+  
+ * Output :
+ 	Element 	 Frequency
+		1 		 3
+		2 		 1
+		6 		 1
+  
+ * */
