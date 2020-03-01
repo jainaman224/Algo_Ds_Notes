ee99a3abfcad80d6b8a395e942b4dcf2ad6fe356
satvikchachra <satvikchachra19@gmail.com>
2020-03-01 14:56:00 +0530

    updated BFS_Disconnected_Graph directory

--- BFS_Disconnected_Graph/BFS_DisconnectedGraph.java
+++ BFS_Disconnected_Graph/BFS_DisconnectedGraph.java
@@ -1,105 +1,92 @@
-import java.util.*; 
-public class BFS_DisconnectedGraph  
-{ 
-    //Implemented graph using HashMap 
-    static HashMap<Integer,LinkedList<Integer>> graph=new HashMap<>(); 
-  
-    //function to add edge in an undirected graph 
-public static void addEdge(int a,int b) 
-{ 
-    if(graph.containsKey(a)) 
-    { 
-        LinkedList<Integer> l=graph.get(a); 
-        l.add(b); 
-        graph.put(a,l); 
-    } 
-    else
-    { 
-        LinkedList<Integer> l=new LinkedList<>(); 
-        l.add(b); 
-        graph.put(a,l); 
-    } 
-} 
-  
+import java.util.*;
+
+public class BFS_DisconnectedGraph {
+	// Implemented graph using HashMap
+	static HashMap<Integer, LinkedList<Integer>> graph = new HashMap<>();
+
+	// function to add edge in an undirected graph
+	public static void addEdge(int a, int b) {
+		if (graph.containsKey(a)) {
+			LinkedList<Integer> l = graph.get(a);
+			l.add(b);
+			graph.put(a, l);
+		} else {
+			LinkedList<Integer> l = new LinkedList<>();
+			l.add(b);
+			graph.put(a, l);
+		}
+	}
+
 //Helper function for BFS  
-public static void bfshelp(int s,ArrayList<Boolean> visited) 
-{ 
-    // Create a queue for BFS  
-    LinkedList<Integer> q=new LinkedList<>(); 
-      
-    // Marking the current node as visited and enqueue it  
-    q.add(s); 
-    visited.set(s,true); 
-      
-    while(!q.isEmpty()) 
-    { 
-        // Dequeue a vertex from queue and print it  
-        int f=q.poll(); 
-        System.out.print(f+" "); 
-          
-        //Check whether the current node is  
-                //connected to any other node or not 
-        if(graph.containsKey(f)) 
-        { 
-        Iterator<Integer> i=graph.get(f).listIterator(); 
-          
-        // Get all adjacent vertices of the dequeued  
-        // vertex f. If an adjacent has not been visited,   
-        // then mark it visited and enqueue it  
-          
-            while(i.hasNext()) 
-            { 
-                int n=i.next(); 
-                if(!visited.get(n)) 
-                { 
-                visited.set(n,true); 
-                q.add(n); 
-                } 
-            } 
-        } 
-    } 
-      
-} 
-  
+	public static void bfshelp(int s, ArrayList<Boolean> visited) {
+		// Create a queue for BFS
+		LinkedList<Integer> q = new LinkedList<>();
+
+		// Marking the current node as visited and enqueue it
+		q.add(s);
+		visited.set(s, true);
+
+		while (!q.isEmpty()) {
+			// Dequeue a vertex from queue and print it
+			int f = q.poll();
+			System.out.print(f + " ");
+
+			// Check whether the current node is
+			// connected to any other node or not
+			if (graph.containsKey(f)) {
+				Iterator<Integer> i = graph.get(f).listIterator();
+
+				// Get all adjacent vertices of the dequeued
+				// vertex f. If an adjacent has not been visited,
+				// then mark it visited and enqueue it
+
+				while (i.hasNext()) {
+					int n = i.next();
+					if (!visited.get(n)) {
+						visited.set(n, true);
+						q.add(n);
+					}
+				}
+			}
+		}
+
+	}
+
 //BFS function to check each node 
-public static void bfs(int vertex) 
-{ 
-    ArrayList<Boolean> visited=new ArrayList<Boolean>(); 
-    //to mark each node as unvisited 
-    for(int i=0;i<vertex;i++) 
-    { 
-        visited.add(i,false); 
-    } 
-    for(int i=0;i<vertex;i++) 
-    { 
-        //to check whether the node is visited or not 
-        if(!visited.get(i)) 
-        { 
-            bfshelp(i,visited); 
-        } 
-    } 
-} 
-  
+	public static void bfs(int vertex) {
+		ArrayList<Boolean> visited = new ArrayList<Boolean>();
+		// to mark each node as unvisited
+		for (int i = 0; i < vertex; i++) {
+			visited.add(i, false);
+		}
+		for (int i = 0; i < vertex; i++) {
+			// to check whether the node is visited or not
+			if (!visited.get(i)) {
+				bfshelp(i, visited);
+			}
+		}
+	}
+
 //Driver Code-The main function 
-    public static void main(String[] args)  
-    { 
+	public static void main(String[] args) {
 
-	//sample input testcase
+		// sample input testcase
 
-        int v=5; 
-        addEdge(0, 4);  
-        addEdge(1, 2);  
-        addEdge(1, 3);  
-        addEdge(1, 4);  
-        addEdge(2, 3);  
-        addEdge(3, 4);  
-        bfs(v); 
+		int v = 5;
+		addEdge(0, 4);
+		addEdge(1, 2);
+		addEdge(1, 3);
+		addEdge(1, 4);
+		addEdge(2, 3);
+		addEdge(3, 4);
+		bfs(v);
 
-	/* sample output 
-	
-	0 4 1 2 3
-	
-	*/
-    }
-    
-} 
+		/*
+		 * sample output
+		 * 
+		 * 0 4 1 2 3
+		 * 
+		 */
+	}
+
+}
