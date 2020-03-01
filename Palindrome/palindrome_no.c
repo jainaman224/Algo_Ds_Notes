8970e8f8bd6d5e6b10f44cc13923dba7d644f31a
Chintamani Raju Virulkar <54358660+DawoodBhai420@users.noreply.github.com>
2020-03-01 21:58:22 +0530

    used 4 space indentation

--- Palindrome/palindrome_no.c
+++ Palindrome/palindrome_no.c
@@ -1,25 +1,25 @@
 #include<stdio.h>
 int main()
 {
-	int n,rem,temp,rev=0;
-	printf("\nEnter a number: ");
-	scanf("%d",&n);
-	temp=n;
-	while(temp!=0)
-	{
-		rem=temp%10;
-		temp=temp/10;
-		rev=(rev*10)+rem;
-	}
-	if(rev!=n)
-	{
-		printf("\nEntered number is not palindrome.\n");
-	}
-	else
-	{
-		printf("\nEntered number is palindrome.\n");
-	}
-	return 0;
+    int n,rem,temp,rev=0;
+    printf("\nEnter a number: ");
+    scanf("%d",&n);
+    temp=n;
+    while(temp!=0)
+    {
+        rem=temp%10;
+	temp=temp/10;
+	rev=(rev*10)+rem;
+    }
+    if(rev!=n)
+    {
+        printf("\nEntered number is not palindrome.\n");
+    }
+    else
+    {
+        printf("\nEntered number is palindrome.\n");
+    }
+    return 0;
 }
 
 
