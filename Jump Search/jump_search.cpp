542f37f90651fb8efdfb66d29af3330a196d5612
Chintamani Raju Virulkar <54358660+DawoodBhai420@users.noreply.github.com>
2020-03-01 22:18:35 +0530

    used 4 space indentation

--- Jump Search/jump_search.cpp
+++ Jump Search/jump_search.cpp
@@ -1,59 +1,58 @@
 #include<iostream>
 #include<cmath> //to use sqrt()
 using namespace std;
-int jump_search(int,int,int);
 int jump_search(int a[], int n, int key)
 {
-	int step=sqrt(n);
-	int pre_step=0;
-	while (a[min(step, n)-1] < key) 
+    int step=sqrt(n);
+    int pre_step=0;
+    while (a[min(step, n)-1] < key) 
     { 
         pre_step = step; 
         step += sqrt(n); 
         if (pre_step >= n)
-		{
-			return -1;	
-		} 
+        {
+            return -1;	
+        } 
     }
     while(a[pre_step]<key)
     {
-    	pre_step++;
-    	if(pre_step==min(step,n))
-    	{
-    		return -1;
-		}
-	}
-	if(a[pre_step]==key)
-	{
-		return pre_step;
-	}
-	else
-	{
-		return -1;
-	}
+        pre_step++;
+        if(pre_step==min(step,n))
+        {
+            return -1;
+        }
+    }
+    if(a[pre_step]==key)
+    {
+        return pre_step;
+    }
+    else
+    {
+        return -1;
+    }
 }
 int main()
 {
-	int no,ele;
-	cout<<"Enter no. of elements in an array: ";
-	cin>>no;
-	int arr[no];
-	cout<<"Enter elements in an array in increasing order: ";
-	for(int i=0;i<no;i++)
-	{
-		cin>>arr[i];
-	}
-	cout<<"Enter a number to be searched from array: ";
-	cin>>ele;
-	if(jump_search(arr,no,ele)==-1)
-	{
-		cout<<ele<<" is not present in array."<<endl;
-	}
-	else
-	{
-		cout<<ele<<" is present at index no. "<<jump_search(arr,no,ele)<<endl;
-	}
-	return 0;
+    int no,ele;
+    cout<<"Enter no. of elements in an array: ";
+    cin>>no;
+    int arr[no];
+    cout<<"Enter elements in an array in increasing order: ";
+    for(int i=0;i<no;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Enter a number to be searched from array: ";
+    cin>>ele;
+    if(jump_search(arr,no,ele)==-1)
+    {
+        cout<<ele<<" is not present in array."<<endl;
+    }
+    else
+    {
+        cout<<ele<<" is present at index no. "<<jump_search(arr,no,ele)<<endl;
+    }
+    return 0;
 }
 
 
