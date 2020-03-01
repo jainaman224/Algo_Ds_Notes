8bdf60dd697981566944a182339afe588f9962d4
Chintamani Raju Virulkar <54358660+DawoodBhai420@users.noreply.github.com>
2020-03-01 22:04:10 +0530

    added 4 space indentation

--- Palindrome/palindrome_string.cpp
+++ Palindrome/palindrome_string.cpp
@@ -3,28 +3,28 @@
 using namespace std;
 void string_palindrome(string s)
 {
-	string reverse=s;
-	int n=s.length();
-	for(int i=0;i<n/2;i++)
-	{
-		swap(reverse[i],reverse[n-i-1]);
-	}
-	if(reverse==s)
-	{
-		cout<<s<<" is Plindrome."<<endl;
-	}
-	else
-	{
-		cout<<s<<" is not Palindrome."<<endl;
-	}
+    string reverse=s;
+    int n=s.length();
+    for(int i=0;i<n/2;i++)
+    {
+        swap(reverse[i],reverse[n-i-1]);
+    }
+    if(reverse==s)
+    {
+        cout<<s<<" is Plindrome."<<endl;
+    }
+    else
+    {
+        cout<<s<<" is not Palindrome."<<endl;
+    }
 }
 int main()
 {
-	string str;
-	cout<<"Enter a string: ";
-	cin>>str;
-	string_palindrome(str);
-	return 0;
+    string str;
+    cout<<"Enter a string: ";
+    cin>>str;
+    string_palindrome(str);
+    return 0;
 }
 
 
