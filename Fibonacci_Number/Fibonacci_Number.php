2055254b12db81c671bcf519493c46aa158c8b01
Muskan Goyal <47412347+goyalmuskan@users.noreply.github.com>
2020-03-01 21:26:31 +0530

    Update Fibonacci_Number.php

--- Fibonacci_Number/Fibonacci_Number.php
+++ Fibonacci_Number/Fibonacci_Number.php
@@ -6,7 +6,10 @@ function FibonacciNum($num){
         return 0;     
     else if ($num == 1) 
         return 1;     
+<<<<<<< Updated upstream
       
+=======
+>>>>>>> Stashed changes
     else
         return (FibonacciNum($num-1) +  
                 FibonacciNum($num-2)); 
@@ -27,4 +30,8 @@ Fibonacci series:
 1 1 2 3 5 8
 */
 
+<<<<<<< Updated upstream
 ?> 
+=======
+?> 
+>>>>>>> Stashed changes
