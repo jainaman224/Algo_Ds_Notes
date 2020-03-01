b0b4139efc83b68845e7871ad1463c98b3d8b767
Sandhya <luckysandy99@gmail.com>
2020-03-01 14:38:30 +0530

    Made input dynamic and added sample input

--- Pollard_Pminusone_Factorization/pollard_pminus1.cpp
+++ Pollard_Pminusone_Factorization/pollard_pminus1.cpp
@@ -4,36 +4,43 @@ using namespace std;
 
 ll modular_pow(ll base, ll exponent, ll modulus)
 {
-	ll result = 1;
-	while(exponent > 0)
-	{
-		if(exponent&1)
-			result = (result*base)%modulus;
-		exponent = exponent >> 1;
-		base = (base*base)%modulus;
-	}
-	return result;
+    ll result = 1;
+    while(exponent > 0)
+    {
+        if(exponent&1)
+            result = (result*base)%modulus;
+        exponent = exponent >> 1;
+        base = (base*base)%modulus;
+    }
+    return result;
 }
 
 ll pollard_p1(ll n, ll B)
 {
-	ll a = 2;
-	ll e = 2;
-	while(e<=B)
-	{
-		a = (modular_pow(a,e,n));
-		e++;
-	}
-	ll p = __gcd(a-1,n);
-	if(p > 1 && p < n)
-		return p;
-	return -1;
+    ll a = 2;
+    ll e = 2;
+    while(e<=B)
+    {
+        a = (modular_pow(a,e,n));
+        e++;
+    }
+    ll p = __gcd(a-1,n);
+    if(p > 1 && p < n)
+        return p;
+    return -1;
 }
 
 
 int main()
 {
-	ll n = 57247159;
-	cout<<"One of the factors of "<<n<<" is "<<pollard_p1(n,8);
-       	return 0;	
+    ll n;
+    cin>>n;
+    cout<<"One of the factors of "<<n<<" is "<<pollard_p1(n,8);
+           return 0;	
 }
+
+/*
+Sample Input
+n = 57247159
+
+*/
