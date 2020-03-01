53a34e5356210a7342197b9e15cc9562c435829f
Chintamani Raju Virulkar <54358660+DawoodBhai420@users.noreply.github.com>
2020-03-01 22:02:12 +0530

    used 4 space indentation.

--- Palindrome/palindrome_no.cpp
+++ Palindrome/palindrome_no.cpp
@@ -2,25 +2,25 @@
 using namespace std;
 int main()
 {
-	int n,rem,rev=0;
-	cout<<"Enter a number: ";
-	cin>>n;
-	int temp=n;
-	while(temp!=0)
-	{
-		rem=temp%10;
-		temp=temp/10;
-		rev=(rev*10)+rem;
-	}
-	if(rev!=n)
-	{
-		cout<<"Entered number is not palindrome."<<endl;
-	}
-	else
-	{
-		cout<<"Entered number is palindrome."<<endl;
-	}
-	return 0;
+    int n,rem,rev=0;
+    cout<<"Enter a number: ";
+    cin>>n;
+    int temp=n;
+    while(temp!=0)
+    {
+        rem=temp%10;
+        temp=temp/10;
+        rev=(rev*10)+rem;
+    }
+    if(rev!=n)
+    {
+        cout<<"Entered number is not palindrome."<<endl;
+    }
+    else
+    {
+        cout<<"Entered number is palindrome."<<endl;
+    }
+    return 0;
 }
 
 
