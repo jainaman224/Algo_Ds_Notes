7d5e6413a4728a55c14be9b118a6c0d5e5002d91
Chintamani Raju Virulkar <54358660+DawoodBhai420@users.noreply.github.com>
2020-03-01 19:50:40 +0530

    Add files via upload

---
+++ Palindrome/palindrome_no.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,rem,rev=0;
+	cout<<"Enter a number: ";
+	cin>>n;
+	int temp=n;
+	while(temp!=0)
+	{
+		rem=temp%10;
+		temp=temp/10;
+		rev=(rev*10)+rem;
+	}
+	if(rev!=n)
+	{
+		cout<<"Entered number is not palindrome."<<endl;
+	}
+	else
+	{
+		cout<<"Entered number is palindrome."<<endl;
+	}
+	return 0;
+}
+
+
+
+/*
+  INPUT:
+	n=12456
+  OUTPUT:
+  	Entered no. is not palindrome.
+  
+  INPUT:
+  	n=12821
+  OUTPUT:
+  	Entered no. is palindrome.
+*/
