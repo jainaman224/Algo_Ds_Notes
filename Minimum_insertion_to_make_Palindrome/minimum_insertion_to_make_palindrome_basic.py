ac1803a8634257d3224567f4adfa59444f128276
Shiv1202 <shivansht9211@gmail.com>
2020-03-02 09:55:59 +0530

    Minimum Insertion to make string palindrome files added.

---
+++ Minimum_insertion_to_make_Palindrome/minimum_insertion_to_make_palindrome_basic.py
@@ -0,0 +1,39 @@
+# Python Program to find min Insertion in a string to make it Palindrome.
+""" -----------------------------------
+Example:
+ab : No. of insertion is required 1 (bab)
+abcd : No. of insertion is required 3 (dcbabcd)
+
+The Naive recursive program.
+--------------------------------------- """
+
+def find_min_insertion(str, l, h):
+    """
+        This Function is Going to return integer value 
+        which represent the minimum char required to insert 
+        in string to make palindrome.
+    """
+    # Base Case.
+    if l > h:
+        return -1
+    if l == h:
+        return 0
+    if l == h - 1:
+        return 0 if str[l] == str[h] else 1
+    # Check if the first and last char are same 
+    # on the basis of the comparison result, deside
+    # which subproblem(s) to call.
+    if str[l] == str[h]:
+        return find_min_insertion(str,l+1,h-1)
+    else:
+        return min(find_min_insertion(str,l,h-1), find_min_insertion(str,l+1,h)) + 1
+
+# Main Function.
+def main():
+    s = "abcd"
+    n = len(s) - 1
+    print(find_min_insertion(s, 0, n))
+
+# Driver Code.
+if __name__ == "__main__":
+    main()
---
+++ Minimum_insertion_to_make_Palindrome/minimum_insertion_to_make_palindrome_optimize.py
@@ -0,0 +1,49 @@
+# Python Program to find min Insertion in a string to make it Palindrome.
+""" -----------------------------------
+Example:
+ab : No. of insertion is required 1 (bab)
+abcd : No. of insertion is required 3 (dcbabcd)
+
+Dynamic Programming based Solution.
+Time Complexity : O(n^2)
+Space Complexity : O(n^2)
+--------------------------------------- """
+# Function for Findind Min.
+def Min(a, b):
+    """
+        This Function is return minimum value B/w a and b.
+    """
+    return min(a, b)
+
+
+
+# Function for Finding Min Insertion.
+def find_min_insertion_DP(s, n):
+    """
+    This Function return minimum insertion value for string to make palindrome.
+    """
+
+    # Create table of size n * n
+    t = [[0 for i in range(n)]for i in range(n)]
+    l, h, g = 0, 0, 0
+    # Fill the Table
+    for g in range(1, n):
+        l = 0
+        for h in range(g, n):
+            if s[l] == s[h]:
+                t[l][h] = t[l+1][h-1]
+            else:
+                t[l][h] = (Min(t[l][h-1], t[l+1][h]) + 1)
+            l += 1
+    return t[0][n - 1]
+
+    
+# Main Function.
+def main():
+    s = "abcd"
+    x = find_min_insertion_DP(s, len(s))
+    print(x)
+
+# Driver Code.
+if __name__ == "__main__":
+    main()
