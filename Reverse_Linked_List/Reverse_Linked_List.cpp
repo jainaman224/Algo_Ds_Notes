47c84eb4e70bb6177d72f0d2bd846956729d0e65
abhishekgupta368 <abhigupta9087@gmail.com>
2020-03-02 16:22:58 +0530

    a_star_algorithm

--- Reverse_Linked_List/Reverse_Linked_List.cpp
+++ Reverse_Linked_List/Reverse_Linked_List.cpp
@@ -91,3 +91,16 @@ int main(){
 	linkedList->print();
 }
 
+//Enter the number of node in linked list: 10
+//
+//Before Reverse
+//Linked List:
+//1 2 3 4 5 6 7 8 9 10
+//
+//After Reverse
+//Linked List:
+//10 9 8 7 6 5 4 3 2 1
+
+
+
+
