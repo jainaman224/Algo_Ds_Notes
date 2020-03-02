2f1ed8c56230b7044adec37998f3bb89beecbc2d
Jasleen Randhawa <54702050+JasleenRandhawa@users.noreply.github.com>
2020-03-02 18:59:24 +0530

    Update Array_Rotation.java
    This file consisted only of a leftRotate() method. I have added the rightRotate() method, so as to complete both types of rotation in the same file.

--- Array_Rotation/Array_Rotation.java
+++ Array_Rotation/Array_Rotation.java
@@ -11,7 +11,15 @@ class Array_Rotation
         arr[i] = temp; 
     } 
     
-    private static void Rotate(int arr[], int rotations, int n) 
+    private static void rightRotate(int arr[], int n)
+    {
+        int temp=arr[n-1], i;
+        //shifting all the elements of the array to right
+        for(i=n-1;i>0;i--)
+            arr[i]=arr[i-1];
+        arr[0] = temp;
+    }
+    private static void RotateDriverLeft(int arr[], int rotations, int n) 
     { 
         // call for rotation one by one
         for (int i = 0; i < rotations; i++) 
@@ -21,16 +29,29 @@ class Array_Rotation
         for (int i = 0; i < n; i++) 
             System.out.print(arr[i] + " ");    
     } 
+    private static void RotateDriverRight(int arr[], int rotations, int n) 
+    { 
+        // call for rotation one by one
+        for (int i = 0; i < rotations; i++) 
+            rightRotate(arr, n); 
+
+        // print the array
+        for (int i = 0; i < n; i++) 
+            System.out.print(arr[i] + " ");    
+    } 
 
     public static void main(String args[]) 
     { 
         // Sample input
-        int arr[] = {1, 2, 3, 4, 5}; 
-        int n = arr.length; 
-        Rotate(arr, 2, n);
+        int arr1[] = {1, 2, 3, 4, 5}; 
+        int arr2[]={1,2,3,4,5};
+        int n = arr1.length; 
+        int m=arr2.length;
+        RotateDriverLeft(arr1, 2, n);
+        System.out.println();
+        RotateDriverRight(arr2,2,m);
     } 
 }
-
 /*
 Input array: {1, 2, 3, 4, 5}
 Sample Output
