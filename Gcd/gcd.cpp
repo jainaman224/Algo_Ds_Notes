667d556dd7414326b5067f001c868604f895ff70
Adharsh M <52920216+AdharshM@users.noreply.github.com>
2020-03-02 23:13:17 +0530

    Created gcd.cpp

---
+++ Gcd/gcd.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+  int a,b;
+  cin>>a>>b;
+  cout<<__gcd(a,b)<<"\n";
+  return 0;
+}
