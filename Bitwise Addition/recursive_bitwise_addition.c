0f941d17d0dfb2b66b3cff7eaa0f4cc0d5c06c63
Chintamani Raju Virulkar <chintamani.virulkar@gmail.com>
2020-03-02 12:37:29 +0530

    bitwise addition in C

---
+++ Bitwise Addition/recursive_bitwise_addition.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int bitwiseAdditionRecursive(int a,int b)
+{
+    if(b==0)
+    {
+        return a;
+    }
+    else
+    {
+        int carry = a & b;
+        return bitwiseAdditionRecursive(a^b, carry<<1);
+    }
+}
+int main()
+{
+    int no1,no2,result;
+    printf("Enter two numbers: ");
+    scanf("%d%d",&no1,&no2);
+	result=bitwiseAdditionRecursive(no1,no2);
+	printf("\nby using recursive bitwise addition method,\n");
+    printf("\n%d + %d = %d",no1,no2,result);
+    return 0;
+}
+
+
+
+
+
+
+/*
+INPUT:
+    Enter two numbers: 12 30
+
+OUTPUT:
+    byusing recursive bitwise addition method,
+    12+30 = 42
+*/
