d56446a794dfdaa0aaf5abe6cfc1dd94ecf59f74
AkanshaKamboj <42874293+AkanshaKamboj@users.noreply.github.com>
2020-03-03 00:48:09 +0530

    Added Binary tree right view in C

---
+++ Binary_Tree_Right_View/BinaryTree_Right_View.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+
+
+/* Inserting elements into Binary Tree*/
+struct Node *create()
+{
+    Node *ptr;
+    int x;
+    printf("Enter data(-1 for no node):");
+    scanf("%d",&x);
+
+    if(x==-1)
+    return NULL;
+
+    ptr = (struct Node*)malloc(sizeof(struct Node));
+    ptr->data = x;
+    printf("Enter left child of %d:\n",x);
+    ptr->left = create();
+    printf("Enter right child of %d:\n",x);
+    ptr->right = create();
+    return ptr;
+}
+
+/*Recursive solution for priniting right view of Binary Tree*/
+void findRightView(struct Node *root, int *ans, int *size, int index)
+{
+    if(root==NULL)
+        return;
+    
+    index++;
+
+    if(index > *size)
+        ans[(*size)++] = root->data;
+    
+    findRightView(root->right, ans, size, index);
+    findRightView(root->left, and, size, index);
+}
+
+int *rightView(struct Node *root, int *size)
+{
+    int *ans = malloc(sizeof(int)*100);
+    *size=0;
+    findRightView(root,ans,size,0);
+
+    return ans;
+}
+
+/* Sample Input
+Enter data(-1 for no node: 1
+Enter left child of 1:
+Enter data(-1 for no node: 2
+Enter left child of 2:
+Enter data(-1 for no node: 6
+Enter left child of 6:
+Enter data(-1 for no node: -1
+Enter right child of 6:
+Enter data(-1 for no node: -1
+Enter right child of 2:
+Enter data(-1 for no node: -1
+Enter right child of 1:
+Enter data(-1 for no node: 3
+Enter left child of 3:
+Enter data(-1 for no node: -1
+Enter right child of 3:
+Enter data(-1 for no node: -1
+
+            1
+           / \
+          2   3
+         /      
+        6
+
+Output :- 1 3 6
+*/
+
+int main()
+{
+    struct Node *root = create();
+
+    int size = 0;
+    int *res = rightView(root, &size);
+
+    for(int i=0; i<size; i++)
+        printf("%d", res[i]);
+}
