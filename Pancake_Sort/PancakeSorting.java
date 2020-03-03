6d38118f6e65a6dc8edd6c9428cde05096e284db
parjanyaacoder <46294122+parjanyaacoder@users.noreply.github.com>
2020-03-03 23:39:02 +0530

    Update and rename PancakeSorting.java to Pancake_Sort/PancakeSorting.java

--- Pancake_Sort/PancakeSorting.java
+++ Pancake_Sort/PancakeSorting.java
@@ -1,3 +1,4 @@
+// Input n=5 array={5,4,3,2,1}
 import java.util.*;
 class Main
 {   
@@ -7,23 +8,28 @@ class Main
         for(int i=0;i<=n;i++)
         {
             temp[i]=a[n-i];
-            }
-            for(int i=0;i<=n;i++)
-            a[i]=temp[i];
         }
-       static int findmax(int a[],int n) //Function to find index of max element upto index n
-        {
-            int max=a[0];int j=0; //j stores the index of max element
-            for(int i=1;i<=n;i++)
-              if(a[i]>max)
-              {max=a[i];
-                  j=i;
-                }
-                return j;
+        for(int i=0;i<=n;i++)
+        a[i]=temp[i];
+     }
+     static int findmax(int a[],int n) //Function to find index of max element upto index n
+     {
+         int max=a[0];int j=0; //j stores the index of max element
+         for(int i=1;i<=n;i++)
+         {  
+             if(a[i]>max)
+             {
+                 max=a[i];
+                 j=i;
+             }
+         }
+              
+         return j;
                 
-            }
+     }
     static void pancakesort(int a[],int n)
-    {  int currentLength;
+    {  
+        int currentLength;
         for(currentLength=n;currentLength>0;--currentLength)
         {
             int j= findmax(a,currentLength-1);
@@ -32,8 +38,8 @@ class Main
                 reverseArray(a,j);
                 reverseArray(a,currentLength-1);
             }
-            }
         }
+     }
     public static void main(String[] args)
     {
         Scanner sc=new Scanner(System.in);
@@ -48,3 +54,4 @@ class Main
         System.out.println();
     }
 }
+// Output 1 2 3 4 5 
