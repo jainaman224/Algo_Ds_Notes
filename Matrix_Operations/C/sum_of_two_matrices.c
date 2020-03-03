4e36b1afdb04385fe91fee017f37bc79842aaf47
Anushka-Rajvanshi <60550664+Anushka-Rajvanshi@users.noreply.github.com>
2020-03-03 19:36:49 +0530

    Added sum_of_two_matrices.c
    Added code to find sum of two matrices in C.

---
+++ Matrix_Operations/C/sum_of_two_matrices.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+void main()
+{ 
+ int A[100][100],B[100][100],C[100][100],m,n,i,j;
+ printf("Enter order of matrices:");
+ scanf("%d %d",&m,&n);
+ //take input for both matrices.
+ printf("ENTER elements of A:");
+ for(i=0;i<m;i++)
+ {
+  for(j=0;j<n;j++)
+  {
+   scanf("%d",&A[i][j]);
+  }
+ }
+ printf("ENTER elements of B:");
+ for(i=0;i<m;i++)
+ {
+  for(j=0;j<n;j++)
+  {
+   scanf("%d",&B[i][j]);
+  }
+ }
+ //add corresponding terms of both matrices and intialise to the third matrix.
+ for(i=0;i<m;i++)
+ {
+  for(j=0;j<n;j++)
+  {  
+   C[i][j]=A[i][j]+B[i][j];
+  }
+ }
+ //print the resultant matrix.
+ printf("Resultant Matrix is \n");
+ for(i=0;i<m;i++)
+ {
+  for(j=0;j<n;j++)
+  {
+   printf("%d \t",C[i][j]);
+  }
+  printf("\n"); 
+ }
+}
