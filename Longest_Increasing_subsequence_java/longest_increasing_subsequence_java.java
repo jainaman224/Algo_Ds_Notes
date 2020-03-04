74ff82690a8dc713f90fa6bbee76c58ed6bb6a85
BhavishayGulati <54773402+BhavishayGulati@users.noreply.github.com>
2020-03-04 16:14:33 +0530

    added comments in the code

--- Longest_Increasing_subsequence_java/longest_increasing_subsequence_java.java
+++ Longest_Increasing_subsequence_java/longest_increasing_subsequence_java.java
@@ -1,12 +1,13 @@
 package com.company;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     static int lengthOfLIS(int[] a) {
+        // checking if the size of the array is equal to 0 if it is then returning 0
         if (a.length == 0) {
             return 0;
         }
@@ -17,22 +18,22 @@ public class Main {
         for (int i = 1; i < a.length; i++) {
             for (int j = 0; j < i; j++) {
                 if (a[i] > a[j]) {
-                    cache[i] = Math.max(cache[i], cache[j] + 1);
+                    cache[i] = Math.max(cache[i], cache[j] + 1); // checking for the longest increasing subsequence
                 }
             }
         }
 
-        return Arrays.stream(cache).max().getAsInt();
+        return Arrays.stream(cache).max().getAsInt();       // returning the size of the longest increasing subsequence
     }
 
 
     public static void main(String[] args) {
-//        int[] arr = {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
-
         Scanner s = new Scanner(System.in);
+        // input the size of the array
         System.out.println("Enter the size of the list");
         int n= s.nextInt();
         int[] arr = new int[n];
+        // input the array elements
         System.out.println("Enter the elements of the array");
         for (int i = 0; i <n ; i++) {
             arr[i]=s.nextInt();
@@ -40,4 +41,8 @@ public class Main {
         System.out.println(lengthOfLIS(arr));
     }
 }
-
+/*
+    Example:
+    input -      int[] arr = {0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15};
+    output - 6
+*/
