8d3b38f1a8e2cd9fb2d66870497d2db87ebdd0dc
Sonali Patel <sonalipatel23600@gmail.com>
2020-03-04 11:33:04 +0530

    indentation modified

--- Boyer_Moore_Algorithm/Boyer_Moore.java
+++ Boyer_Moore_Algorithm/Boyer_Moore.java
@@ -5,87 +5,72 @@ import java.util.*;
 class Main{ 
 	
     static int NO_OF_CHARACTERS = 256; 
-	
-	static int max_of_a_and_b (int a, int b) { return (a > b)? a: b; } 
+    static int max_of_a_and_b (int a, int b){ 
+        return (a > b)? a: b; 
+    } 
+    
+    static void badCharacterHeuristic( char []str, int size,int badcharacter[]){ 
+        int i; 
 
-	static void badCharHeuristic( char []str, int size,int badcharacter[]) { 
-	int i; 
+        // Initialize all elements of badcharacter as -1 
+        for (i = 0; i < NO_OF_CHARACTERS; i++){ 
+            badcharacter[i] = -1; 
+        }
+        // Fill the actual value of last occurrence of a character 
+        for (i = 0; i < size; i++){ 
+            badcharacter[(int) str[i]] = i;
+        }   
+    } 
 
-	// Initialize all elements of badcharacter as -1 
-	for (i = 0; i < NO_OF_CHARACTERS; i++) 
-		badcharacter[i] = -1; 
+    // A pattern searching function 
+    static void search( char text[], char pattern[]){ 
+        int m = pattern.length; 
+        int n = text.length; 
 
-	// Fill the actual value of last occurrence 
-	// of a character 
-	for (i = 0; i < size; i++) 
-		badcharacter[(int) str[i]] = i; 
-	} 
+        int badCharacterHeuristic[] = new int[NO_OF_CHARACTERS]; 
 
-	// A pattern searching function 
-	static void search( char text[], char pattern[]) 
-	{ 
-	int m = pattern.length; 
-	int n = text.length; 
+        /* Fill the bad character array by calling the preprocessing function badCharHeuristic() for given pattern */
+        badCharacterHeuristic(pattern, m, badcharacter); 
 
-	int badcharacter[] = new int[NO_OF_CHARACTERS]; 
+        int s = 0; // s is shift of the pattern with respect to text 
 
-	/* Fill the bad character array by calling 
-		the preprocessing function badCharHeuristic() 
-		for given pattern */
-	badCharHeuristic(pattern, m, badcharacter); 
+        while(s <= (n - m)){ 
+            int j = m-1; 
 
-	int s = 0; // s is shift of the pattern with 
-				// respect to text 
-	while(s <= (n - m)) 
-	{ 
-		int j = m-1; 
+            /* Keep reducing index j of pattern while characters of pattern and text are matching at this shift s */
+            while(j >= 0 && pattern[j] == text[s+j]) 
+                j--; 
 
-		/* Keep reducing index j of pattern while 
-			characters of pattern and text are 
-			matching at this shift s */
-		while(j >= 0 && pattern[j] == text[s+j]) 
-			j--; 
+            /* If the pattern is present at current shift, then index j will become -1 after the above loop */
+            if (j < 0){ 
+                System.out.println("Patterns occur at shift = " + s); 
 
-		/* If the pattern is present at current 
-			shift, then index j will become -1 after 
-			the above loop */
-		if (j < 0) 
-		{ 
-			System.out.println("Patterns occur at shift = " + s); 
+                // Shift the pattern so that the next character in text aligns with the last occurrence of it in pattern. 
+                // The condition s+m < n is necessary for the case when pattern occurs at the end of text 
+                s += (s+m < n)? m-badcharacter[text[s+m]] : 1; 
 
-			/* Shift the pattern so that the next 
-				character in text aligns with the last 
-				occurrence of it in pattern. 
-				The condition s+m < n is necessary for 
-				the case when pattern occurs at the end 
-				of text */
-			s += (s+m < n)? m-badcharacter[text[s+m]] : 1; 
+            } else
+                s += max_of_a_and_b(1, j - badcharacter[text[s+j]]); 
+        } 
+    } 
 
-		} 
-
-		else
-			s += max_of_a_and_b(1, j - badcharacter[text[s+j]]); 
-	} 
-	} 
-
-	/* Driver program*/
-	public static void main(String []args) { 
+    /* Driver program*/
+    public static void main(String []args) { 
 
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-		char text[] = s.toCharArray();
-		s = sc.nextLine();
+        char text[] = s.toCharArray();
+        s = sc.nextLine();
         char pattern[] = s.toCharArray();
-		search(text, pattern); 
-	} 
+        search(text, pattern); 
+    } 
 } 
 
 /* Samle input and output
- Input: AABAACAADAABAABA
-		AABA
- Output:
- pattern occurs at shift = 0
- pattern occurs at shift = 9
- pattern occurs at shift = 12 */
+Input: AABAACAADAABAABA AABA
+Output:
+pattern occurs at shift = 0
+pattern occurs at shift = 9
+pattern occurs at shift = 12 */
 
 
