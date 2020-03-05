bc2caaca9c744d4b752e59ba8f2358a78a0a4be4
biekymahato <60212331+biekymahato@users.noreply.github.com>
2020-03-06 01:07:39 +0530

    changes request-2 #1985

--- Optimal_Strategy_For_A_Game (Set-1)/optimal_strategy_for_a_game_set_1.cpp
+++ Optimal_Strategy_For_A_Game (Set-1)/optimal_strategy_for_a_game_set_1.cpp
@@ -7,51 +7,55 @@ removes it from the row permanently and receives the value of the coin.
 Determine the maximum possible amount of money we can definitely win if we move first.
 Note - Note: The opponent is as clever as the user.
 */
-
 // C++ program to find out maximum value from a given sequence of coins
+
 #include <bits/stdc++.h>
 using namespace std;
 
 /*Returns optimal value possible that a player can
 collect from an array of coins of size n. Note
 than n must be even*/
-
 int optimalStrategyOfGame(int* arr, int n)
 {
-// Create a table to store solutions of subproblems
+    // Create a table to store solutions of subproblems
     int table[n][n];
-/*Fill table using above recursive formula. Note
-that the table is filled in diagonal fashion from diagonal elements to table[0][n-1] which is the result.*/
-    for (int gap = 0; gap < n; ++gap)
-       {
-         for (int i = 0, j = gap; j < n; ++i, ++j)
-           {
-/*Here x is value of F(i+2, j), y is F(i+1, j-1) and
-z is F(i, j-2) in above recursive formula*/
-               int x = ((i + 2) <= j) ? table[i + 2][j] : 0;
-               int y = ((i + 1) <= (j - 1)) ? table[i + 1][j - 1] : 0;
-               int z = (i <= (j - 2)) ? table[i][j - 2] : 0;
-               table[i][j] = max(arr[i] + min(x, y), arr[j] + min(y, z));
-            }
+
+    /*Fill table using above recursive formula.
+    Note that the table is filled in diagonal fashion from
+    diagonal elements to table[0][n-1] which is the result.*/
+      for (int gap = 0; gap < n; ++gap){
+         for (int i = 0, j = gap; j < n; ++i, ++j){
+
+    /*Here x is value of F(i+2, j), y is F(i+1, j-1) and
+    z is F(i, j-2) in above recursive formula*/
+             int x = ((i + 2) <= j) ? table[i + 2][j] : 0;
+             int y = ((i + 1) <= (j - 1)) ? table[i + 1][j - 1] : 0;
+             int z = (i <= (j - 2)) ? table[i][j - 2] : 0;
+             table[i][j] = max(arr[i] + min(x, y), arr[j] + min(y, z));
          }
-return table[0][n - 1];
+      }
+
+      return table[0][n - 1];
 }
 
 // Driver Code to test above function
 int main()
 {
      int arr1[1005], n;
-     //Number of values in array you want to store
+
+    //Number of values in array you want to store
      cin >> n;
-     //Input taken in array
+
+    //Input taken in array
      for(int i = 0 ; i < n ; i ++ )
-             cin >> arr1[i];
+          cin >> arr1[i];
 
     //function call for calculating it's optimal strategy
     printf("%d\n", optimalStrategyOfGame(arr1, n));
 
 return 0;
 }
+
 /*
 Input for test case 1:
 4     // n = 4
