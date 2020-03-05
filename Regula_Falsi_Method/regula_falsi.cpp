6d594a0a4ea6064a367378f0721b1e63b3c7a6c2
Nikhil patro <nikhilpatro03@gmail.com>
2020-03-05 19:47:42 +0530

    added C++ implementation of regula falsi method

--- Regula_Falsi_Method/regula_falsi.cpp
+++ Regula_Falsi_Method/regula_falsi.cpp
@@ -7,36 +7,53 @@ In simple terms, the method is the trial and error technique of using test ("fal
 then adjusting the test value according to the outcome. This is sometimes also referred to as "guess and check".
 Versions of the method predate the advent of algebra and the use of equations.*/
 
-#include<stdio.h>
-#include<math.h>
 
-float f(float x) {
-    return ((x * x * x) - (2 * x) - 5);
+#include<iostream>
+#include<cmath>
+#define error 0.001
+using namespace std;
+
+float f(float x)    // f(x) = x^3 -2*x -5
+{
+    return ( (x*x*x) - 3*x - 5);
 }
-
-int main() {
-    float x1, x2, x3, f1, f2, f3;
-    int i;
-    printf("Enter x1 and x2:");
-    scanf("%f %f", &x1, &x2);
-    do {
-        f1 = f(x1);
+int main()
+{
+    float x1,x2,x3,f1,f2,f3=INT_MAX;
+    int i=1;
+    cout<<"Enter x1 and x2 : "<<endl;
+    cin>>x1>>x2;
+    while(abs(f3) > error)
+    {   f1 = f(x1);
         f2 = f(x2);
-        x3 = x1 - (x2 - x1) / (f2 - f1) * f1;
-        f3 = f(x3);
-        if (f1 * f3 > 0) {
+        x3 =x1 - (x2-x1) / (f2-f1) * f1 ;
+        f3=f(x3);
+        if (f1 * f3 > 0)
+        {
             x2 = x3;
         }
-        else {
+        else 
+        {
             x1 = x3;
         }
+        cout<<"\nThe no. of iteration : "<<i<<" root is : "<<x3<<endl;
         i++;
-        printf("Number of iteration %d\t root is %f\n", i, x3);
-    } while (fabs(f3) > 0.001);
+    }
+    return 0;
 }
 
-//Enter x1 and x2:2
-//3
-//Number of iteration 21985	 root is 2.058824
-//Number of iteration 21986	 root is 2.096559
-//Number of iteration 21987	 root is 2.094511
+
+
+/*
+ OUTPUT==================================================================================================================
+Enter x1 and x2 :
+2 3
+
+The no. of iteration : 1 root is : 2.1875
+
+The no. of iteration : 2 root is : 2.29527
+
+The no. of iteration : 3 root is : 2.27819
+
+The no. of iteration : 4 root is : 2.27906
+*/
