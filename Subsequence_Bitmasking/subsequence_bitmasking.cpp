d4752fc36f437dd8dcfe421b929e52657bdd6be3
unknown <ashishnagpal2498@gmail.com>
2020-03-06 00:59:35 +0530

    Spaces and input output added in file

--- Subsequence_Bitmasking/subsequence_bitmasking.cpp
+++ Subsequence_Bitmasking/subsequence_bitmasking.cpp
@@ -4,33 +4,45 @@
 using namespace std;
 void subsequence_bitmasking(string s)
 {
-    //Calculate length of string 
+    //Calculate length of string
     int len = s.length();
 
     //Number of subsequence are always 1>>length
-    
-    for(int i=0;i<(1<<len);i++)
+    int no_of_subseq = 1 << len;
+    for (int i = 0 ; i < no_of_subseq ; i++)
     {
-        int m=0;
-        string ans="";
-        int j=i;
-        while(j)
+        int m = 0;
+        string ans = "";
+        int j = i;
+        while (j)
         {
-            if(j&1)
+            if (j & 1)
             {
-                ans+=s[m];
+                ans += s[m];
             }
             m++;
-            j = j>>1;
+            j = j >> 1;
         }
-        cout<<ans<<" ";
+        cout << ans << " ";
     }
 }
 int main()
 {
     string str;
-    cout<<"Enter string"<<endl;
-    cin>>str;
+    cout << "Enter string" << endl;
+    cin >> str;
     subsequence_bitmasking(str);
+    cout<<endl;
     return 0;
 }
+
+/*
+Sample Input
+
+Enter string
+abc
+
+Sample Output
+
+ a b ab c ac bc abc
+*/
