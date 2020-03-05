08626e064c542c37f1536841d5ae55aef76a4ece
Ritish Singh <54978105+ritish099@users.noreply.github.com>
2020-03-05 21:50:23 +0530

    Update Factorial.cpp

--- Factorial/Factorial.cpp
+++ Factorial/Factorial.cpp
@@ -1,33 +1,37 @@
 #include <iostream>
 using namespace std;
-main()
+main() 
 {
-    int t,n,a[200000],i,j,k,l,m;
-    cout<<"ENTER THE NUMBER\n";
-	cin>>n;
-        m=1;
-        a[0]=1;
-        for(j=2;j<=n;j++)
-        {
-            l=0;
-            for(k=0;k<m;k++)
-            {
-                a[k]=a[k]*j+l;
-                l=a[k]/10;
-                a[k]=a[k]%10;
-            }
-            while(l)
-            {
-	            a[k]=l%10;
-                k++;
-                m++;
-                l=l/10;
-            }
-        }
-        printf("Answer is : ");
-        for(i=m-1;i>=0;i--)
-            cout<<a[i];
-    return 0;
+  int t, n, a[200000], i, j, k, l, m;
+  cout << "ENTER THE NUMBER\n";
+  cin >> n;
+  m = 1;
+  a[0] = 1;
+
+  // Code to find the factorial
+  for (j = 2; j <= n; j++) 
+    {
+    l = 0;
+    for (k = 0; k < m; k++) {
+      a[k] = a[k] * j + l;
+      l = a[k] / 10;
+      a[k] = a[k] % 10;
+    }
+
+    // Code to break a number having more than one digit into array of single
+    // digits
+    while (l) 
+    {
+      a[k] = l % 10;
+      k++;
+      m++;
+      l = l / 10;
+    }
+  }
+  printf("Answer is : ");
+  for (i = m - 1; i >= 0; i--)
+    cout << a[i];
+  return 0;
 }
 /*ENTER THE NUMBER
 5
