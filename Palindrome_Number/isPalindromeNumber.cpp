1a6d4d99e8e74dc12c8cbf3b41a36f9041f50f2c
Jalaj <jalajdorai100@gmail.com>
2020-03-05 22:46:19 +0530

    Palindrome Number Program

---
+++ Palindrome_Number/isPalindromeNumber.cpp
@@ -0,0 +1,36 @@
+/*Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+
+Example 1:
+
+Input: 121
+Output: true
+Example 2:
+
+Input: -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.*/
+
+ bool isPalindrome(int x) {
+        
+        int y=x;
+        if(x<0)
+            return false;
+
+        
+        long int d,rev=0;
+        
+        
+        
+        while(y>0)
+        {
+            d=y%10;
+            rev=rev*10 + d;
+            y=y/10;
+        }
+        
+        if(rev==x)
+        return true;
+        else
+            return false;
+        
+    }
