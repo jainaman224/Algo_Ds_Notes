e13051df3fb480cb52ba46dd6e58d91d159c3a94
aryangulati <42711978+aryangulati@users.noreply.github.com>
2020-03-06 18:40:27 +0530

    Create Bubble sort in Rust

---
+++ Bubble_Sort/Bubble_Sort.rs
@@ -0,0 +1,17 @@
+fn bubble_sort(numbers: &Vec<i64>, compare_fn: |i64, i64| -> i64) -> Vec<i64> {
+    let mut temp;
+    let mut target = numbers.clone();
+    let length = numbers.len();
+
+    for _ in range(0, length) {
+        for j in range(0, length - 1) {
+            if compare_fn(target[j], target[j+1]) > 0  {
+                temp = target[j+1];
+                target[j+1] = target[j];
+                target[j] = temp;
+            }
+        }
+    }
+
+    target
+}
