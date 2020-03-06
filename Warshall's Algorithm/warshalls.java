d056d8aa06d8b443f3619d6478b8cbf8d26c8121
Rohit Singh Rathaur <42641738+TeAmP0is0N@users.noreply.github.com>
2020-03-06 16:22:04 +0530

    Update warshalls.java

--- Warshall's Algorithm/warshalls.java
+++ Warshall's Algorithm/warshalls.java
@@ -1,66 +1,90 @@
 // A Java program for Floyd Warshall All Pairs Shortest 
 // Path algorithm. 
-import java.util.*; 
-import java.lang.*; 
-import java.io.*; 
-class AllPairShortestPath 
-{ 
-	final static int INF = 99999, V = 4; 
-  void floydWarshall(int graph[][]) 
-	{ 
-		int dist[][] = new int[V][V]; 
-		int i, j, k; 
-		for (i = 0; i < V; i++) 
-			for (j = 0; j < V; j++) 
-				dist[i][j] = graph[i][j]; 
-		for (k = 0; k < V; k++) 
-		{ 
-			// Pick all vertices as source one by one 
-			for (i = 0; i < V; i++) 
-			{ 
-				// Pick all vertices as destination for the 
-				// above picked source 
-				for (j = 0; j < V; j++) 
-				{ 
-					// If vertex k is on the shortest path from 
-					// i to j, then update the value of dist[i][j] 
-					if (dist[i][k] + dist[k][j] < dist[i][j]) 
-						dist[i][j] = dist[i][k] + dist[k][j]; 
-				} 
-			} 
-		} 
-	// Print the shortest distance matrix 
-		printSolution(dist); 
-	} 
-void printSolution(int dist[][]) 
-	{ 
-		System.out.println("The following matrix shows the shortest "+ 
-						"distances between every pair of vertices"); 
-		for (int i=0; i<V; ++i) 
-		{ 
-			for (int j=0; j<V; ++j) 
-			{ 
-				if (dist[i][j]==INF) 
-					System.out.print("INF "); 
-				else
-					System.out.print(dist[i][j]+" "); 
-			} 
-			System.out.println(); 
-		} 
-	} 
-	public static void main (String[] args) 
-	{ 
-		
-		int graph[][] = { {0, 5, INF, 10}, 
-						{INF, 0, 3, INF}, 
-						{INF, INF, 0, 1}, 
-						{INF, INF, INF, 0} 
-						}; 
-		AllPairShortestPath a = new AllPairShortestPath(); 
-
-		// Print the solution 
-		a.floydWarshall(graph); 
-	} 
-} 
-
+import java.util.Scanner;
+ 
+public class FloydWarshall
+{
+    private int distancematrix[][];
+    private int numberofvertices;
+    public static final int INFINITY = 999;
+ 
+    public FloydWarshall(int numberofvertices)
+    {
+        distancematrix = new int[numberofvertices + 1][numberofvertices + 1];
+        this.numberofvertices = numberofvertices;
+    }
+ 
+    public void floydwarshall(int adjacencymatrix[][])
+    {
+        for (int source = 1; source <= numberofvertices; source++)
+        {
+            for (int destination = 1; destination <= numberofvertices; destination++)
+            {
+                distancematrix[source][destination] = adjacencymatrix[source][destination];
+            }
+        }
+ 
+        for (int intermediate = 1; intermediate <= numberofvertices; intermediate++)
+        {
+            for (int source = 1; source <= numberofvertices; source++)
+            {
+                for (int destination = 1; destination <= numberofvertices; destination++)
+                {
+                    if (distancematrix[source][intermediate] + distancematrix[intermediate][destination]
+                         < distancematrix[source][destination])
+                        distancematrix[source][destination] = distancematrix[source][intermediate] 
+                            + distancematrix[intermediate][destination];
+                }
+            }
+        }
+ 
+        for (int source = 1; source <= numberofvertices; source++)
+            System.out.print("\t" + source);
+ 
+        System.out.println();
+        for (int source = 1; source <= numberofvertices; source++)
+        {
+            System.out.print(source + "\t");
+            for (int destination = 1; destination <= numberofvertices; destination++)
+            {
+                System.out.print(distancematrix[source][destination] + "\t");
+            }
+            System.out.println();
+        }
+    }
+ 
+    public static void main(String... arg)
+    {
+        int adjacency_matrix[][];
+        int numberofvertices;
+ 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the number of vertices");
+        numberofvertices = scan.nextInt();
+ 
+        adjacency_matrix = new int[numberofvertices + 1][numberofvertices + 1];
+        System.out.println("Enter the Weighted Matrix for the graph");
+        for (int source = 1; source <= numberofvertices; source++)
+        {
+            for (int destination = 1; destination <= numberofvertices; destination++)
+            {
+                adjacency_matrix[source][destination] = scan.nextInt();
+                if (source == destination)
+                {
+                    adjacency_matrix[source][destination] = 0;
+                    continue;
+                }
+                if (adjacency_matrix[source][destination] == 0)
+                {
+                    adjacency_matrix[source][destination] = INFINITY;
+                }
+            }
+        }
+ 
+        System.out.println("The Transitive Closure of the Graph");
+        FloydWarshall floydwarshall = new FloydWarshall(numberofvertices);
+        floydwarshall.floydwarshall(adjacency_matrix);
+        scan.close();
+    }
+}
 
