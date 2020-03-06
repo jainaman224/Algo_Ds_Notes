5df51ad2ddad74d94dba800b5d20b77fc370d32c
aryangulati <42711978+aryangulati@users.noreply.github.com>
2020-03-06 09:13:55 +0530

    Update Prims_Algorithm.js

--- Prims_Algorithm/Prims_Algorithm.js
+++ Prims_Algorithm/Prims_Algorithm.js
@@ -52,3 +52,34 @@ primsMST() {
    return MST;
 }
 
+/* Sample INPUT:
+let g = new Graph();
+g.addNode("A");
+g.addNode("B");
+g.addNode("C");
+g.addNode("D");
+g.addNode("E");
+g.addNode("F");
+g.addNode("G");
+
+
+g.addEdge("A", "C", 100);
+g.addEdge("A", "B", 3);
+g.addEdge("A", "D", 4);
+g.addEdge("C", "D", 3);
+g.addEdge("D", "E", 8);
+g.addEdge("E", "F", 10);
+g.addEdge("B", "G", 9);
+g.primsMST().display();
+*/
+
+/* Sample Output:
+A->B, D
+B->A, G
+D->A, C, E
+C->D
+E->D, F
+G->B
+F->E
+*/
+
