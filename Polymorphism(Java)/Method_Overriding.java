0c2ff8d2f0844f3c039880ced76ead76f284e648
Devkant Gorai <51082429+CSElonewolf@users.noreply.github.com>
2020-03-07 12:31:49 +0530

    updated the indentation

--- Polymorphism(Java)/Method_Overriding.java
+++ Polymorphism(Java)/Method_Overriding.java
@@ -1,55 +1,65 @@
 //Super-class or Parent Class
-public class MainRepo {
+public class MainRepo 
+{
    private String name;
    private String address;
    private int number;
 
-   public MainRepo(String name, String address, int number) {
+   public MainRepo(String name, String address, int number) 
+   {
       System.out.println("Constructing an Employee");
       this.name = name;
       this.address = address;
       this.number = number;
    }
 
-   public void mailCheck() {
+   public void mailCheck() 
+   {
       System.out.println("Mailing a check to " + this.name + " " + this.address);
    }
    
-   public String getName() {
+   public String getName()
+   {
       return name;
    }
 
 }
 
 //Sub-class or Child Class
-public class ForkedRepo extends MainRepo {
+public class ForkedRepo extends MainRepo 
+{
    private double salary; // Annual salary
    
-   public ForkedRepo(String name, String address, int number, double salary) {
+   public ForkedRepo(String name, String address, int number, double salary) 
+   {
       super(name, address, number);
       setSalary(salary);
    }
    
-   public void mailCheck() {
+   public void mailCheck() 
+   {
       System.out.println("Within mailCheck of Salary class ");
       System.out.println("Mailing check to " + getName()
       + " with salary " + salary);
    }
    
-   public double getSalary() {
+   public double getSalary() 
+   {
       return salary;
    }
    
-   public void setSalary(double newSalary) {
+   public void setSalary(double newSalary) 
+     {
       if(newSalary >= 0.0) {
          salary = newSalary;
       }
-   }
 }
 //Driver Class
-public class MethodOverriding {
+public class MethodOverriding 
+{
 
-   public static void main(String [] args) {
+   public static void main(String [] args)
+   {
       ForkedRepo s = new Forkedrepo("Devkant Gorai", "Birbhum, WB", 3, 36000.00);
       MainRepo e = new Forkedrepo("John Adams", "Boston, MA", 2, 24000.00);
       System.out.println("Call mailCheck using Salary reference --");   
