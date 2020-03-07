6262f12fef50ff7cc958c1c6615118fddd45ea54
Devkant Gorai <51082429+CSElonewolf@users.noreply.github.com>
2020-03-07 12:29:55 +0530

    updated the indentation

--- Polymorphism(Java)/Method_Overloading.java
+++ Polymorphism(Java)/Method_Overloading.java
@@ -1,24 +1,28 @@
-class MethodOverloading {
-
-    // this method accepts int
-    private static void display(int a){
+class MethodOverloading 
+{
+       // this method accepts int
+    private static void display(int a)
+    {
         System.out.println("Got Integer data.");
     }
-
     // this method accepts String object
-    private static void display(String a){
+    private static void display(String a)
+    {
         System.out.println("Got String object.");
     }
     //this method accepts float value
-     private static void display(float a){
+     private static void display(float a)
+    {
         System.out.println("Got Float data.");
     }
     //this method accepts three integer value
-     private static void display(int a,int b,int c){
+     private static void display(int a,int b,int c)
+    {
         System.out.println("Got Three Integer data.");
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         display(1);
         display("Hello");
         display(2.0);
