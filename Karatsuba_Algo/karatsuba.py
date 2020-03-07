13c1cbc92e22f18f1ba0ee582b6cbb2d2bbc0b11
lakshyajit165 <lakshyajit165@gmail.com>
2020-03-07 11:08:28 +0530

    modified files for karatsuba multiplication

--- Karatsuba_Algo/karatsuba.py
+++ Karatsuba_Algo/karatsuba.py
@@ -2,14 +2,13 @@
 
 from math import ceil, floor
 
-
 def karatsuba(x,y):
-    #base case
+    # base case
     if x < 10 and y < 10: # in other words, if x and y are single digits
         return x*y
 
     n = max(len(str(x)), len(str(y)))
-    m = ceil(n/2)   #Cast n into a float because n might lie outside the representable range of integers.
+    m = ceil(n/2)   # Cast n into a float because n might lie outside the representable range of integers.
 
     a = floor(x / 10**m)
     b = x % (10**m)
@@ -24,8 +23,8 @@ def karatsuba(x,y):
 
     return int(ac*(10**(m*2)) + ad_plus_bc*(10**m) + bd)
 
+# INPUT 123456789, 987654321
+a,b = map(int, input().split(' '))
 
-#a,b = map(int, input().split(' '))
-
-# below line should print 121932631112635269 as output
-print(karatsuba(123456789, 987654321))
+# OUTPUT 121932631112635269 
+print(karatsuba(a, b))
