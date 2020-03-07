2d55a2f8f5e4dadc046bf5daff8d5bf594c5786e
harshlohia11 <harsh.lohia11@gmail.com>
2020-03-07 15:43:18 +0530

    My spiral program done and commited

---
+++ Spiral_Array/spiral_square_c++.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a[10][10],i,j,k=1,t;
+	int r1,r2,c2,c1,n;
+	cout<<"\nEnter the limit ";
+	cin>>n;
+	t=n*n;
+	r1=0;r2=n-1,c1=0,c2=n-1;
+	while(k<=t)
+	{
+		for(j=c1;j<=c2;j++)
+			a[r1][j]=k++;
+		  
+		for(i=r1+1;i<=r2;i++)
+			a[i][c2]=k++;
+		 
+		for(j=c2-1;j>=c1;j--)
+			a[r2][j]=k++; 
+		 
+		for(i=r2-1;i>r1;i--)
+			a[i][c1]=k++;
+		 
+		r1++;
+		r2--;
+		c1++;
+		c2--;
+	}
+	
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+			cout<<a[i][j]<<"\t";
+			cout<<"\n";	
+	}
+}
