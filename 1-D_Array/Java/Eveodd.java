44f7717c1433ee06f541450e6b16da0e9e16d2a2
Anushka-Rajvanshi <60550664+Anushka-Rajvanshi@users.noreply.github.com>
2020-03-07 17:00:39 +0530

    Update Eveodd.java

--- 1-D_Array/Java/Eveodd.java
+++ 1-D_Array/Java/Eveodd.java
@@ -1,38 +1,43 @@
-import java.util.*;
+import java.util.* ;
 class Eveodd
 {
- public static void main(String args[])
- {
-  int o=0,e=0,i,n;
-  Scanner sc=new Scanner(System.in);
-  System.out.print("Enter number of elemnts in array:");
-  n=sc.nextInt();
-  int arr[]= new int[n];
-  System.out.print("Enter elements:");
-  for(i=0;i<n;i++)
-   {
-    arr[i]=sc.nextInt();
-   }
-  System.out.println("Even numbers of the array are:");
-  for(i=0;i<n;i++)
-   {
-    if((arr[i]%2)==0)
-     {
-      e++;
-      System.out.println(arr[i]+" ");
-     }
+    public static void main(String args[])
+    {
+        int o = 0,e = 0,i ,n;
+        Scanner sc = new Scanner(System.in);
+        //take input for the array size
+        System.out.print("Enter number of elemnts in array:");
+        n=sc.nextInt();
+        //declare array of size n
+        int arr[] = new int[n];
+        System.out.print("Enter elements:");
+        //take input for array elements
+        for(i = 0;i < n;i ++ )
+        {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Even numbers of the array are:");
+        for(i = 0;i < n;i ++ )
+        {    
+            //numbers divisible by 2 are even.
+            if((arr[i] % 2) == 0)
+            {
+                e ++ ;
+                System.out.println(arr[i] + " ");
+            }
+        }
+        System.out.println("Odd numbers of the array are:");
+        for(i = 0;i < n;i ++)
+        {
+            //numbers which are not divisible by 2 are odd.
+            if((arr[i] % 2) != 0)
+            {
+                o++ ;
+                System.out.println(arr[i] + " ");
+            }
+        }
+        System.out.println("Number of even terms in the array is:" + e +  "\nAnd number of odd terms is:" +o);
     }
-  System.out.println("Odd numbers of the array are:");
-  for(i=0;i<n;i++)
-   {
-    if((arr[i]%2)!=0)
-     {
-      o++;
-      System.out.println(arr[i]+" ");
-     }
-    }
-  System.out.println("Number of even terms in the array is:" +e+ "\nAnd number of odd terms is:" +o);
- }
 }
 //sample output:
 /*Enter number of elemnts in array:6
