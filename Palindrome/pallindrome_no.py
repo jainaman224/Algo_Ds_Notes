569004059a214125a74b665e21d2f41ea429e272
Raksha <raksha009btcse19@igdtuw.ac.in>
2020-03-08 18:32:07 +0530

    pallindrome number in python

---
+++ Palindrome/pallindrome_no.py
@@ -0,0 +1,35 @@
+#Python program to find whether a number is a pallindrome or not
+
+# Function to reverse a number
+def reverse(num):
+
+    #Initialize variable
+    rev=0
+
+    while(num>0):
+        dig=num%10
+        rev=rev*10+dig
+        num=num//10
+    return rev    # Returning reversed number
+
+    
+# --- main --- 
+num=int(input("Enter a number:"))  #User Input
+
+a  = reverse(num) #Function call
+
+if(a==num):  #Comparing the reversed number with original number
+    print("Number entered is palindrome!")
+else:
+    print("Number entered is not a palindrome!")
+
+
+'''
+TEST CASES
+
+Enter a number:3445443
+Number entered is palindrome!
+
+Enter a number:234
+Number entered is not a palindrome!
+'''
