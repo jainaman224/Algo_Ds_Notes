287ee4c1091a8149114eec16a4d54ed4e6d0288d
Ritish Singh <54978105+ritish099@users.noreply.github.com>
2020-03-08 12:15:51 +0530

    Update Factorial.c

--- Factorial/Factorial.c
+++ Factorial/Factorial.c
@@ -1,5 +1,5 @@
-/* This code is used to find factorial
-of large numbers*/
+/* This code is used to find factorial of large numbers*/
+
 #include <stdio.h>
 int main() 
 {
@@ -11,27 +11,28 @@ int main()
 
   // Code to find the factorial
   for (j = 2; j <= n; j++) 
-   {
+  {
     l = 0;
-    for (k = 0; k < m; k++) {
-      a[k] = a[k] * j + l;
-      l = a[k] / 10;
-      a[k] = a[k] % 10;
+    for (k = 0; k < m; k++) 
+    {
+        a[k] = a[k] * j + l;
+        l = a[k] / 10;
+        a[k] = a[k] % 10;
     }
 
     // Code to break a number having more than one digit into array of single
     // digits
     while (l) 
     {
-      a[k] = l % 10;
-      k++;
-      m++;
-      l = l / 10;
+        a[k] = l % 10;
+        k ++;
+        m ++;
+        l = l / 10;
     }
   }
   printf("Answer is : ");
   for (i = m - 1; i >= 0; i--)
-    printf("%d", a[i]);
+      printf("%d", a[i]);
   return 0;
 }
 /*ENTER THE NUMBER
