e5c769b4bedfe09901a0af72998f3fdb9872e161
Ritish Singh <54978105+ritish099@users.noreply.github.com>
2020-03-08 12:33:31 +0530

    Update Factorial.java

--- Factorial/Factorial.java
+++ Factorial/Factorial.java
@@ -1,20 +1,19 @@
-/* This code is used to find factorial
-of large numbers*/
+/* This code is used to find factorial of large numbers*/
 import java.util.*;
 import java.lang.*;
 import java.math.*;
 class Factorial 
 {
-  public static void main(String[] args) throws java.lang.Exception 
-  {
-      Scanner s = new Scanner(System.in);
-      int n = s.nextInt();
-      BigInteger f = new BigInteger("1");
-      for (int j = 1; j <= n; j++) 
-      {
-        f = f.multiply(BigInteger.valueOf(j));
-      }
-      System.out.println(f);
+    public static void main(String[] args) throws java.lang.Exception 
+    {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        BigInteger f = new BigInteger("1");
+        for (int j = 1; j <= n; j++) 
+        {
+            f = f.multiply(BigInteger.valueOf(j));
+        }
+        System.out.println(f);
   }
 }/*ENTER THE NUMBER
 5
