71fb1f53f4b8b62b4e823847aa0fdb7688952fcf
riajha02 <32812168+RiaJha02@users.noreply.github.com>
2020-03-09 01:32:01 +0530

    Bitmasking solution in cpp and java

--- N_queen_using_bitmasking/N_queen.cpp
+++ N_queen_using_bitmasking/N_queen.cpp
@@ -2,20 +2,16 @@
 using namespace std;
 
 const int N = 100;
-const int num = 2*N;
-vector<int> col(num,0);
-vector<int> d1(num,0);
-vector<int> d2(num,0);
+bitset<N> col,d1,d2;
 
-//These are the vector of data type bitset i.e. either 0 or 1 is present inside the vector, for checking the already placed queen area
-int board[N][N] = {0};
+char board[N][N];
 
 void show(int n)
 {
     for(int i=0;i<n;i++)
     {
         for(int j=0;j<n;j++)
-            cout<<board[i][j]<<" ";
+            cout<<board[i][j];
         cout<<endl;
     }
     cout<<endl;
@@ -24,10 +20,8 @@ void show(int n)
 
 void N_queen(int r,int n)
 {
-    // when reached the last cell
     if(r==n)
     { 
-        //for print the board
         show(n); 
         return; 
     }
@@ -35,90 +29,44 @@ void N_queen(int r,int n)
     {
         if(!col[i] && !d1[r-i+n-1] && !d2[r+i])
         {
-            // once we find such an area just mark 1 regarding the diagonal vertices and columns
             col[i] = 1;
             d1[r-i+n-1] = 1;
             d2[r+i] = 1;
-            board[r][i] = 1; // placing a queen at this position
+            board[r][i] = 'Q';
             N_queen(r+1,n);
-            //once recur place 0 again at that position
             col[i] = 0;
             d1[r-i+n-1] = 0;
             d2[r+i] = 0;
-            board[r][i] = 0;// removing the queen from that cell
+            board[r][i] = '.';
         }
     }
 }
 
 int main()
 {
-    //Number of rows and columns on the chess board
     int n;
     cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            board[i][j] = '.';
+        }
+    }
     N_queen(0,n);
 }
 
 /*
 Sample Input: 
-5
+4
 Sample Output: 
-1 0 0 0 0 
-0 0 1 0 0 
-0 0 0 0 1 
-0 1 0 0 0 
-0 0 0 1 0 
+.Q..
+...Q
+Q...
+..Q.
 
-1 0 0 0 0 
-0 0 0 1 0 
-0 1 0 0 0 
-0 0 0 0 1 
-0 0 1 0 0 
-
-0 1 0 0 0 
-0 0 0 1 0 
-1 0 0 0 0 
-0 0 1 0 0 
-0 0 0 0 1 
-
-0 1 0 0 0 
-0 0 0 0 1 
-0 0 1 0 0 
-1 0 0 0 0 
-0 0 0 1 0 
-
-0 0 1 0 0 
-1 0 0 0 0 
-0 0 0 1 0 
-0 1 0 0 0 
-0 0 0 0 1 
-
-0 0 1 0 0 
-0 0 0 0 1 
-0 1 0 0 0 
-0 0 0 1 0 
-1 0 0 0 0 
-
-0 0 0 1 0 
-1 0 0 0 0 
-0 0 1 0 0 
-0 0 0 0 1 
-0 1 0 0 0 
-
-0 0 0 1 0 
-0 1 0 0 0 
-0 0 0 0 1 
-0 0 1 0 0 
-1 0 0 0 0 
-
-0 0 0 0 1 
-0 1 0 0 0 
-0 0 0 1 0 
-1 0 0 0 0 
-0 0 1 0 0 
-
-0 0 0 0 1 
-0 0 1 0 0 
-1 0 0 0 0 
-0 0 0 1 0 
-0 1 0 0 0
+..Q.
+Q...
+...Q
+.Q..
 */
---
+++ N_queen_using_bitmasking/N_queen.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+class Solution {
+    public static List<List<String>> solveNQueens(int n) {
+        List<List<String>> res=new ArrayList<List<String>>();
+        String[] queens=new String[n];
+        char[] initial=new char[n];
+        Arrays.fill(initial,'.');
+        Arrays.fill(queens,String.valueOf(Arrays.copyOf(initial,n)));
+        int[] flag=new int[5*n-2];
+        Arrays.fill(flag,1);
+        solve(res,queens,flag,0,n);
+        return res;
+    }
+    private static void solve(List<List<String>> res,String[] queens,int[] flag,int row,int n){
+        if(row==n){
+            res.add(new ArrayList<String>(Arrays.asList(queens)));
+            return;
+        }
+        for(int col=0;col!=n;col++){
+            if(flag[col]==1 && flag[n+col+row]==1 && flag[4*n-2+col-row]==1){
+                flag[col]=0;
+                flag[n+col+row]=0;
+                flag[4*n-2+col-row]=0;
+                char[] chars=queens[row].toCharArray();
+                
+                chars[col]='Q';
+                queens[row]=String.valueOf(chars);
+                
+                solve(res,queens,flag,row+1,n);
+                
+                chars=queens[row].toCharArray();
+                chars[col]='.';
+                queens[row]=String.valueOf(chars);
+                
+                flag[col]=1;
+                flag[n+col+row]=1;
+                flag[4*n-2+col-row]=1;
+            }
+        }
+    }
+    public static void main(String[] args)  
+    { 
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        List<List<String>> ans;
+        ans = solveNQueens(n); 
+        for(int i=0;i!=ans.size();i++)
+        {
+            List<String> arr = ans.get(i);
+            for(int j=0;j!=arr.size();j++)
+            {
+               System.out.println(arr.get(j));
+            }
+            System.out.print("\n");
+        }
+        input.close();
+    }  
+}
+
+/*
+Sample Input: 
+4
+Sample Output: 
+.Q..
+...Q
+Q...
+..Q.
+
+..Q.
+Q...
+...Q
+.Q..
+*/
