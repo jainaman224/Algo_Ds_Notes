70b970e6c76a3bbf056e8e042838d4483136b24a
Chanpreet Singh <34856246+chanpreet1999@users.noreply.github.com>
2020-03-08 23:57:37 +0530

    Update PowerOfAnyGivenNo.cpp

--- Power_Of_Any_Number/PowerOfAnyGivenNo.cpp
+++ Power_Of_Any_Number/PowerOfAnyGivenNo.cpp
@@ -9,36 +9,36 @@
 using namespace std;
 
 bool isPowerOfGivenNo(int number, int base) {
-        if(number == 0)
-		{
-            //0 power anything is 0
-            if(base == 0)
-                return true;
+if(number == 0)
+	{
+    //0 power anything is 0
+    if(base == 0)
+	    return true;
 
-            //0 is not the power of any number
-            return false;
-		}
-
-		//explicitly check for 1 because log(1) = 0
-		if(base == 1)
-        {
-            if(number == 1)
-                return true;
-            else
-                return false;
-		}
-
-		// -8 cannot be power of 2 but can be power of -2
-		if( number < 0 && base > 0)
-			return false;
-
-		//converting to positive
-		number = abs(number);
-		base = abs(base);
-
-		float logValue = (float) ( log(number)/log(base)) ;
-		return logValue == floor(logValue);
+    //0 is not the power of any number
+    return false;
 	}
+
+	//explicitly check for 1 because log(1) = 0
+	if(base == 1)
+    {
+     if(number == 1)
+	    return true;
+     else
+	    return false;
+	}
+
+	// -8 cannot be power of 2 but can be power of -2
+	if( number < 0 && base > 0)
+		return false;
+
+	//converting to positive
+	number = abs(number);
+	base = abs(base);
+
+	float logValue = (float) ( log(number)/log(base)) ;
+	return logValue == floor(logValue);
+}
 
 
 // driver program
