7a23a4165f45a54c2884169bcdb4aa5a1bceaf99
RedEye00 <55008117+RedEye00@users.noreply.github.com>
2020-03-08 15:38:13 +0530

    After making mentioned changes on files

--- Last_Digit_of_Nth_Fibonacci_Number/Last_Digit_of_Nth_Fibonacci_Number.cpp
+++ Last_Digit_of_Nth_Fibonacci_Number/Last_Digit_of_Nth_Fibonacci_Number.cpp
@@ -2,7 +2,7 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main() {
     int a[60], n, i;
     a[0] = 0;
     a[1] = 1;
@@ -10,11 +10,11 @@ int main(){
     for(i = 2 ; i < 60 ; i++ ){        //for storing the last digits of first 60 fibonacci numbers
         a[i] = (a[i-1] + a[i-2]) % 10;//Now we will store last digits in array and their index as position in first 60 numbers fibonacci numbers 
     } 
-    cin>> n ;  
+    cin >> n ;  
 
-    cout<< "\nThe Position of Fibonacci Number: " << n ; //getting the required number
+    cout << "\nThe Position of Fibonacci Number: " << n ; //getting the required number
 
-    cout<< "\nThe Last Digit of this Number : " << a[n % 60] ;//print the last digit given in that position
+    cout << "\nThe Last Digit of this Number : " << a[n % 60] ;//print the last digit given in that position
     return 0;
 }
 /*
