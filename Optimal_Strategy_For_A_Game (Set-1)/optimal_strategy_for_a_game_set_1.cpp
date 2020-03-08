6c97e9d70d1372753e8527b07a1aec5d40b1c098
biekymahato <60212331+biekymahato@users.noreply.github.com>
2020-03-08 13:10:22 +0530

    changes the request #1985

--- Optimal_Strategy_For_A_Game (Set-1)/optimal_strategy_for_a_game_set_1.cpp
+++ Optimal_Strategy_For_A_Game (Set-1)/optimal_strategy_for_a_game_set_1.cpp
@@ -23,19 +23,19 @@ int optimalStrategyOfGame(int* arr, int n)
     /*Fill table using above recursive formula.
     Note that the table is filled in diagonal fashion from
     diagonal elements to table[0][n-1] which is the result.*/
-      for (int gap = 0; gap < n; ++gap){
-         for (int i = 0, j = gap; j < n; ++i, ++j){
+    for (int gap = 0; gap < n; ++gap){
+        for (int i = 0, j = gap; j < n; ++i, ++j){
 
-    /*Here x is value of F(i+2, j), y is F(i+1, j-1) and
-    z is F(i, j-2) in above recursive formula*/
-             int x = ((i + 2) <= j) ? table[i + 2][j] : 0;
-             int y = ((i + 1) <= (j - 1)) ? table[i + 1][j - 1] : 0;
-             int z = (i <= (j - 2)) ? table[i][j - 2] : 0;
-             table[i][j] = max(arr[i] + min(x, y), arr[j] + min(y, z));
-         }
-      }
+            /*Here x is value of F(i+2, j), y is F(i+1, j-1) and
+            z is F(i, j-2) in above recursive formula*/
+            int x = ((i + 2) <= j) ? table[i + 2][j] : 0;
+            int y = ((i + 1) <= (j - 1)) ? table[i + 1][j - 1] : 0;
+            int z = (i <= (j - 2)) ? table[i][j - 2] : 0;
+            table[i][j] = max(arr[i] + min(x, y), arr[j] + min(y, z));
+        }
+    }
 
-      return table[0][n - 1];
+    return table[0][n - 1];
 }
 
 // Driver Code to test above function
