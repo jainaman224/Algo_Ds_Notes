fecab68cd7f74eba1a2486784ede809f43e0d3dc
Debojyoti Chakraborty <50295688+darkdebo@users.noreply.github.com>
2020-03-09 11:25:19 +0530

    Update Bead_Sort.rb

--- Bead_Sort/Bead_Sort.rb
+++ Bead_Sort/Bead_Sort.rb
@@ -12,5 +12,15 @@ class Array
   end
 end
  
+def myarray(a,n)
+  for i in 0..n
+    a[i] = gets.chomp.to_i
+  end
+  return a
+end
+
 # Driver code:
-p [2,31,34,324,2].beadsort
+a = Array.new
+n = gets.chomp.to_i
+new_arr = myarray(a,n)
+print(new_arr.beadsort)
