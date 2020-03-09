e6eba690f82682d29825f96e6fc57ab5731fae68
Chanpreet Singh <34856246+chanpreet1999@users.noreply.github.com>
2020-03-09 18:56:17 +0530

    Update CountFrequencyOfArrayElements.java

--- Freq_Of_Elements_Of_Array/CountFrequencyOfArrayElements.java
+++ Freq_Of_Elements_Of_Array/CountFrequencyOfArrayElements.java
@@ -8,35 +8,36 @@ import java.util.Scanner;
 public class CountFrequencyOfArrayElements {
 
 public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
+    Scanner sc = new Scanner(System.in);
 
-//input size of array
-int n = sc.nextInt();
+    //input size of array
+    int n = sc.nextInt();
 
-//input array
-int a[] = new int[n];
-for(int i = 0; i < n; i++)
-	a[i] = sc.nextInt();
+    //input array
+    int a[] = new int[n];
+    for(int i = 0; i < n; i++)
+        a[i] = sc.nextInt();
 
-countFreq(a, n);
-sc.close();
+    countFreq(a, n);
+    sc.close();
 }
 
 //function to count frequency 
 private static void countFreq(int[] a, int n) {
-HashMap<Integer, Integer> hm = new HashMap<>(); // using hashmap to store the freq
+   // using hashmap to store the freq
+    HashMap<Integer, Integer> hm = new HashMap<>(); 
 
-for(int i = 0; i < n; i++)
-{
-if( hm.containsKey(a[i]) )
-	hm.put( a[i], hm.get(a[i]) + 1);
-else
-	hm.put(a[i], 1);
-}
+    for(int i = 0; i < n; i++)
+    {
+        if( hm.containsKey(a[i]) )
+            hm.put( a[i], hm.get(a[i]) + 1);
+        else
+            hm.put(a[i], 1);
+    }
 
-//display the frequencies	
-System.out.println("Element \t Frequency");
-hm.forEach(
+    //display the frequencies	
+    System.out.println("Element \t Frequency");
+    hm.forEach(
 	(k,v) ->
 	{
 	    System.out.println(k+" \t\t "+v);
