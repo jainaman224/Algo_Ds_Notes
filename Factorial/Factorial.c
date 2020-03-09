46d93c38c08866f744e935aa5ed5cf3da73f31c0
Ritish Singh <54978105+ritish099@users.noreply.github.com>
2020-03-09 19:22:25 +0530

    Update Factorial.c

--- Factorial/Factorial.c
+++ Factorial/Factorial.c
@@ -12,23 +12,23 @@ int main()
   // Code to find the factorial
   for (j = 2; j <= n; j++) 
   {
-    l = 0;
-    for (k = 0; k < m; k++) 
-    {
-        a[k] = a[k] * j + l;
-        l = a[k] / 10;
-        a[k] = a[k] % 10;
-    }
+     l = 0;
+     for (k = 0; k < m; k++) 
+     {
+         a[k] = a[k] * j + l;
+         l = a[k] / 10;
+         a[k] = a[k] % 10;
+     }
 
-    // Code to break a number having more than one digit into array of single
-    // digits
-    while (l) 
-    {
-        a[k] = l % 10;
-        k ++;
-        m ++;
-        l = l / 10;
-    }
+     // Code to break a number having more than one digit into array of single
+     // digits
+     while (l) 
+     {
+         a[k] = l % 10;
+         k ++;
+         m ++;
+         l = l / 10;
+     }
   }
   printf("Answer is : ");
   for (i = m - 1; i >= 0; i--)
