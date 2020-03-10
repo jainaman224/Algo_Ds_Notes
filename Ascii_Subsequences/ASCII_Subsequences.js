b3272090ca3fad77240fa3ace19518d38c39a171
Aman-Codes <54680709+Aman-Codes@users.noreply.github.com>
2020-03-10 12:50:56 +0530

    Improved Indentation
    Improved Indentation

--- Ascii_Subsequences/ASCII_Subsequences.js
+++ Ascii_Subsequences/ASCII_Subsequences.js
@@ -4,7 +4,7 @@ This problem is same as the "Subsequences" question just instead of two function
 - One with a letter added.
 - One with the ascii code of the letter added to the string.
 - For example:-
-Take as input str, a string. We are concerned with all the possible ASCII-subsequences of str. E.g. “ab” has following ascii-subsequences “”, “b”, “98”, “a”, “ab”, “a98”, “97”, “97b”, “9798”
+Take as input str, a string. We are concerned with all the possible ASCII-subsequences of str. E.g. "ab" has following ascii-subsequences "", "b", "98", "a", "ab", "a98", "97", "97", "9798"
 a. Write a recursive function which returns the count of ASCII-subsequences for a given string. Print the value returned.
 b. Write a recursive function which prints all possible ASCII-subsequences for a given string (void is the return type for function).
 */
