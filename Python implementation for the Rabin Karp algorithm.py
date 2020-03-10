162adc508478a26c5bdd8b31ecc8545a2ec1d951
Amit sharma <54413110+Amitsharma45@users.noreply.github.com>
2020-03-10 19:25:35 +0530

    Update Python implementation for the Rabin Karp algorithm.py

--- Python implementation for the Rabin Karp algorithm.py
+++ Python implementation for the Rabin Karp algorithm.py
@@ -1,4 +1,3 @@
-
 d = 256
 def search(pat, txt, q): 
 	M = len(pat) 
@@ -8,43 +7,27 @@ def search(pat, txt, q):
 	p = 0 
 	t = 0  
 	h = 1
-
-
 	for i in range(M-1): 
-		h = (h * d)% q 
-
-	 
+		h = (h * d)% q  
 	for i in range(M): 
 		p = (d * p + ord(pat[i]))% q 
 		t = (d * t + ord(txt[i]))% q 
-
-	
 	for i in range(N-M + 1):  
-		if p == t: 
-			 
+		if p == t:  
 			for j in range(M): 
 				if txt[i + j] != pat[j]: 
 					break
-
 			j+= 1
-			
 			if j == M: 
 				print ("Pattern found at index " + str(i)) 
-
-		 
 		if i < N-M: 
 			t = (d*(t-ord(txt[i])*h) + ord(txt[i + M]))% q 
-
-			
 			if t < 0: 
 				t = t + q 
-
-
 txt = input("Enter text\n")
 pat = input("Enter pattern\n")
 q = 101 
 search(pat, txt, q) 
-
 # This code is contributed by Amit sharma 
 '''
 test 1
