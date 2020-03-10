f5dfbbb7de7ff074d81488525863c3c14a816792
raksha009 <raksha009btcse19@igdtuw.ac.in>
2020-03-11 02:05:00 +0530

    pallindrome string python

---
+++ Palindrome/pallindrome_string.py
@@ -0,0 +1,33 @@
+#Python program to find whether a string is a pallindrome or not
+
+# Function to reverse a String
+def reverse(str):
+
+    #Initialize variable
+    rev=""
+
+    for i in range (len(str)-1,-1,-1):
+        rev = rev + str[i]
+    return rev    # Returning reversed number
+
+    
+# --- main --- 
+string=raw_input(("Enter a string: "))  #User Input
+
+a  = reverse(string) #Function call
+
+if(a==string):  #Comparing the reversed String with original String
+    print("String entered is palindrome!")
+else:
+    print("String entered is not a palindrome!")
+
+'''
+TEST CASES
+
+Enter a string: affcffa
+String entered is palindrome!
+
+Enter a string: abbcd
+String entered is not a palindrome!
+
+'''
