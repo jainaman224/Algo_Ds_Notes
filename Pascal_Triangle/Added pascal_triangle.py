4131d9942f90f89b0e1b125dc945b10be6a47dc2
Aditya Gupta <56713924+aftex261@users.noreply.github.com>
2020-03-11 18:51:39 +0530

    Update Added pascal_triangle.py

--- Pascal_Triangle/Added pascal_triangle.py
+++ Pascal_Triangle/Added pascal_triangle.py
@@ -1,14 +1,14 @@
 n = int(input())
 arr = []
 for i in range(n):
-  arr.append([])
-  arr[i].append(1)
-  for j in range(1, i):
-    arr[i].append(arr[i - 1][j - 1] + arr[i - 1][j])
-  if(n != 0):
+    arr.append([])
     arr[i].append(1)
+    for j in range(1, i):
+        arr[i].append(arr[i - 1][j - 1] + arr[i - 1][j])
+    if(n != 0):
+        arr[i].append(1)
 for i in range(n):
-  print(" " * (n - i), end = " ", sep = " ")
-  for _ in range(0, i + 1):
-    print('{0:6}'.format(arr[i][_]), end = " ", sep = " ")
-  print()
+    print(" " * (n - i), end = " ", sep = " ")
+    for _ in range(0, i + 1):
+        print('{0:6}'.format(arr[i][_]), end = " ", sep = " ")
+    print()
