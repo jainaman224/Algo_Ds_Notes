27a158cffeea08c08f95cf7683acc1510bc9c601
sheetalssr <42754054+sheetalssr@users.noreply.github.com>
2020-03-12 00:09:47 +0530

    Update Pigeonhole_Sort.js

--- Pigeonhole_Sort/Pigeonhole_Sort.js
+++ Pigeonhole_Sort/Pigeonhole_Sort.js
@@ -4,42 +4,43 @@ var n=prompt("enter the size of your array");
 var arr = [];
 for(i=0; i<n; i++) 
 {
-	//Taking Input from user
-	arr[i] = prompt('Enter Element ' + (i+1));
+    //Taking Input from user
+    arr[i] = prompt('Enter Element ' + (i+1));
 }
 //function for pigeonsort
 function pigeonsort(array)
 {
-	//finding maximum value of array 
-	var max = Math.max.apply(null,array); //19
+    //finding maximum value of array 
+    var max = Math.max.apply(null,array); //19
   
-	//finding minimum value of array 
-	var min = Math.min.apply(null,array); //2
+    //finding minimum value of array 
+    var min = Math.min.apply(null,array); //2
   
-  	//finding the size of hole array to be created
-	var holerange=max-min+1;
+    //finding the size of hole array to be created
+    var holerange=max-min+1;
   
-  	//initialising hole array with value 0
-	var hole = Array(holerange).fill(0);
+    //initialising hole array with value 0
+    var hole = Array(holerange).fill(0);
   
-  	//filling the hole array
-	for(i=0;i<array.length;i++)
-	{
-		hole[array[i]-min]++;
-	}
-	count = 0;
+    //filling the hole array
+    for(i=0;i<array.length;i++)
+    {
+        hole[array[i]-min]++;
+    }
+	
+    count = 0;
   
-  	//finally putting the values in sorted manner
-	for(var j = 0 ; j<holerange ; j++)
-	{
-		while(hole[j]!==0)
-		{
-			array[count]=j+min;
-      			hole[j]--;
-      			count++;
-    		}
-  	}
-	return array;
+    //finally putting the values in sorted manner
+    for(var j = 0 ; j<holerange ; j++)
+    {
+        while(hole[j]!==0)
+        {
+            array[count]=j+min;
+            hole[j]--;
+      	    count++;
+    	}
+    }
+    return array;
 }
 
 console.log(pigeonsort(arr));
