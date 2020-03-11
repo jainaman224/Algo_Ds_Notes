f8452df073c30d29085fb508cc1cb89a68b98beb
Devforlife07 <Devansh.forlife@gmail.com>
2020-03-12 00:05:53 +0530

    Added More Comments And Moved sample i/o at end

--- treezigzagtraversal/zigzag.java
+++ treezigzagtraversal/zigzag.java
@@ -1,16 +1,11 @@
 import java.util.*;
 
-/**
- * zigzag
- */
-// sample input
-// 10 true 20 true 40 false false true 50 false false true 30 true 60 false
-// false true 73 false false
-// sample output
-// 10 30 20 40 50 60 73
 public class zigzag {
     static Scanner sc = new Scanner(System.in);
 
+    /*
+     * Declaring Class node which serves the purpose of a node in Binary Tree
+     */
     static class node {
         int data;
         node left;
@@ -25,6 +20,11 @@ public class zigzag {
 
     private static node root;
 
+    /*
+     * Function To Create Tree.Data should be given in preorder format If a left or
+     * right node exists for current node input should be true followed by data for
+     * node otherwise false if no child node exists
+     */
     private static node buildtree() {
         int data = sc.nextInt();
         node n = new node(data);
@@ -37,6 +37,12 @@ public class zigzag {
         return n;
     }
 
+    /*
+     * Function to display tree in zigzag level traversal Two Stacks are used one to
+     * keep record of current level and another to keep record of next level.The
+     * Left to right or right to left order is maintained by boolean variable
+     * islr.At the end the two stacks are swapped.
+     */
     private static void zigzag() {
         if (root == null)
             return;
@@ -73,5 +79,13 @@ public class zigzag {
         root = buildtree();
         zigzag();
     }
-
 }
+
+/*
+ * Sample Input 10 true 20 true 40 false false true 50 false false true 30 true
+ * 60 false false true 73 false false
+ */
+
+/*
+ * sample output 10 30 20 40 50 60 73
+ */
