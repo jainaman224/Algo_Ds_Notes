8044a4c079dbac4bf4b75ab023d85d9184b6610f
Pooja <pooja016mca19@igdtuw.ac.in>
2020-03-12 01:18:11 +0530

    all changes has been made

--- Armstrong_Number/armstrong_number.dart
+++ Armstrong_Number/armstrong_number.dart
@@ -1,43 +1,46 @@
-//armstorng number
+/*
+    program to check for armstrong number.
+    An Armstrong number is a number that is equal to the sum of digits raise to the power total number of digits in the number
+*/
 
 import 'dart:io';
 import 'dart:math';
 
-
 bool is_armstrong(int n,int l)
 {
-    int n1,rem,sum=0; 
-    n1=n;
-    
-    /*
-        An Armstrong number is a number that is equal to the sum of digits raise to the power total number of digits in the number
-    */
-
-    while(n1>0)
+    int n1, rem, sum = 0; 
+    n1 = n;
+   
+    while(n1 > 0)
     {
-        rem=n1%10;
-        sum=sum+pow(rem,l);
-        n1=(n1/10).toInt();
-        
+        rem = n1 % 10;
+        sum = sum + pow(rem,l);
+        n1 = (n1 / 10).toInt();
     }
-    if(sum==n)
+    
+    if(sum == n)
         return true;
     
     return false;
 }
-main(){
-    
+
+main()
+{
     print("Enter a number  ");
-    var input=stdin.readLineSync();
+    var input = stdin.readLineSync();
     
-    var len=input.length;
-    var num=int.parse(input);
+    int len = input.length;              //to clalculate the length of string
+    int num = int.parse(input);         //conveting string into integer    
     
-    bool check=is_armstrong(num,len);
-    
-    if(check==true)
+    bool check = is_armstrong(num,len);
+
+    if(check == true)
         print("YES");
     else
         print("NO");
-    
 }
+
+/*
+Enter a number 407
+YES
+*/
