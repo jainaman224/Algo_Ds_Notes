2fc3429b35a390aa2847d97319f445429b543c19
Souma Kanti Ghosh <ghoshsoumakanti@gmail.com>
2020-03-11 13:01:52 +0530

    Updated Linear_Search.rs
    updated Linear_Search.rs with dynamic inputs, the sample I/O, proper spacing.

--- Linear_Search/Linear_Search.rs
+++ Linear_Search/Linear_Search.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn linear_search<T: Ord>(vec: &Vec<T>, key: &T) -> Option<usize> {  
     let mut result: Option<usize> = None;
     for (i, n) in vec.into_iter().enumerate() {
@@ -7,9 +9,32 @@ fn linear_search<T: Ord>(vec: &Vec<T>, key: &T) -> Option<usize> {
     };
     result
 } 
+
 fn main() {
-    let some_vec = vec![1,2,3,4,5,6,7,8,9,10]; 
-    match linear_search(&some_vec, &6) {
+    /* Sample Input :-
+        1
+        2
+        3
+        4
+        6
+        Sample Output :-
+        The element is present at index: 4
+    */
+    println!("Enter 5 elements :- ");
+    let mut input = String::new();
+    let mut some_vec = vec![];
+    for i in 0..5 {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Error reading input");
+        let my_int: i32 = input.trim().parse().expect("Not a valid number");
+        some_vec.push(my_int);
+    }
+    println!("Enter the element to be searched in the array :- ");    
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Error reading input");
+    let my_int: i32 = input.trim().parse().expect("Not a valid number");
+    
+    match linear_search(&some_vec, &my_int) {
         Some(val) => println!("The element is present at index: {}", val),
         None => println!("The element is not present")
     }; 
