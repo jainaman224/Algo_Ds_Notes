f6c6ff7e9d82a881514ed72213f1c79a1b32ed1a
riajha02 <32812168+RiaJha02@users.noreply.github.com>
2020-03-13 02:11:09 +0530

    Update Backtracking_using_bitmasking.cpp

--- N_queen_using_bitmasking/Backtracking_using_bitmasking.cpp
+++ N_queen_using_bitmasking/Backtracking_using_bitmasking.cpp
@@ -1,77 +1,42 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-const int N = 100;
-bitset<N> col,d1,d2;
-
-char board[N][N];
+#define d (1<<n)-1
+int cnt=0;
 
 //This is an optimised approach than the normal backtracking approach. 
 //Here we don’t need to write is Safe Positon Function which works in linear time instead we use bitsets which work in O(1) time.
 
-void show(int n)
+void innerrecurse(int row,int left,int right,int n)
 {
-    for(int i=0;i<n;i++)
+    if(row==d)
     {
-        for(int j=0;j<n;j++)
-            cout<<board[i][j];
-        cout<<endl;
+        cnt++;
+        return;
     }
-    cout<<endl;
-}
-
-
-void N_queen(int r,int n)
-{
-    if(r==n)
-    { 
-        show(n); 
-        return; 
-    }
-    for(int i=0;i<n;i++)
+    int pos = d & ~(left|row|right);
+    while(pos > 0)
     {
-        if(!col[i] && !d1[r-i+n-1] && !d2[r+i])
-        {
-            // bit is set to 1 for the following column and diagonals
-            col[i] = 1;
-            d1[r-i+n-1] = 1;
-            d2[r+i] = 1;
-            board[r][i] = 'Q';
-            N_queen(r+1,n);
-            // backtrack the path
-            col[i] = 0;
-            d1[r-i+n-1] = 0;
-            d2[r+i] = 0;
-            board[r][i] = '.'; 
-        }
+        int bit = pos & (-pos);
+        pos -= bit;
+        innerrecurse(row|bit, (left|bit)<<1, (right|bit)>>1,n);
     }
 }
 
-int main()
-{
+int main() {
     int n;
     cin>>n;
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            board[i][j] = '.';
-        }
-    }
-    N_queen(0,n);
+    innerrecurse(0,0,0,n);
+    cout<<cnt;
 }
 
 /*
 Sample Input: 
 4
 Sample Output: 
-.Q..
-...Q
-Q...
-..Q.
+2
 
-..Q.
-Q...
-...Q
-.Q..
+Sample Input:
+5
+Sample Output:
+10
 */
