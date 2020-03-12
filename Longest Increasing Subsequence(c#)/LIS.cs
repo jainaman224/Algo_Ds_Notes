e14e229833e2e265c4fc85a6f15739e30c4cf9ea
biekymahato <60212331+biekymahato@users.noreply.github.com>
2020-03-13 04:05:40 +0530

    Longest Increasing Subsequence(C#)

---
+++ Longest Increasing Subsequence(c#)/LIS.cs
@@ -0,0 +1,43 @@
+/* Dynamic Programming C# implementation of LIS problem */
+
+using System ;
+class LIS
+{
+    /* lis() returns the length of the longest increasing
+    subsequence in arr[] of size n */
+    static int lis(int []arr,int n)
+    {
+        int []lis = new int[n];
+        int i,j,max = 0;
+
+        //Initialize LIS values for all indexes
+        for ( i = 0; i < n; i++ )
+            lis[i] = 1;
+
+        //Compute optimized LIS values in bottom up manner
+        for ( i = 1; i < n; i++ )
+            for ( j = 0; j < i; j++ )
+                        if ( arr[i] > arr[j] && lis[i] < lis[j] + 1)
+                    lis[i] = lis[j] + 1;
+
+        //Pick maximum of all LIS values
+        for ( i = 0; i < n; i++ )
+            if ( max < lis[i] )
+                max = lis[i];
+
+            return max;
+    }
+
+    public static void Main()
+    {
+        int []arr = { 10, 22, 9, 33, 21, 50, 41, 60 };
+            int n = arr.Length;
+            Console.WriteLine("Length of lis is " + lis( arr, n ) + "\n" );
+    }
+
+}
+
+/*
+Ouput :
+Length of lis is 5
+*/
