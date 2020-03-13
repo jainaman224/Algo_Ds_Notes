c095da5a625c66e5a862986bb458002bf8de56af
riajha02 <32812168+RiaJha02@users.noreply.github.com>
2020-03-14 02:26:02 +0530

    Update Backtracking_using_bitmasking.java

--- N_queen_using_bitmasking/Backtracking_using_bitmasking.java
+++ N_queen_using_bitmasking/Backtracking_using_bitmasking.java
@@ -1,80 +1,62 @@
-import java.util.*;
+import java.util.*; 
 
-class Solution {
-    public static List<List<String>> solveNQueens(int n) {
-        List<List<String>> res=new ArrayList<List<String>>();
-        String[] queens=new String[n];
-        char[] initial=new char[n];
-        Arrays.fill(initial,'.');
-        Arrays.fill(queens,String.valueOf(Arrays.copyOf(initial,n)));
-        int[] flag=new int[5*n-2];
-        Arrays.fill(flag,1);
-        solve(res,queens,flag,0,n);
-        return res;
+class Solution 
+{
+    //Keeps track of the # of valid solutions
+    static int ans = 0;
+
+    public static int power(int x, int y) 
+    { 
+        if(y==0)
+            return 1;
+        int result = power(x,y/2); 
+        if(y%2==0)
+            return result*result;
+        else
+            return x*result*result;
     }
-    private static void solve(List<List<String>> res,String[] queens,int[] flag,int row,int n){
-        if(row==n){
-            res.add(new ArrayList<String>(Arrays.asList(queens)));
+
+    //Checks all possible board configurations
+    public static void solveNQueens(int left,int col,int right,int n,int done)
+    {
+        //All columns are occupied, so the solution must be complete
+        if(col==done)
+        {
+            ans++;
             return;
         }
-        for(int col=0;col!=n;col++){
-            
-            /*Here we use bit patterns to keep track of queen placements for the columns, left diagonals and right diagonals.
-            1 in the bit pattern denotes that there is a queen attacking that column or diagonal line and 
-            0 means that no queens are attacking that position.*/
-            
-            if(flag[col]==1 && flag[n+col+row]==1 && flag[4*n-2+col-row]==1){
-                flag[col]=0;
-                flag[n+col+row]=0;
-                flag[4*n-2+col-row]=0;
-                char[] chars=queens[row].toCharArray();
-                
-                chars[col]='Q';
-                queens[row]=String.valueOf(chars);
-                
-                solve(res,queens,flag,row+1,n);
-                
-                // backtrack the path
-                chars=queens[row].toCharArray();
-                chars[col]='.';
-                queens[row]=String.valueOf(chars);
-                
-                flag[col]=1;
-                flag[n+col+row]=1;
-                flag[4*n-2+col-row]=1;
-            }
+        
+        //Gets a bit sequence with "1"s where ever there is an open "slot"
+        int pos = ~(left|col|right) & done;
+        
+        //Loops as long as there is a valid place to put another queen.
+        while(pos>0)
+        {
+            int bit = pos & (-pos);
+            pos = pos - bit;
+            solveNQueens((left|bit)>>1, col|bit, (right|bit)<<1, n, done);
         }
     }
+
     public static void main(String[] args)  
     { 
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-        List<List<String>> ans;
-        ans = solveNQueens(n); 
-        for(int i=0;i!=ans.size();i++)
-        {
-            List<String> arr = ans.get(i);
-            for(int j=0;j!=arr.size();j++)
-            {
-               System.out.println(arr.get(j));
-            }
-            System.out.print("\n");
-        }
+        //Helps identify valid solutions
+        int done = power(2,n)-1;
+        solveNQueens(0,0,0,n,done); 
+        System.out.print(ans);
         input.close();
     }  
-}
-
+};
 /*
 Sample Input: 
 4
 Sample Output: 
-.Q..
-...Q
-Q...
-..Q.
+2
 
-..Q.
-Q...
-...Q
-.Q..
+Sample Input: 
+5
+Sample Output: 
+10
 */
