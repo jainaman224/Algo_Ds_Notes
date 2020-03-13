3e1f7c4e03bb5124cc44c8dffb7712444d0914f7
AkanshaKamboj <42874293+AkanshaKamboj@users.noreply.github.com>
2020-03-13 23:17:45 +0530

    Update Linked_List_MergeSort.java

--- Linked_List_Merge_Sort/Linked_List_MergeSort.java
+++ Linked_List_Merge_Sort/Linked_List_MergeSort.java
@@ -1,3 +1,27 @@
+/*JAVA program for sorting a Single Linked List using Merge Sort technique. Merge Sort uses divide and conquer technique i.e. 
+ it divides the input array into two halves, calls itself recursively for the two halves until one element remains and then 
+ merges the two halves
+ 
+                                          | 6 | 5 | 4 | 3 | 2 | 1 |           *
+                                                    /   \                     *
+                                                   /     \                    *
+                                      | 6 | 5 | 4 |      | 3 | 2 | 1 |        *
+                                          /  \                /  \            *        DIVIDE
+                                         /    \              /    \           *
+                                | 6 | 5 |    | 4 |   | 3 | 2 |    | 1 |       *     
+                                  /   \        |       /   \        |         *
+                                |6|   |5|      |     |3|   |2|      |         *
+                                 \     /       |      \     /       |
+                                  \   /        |       \   /        |
+                                | 5 | 6 |      |     | 2 | 3 |      |         *
+                                   \           |         \          |         *
+                                    \          |          \         |         *
+                                    | 4 | 5 | 6 |         | 1 | 2 | 3 |       *        MERGE
+                                          \                     /             *
+                                           \                   /              *
+                                         | 1 | 2 | 3 | 4 | 5 | 6 |            *
+ */
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
@@ -19,15 +43,15 @@ class LinkedList
 
     public static Node merge(Node l1, Node l2)
     {
-        if(l1==null)
+        if(l1 == null)
         return l2;
     
-        if(l2==null)
+        if(l2 == null)
         return l1;
 
         Node head = null;
     
-        if(l1.data<l2.data)
+        if(l1.data < l2.data)
         {
             head = l1;
             l1 = l1.next;
@@ -38,11 +62,11 @@ class LinkedList
             l2 = l2.next;
         }
     
-        Node ptr=head;
+        Node ptr = head;
     
-        while(l1!=null && l2!=null)
+        while(l1 != null && l2 != null)
         {
-            if(l1.data<l2.data)
+            if(l1.data < l2.data)
             {
                 ptr.next = l1;
                 l1 = l1.next;
@@ -55,7 +79,7 @@ class LinkedList
             ptr = ptr.next;
         }
     
-        if(l1!=null)
+        if(l1 != null)
             ptr.next = l1;
         else
             ptr.next = l2;
@@ -76,13 +100,13 @@ class LinkedList
         /*Dividing the linked list into two equal parts as done in merge sort*/
         while(ptr2 !=null && ptr2.next != null)
         {
-            ptr1=ptr1.next;
+            ptr1 = ptr1.next;
             if(ptr2.next != null)
-                ptr2=ptr2.next.next;
+                ptr2 = ptr2.next.next;
         }
     
-        ptr2=ptr1.next;
-        ptr1.next=null;
+        ptr2 = ptr1.next;
+        ptr1.next = null;
     
         return merge(mergeSort(head),mergeSort(ptr2));
     }
@@ -100,16 +124,6 @@ class LinkedList
         }
         System.out.print("NULL");
     }
-
-    /*
-    Sample Input
-    1 - Test cases
-    5 - Total number of elements to be inserted in linked list
-    23 2 34 5 1 - Adding the contents of the linked list
-
-    Sample Output
-    1 2 5 23 34
-    */
 
     public static void main (String[] args) 
     {
@@ -138,3 +152,14 @@ class LinkedList
         }
     }
 }
+
+/*
+    Sample Input
+    1 - Test cases
+    5 - Total number of elements to be inserted in linked list
+    23 2 34 5 1 - Adding the contents of the linked list
+
+    Sample Output
+    1 2 5 23 34
+*/
+
