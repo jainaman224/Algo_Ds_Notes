dcedc3c4637f0b714070c264d8ced9f9bfd3dd44
Shubhangi Gupta <58917829+shubhigupta991@users.noreply.github.com>
2020-03-13 15:35:44 +0530

    Update difference.cpp

--- 2_D_Array/C++/difference.cpp
+++ 2_D_Array/C++/difference.cpp
@@ -6,59 +6,59 @@ int main()
 {
     int m,n,k,l;
     cout << "Enter size of 1st array :" << endl;
-    cout<<"Row :";
-    cin>>m;
-    cout<<"Column :";
-    cin>>n;
-    cout<< "Enter size of 2st array :" << endl;
-    cout<<"Row :";
-    cin>>k;
-    cout<<"Column :";
-    cin>>l;
+    cout << "Row :";
+    cin >> m;
+    cout << "Column :";
+    cin >> n;
+    cout << "Enter size of 2nd array :" << endl;
+    cout << "Row :";
+    cin >> k;
+    cout << "Column :";
+    cin >> l;
     int a[m][n], b[k][l];
-    if(m==k && n==l)
+    if(m == k && n == l)
     {
         //for storing first matrix.
-        cout<<"Enter 1st array :"<<endl;
+        cout << "Enter 1st array :" << endl;
         for(int i=0; i<m; i++)
         {
             for(int j=0; j<n; j++)
             {
-                cin>>a[i][j];
+                cin >> a[i][j];
             }
         }
         //for storing second matrix.
-        cout<<"Enter 2nd array :"<<endl;
-        for(int i=0; i<m; i++)
+        cout << "Enter 2nd array :" << endl;
+        for(int i = 0; i < m; i++)
         {
-            for(int j=0; j<n; j++)
+            for(int j = 0; j < n; j++)
             {
-                cin>>b[i][j];
+                cin >> b[i][j];
             }
         }
         int c[m][n];
 	//calculating subtraction of a and b matrices.
-        for(int i=0; i<m; i++)
+        for(int i = 0; i < m; i++)
         {
-            for(int j=0; j<n; j++)
+            for(int j = 0; j < n; j++)
             {
-                c[i][j]=a[i][j]-b[i][j];
+                c[i][j] = a[i][j] - b[i][j];
             }
         }
-        cout<<endl<<"Subtracted Array :"<<endl;
+        cout << endl << "Subtracted Array :" << endl;
 	//for displaying output matrix.
-        for(int i=0; i<m; i++)
+        for(int i = 0; i < m; i++)
         {
-            for(int j=0; j<n; j++)
+            for(int j = 0; j < n; j++)
             {
-                cout<<c[i][j]<<"\t";
+                cout << c[i][j] << "\t";
             }
-            cout<<"\n";
+            cout << "\n";
         }
     }
     else
     {
-        cout<<endl<<"The two matrices cannot be subtracted due to difference in size..."<<endl;
+        cout << endl << "The two matrices cannot be subtracted due to difference in size..." << endl;
     }
     return 0;
 }
