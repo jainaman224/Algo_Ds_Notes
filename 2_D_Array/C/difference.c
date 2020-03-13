f1b28e6182333d8e0fcb79e35544cb71e4fbf108
Shubhangi Gupta <58917829+shubhigupta991@users.noreply.github.com>
2020-03-13 15:53:51 +0530

    Update difference.c

--- 2_D_Array/C/difference.c
+++ 2_D_Array/C/difference.c
@@ -9,46 +9,46 @@ int main()
     scanf("%d",&m);
     printf("Column :");
     scanf("%d",&n);
-    printf("Enter size of 2st array :\n");
+    printf("Enter size of 2nd array :\n");
     printf("Row :");
     scanf("%d",&k);
     printf("Column :");
     scanf("%d",&l);
     int a[m][n], b[k][l];
-    if(m==k && n==l)
+    if(m == k && n == l)
     {
         //for storing first matrix.
         printf("Enter 1st array : \n");
-        for(int i=0; i<m; i++)
+        for(int i = 0; i < m; i++)
         {
-            for(int j=0; j<n; j++)
+            for(int j = 0; j < n; j++)
             {
                 scanf("%d",&a[i][j]);
             }
         }
         //for storing second matrix.
         printf("Enter 2nd array :\n");
-        for(int i=0; i<m; i++)
+        for(int i = 0; i < m; i++)
         {
-            for(int j=0; j<n; j++)
+            for(int j = 0; j < n; j++)
             {
                 scanf("%d",&b[i][j]);
             }
         }
         int c[m][n];
 	//calculating subtraction of a and b matrices.
-        for(int i=0; i<m; i++)
+        for(int i = 0; i < m; i++)
         {
-            for(int j=0; j<n; j++)
+            for(int j = 0; j < n; j++)
             {
-                c[i][j]=a[i][j]-b[i][j];
+                c[i][j] = a[i][j] - b[i][j];
             }
         }
         printf("Subtracted Array :\n");
 	//for displaying output matrix.
-        for(int i=0; i<m; i++)
+        for(int i = 0; i < m; i++)
         {
-            for(int j=0; j<n; j++)
+            for(int j = 0; j < n; j++)
             {
                 printf("%d\t",c[i][j]);
             }
