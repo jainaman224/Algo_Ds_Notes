3451aa47bd49273649bf7d68f102214cf0e29753
Aakash Dinkar <35952953+aakashdinkar@users.noreply.github.com>
2020-03-14 12:08:11 +0530

    create cocktail_sort.go

---
+++ Cocktail_Sort/cocktail_sort.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func cocktail_sort(arr[] int,n int){
+	end := n - 1
+  var i int
+  for {
+        swapped := false
+        i=0
+        
+        //comparison of elements from the front
+        
+        for i < end {
+            if arr[i] > arr[i+1] {
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swapped = true
+            }
+            i = i + 1 
+        }
+        if !swapped {
+            return
+        }
+        swapped = false
+        end = end - 1
+        i = end -1
+        
+        //comparison of elements from backward
+        
+        for i >= 0 {
+            if arr[i] > arr[i+1] {
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swapped = true
+            }
+            i = i - 1
+        }
+        if !swapped {
+            return
+        }
+    }
+}
+
+func main(){
+	var n int
+	fmt.Print("Enter the number of elements:")
+	fmt.Scan(&n)
+	arr:=make([]int, n)
+	i:=0
+	for(i<n){
+		fmt.Print("Enter element ",i+1,":")
+		fmt.Scan(&arr[i])
+		i = i + 1
+	}
+	cocktail_sort(arr,n)
+	fmt.Print("Sorted Arry is: ",arr)
+}
