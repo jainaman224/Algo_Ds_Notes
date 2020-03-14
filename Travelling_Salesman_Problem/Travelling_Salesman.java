2e08e18587a76416ceea2b27bcff71a20f08533a
Mansi Agrawal <46123053+mansikagrawal@users.noreply.github.com>
2020-03-14 09:59:47 +0530

    Travelling_Salesman.java

--- Travelling_Salesman_Problem/Travelling_Salesman.java
+++ Travelling_Salesman_Problem/Travelling_Salesman.java
@@ -5,56 +5,51 @@ class TSP
 { 
 	// Function to find the minimum weight 
 	// Hamiltonian Cycle 
-  static int tspfn( int[][] graph, boolean[] visited, int currPosition, int n, int count, int cost, int answer) 
+static int tspfn( int[][] graph, boolean[] visited, int currPosition, int n, int count, int cost, int answer) 
+{ 
+	// If last node is reached and it has a link 
+	// to the starting node i.e the source then 
+	// keep the minimum value out of the total cost 
+	// of traversal and "answer" 
+	// Finally return to check for more possible values 
+	if( count == n && graph[currPosition][0] > 0) 
 	{ 
-		// If last node is reached and it has a link 
-		// to the starting node i.e the source then 
-		// keep the minimum value out of the total cost 
-		// of traversal and "answer" 
-		// Finally return to check for more possible values 
-		
-    if( count == n && graph[currPosition][0] > 0) 
-		{ 
-			answer = Math.min( answer, cost + graph[currPosition][0]); 
-			return answer; 	
-    	} 
+	answer = Math.min( answer, cost + graph[currPosition][0]); 
+	return answer; 	
+    } 
     
-		// BACKTRACKING STEP 
-		// Loop to traverse the adjacency list 
-		// of currPos node and increasing the count 
-		// by 1 and cost by graph[currPos,i] value 
-		
+	// BACKTRACKING STEP 
+	// Loop to traverse the adjacency list 
+	// of currPos node and increasing the count 
+	// by 1 and cost by graph[currPos,i] value 
     for( int i = 0; i < n; i++) 
-		{ 
-    
-			if( visited[i] == false && graph[currPosition][i] > 0) 
-			{ 
-				// Mark as visited 
-				visited[i] = true; 
-				answer = tspfn( graph, visited, i, n, count + 1, cost + graph[currPosition][i], answer); 
-				// Mark ith node as unvisited 
-				visited[i] = false; 
-		
-     } 
-		} 
-		return answer; 
-	} 
-	public static void main( String[] args) 
+    { 
+     if( visited[i] == false && graph[currPosition][i] > 0) 
 	{ 
-
-		// n is the number of nodes i.e. V 
+	 // Mark as visited 
+	 visited[i] = true; 
+     answer = tspfn( graph, visited, i, n, count + 1, cost + graph[currPosition][i], answer); 
+     // Mark ith node as unvisited 
+     visited[i] = false; 
+	   } 
+    } 
+	return answer; 
+	} 
+public static void main( String[] args) 
+	{ 
+        // n is the number of nodes i.e. V 
 	    int n;
 	    System.out.println(" Enter the number of villages: ");
-	    Scanner in = new Scanner(System.in);
+	    Scanner in = new Scanner( System.in );
 	    n = in.nextInt();
 		int graph[][] = new int[n][n];
 		System.out.println( "Enter the cost matrix " );
-		for(int i=0;i<n;i++)
-			{
-			    System.out.println( "Enter the cost matrix for village " + i+1 );
-				for(int j=0;j<n;j++)
-					graph[i][j]=in.nextInt();
-			}
+		for(int i=0; i<n; i++)
+		{
+		 System.out.println( "Enter the cost matrix for village " + i+1 );
+		 for(int j=0; j<n; j++)
+		 graph[i][j] = in.nextInt();
+		}
 		// Boolean array to check if a node 
 		// has been visited or not 
 		boolean[] visited = new boolean[n]; 
@@ -65,8 +60,7 @@ class TSP
     
 		// Find the minimum weight Hamiltonian Cycle 
 		answer = tspfn(graph, visited, 0, n, 1, 0, answer); 
-		// answer is the minimum weight Hamiltonian Cycle 
-		
+        // answer is the minimum weight Hamiltonian Cycle 
 		System.out.println(" The minimum cost is " +answer); 
 	} 
 } 
