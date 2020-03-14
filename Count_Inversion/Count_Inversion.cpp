bd24ba5da9e7f0f8979573f66802a3dd4b7d34d0
Mansi Agrawal <46123053+mansikagrawal@users.noreply.github.com>
2020-03-14 21:31:10 +0530

    Count_Inversion.cpp

--- Count_Inversion/Count_Inversion.cpp
+++ Count_Inversion/Count_Inversion.cpp
@@ -71,9 +71,9 @@ int main ()
     int n;
     cin >> n;
     int arr[n];
-    for(int i=0; i<n; i++)
+    for(int i=0; i < n; i++)
     cin >> arr[i];
-    cout << (inversions_count(arr, n))<<endl;
+    cout << (inversions_count(arr, n)) << endl;
     return 0;
 }
 
