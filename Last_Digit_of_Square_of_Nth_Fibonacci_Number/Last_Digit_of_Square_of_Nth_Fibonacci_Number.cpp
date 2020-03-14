4850a0d82a5720071df41a32270da57c43b46cbf
RedEye00 <55008117+RedEye00@users.noreply.github.com>
2020-03-14 21:19:00 +0530

    Issues Resolved please review

--- Last_Digit_of_Square_of_Nth_Fibonacci_Number/Last_Digit_of_Square_of_Nth_Fibonacci_Number.cpp
+++ Last_Digit_of_Square_of_Nth_Fibonacci_Number/Last_Digit_of_Square_of_Nth_Fibonacci_Number.cpp
@@ -8,28 +8,30 @@ int main(){
     a[1] = 1;
     b[0] = 0;
     b[1] = 1;
-    //Using the concept that after 60 numbers in a fibonacci numbers the last digit repeats itself again and again
-    for(i = 2 ; i < 60 ; i++ ){        //for storing the last digits of squares of first 60 fibonacci numbers
-        a[i] = (a[i-1] + a[i-2]) % 10;       
+    
+    /*Using the concept that after 60 numbers in a fibonacci 
+    numbers the last digit repeats itself again and again*/
+    for(i = 2 ; i < 60 ; i++){        //for storing the last digits of squares of first 60 fibonacci numbers
+        a[i] = (a[i - 1] + a[i - 2]) % 10;       
         c = a[i];                        
         b[i] = (c * c) % 10;            //Now we will store last digits in new array and their index as position in fibonacci series 
     } 
-    cin>> n ;  
-
-    cout<< "\nThe Position of Fibonacci Number: " << n ; //getting the required number
+    cin>> n ;
     
-    cout<< "\nThe Last Digit of Square of this Number : " << b[n % 60] ;//print the last digit given in that position
+    cout<< " \n The Last Digit of Square of this Number : " << b[ n % 60] ;//print the last digit given in that position
+    
     return 0;
 }
 /*
 Sample Input 1:
 2344
+
 Sample Output 1:
-The Position of Fibonacci Number: 2344
 The Last Digit of Square of this Number : 9
+
 Sample Input 2:
 267
+
 Sample Output 2:
-The Position of Fibonacci Number : 267
 The Last Digit of Square of this Number : 4
 */
