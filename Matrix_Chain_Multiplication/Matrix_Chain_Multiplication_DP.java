ded80ce064f7c6f408ede07783d12cf6d7a519b9
Ekta Gupta <50185634+eKta37@users.noreply.github.com>
2020-03-14 11:39:10 +0530

    Update Matrix_Chain_Multiplication_DP.java

--- Matrix_Chain_Multiplication/Matrix_Chain_Multiplication_DP.java
+++ Matrix_Chain_Multiplication/Matrix_Chain_Multiplication_DP.java
@@ -8,56 +8,56 @@ import java.util.Scanner;
 
 public class matrixChainMultiplication { 
 	
-	//function to find the optimized number of operations required for multiplication using DP
+	// function to find the optimized number of operations required for multiplication using DP
 	static int orderOfMultiplication(int arr[]) { 
 		int size = arr.length;
 		
-		//2D array to store optimal solution for all i
+		// 2D array to store optimal solution for all i
 		int dp[][] = new int[size][size]; 
 		
 		// cost will be zero for multiplying one matrix
-		for (int i=1; i<size; i++) { 
+		for (int i = 1; i < size; i++) { 
 			dp[i][i] = 0; 
 		}
 		
 		// l is the chain length
-		for (int l=2; l<size; l++) { 
-			for (int i=1; i<size-l+1; i++){ 
-				int j = i+l-1; 
+		for (int l = 2; l < size; l++) { 
+			for (int i = 1; i < (size - l + 1); i++){ 
+				int j = i + l - 1;  
 				if(j == size) {
 					continue; 
 				}
 				dp[i][j] = Integer.MAX_VALUE; 
-				for (int k=i; k<=j-1; k++) { 
+				for (int k = i; k <= j - 1; k++) { 
 					// temp is cost/scalar multiplications 
-					int temp = dp[i][k] + dp[k+1][j] + (arr[i-1] * arr[k] * arr[j]); 
+					int temp = dp[i][k] + dp[k + 1][j] + (arr[i - 1] * arr[k] * arr[j]); 
 					
-					//if this order gives better solution
+					// if this order gives better solution
 					if (temp < dp[i][j]) {
-						//update dp[i][j]
+						// update dp[i][j]
 						dp[i][j] = temp; 
 					}
 				} 
 			} 
 		} 
-		//optimal solution for the chain multiplication will be at the dp[1][size-1]
-		return dp[1][size-1]; 
+		// optimal solution for the chain multiplication will be at the dp[1][size-1]
+		return dp[1][size - 1]; 
 	} 
 
-	//Main function
+	//`Main function
 	public static void main(String args[]) { 
 		Scanner s = new Scanner(System.in);
 		System.out.print("Enter size of array - ");
 		int size = s.nextInt();
 		
-		//storing matrices sizes given by user
+		//`storing matrices sizes given by user
 		int arr[] = new int[size];
 		System.out.print("Enter values of array - ");
-		for(int i=0; i<size; i++) {
+		for(int i = 0; i <s ize; i++) {
 			arr[i] = s.nextInt();
 		}
 		
-		//function calling
+		//`function calling
 		int ans = orderOfMultiplication(arr); 
 		System.out.println("Minimum number of operations required - " + ans);
 		s.close();
@@ -68,8 +68,8 @@ public class matrixChainMultiplication {
  * =========================================================
  * Sample Input-Output
  * =========================================================
- * Input :  Enter size of array - 4
- * 			Enter values of array - 2 4 6 8
- * Output : Minimum number of operations required - 144
+ * Input :  	Enter size of array - 4
+ * 		Enter values of array - 2 4 6 8
+ * Output : 	Minimum number of operations required - 144
  * =========================================================
  */
