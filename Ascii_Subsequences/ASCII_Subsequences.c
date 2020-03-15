69ec5620633a5ca3e18979c25b2279551ab92d07
Aman-Codes <54680709+Aman-Codes@users.noreply.github.com>
2020-03-15 14:34:48 +0530

    Update ASCII_Subsequences.c

--- Ascii_Subsequences/ASCII_Subsequences.c
+++ Ascii_Subsequences/ASCII_Subsequences.c
@@ -3,24 +3,14 @@ This problem is same as the "Subsequences" question just instead of two function
 - One with the same string.
 - One with a letter added.
 - One with the ASCII code of the letter added to the string.
-- For example:-
-Take as input str, a string. We are concerned with all the possible ASCII-subsequences of str. E.g. “ab” has following ASCII-subsequences “”, “b”, “98”, “a”, “ab”, “a98”, “97”, “97b”, “9798”
-a. Write a recursive function which returns the count of ASCII-subsequences for a given string. Print the value returned.
-b. Write a recursive function which prints all possible ASCII-subsequences for a given string (void is the return type for function).
 */
-//*********************//
-/*Time Complexity:
-O(3^N) where N = String Length.*/
-//*******************//
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <math.h>
 
-#define SIZE 100
-
-char str[SIZE];
+char str[100];
 
 void subseq(char s[], int i, char str[])
 {
@@ -31,22 +21,27 @@ void subseq(char s[], int i, char str[])
     }
 
     // The 3 function calls in the same order as explained above.
-    subseq(s, i + 1, str);  // First function call
+    // First function call
+    subseq(s, i + 1, str);  
 
     int i1;
     char s2[strlen(s) + 2];
 
     for(i1 = 0; i1 < strlen(s); i1++)
     {
-        s2[i1] = s[i1]; // Copy string from s to s2
+        // Copy string from s to s2
+        s2[i1] = s[i1]; 
     }
-
-    s2[i1++] = str[i]; // Concatinate ith character of string str to s2
+    
+    // Concatinate ith character of string str to s2
+    s2[i1++] = str[i]; 
     s2[i1] = '\0';
-
-    subseq(s2, i + 1, str); // Second function call
-
-    int i2, n = (int)str[i]; // Integer n stores the ASCII value of the ith character of string str
+    
+    // Second function call
+    subseq(s2, i + 1, str); 
+    
+    // Integer n stores the ASCII value of the ith character of string str
+    int i2, n = (int)str[i]; 
     char s3[strlen(s) + 2];
 
     // Create AsciiString to store the ASCII value of the ith character of string str
@@ -54,7 +49,7 @@ void subseq(char s[], int i, char str[])
 
     if(n < 10)
     {
-        AsciiString[0] = (char)(n + 48); // 48 is the ASCII value of 0
+        AsciiString[0] = (char)(n + 48);
         AsciiString[1] = '\0';
     }
     else if(n<100)
@@ -73,19 +68,21 @@ void subseq(char s[], int i, char str[])
 
     for(i2 = 0; i2 < strlen(s); i2++)
     {
-        s3[i2] = s[i2]; // Copy string from s to s3
+        // Copy string from s to s3
+        s3[i2] = s[i2]; 
     }
 
     int i3 = i2;
 
     for(; i2 < strlen(AsciiString) + strlen(s); i2++)
     {
-       s3[i2] = AsciiString[i2 - i3]; // Concatinate AsciiString into s3
+       // Concatinate AsciiString into s3
+       s3[i2] = AsciiString[i2 - i3]; 
     }
 
     s3[i2] = '\0';
-
-    subseq(s3, i + 1, str); // Third function call
+    // Third function call
+    subseq(s3, i + 1, str); 
 
 }
 
