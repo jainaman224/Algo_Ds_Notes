4bc9849dcd1685bb13d1ca600266cb864b2169b2
darkdebo <debomastet335@gmail.com>
2020-03-15 14:24:48 +0530

    done with rufo

--- Armstrong_Number/Armstrong_Number.rb
+++ Armstrong_Number/Armstrong_Number.rb
@@ -1,42 +1,41 @@
 # A positive integer of n digits is called an Armstrong number of order n (order is number of digits) if
-# abcd... = pow(a,n) + pow(b,n) + pow(c,n) + pow(d,n) + .... 
+# abcd... = pow(a,n) + pow(b,n) + pow(c,n) + pow(d,n) + ....
 
 # To calculate the order of the number
 def order(num)
-	# variable to store of the number 
-    count = 0
-    while (num != 0) 
-        count = count + 1
-        num = num / 10
-    end
-    return count 
+  # variable to store of the number
+  count = 0
+  while (num != 0)
+    count = count + 1
+    num = num / 10
+  end
+  return count
 end
 
-# Function to check whether the given number is 
-# Armstrong number or not 
-def Armstrong (num) 
-    count = order(num) 
-    temp = num 
-    sum1 = 0
-    boolean = false
-    while (temp != 0)
-        d = temp % 10
-        sum1 = sum1 + d**count
-        temp = temp / 10
-    end
+# Function to check whether the given number is
+# Armstrong number or not
+def Armstrong(num)
+  count = order(num)
+  temp = num
+  sum1 = 0
+  boolean = false
+  while (temp != 0)
+    d = temp % 10
+    sum1 = sum1 + d ** count
+    temp = temp / 10
+  end
 
-    # If condition satisfies 
-	
-    if (num != sum1)
-        return boolean
-    else if(num == sum1)
-        boolean=true
-        return boolean 
-    end
-    end 
+  # If condition satisfies
+
+  if (num != sum1)
+    return boolean
+  else if (num == sum1)
+    boolean = true
+    return boolean
+  end   end
 end
 
-# Driver Program 
+# Driver Program
 num = gets.chomp.to_i
 print(Armstrong(num))
 
