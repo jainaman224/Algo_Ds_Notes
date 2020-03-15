f081da35f5f9b7985fa2d611a8aa463710ce1811
Aman-Codes <54680709+Aman-Codes@users.noreply.github.com>
2020-03-15 14:38:54 +0530

    Update ASCII_Subsequences.js

--- Ascii_Subsequences/ASCII_Subsequences.js
+++ Ascii_Subsequences/ASCII_Subsequences.js
@@ -3,15 +3,7 @@ This problem is same as the "Subsequences" question just instead of two function
 - One with the same string.
 - One with a letter added.
 - One with the ascii code of the letter added to the string.
-- For example:-
-Take as input str, a string. We are concerned with all the possible ASCII-subsequences of str. E.g. "ab" has following ascii-subsequences "", "b", "98", "a", "ab", "a98", "97", "97", "9798"
-a. Write a recursive function which returns the count of ASCII-subsequences for a given string. Print the value returned.
-b. Write a recursive function which prints all possible ASCII-subsequences for a given string (void is the return type for function).
 */
-//*********************//
-/*Time Complexity:
-O(3^N) where N = String Length.*/
-//*******************//
 
 function subseq(s, i, str)
 {
@@ -22,10 +14,13 @@ function subseq(s, i, str)
 	}
     
 	// The 3 function calls in the same order as explained above.
-	subseq(s, i + 1, str); // First function call
-	subseq(s + str[i], i + 1, str); // Second function call
+	// First function call
+	subseq(s, i + 1, str); 
+	// Second function call
+	subseq(s + str[i], i + 1, str); 
 	var n = str.charCodeAt(i); 
-	subseq(s + n.toString(), i + 1, str); // Third functon call with the ASCII value of the ith element of the string
+	// Third function call with the ASCII value of the ith element of the string
+	subseq(s + n.toString(), i + 1, str); 
 
 }
 
