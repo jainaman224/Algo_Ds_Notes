094d128fe0a11488379643f8551067995bf7ac57
Aman-Codes <54680709+Aman-Codes@users.noreply.github.com>
2020-03-16 09:06:56 +0530

    Update ASCII_Subsequences.js

--- Ascii_Subsequences/ASCII_Subsequences.js
+++ Ascii_Subsequences/ASCII_Subsequences.js
@@ -5,30 +5,29 @@ This problem is same as the "Subsequences" question just instead of two function
 - One with the ascii code of the letter added to the string.
 */
 
-function subseq(s, i, str)
+function subseq(s, i, str) 
 {
-	if(i === str.length) 
-	{
-		console.log(s);
-		return;
-	}
+    if(i === str.length) 
+    {
+        console.log(s);
+        return;
+    }
     
-	// The 3 function calls in the same order as explained above.
-	// First function call
-	subseq(s, i + 1, str); 
-	// Second function call
-	subseq(s + str[i], i + 1, str); 
-	var n = str.charCodeAt(i); 
-	// Third function call with the ASCII value of the ith element of the string
-	subseq(s + n.toString(), i + 1, str); 
-
+    // The 3 function calls in the same order as explained above.
+    // First function call
+    subseq(s, i + 1, str); 
+    // Second function call
+    subseq(s + str[i], i + 1, str); 
+    var n = str.charCodeAt(i); 
+    // Third function call with the ASCII value of the ith element of the string
+    subseq(s + n.toString(), i + 1, str); 
 }
 
 ( function IIFE() {
     var str = prompt("Enter the string");
-	console.log( "Number of Subsequences : ", Math.pow(3, str.length));
-	subseq("", 0, str);
-	return 0;
+    console.log( "Number of Subsequences : ", Math.pow(3, str.length));
+    subseq("", 0, str);
+    return 0;
 })();
 
 /*Sample Input: Enter the string:
