075fbdfe9ea1b5c719c2c34931735da652f8d8af
Manas2520 <60315152+Manas2520@users.noreply.github.com>
2020-03-17 17:19:45 +0530

    Number_Conversion(jainaman224#964)
    Updated Decimal_to_Binary.cpp

---
+++ Number_Conversion/C++/Decimal_To_Binary.cpp
@@ -0,0 +1,38 @@
+// C++ program to convert numbers from decimal to binary form
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+// Function to convert the number from decimal to binary form
+
+void printBinary(int n){
+
+    int arlength=log2(n)+1; // command to find out number of bits in the given number which decides the size of the array
+
+    int b[arlength], j=0; // Array to store the binary number
+
+    while(n!=0){
+        b[j]=n%2;
+        n=n/2;
+        j++;
+    }
+
+    cout<<"\nThe binary form is : ";
+    for(j=arlength-1; j>=0; j--)
+        cout<<b[j];
+}
+
+int main()
+{
+    cout<<"Enter the number you wish to convert:"<<endl;
+    int n;
+    cin>>n;
+    printBinary(n);
+    cout<<"\n"; // Calling function printBinary to convert the number to binary form
+    return 0;
+}
+/*
+Input : 25
+Output : 11001
+*/
