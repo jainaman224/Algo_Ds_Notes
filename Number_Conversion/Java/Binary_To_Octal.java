21acf4bb414997025954c2d6372b09d61bd1e49c
Mrunal <44579222+mruna7@users.noreply.github.com>
2020-03-17 13:33:34 +0530

    Update Binary_To_Octal.java

--- Number_Conversion/Java/Binary_To_Octal.java
+++ Number_Conversion/Java/Binary_To_Octal.java
@@ -1,16 +1,18 @@
 import java.io.*; 
 import java.util.Scanner;
-public class Binary_To_Octal {
+public class Binary_To_Octal 
+{
 	public static void binToOctal(long num)
-	{  int n=0,decimal=0;
+	{  
+		int n=0,decimal=0;
 		//converting binary to decimal
-	while(num > 0)
-	{
+	    while(num > 0)
+	   {
 		long temp = num%10;  
 		decimal += temp*Math.pow(2, n);  
 		num = num/10;  
 		n++;  
-	}
+	    }
 	//decimal to octal
 	int octal[] = new int[20];
 	int i = 0;
@@ -34,7 +36,6 @@ public class Binary_To_Octal {
         binToOctal(num);  
         sc.close();
 	}
-
 }
 /*
 Input:
