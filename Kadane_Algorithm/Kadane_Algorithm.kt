d46e0766eaf7a8b028bf8c3207154232f9c0892a
yuktta <yukta.mittal2@gmail.com>
2020-03-17 21:29:10 +0530

    Added Kadane's Algorithm in Kotlin with reference to issue #2247

--- Kadane_Algorithm/Kadane_Algorithm.kt
+++ Kadane_Algorithm/Kadane_Algorithm.kt
@@ -1,13 +1,15 @@
-/* Kadagne's Algorithm in Kotlin*/
+// Kadagne's Algorithm in Kotlin
 
-fun main(args: Array<String>) {
-    val numbers = readLine()!!.split(",").map(String::toInt)
-    var maxSum = 0
-    var sum = 0
-    for (number in numbers) {
-        sum += number
-        maxSum = Math.max(sum, maxSum)
-        sum = Math.max(sum, 0)
-    }
-    print(maxSum)
+fun main(args : Array<String>) 
+{
+     val numbers = readLine()!!.split(",").map(String :: toInt) //Input from User
+     var maxSum = 0   
+     var sum = 0
+     for (number in numbers) 
+     {                               //finding the maxSum
+          sum += number
+          maxSum = Math.max(sum, maxSum)
+          sum = Math.max(sum, 0)
+     }
+     print(maxSum)
 }
