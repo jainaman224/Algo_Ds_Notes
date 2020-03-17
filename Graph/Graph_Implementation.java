1ef4ce37f2fd4b24b7d2e5552d99fa96828c2b83
Ritika jain <ritikajain49026@gmail.com>
2020-03-17 23:33:56 +0530

    Update Graph_Implementation.java

--- Graph/Graph_Implementation.java
+++ Graph/Graph_Implementation.java
@@ -5,22 +5,18 @@ import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
 import java.util.*;
-
 class Graph {
     private class Vertex {
         String name;
         HashMap<Vertex, Integer> nbrs;
-
         Vertex(String name) {
             this.name = name;
             this.nbrs = new HashMap<>();
         }
-
         public boolean equals(Object other) {
             Vertex ov = (Vertex) other;
             return this.name.equals(ov.name);
         }
-
         public void display() {
             String str = "";
             str = this.name + "=>";
@@ -30,44 +26,33 @@ class Graph {
             }
             str = str + "END";
             System.out.println(str);
-
         }
     }
-
     HashMap<String, Vertex> vtces;
-
     public Graph() {
         this.vtces = new HashMap<>();
     }
-
     public int numvertices() {
         return this.vtces.size();
     }
-
     public void addVertex(String name) {
         if (this.vtces.containsKey(name)) {
             return;
         }
-
         Vertex vtx = new Vertex(name);
         this.vtces.put(name, vtx);
     }
-
     public void removeVertex(String name) {
         if (!this.vtces.containsKey(name)) {
             return;
         }
-
         Vertex vtx = this.vtces.get(name);
         Set<Vertex> nbrs = vtx.nbrs.keySet();
         for (Vertex nbr : nbrs) {
             nbr.nbrs.remove(vtx);
         }
-
         this.vtces.remove(name);
-
     }
-
     public int numedges() {
         int rv = 0;
         Collection<Vertex> vtces = this.vtces.values();
@@ -77,31 +62,24 @@ class Graph {
         rv = rv / 2;
         return rv;
     }
-
     public void addEdge(String name1, String name2, int cost) {
         Vertex vtx1 = this.vtces.get(name1);
         Vertex vtx2 = this.vtces.get(name2);
         if (vtx1 == null || vtx2 == null || vtx1.nbrs.containsKey(vtx2)) {
             return;
         }
-
         vtx1.nbrs.put(vtx2, cost);
         vtx2.nbrs.put(vtx1, cost);
-
     }
-
     public void removeEdge(String name1, String name2) {
         Vertex vtx1 = this.vtces.get(name1);
         Vertex vtx2 = this.vtces.get(name2);
         if (vtx1 == null || vtx2 == null || !vtx1.nbrs.containsKey(vtx2)) {
             return;
         }
-
         vtx1.nbrs.remove(vtx2);
         vtx2.nbrs.remove(vtx1);
-
     }
-
     public void display() {
         Collection<Vertex> vtces = this.vtces.values();
         for (Vertex vtx : vtces) {
@@ -110,9 +88,7 @@ class Graph {
         System.out.println("*****************");
     }
 }
-
 class client {
-
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         // TODO Auto-generated method stub
@@ -132,7 +108,6 @@ class client {
         }
         graph.display();
     }
-
 }
 
 /*
