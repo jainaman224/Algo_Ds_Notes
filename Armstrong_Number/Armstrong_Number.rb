dd447d677e9463fcb6829f3504ad3c5d7f093569
darkdebo <debomastet335@gmail.com>
2020-03-17 15:27:34 +0530

    Changes to be committed: 	modified:   Armstrong_Number/Armstrong_Number.rb

--- Armstrong_Number/Armstrong_Number.rb
+++ Armstrong_Number/Armstrong_Number.rb
@@ -4,27 +4,27 @@
 # To calculate the order of the number
 def order(num)
   # variable to store of the number
-  count = 0
-  while (num != 0)
-    count = count + 1
-    num = num / 10
-  end
-  return count
+    count = 0
+    while (num != 0)
+        count = count + 1
+        num = num / 10
+    end
+    return count
 end
 
 # Function to check whether the given number is
 # Armstrong number or not
 def Armstrong(num)
-  count = order(num)
-  temp = num
-  sum1 = 0
-  boolean = false
-  while (temp != 0)
-    d = temp % 10
-    sum1 = sum1 + d ** count
-    temp = temp / 10
-  end
-  return num == sum1
+    count = order(num)
+    temp = num
+    sum1 = 0
+    boolean = false
+    while (temp != 0)
+        d = temp % 10
+        sum1 = sum1 + d ** count
+        temp = temp / 10
+    end
+    return num == sum1
 end
 
 # Driver Program
