c3f4cc305b5c5286dd038ce6bbdf6d472e0a84ff
Chandrika Deb <chandrikadeb7@gmail.com>
2020-03-17 11:57:21 +0530

    Updated

--- Minesweeper/minesweeper.cpp
+++ Minesweeper/minesweeper.cpp
@@ -14,12 +14,12 @@ void clrscr()
 
 bool isvalid(int row, int col)
 {
-   return (row >= 0) && (row < SIDE) && (col >= 0) && (col < SIDE);
+    return (row >= 0) && (row < SIDE) && (col >= 0) && (col < SIDE);
 }
 
 bool ismine(int row, int col, char board[][max_side])
 {
-    if(board[row][col]=='B')
+    if(board[row][col] == 'B')
         return (true);
     else
         return (false);
@@ -36,33 +36,33 @@ void board(char myboard[][max_side])
 {
     clrscr();
     printf("\n\n\t\t\t    ");
-    for(int i=0;i<SIDE;i++)
+    for(int i = 0; i < SIDE; i++)
     {
-        if (i>9)
+        if (i > 9)
             printf("%d ",i/10);
-	    else
-	        printf("  ");
-    }		
+        else
+            printf("  ");
+    }
     printf("\n\t\t\t    ");
-    for(int i=0;i<SIDE;i++)
-	    printf("%d ",i%10);
+    for(int i = 0; i < SIDE; i++)
+        printf("%d ",i%10);
     printf("\n\n");
-    for(int i=0;i<SIDE;i++)
+    for(int i = 0; i < SIDE; i++)
     {
         printf("\t\t\t    ");
-        for(int j=0;j<SIDE;j++)
-		{
-	    	printf("%c ",myboard[i][j]);
-		}
-		printf(" %2d",i);
-		printf("\n");
+        for(int j = 0; j < SIDE; j++)
+        {
+            printf("%c ",myboard[i][j]);
+        }
+        printf(" %2d",i);
+        printf("\n");
     }
     return;
 }
 
 int countadjacent(int row, int col, int mines[][2], char realboard[][max_side])
 {
-    int count=0;
+    int count = 0;
     if(isvalid(row - 1, col) == true)
     {
     	if(ismine(row - 1, col, realboard) == true)
@@ -110,99 +110,98 @@ bool playuntil(char myboard[][max_side], char realboard[][max_side], int mines[]
 {
 	if(myboard[row][col] != '-')
 	    return false;
-	int i,j;
+	int i, j;
 	if(realboard[row][col] == 'B')
 	{
-	    myboard[row][col]='B';
-	    for(i=0;i<MINES;i++)
-	        myboard[mines[i][0]][mines[i][1]]='B';	
+	    myboard[row][col] = 'B';
+	    for(i = 0;i < MINES; i++)
+	        myboard[mines[i][0]][mines[i][1]] = 'B';	
 	    board(myboard);
 	    printf ("\nYou lost!\n");
 	    return (true);
 	}
 	else
-	{
-	    int count=countadjacent(row,col,mines,realboard);
-	    (*moves_left)--;
-	    myboard[row][col]= count+'0';
-	    if(!count)
-	    {
-			if(isvalid(row - 1, col) == true)
-			{
-		    	if(ismine(row - 1, col, realboard) == false)
-	   				playuntil(myboard, realboard, mines, row - 1, col, moves_left);
-			}   
-			if (isvalid (row + 1, col) == true)
-			{
-		    	if (ismine (row + 1, col, realboard) == false)
-					playuntil(myboard, realboard, mines, row + 1, col, moves_left);
-			}
-			if (isvalid (row, col + 1) == true)
-			{
-		    	if (ismine (row, col + 1, realboard) == false)
-		        	playuntil(myboard, realboard, mines, row, col + 1, moves_left);
-			}
+    {
+        int count = countadjacent(row, col, mines, realboard);
+        (*moves_left)--;
+        myboard[row][col] = count + '0';
+        if(!count)
+        {
+            if(isvalid(row - 1, col) == true)
+            {
+                if(ismine(row - 1, col, realboard) == false)
+                    playuntil(myboard, realboard, mines, row - 1, col, moves_left);
+            }
+            if (isvalid (row + 1, col) == true)
+            {
+                if (ismine (row + 1, col, realboard) == false)
+                    playuntil(myboard, realboard, mines, row + 1, col, moves_left);
+            }
+            if (isvalid (row, col + 1) == true)
+            {
+                if (ismine (row, col + 1, realboard) == false)
+                    playuntil(myboard, realboard, mines, row, col + 1, moves_left);
+            }
             if (isvalid (row, col - 1) == true)
-			{
-		    	if (ismine (row, col - 1, realboard) == false)
-					playuntil(myboard, realboard, mines, row, col - 1, moves_left);
-			}
-			if (isvalid (row - 1, col + 1) == true)
-			{
-		    	if (ismine (row - 1, col + 1, realboard) == false)
-					playuntil(myboard, realboard, mines, row - 1, col + 1, moves_left);
-			}     
-			if (isvalid (row - 1, col - 1) == true)
-			{
-		    	if (ismine (row - 1, col - 1, realboard) == false)
-					playuntil(myboard, realboard, mines, row - 1, col - 1, moves_left);
-			}
+            {
+                if (ismine (row, col - 1, realboard) == false)
+                    playuntil(myboard, realboard, mines, row, col - 1, moves_left);
+            }
+            if (isvalid (row - 1, col + 1) == true)
+            {
+                if (ismine (row - 1, col + 1, realboard) == false)
+                    playuntil(myboard, realboard, mines, row - 1, col + 1, moves_left);
+            }
+            if (isvalid (row - 1, col - 1) == true)
+            {
+                if (ismine (row - 1, col - 1, realboard) == false)
+                    playuntil(myboard, realboard, mines, row - 1, col - 1, moves_left);
+            }
             if (isvalid (row + 1, col + 1) == true)
-			{
-		    	if (ismine (row + 1, col + 1, realboard) == false)
-					playuntil(myboard, realboard, mines, row + 1, col + 1, moves_left);
-			}
-        	if (isvalid (row + 1, col - 1) == true)
-			{
-		    	if (ismine (row + 1, col - 1, realboard) == false)
-					playuntil(myboard, realboard, mines, row + 1, col - 1, moves_left);
-			}
-
-	    }
-	    return (false);
-	}
+            {
+                if (ismine (row + 1, col + 1, realboard) == false)
+                    playuntil(myboard, realboard, mines, row + 1, col + 1, moves_left);
+            }
+            if (isvalid (row + 1, col - 1) == true)
+            {
+                if (ismine (row + 1, col - 1, realboard) == false)
+                    playuntil(myboard, realboard, mines, row + 1, col - 1, moves_left);
+            }
+        }
+        return (false);
+    }
 }
 
 void placemines(int mines[][2],char realboard[][max_side])
 {
     bool mark[max_side*max_side];
-    memset(mark,false,sizeof(mark));
-    for(int i=0;i<MINES;)
+    memset(mark, false, sizeof(mark));
+    for(int i = 0; i < MINES;)
     {
-		int random=rand()%(SIDE*SIDE);
- 		int x=random/SIDE;
-		int y=random%SIDE;
-		if(mark[random] == false) //add mine if not present at position random
-		{
-			mines[i][0]=x;
-	        mines[i][1]=y;
-	        realboard[mines[i][0]][mines[i][1]]='B';
-	        mark[random]=true;
-	        i++;
-		}
+        int random = rand() % (SIDE*SIDE);
+        int x = random/SIDE;
+        int y = random%SIDE;
+        if(mark[random] == false) //add mine if not present at position random
+        {
+            mines[i][0] = x;
+            mines[i][1] = y;
+            realboard[mines[i][0]][mines[i][1]] = 'B';
+            mark[random] = true;
+            i++;
+        }
     }
 }
 
 void initialise(char realboard[][max_side], char myboard[][max_side])
 {
     srand(time(NULL)); //initalising random so that same config doesn't arise
-    int i,j;
-    for(i=0;i<SIDE;i++)
-	    for(j=0;j<SIDE;j++)
-	    {
-	        myboard[i][j]=realboard[i][j]='-';
-	    }
-	    return;
+    int i, j;
+    for(i = 0; i < SIDE; i++)
+        for(j = 0; j < SIDE; j++)
+        {
+            myboard[i][j] = realboard[i][j] = '-';
+        }
+    return;
 }
 
 void cheatmines (char realboard[][max_side])
@@ -214,79 +213,79 @@ void cheatmines (char realboard[][max_side])
 
 void replacemine(int row, int col, char board[][max_side])
 {
-	for(int i=0;i<SIDE;i++)
-	{
-		for(int j=0;j<SIDE;j++)
-		{
-			if(board[i][j]!='B')
-			{
-			    board[i][j]='B';
-				board[row][col]='-';
-				return;
-			}
-		}
-	}		
+    for(int i = 0; i < SIDE; i++)
+    {
+        for(int j = 0; j < SIDE; j++)
+        {
+            if(board[i][j] != 'B')
+            {
+                board[i][j] = 'B';
+                board[row][col] = '-';
+                return;
+            }
+        }
+    }
 }
 
 void play()
 {
-	bool gameover=false;
-	char realboard[max_side][max_side],myboard[max_side][max_side];
-	int moves_left=SIDE*SIDE-MINES;
-	int x,y;
-	int mines[max_mine][2]; //stores (x,y) of all mines
-	initialise(realboard,myboard);
-	placemines(mines,realboard);
-	//if you want cheat and win 
-	//cheatmines(realboard);
-	int currentmoveindex=0;
-	while(gameover == false)
-	{
-		printf ("Current Status of Board : \n");
-		board(myboard);
-		move(&x,&y);
-		//if first move is mine
-		if(currentmoveindex == 0)
-		{
-			if(ismine(x,y,realboard) == true) //first attempt is a mine
-				replacemine(x,y,realboard); //replace the mine at that location
-		}
-		currentmoveindex++;
-		gameover = playuntil(myboard,realboard,mines,x,y,&moves_left);
-		if((gameover == false) && (moves_left == 0))
-		{
-			printf ("\nYou won !\n");
-			gameover=true;
-		}
-	}
+    bool gameover = false;
+    char realboard[max_side][max_side],myboard[max_side][max_side];
+    int moves_left = SIDE*SIDE-MINES;
+    int x,y;
+    int mines[max_mine][2]; //stores (x,y) of all mines
+    initialise(realboard, myboard);
+    placemines(mines, realboard);
+    //if you want cheat and win 
+    //cheatmines(realboard);
+    int currentmoveindex = 0;
+    while(gameover == false)
+    {
+        printf ("Current Status of Board : \n");
+        board(myboard);
+        move(&x, &y);
+        //if first move is mine
+        if(currentmoveindex == 0)
+        {
+            if(ismine(x, y, realboard) == true) //first attempt is a mine
+                replacemine(x, y, realboard); //replace the mine at that location
+        }
+        currentmoveindex++;
+        gameover = playuntil(myboard, realboard, mines, x, y, &moves_left);
+        if((gameover == false) && (moves_left == 0))
+        {
+            printf ("\nYou won !\n");
+            gameover = true;
+        }
+    }
 }
 
 void chdiff()
 {
     clrscr();
-    cout<<"\nMINESWEEPER GAME";
-    cout<<"\n\nCHOOSE YOUR DIFFICULTY LEVEL : ";
-    cout<<"\n\n0.BEGINNER\n1.MEDIUM\n2.HARD";
-    cout<<"\n\nENTER YOUR LEVEL (0-2) : ";
+    cout << "\nMINESWEEPER GAME";
+    cout << "\n\nCHOOSE YOUR DIFFICULTY LEVEL : ";
+    cout << "\n\n0.BEGINNER\n1.MEDIUM\n2.HARD";
+    cout << "\n\nENTER YOUR LEVEL (0-2) : ";
     int ch;
-    cin>>ch;
+    cin >> ch;
     if(ch == 0)
     {
-	    SIDE=9; 
-	    MINES=10;
+        SIDE = 9; 
+        MINES = 10;
     }
     else if(ch == 1)
-	{
-		SIDE=16; 
-	  	MINES=40;
-	}
-	else if(ch == 2)
-	{
-		SIDE=24; 
-		MINES=99;
-	}   
-	else	
-		exit(0);
+    {
+        SIDE = 16;
+        MINES = 40;
+    }
+    else if(ch == 2)
+    {
+        SIDE = 24;
+        MINES = 99;
+    }
+    else
+        exit(0);
 }
 
 int main()
