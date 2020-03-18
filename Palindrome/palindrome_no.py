0dd008f0f59fbaa866b09a86fda2f674f53c4596
Raksha <57195964+raksha009@users.noreply.github.com>
2020-03-18 17:25:08 +0530

    Update palindrome_no.py

--- Palindrome/palindrome_no.py
+++ Palindrome/palindrome_no.py
@@ -6,19 +6,18 @@ def reverse(num):
     # Initialize variable
     rev=0
 
-    while(num>0):
-        dig=num%10
-        rev=rev*10+dig
-        num=num//10
+    while ( num > 0 ) :
+        dig = num % 10
+        rev = rev * 10 + dig
+        num = num // 10
     return rev    # Returning reversed number
 
     
 # --- main --- 
-num=int(input("Enter a number:"))  #User Input
+num=int(input("Enter a number:"))
+a  = reverse(num)
 
-a  = reverse(num) #Function call
-
-if(a==num):  #Comparing the reversed number with original number
+if(a == num):  #Comparing the reversed number with original number
     print("Number entered is palindrome!")
 else:
     print("Number entered is not a palindrome!")
@@ -26,10 +25,11 @@ else:
 
 '''
 TEST CASES
-
+INPUT
 Enter a number:3445443
 Number entered is palindrome!
 
+OUTPUT
 Enter a number:234
 Number entered is not a palindrome!
 '''
