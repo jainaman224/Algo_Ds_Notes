6951b9d139f524e9f076d9d634d8dac4b492e2e2
Bhavyaa Arora <44175278+Bhavyaa08@users.noreply.github.com>
2020-03-18 13:51:27 +0530

    Update Is_Binary_Search_Tree.java

--- Is_Binary_Search_Tree/Is_Binary_Search_Tree.java
+++ Is_Binary_Search_Tree/Is_Binary_Search_Tree.java
@@ -1,86 +1,54 @@
+import java.util.Scanner;
+
+public class BinaryTree {
+	
+    class Node {
+	int data;
+	Node left;
+	Node right;
+	    
+	public Node(int x) 
+	{
+	    this.data = x;
+	}
+     }
     
-    import java.util.Scanner;
-
-    public class BinaryTree {
-        class Node {
-	    int data;
-	    Node left;
-	    Node right;
-	}
+    public Node root;
     
-	private Node root;
-	private int size;
-	public static Scanner scn = new Scanner(System.in);
+    public static Scanner scn = new Scanner(System.in);
+   
+    public static boolean isBST(Node node) {
 
-	public BinaryTree() {
-	    this.root = this.takeInput(null, false);
-	}
+ 	if (node == null)
+	    return true;
 
-	public Node takeInput(Node parent, boolean ilc) {
+	boolean l = isBST(node.left); // l will store whether the left subtree is BST or not
+	boolean r = isBST(node.right); // r will store whether the right subtree is BST or not
 
-	    int cdata = scn.nextInt();
-	    Node child = new Node();
-	    child.data = cdata;
-	    this.size++;
+	//the below given conditions will return false if the current node doesnt satisfy the property of a BST
+	if(node.left != null && node.data < node.left.data)
+	    return false;
 
-	    // left
-	    boolean hlc = scn.nextBoolean();
+	if(node.right != null && node.data > node.right.data)
+	    return false;
 
-	    if (hlc) {
-		child.left = this.takeInput(child, true);
-	    }
-
-	    // right
-	    boolean hrc = scn.nextBoolean();
-  
-            if (hrc) {
-	        child.right = this.takeInput(child, false);
-	    }
-
-	    // return
-	    return child;
-	}
-
-	public boolean isBST() {
-	    return isBST(root);
-	}
-
-	private boolean isBST(Node node) {
-
-	    if (node == null)
-		return true;
-
-	    boolean l = isBST(node.left); 
-	    // l will store whether the left subtree is BST or not
-	    boolean r = isBST(node.right); 
-	    // r will store whether the right subtree is BST or not
-
-	    //the below given conditions will return false if the current node doesnt satisfy the property of a BST
-	    if(node.left != null && node.data < node.left.data)
-	        return false;
-
-	    if(node.right != null && node.data > node.right.data)
-		return false;
-
-	    //if the curr node satifies the BST property then we will return true if both l and r subtree are BST or not
-	    return l && r;
-	}
-
-	public static void main(String[] args) {
-
-	    BinaryTree bt = new BinaryTree();
-	    System.out.println(bt.isBST());
-	}
+	//if the curr node satifies the BST property then we will return true if both l and r subtree are BST or not
+	return l && r;
     }
 
-   /*Sample Input :
-     4 true 2 true 1 false false true 3 false false true 6 true 5 false false true 7 false false
-     Sample Output : true
-     Also The tree is :
-	2-> 4 <-6
-	1-> 2 <-3
-	. -> 1 <- .
-	. -> 3 <- .
-	5-> 6 <-7
-	. -> 5 <- .
-	. -> 7 <- . */
+    public static void main(String[] args) {
+
+	Node root = new Node(5); 
+    	root.left = new Node(2); 
+    	root.right = new Node(15); 
+    	root.left.left = new Node(1); 
+    	root.left.right = new Node(4); 
+	System.out.println(isBST(root));
+    }
+}
+
+  /*Sample Input :- 	
+	2-> 5 <-15
+	1-> 2 <-4
+    Output :- true
+  */
