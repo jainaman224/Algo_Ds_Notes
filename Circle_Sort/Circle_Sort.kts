6767a7c4ca97a22707df91dfe14dcb9793577e38
Anuj Singh <43649349+myselfanuj@users.noreply.github.com>
2020-03-20 02:10:42 +0530

    Arranged Code

--- Circle_Sort/Circle_Sort.kts
+++ Circle_Sort/Circle_Sort.kts
@@ -2,15 +2,16 @@ fun<T: Comparable<T>> circleSort(array: Array<T>, lo: Int, hi: Int, nSwaps: Int)
     if (lo == hi) return nSwaps
  
     fun swap(array: Array<T>, i: Int, j: Int) {
-        val temp  = array[i]
-        array[i]  = array[j]
-        array[j]  = temp
+        val temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
     }
  
-    var high  = hi
-    var low   = lo
-    val mid   = (hi - lo) / 2
+    var high = hi
+    var low  = lo
+    val mid  = (hi - lo) / 2
     var swaps = nSwaps
+   
     while (low < high) {
         if (array[low] > array[high]) {
             swap(array, low, high)
@@ -24,19 +25,23 @@ fun<T: Comparable<T>> circleSort(array: Array<T>, lo: Int, hi: Int, nSwaps: Int)
             swap(array, low, high + 1)
             swaps++
         }
+    
     swaps = circleSort(array, lo, lo + mid, swaps)
     swaps = circleSort(array, lo + mid + 1, hi, swaps)
     return swaps
 }
  
 fun main(args: Array<String>) {
-    val array = arrayOf(6, 7, 8, 9, 2, 5, 3, 4, 1)
-    println("Original: ${array.asList()}")
-    while (circleSort(array, 0, array.size - 1, 0) != 0) ; // empty statement
-    println("Sorted  : ${array.asList()}")
+    // -- ExampleArray1 --
+    val array = arrayOf(6, 7, 8, 9, 2, 5, 3, 4, 1) 
+    println("Original Array: ${array.asList()}")
+    while (circleSort(array, 0, array.size - 1, 0) != 0) ; 
+    println("Sorted Array : ${array.asList()}") //Sorted Array
     println()
-    val array2 = arrayOf("the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog")
-    println("Original: ${array2.asList()}")
+    
+    // -- ExampleArray2 --
+    val array2 = arrayOf("the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog")  
+    println("Original Array : ${array2.asList()}")
     while (circleSort(array2, 0, array2.size - 1, 0) != 0) ;
-    println("Sorted  : ${array2.asList()}")    
+    println("Sorted  Array : ${array2.asList()}")    //Sorted Array
 }
