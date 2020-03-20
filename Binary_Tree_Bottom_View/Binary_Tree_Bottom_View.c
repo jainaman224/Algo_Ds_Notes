a548a8d5ebf1e0b1b4945a113b6626ed72982fe3
AkanshaKamboj <42874293+AkanshaKamboj@users.noreply.github.com>
2020-03-20 10:49:30 +0530

    Update Binary_Tree_Bottom_View.c

--- Binary_Tree_Bottom_View/Binary_Tree_Bottom_View.c
+++ Binary_Tree_Bottom_View/Binary_Tree_Bottom_View.c
@@ -4,7 +4,7 @@
 #include<stdlib.h>
 #include<limits.h>
 
-/* A binary tree node has data, pointer to left child and a pointer to right child */
+// A binary tree node has data, pointer to left child and a pointer to right child
 struct Node
 {
     int data;
@@ -12,61 +12,59 @@ struct Node
     struct Node* right;
 };
 
-
 /* Inserting elements into Binary Tree*/
 struct Node *create()
 {
     Node *ptr;
     int x;
     printf("Enter data(-1 for no node):");
-    scanf("%d",&x);
+    scanf("%d", &x);
 
     if(x == -1)
     return NULL;
 
     ptr = (struct Node*)malloc(sizeof(struct Node));
-    ptr->data = x;
+    ptr -> data = x;
     
     printf("Enter left child of %d:\n", x);
-    ptr->left = create();
+    ptr -> left = create();
     
     printf("Enter right child of %d:\n", x);
-    ptr->right = create();
+    ptr -> right = create();
     
     return ptr;
 }
 
-/* Calculating the level of the node and the vertical distance of the node and 
-    finding the nodes that appear as the top view*/
-
+// Calculating the level and the vertical distance of the node and finding the nodes that appear as the top view
 void findBottomView(struct Node *root, int *a, int *b, int level, int dist)
 {
     if(root == NULL)
         return;
 
-    if(level > a[50+dist])
+    if(level > a[50 + dist])
     {
-        a[50+dist] = level;
-        b[50+dist] = root->data;
+        a[50 + dist] = level;
+        b[50 + dist] = root -> data;
     }
     
     # Recursively calling the right and the left child to get the bottom view
     
-    findBottomView(root->right, a, b, level+1, dist+1);
-    
-    findBottomView(root->left, a, b, level+1, dist-1);
+    findBottomView(root -> right, a, b, level + 1, dist + 1);
+    findBottomView(root -> left, a, b, level + 1, dist - 1);
 }
 
 int *bottomView(struct Node* root, int *len) {
     
-    int i, o=0;
-
-    int *a = (int *)calloc(100,sizeof(int));
-    int *b = (int *)calloc(100,sizeof(int));
-    int *c = (int *)calloc(100,sizeof(int));
+    int i, o = 0;
     
-    /* Initialising a(level of the node) and b(Vertical distance of the node from root) as INT_MIN as we need to print 
-       bottom most elements*/
+    // a stores level of the node
+    // b stores vertical distance of the node from root
+    
+    int *a = (int *)calloc(100, sizeof(int));
+    int *b = (int *)calloc(100, sizeof(int));
+    int *c = (int *)calloc(100, sizeof(int));
+    
+    // Initialising a and b as INT_MIN as we need to print bottom most elements
     for(i = 0; i < 100; i++)
     {
         a[i] = INT_MIN;
