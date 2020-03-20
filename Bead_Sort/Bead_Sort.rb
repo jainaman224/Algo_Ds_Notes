00345d25e8adf76d1c2e51c424f63b5ee9c7e2b7
darkdebo <debomastet335@gmail.com>
2020-03-20 19:39:06 +0530

    Done with indentation  Changes to be committed: 	modified:   Bead_Sort.rb

--- Bead_Sort/Bead_Sort.rb
+++ Bead_Sort/Bead_Sort.rb
@@ -4,28 +4,28 @@
 # Implementation of Bead sort in Ruby
 
 class Array
-  # BeadSort algorithm function
-  def beadsort
-    # this will take the matrix and make transpose of it to sort.
-    map { |e| [1] * e }.columns.columns.map(&:length)
-  end
-
-  # Columns for the matrix
-  def columns
-    y = length
-    x = map(&:length).max
-    Array.new(x) do |row|
-      Array.new(y) { |column| self[column][row] }.compact # it remove null value
+    # BeadSort algorithm function
+    def beadsort
+        # this will take the matrix and make transpose of it to sort.
+        map { |e| [1] * e }.columns.columns.map(&:length)
     end
-  end
+
+    # Columns for the matrix
+    def columns
+        y = length
+        x = map(&:length).max
+        Array.new(x) do |row|
+        Array.new(y) { |column| self[column][row] }.compact # it remove null value
+        end
+    end
 end
 
 # function for taking dynamic array input.
 def myarray(a, n)
-  for i in 0..n
-    a[i] = gets.chomp.to_i
-  end
-  return a
+    for i in 0..n
+        a[i] = gets.chomp.to_i
+    end
+    return a
 end
 
 # Driver code:
@@ -34,8 +34,8 @@ n = gets.chomp.to_i
 new_arr = myarray(a, n)
 print(new_arr.beadsort.reverse())
 
-#Output
-#Before sorting
-#23 2 12 54 90 102 32
-#After sorting
-#2 12 23 32 54 90 102
+# Output
+# Before sorting
+# 23 2 12 54 90 102 32
+# After sorting
+# 2 12 23 32 54 90 102
