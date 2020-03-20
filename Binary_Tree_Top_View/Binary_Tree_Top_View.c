c16f0a146a6278e09d8b300bb5bf80bac8c3c188
AkanshaKamboj <42874293+AkanshaKamboj@users.noreply.github.com>
2020-03-20 10:55:15 +0530

    Update Binary_Tree_Top_View.c

--- Binary_Tree_Top_View/Binary_Tree_Top_View.c
+++ Binary_Tree_Top_View/Binary_Tree_Top_View.c
@@ -13,8 +13,7 @@ struct Node
     struct Node* right;
 };
 
-
-/* Inserting elements into Binary Tree*/
+// Inserting elements into Binary Tree
 struct Node *create()
 {
     Node *ptr;
@@ -27,26 +26,24 @@ struct Node *create()
     return NULL;
 
     ptr = (struct Node*)malloc(sizeof(struct Node));
-    ptr->data = x;
+    ptr -> data = x;
     
     printf("Enter left child of %d:\n",x);
-    ptr->left = create();
+    ptr -> left = create();
     
     printf("Enter right child of %d:\n",x);
-    ptr->right = create();
+    ptr -> right = create();
     
     return ptr;
 }
 
-/* Calculating the level of the node and the vertical distance of the node and 
-    finding the nodes that appear as the top view*/
-
+// Calculating the level and the vertical distance of the node and finding the nodes that appear as the top view
 void findTopView(struct Node *root, int *a, int *b, int level, int dist)
 {
     if(root == NULL)
         return;
 
-    if(level < a[50+dist])
+    if(level < a[50 + dist])
     {
         a[50 + dist] = level;
         b[50 + dist] = root->data;
@@ -60,14 +57,15 @@ void findTopView(struct Node *root, int *a, int *b, int level, int dist)
 
 int *topView(struct Node* root, int *len) {
     
-    int i,o=0;
-
-    int *a = (int *)calloc(100,sizeof(int));
-    int *b = (int *)calloc(100,sizeof(int));
-    int *c = (int *)calloc(100,sizeof(int));
+    int i,o = 0;
     
-    /* Initialising a(level of the node) and b(Vertical distance of the node from root) as INT_MAX as we need to print 
-       top most elements*/
+    // a stores the level of the node
+    // b stores the Vertical distance of the node from root
+    int *a = (int *)calloc(100, sizeof(int));
+    int *b = (int *)calloc(100, sizeof(int));
+    int *c = (int *)calloc(100, sizeof(int));
+    
+    // Initialising a and b as INT_MAX as we need to print top most elements
     for(i = 0; i < 100; i++)
     {
         a[i] = INT_MAX;
