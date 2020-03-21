6dcf17e74d63f235f1d21d6315dfd88d2056b150
Abhishek Gupta <39022530+abhishekgupta368@users.noreply.github.com>
2020-03-21 11:55:52 +0530

    space_update4

--- Reverse_Linked_List/Reverse_Linked_List.cpp
+++ Reverse_Linked_List/Reverse_Linked_List.cpp
@@ -1,31 +1,32 @@
 #include<bits/stdc++.h>
+
 using namespace std;
 
 class Node {
-	/*
-	this class is used to create node
-	---------
-	| 0  |   |->NULL
-	---------
-	*/
+    /*
+    this class is used to create node
+    ---------
+    | 0  |   |->NULL
+    ---------
+    */
     public:
-       int data;
-       Node* next;
-       Node(int n){
-           data = n;
-	   next = NULL;
-       }
+    int data;
+    Node * next;
+    Node(int n) {
+        data = n;
+        next = NULL;
+    }
 };
 
 class LinkedList {
     private:
-        Node* head;
-        Node* tail;
+    Node * head;
+    Node * tail;
     public:
-        LinkedList(){
-	    head = NULL;
-	    tail = NULL;
-	}
+        LinkedList() {
+            head = NULL;
+            tail = NULL;
+        }
     /*
 	this will add node to the linkedlist node and refer it to node type pointer
 	---------   ---------  	---------
@@ -33,63 +34,62 @@ class LinkedList {
 	---------   ---------   ---------
     */
     // here, we insert node to the queue
-    void addNode(int data){
-    // if head node is empty, then, initialize it.
-    // else increasse append the node using tail pointer. 
-        if(head == NULL){
-            Node* node = new Node(data);
-	    node -> next = head;
-	    head = node;
-	    tail = node;
-	}
-	else{
-	    Node* node = new Node(data);
-	    tail -> next = node;
-	    tail = node;
+    void addNode(int data) {
+        // if head node is empty, then, initialize it.
+        // else increasse append the node using tail pointer. 
+        if (head == NULL) {
+            Node * node = new Node(data);
+            node -> next = head;
+            head = node;
+            tail = node;
+        } else {
+            Node * node = new Node(data);
+            tail -> next = node;
+            tail = node;
         }
-     }
-     void rev(){
-	 // Initialize current=head, previous=NULL and 
-         // next_node=NULL 
-         Node* current  = head;
-	 Node* previous = NULL;
-	 Node* next_node = NULL;
-	 while(current != NULL){
-	    // Store next 
-	    next_node = current->next;
-	    // Reverse current node's pointer 
-	    current -> next = previous;
-	    // Move pointers one position ahead.
-	    previous = current;
-	    current = next_node;
-	  }
-	  head = previous;
-      }
-	
-      void print() {
-          Node* temp = head;
-	  cout << "Linked List: " << endl;
-	  while(temp!=NULL){
-	      cout << temp -> data <<" ";
-	      temp = temp -> next;
-	  }
-	  cout << endl;
-       }
-       ~LinkedList() {
-            delete head;
-	    delete tail;
-       }
+    }
+    void rev() {
+        // Initialize current=head, previous=NULL and 
+        // next_node=NULL 
+        Node * current = head;
+        Node * previous = NULL;
+        Node * next_node = NULL;
+        while (current != NULL) {
+            // Store next 
+            next_node = current -> next;
+            // Reverse current node's pointer 
+            current -> next = previous;
+            // Move pointers one position ahead.
+            previous = current;
+            current = next_node;
+        }
+        head = previous;
+    }
+
+    void print() {
+        Node * temp = head;
+        cout << "Linked List: " << endl;
+        while (temp != NULL) {
+            cout << temp -> data << " ";
+            temp = temp -> next;
+        }
+        cout << endl;
+    }
+    ~LinkedList() {
+        delete head;
+        delete tail;
+    }
 };
 
-int main(){
+int main() {
     int n;
     // dynamic allocation 
-    LinkedList* linkedList = new LinkedList();
+    LinkedList * linkedList = new LinkedList();
     // input will be given here 
     cout << "Enter the number of node in linked list: ";
     cin >> n;
-    for(int i = 1 ; i <= n ; i++){
-        linkedList->addNode(i);
+    for (int i = 1; i <= n; i++) {
+        linkedList -> addNode(i);
     }
     cout << endl;
     cout << "Before Reverse" << endl;
