02ff3569978e63f3e0090b792bb092e67342e7a1
ANSHUMAN GUPTA <59006894+anshuman1510@users.noreply.github.com>
2020-03-22 08:29:27 +0530

    Update gcd.c

--- Gcd/gcd.c
+++ Gcd/gcd.c
@@ -4,22 +4,22 @@
 
 int gcd (int a, int b)
 {        
-     if (b==0)
-         return a;
+    if (b==0)
+        return a;
      /* Using recursion for checking gcd*/
-     else 
-         return gcd (b,a%b);
+    else 
+        return gcd (b,a%b);
 }
 
 main()
 {      
-     int a, b, hcf;
-     printf ("Input two numbers : ");
-     scanf ("%d%d",&a,&b);
-     /* Function Calling*/
-     hcf = gcd (a,b);
-     printf ("GCD = %d", hcf);
-     return 0;
+    int a, b, hcf;
+    printf ("Input two numbers : ");
+    scanf ("%d%d",&a,&b);
+    /* Function Calling*/
+    hcf = gcd (a,b);
+    printf ("GCD = %d", hcf);
+    return 0;
 }
 
 /* 
