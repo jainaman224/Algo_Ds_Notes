7686f78e64feea24dc4741dbc3d90492cab0a31c
Soham Pal <49874099+p-soham11@users.noreply.github.com>
2020-03-22 16:54:51 +0530

    Update Floyd_Warshall_Algorithm.c

--- Floyd_Warshall_Algorithm/Floyd_Warshall_Algorithm.c
+++ Floyd_Warshall_Algorithm/Floyd_Warshall_Algorithm.c
@@ -3,54 +3,63 @@
 #include<stdio.h>
 
 //Number of vertices in the graph
-#define V 4
+#define A 4
 
 //Define INF(infinite) as a large value. This will be used for
 //the vertices that are not connected with each other
 #define INF 1000000
 
-// A function to print the solution matrix
-void printSolution(int dist[][V]);          
+//A function to print the solution matrix
+void printSolution(int dist[][A]);          
 
-//Function to solve ALL-PAIR SHORTEST PATH problem 
+//Function to solve ALL-PAIR SHORTEST PATH problem
 //using Floyd Warshall Algorithm
-void floydWarshall (int graph[][V])
+void floydWarshall (int graph[][A])
 {
 
-    int dist[V][V], i, j, k;            //dist[][] will hold the final shortest distance between every pair of vertices
+    //dist[][] will hold the final shortest distance between
+    //every pair of vertices
+    int dist[A][A], i, j, k;
 
-    for (i = 0; i < V; i++)
-        for (j = 0; j < V; j++)
+    for (i = 0; i < A; i++)
+        for (j = 0; j < A; j++)
             dist[i][j] = graph[i][j];
 
-    /* Add all vertices one by one to the set of intermediate vertices.
-        ---> Before start of an iteration, we have shortest distances between all pairs of vertices such that the shortest distances
-        consider only the vertices in set {0, 1, 2, .. k-1} as intermediate vertices.
-        ----> After the end of an iteration, vertex no. k is added to the set of intermediate vertices and the set becomes {0, 1, 2, .. k} */
+    /* Add all vertices one by one to the set of intermediate vertices
+    ---> Before start of an iteration, we have shortest distances
+    between all pairs of vertices such that the shortest distances
+    consider only the vertices in set {0, 1,..k-1} as intermediate vertices
+    ----> After the end of an iteration, vertex no. k is added to
+    the set of intermediate vertices and the set becomes {0, 1, 2,..k} */
 
-    for (k = 0; k < V; k++)
+    for (k = 0; k < A; k++)
     {
-        for (i = 0; i < V; i++)             // Pick all vertices as source one by one
+        //Pick all vertices as source one by one
+        for (i = 0; i < A; i++)
         {
-            for (j = 0; j < V; j++)         // Pick all vertices as destination for the above picked source
+            //Pick all vertices as destination for the above picked source
+            for (j = 0; j < A; j++)
             {
-                // If vertex k is on the shortest path from i to j, then update the value of dist[i][j]
+                //If vertex k is on the shortest path from i to j,
+                //then update the value of dist[i][j]
                 if (dist[i][k] + dist[k][j] < dist[i][j])
                     dist[i][j] = dist[i][k] + dist[k][j];
             }
         }
     }
 
-    printSolution(dist);            // Print the shortest distance matrix
+    //Print the shortest distance matrix
+    printSolution(dist);   
 }
 
-void printSolution(int dist[][V])               //Function to print the solution
+//Function to print the solution
+void printSolution(int dist[][A])
 {
     printf ("The following matrix shows the shortest distances"
             " between every pair of vertices: \n");
-    for (int i = 0; i < V; i++)
+    for (int i = 0; i < A; i++)
     {
-        for (int j = 0; j < V; j++)
+        for (int j = 0; j < A; j++)
         {
             if (dist[i][j] == INF)
                 printf("%7s", "INF");
@@ -64,20 +73,21 @@ void printSolution(int dist[][V])               //Function to print the solution
 int main()
 {
 
-    int graph[V][V] = { {0,   9,  INF, 8},
+    int graph[A][A] = { {0,   9,  INF, 8},
                         {INF, 0,   6, INF},
                         {INF, INF, 0,   5},
                         {INF, INF, INF, 0}
                       };
 
-    floydWarshall(graph);           // Calling function to execute Floyd Warshall Algorithm
+    //Calling function to execute Floyd Warshall Algorithm
+    floydWarshall(graph);
     return 0;
 }
 
 
-// Sample Input :
+//Sample Input :
 
-/* The following weighted graph is our input
+/*The following weighted graph is our input
             8
        (0)------->(3)
         |         /|\
@@ -85,12 +95,13 @@ int main()
         |          | 5
        \|/         |
        (1)------->(2)
-            6           
-            
+            6
+
     OUTPUT :
-    
-    The following matrix shows the shortest distance between every pair of vertices:
-    
+
+    The following matrix shows the shortest distance between
+    every pair of vertices:
+
           0     9    15    8
         INF     0     6   11
         INF   INF     0    5
