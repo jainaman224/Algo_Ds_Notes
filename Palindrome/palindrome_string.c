33b9e29de1df5ea6b5a60cd6842641ae84719782
ANSHUMAN GUPTA <59006894+anshuman1510@users.noreply.github.com>
2020-03-22 08:07:01 +0530

    Update palindrome_string.c

--- Palindrome/palindrome_string.c
+++ Palindrome/palindrome_string.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-#include <string.h> /*used string library header file for string functions.*/
+#include <string.h>    /*used string library header file for string functions.*/
+
 main()
-{
-	char str[200], rev[200];
-	printf ("Enter a string : ");
-	gets (str); /*This function is used to read multiword string (one line) from keyboard*/
-	strcpy (rev,str); /*This function is used to copy one string into another*/
-	strrev (rev); /*This function is used to reverse the giveen string*/
-	if (strcmp(str,rev)==0) /*To compare two strings.*/
-	printf ("\nThe Given string is Palindrome");
-	else
-	printf ("\nThe Given string is NOT Palindrome");
-	return 0;
+{          
+     char str[200], rev[200];
+     printf ("Enter a string : ");
+     gets (str);   /*This function is used to read multiword string (one line) from keyboard*/
+     strcpy (rev, str);   /*This function is used to copy one string into another*/
+     strrev (rev);   /*This function is used to reverse the giveen string*/
+     if (strcmp (str, rev) == 0)   /*To compare two strings.*/
+         printf ("\nThe Given string is Palindrome");
+     else
+         printf ("\nThe Given string is NOT Palindrome");
+     return 0;
 }
 
 /* Sample Input 1 :-
