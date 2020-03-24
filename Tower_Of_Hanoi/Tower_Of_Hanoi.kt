96121a8f58846d5d0bbb6b9a1c92c0e52857ae6a
Chand Bud <47664980+chandbud5@users.noreply.github.com>
2020-03-24 10:59:50 +0530

    Update Tower_Of_Hanoi.kt

--- Tower_Of_Hanoi/Tower_Of_Hanoi.kt
+++ Tower_Of_Hanoi/Tower_Of_Hanoi.kt
@@ -1,10 +1,9 @@
 /*
-    @author : Chand Bud
-    @date : 18/03/2020
-
     This is the solution for tower of hanoi problem using recursion implemented in Kotlin
+    Tower of Hanoi is an application of recursion in which our aim is to shift n disks from source tower(peg) to destination tower(peg)
+    We can use third tower as auxilary tower. In it's solution we first try to shift n-1 disks.
+    Minimum number of steps required for this are 2^n-1 where n is number of disks.
 */
-
 import java.util.Scanner
 
 fun main() {
