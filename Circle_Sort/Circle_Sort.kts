61e0033aee0d9508c6ae9d8d39f456e704380b14
Anuj Singh <43649349+myselfanuj@users.noreply.github.com>
2020-03-27 01:21:48 +0530

    Update Circle_Sort.kts

--- Circle_Sort/Circle_Sort.kts
+++ Circle_Sort/Circle_Sort.kts
@@ -34,14 +34,22 @@ fun<T: Comparable<T>> circleSort(array: Array<T>, lo: Int, hi: Int, nSwaps: Int)
 fun main(args: Array<String>) {
     // -- ExampleArray1 --
     val array = arrayOf(6, 7, 8, 9, 2, 5, 3, 4, 1) 
-    println("Original Array: ${array.asList()}")
+    println("Unsorted Array: ${array.asList()}")
     while (circleSort(array, 0, array.size - 1, 0) != 0) ; 
     println("Sorted Array : ${array.asList()}") //Sorted Array
     println()
     
     // -- ExampleArray2 --
     val array2 = arrayOf("the", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog")  
-    println("Original Array : ${array2.asList()}")
+    println("Unsorted Array : ${array2.asList()}")
     while (circleSort(array2, 0, array2.size - 1, 0) != 0) ;
     println("Sorted  Array : ${array2.asList()}")    //Sorted Array
 }
+
+
+//output
+Unsorted Array: [6, 7, 8, 9, 2, 5, 3, 4, 1]
+Sorted Array : [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+Unsorted Array : [the, quick, brown, fox, jumps, over, the, lazy, dog]
+Sorted  Array : [brown, dog, fox, jumps, lazy, over, quick, the, the]
