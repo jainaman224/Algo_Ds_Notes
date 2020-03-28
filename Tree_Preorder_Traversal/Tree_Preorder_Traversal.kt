bb5e95e1cddec7489b26f21bf89caf702777362f
PrachieNaik <42534757+PrachieNaik@users.noreply.github.com>
2020-03-28 22:08:47 +0530

    Update Tree_Preorder_Traversal.kt
    Taking dynamic input

--- Tree_Preorder_Traversal/Tree_Preorder_Traversal.kt
+++ Tree_Preorder_Traversal/Tree_Preorder_Traversal.kt
@@ -1,7 +1,8 @@
-class Node<Element>(
-    var key:Element,
-    var left:Node<Element>? = null,
-    var right:Node<Element>? = null  
+import java.util.*
+class Node<Int>(
+    var key:Int,
+    var left:Node<Int>? = null,
+    var right:Node<Int>? = null  
 )
 {
     fun preorderTraversal()
@@ -13,21 +14,37 @@ class Node<Element>(
 }
 
 fun main() {
-    val zero = Node(0)
-    val one = Node(1)
-    val two = Node(2)
-    val three = Node(3)
-    val four = Node(4)
-    val five = Node(5)
-    
-    zero.left = one
-    zero.right = two
-    one.left = three
-    one.right = four
-    two.left = five
-    
+    var read = Scanner(System.`in`)
+    println("Enter the size of Array:")
+    val arrSize = read.nextLine().toInt()
+    var arr = IntArray(arrSize)
+    val nodes = mutableListOf<Node<Int>>()
+    println("Enter the array respresentaion of binary tree")
+    for(i in 0 until arrSize)
+    {
+        arr[i] = read.nextLine().toInt()
+        nodes.add(Node(arr[i]))
+    }
+    for(i in 0..arrSize-2)
+    {
+        if((i*2)+1<arrSize && arr[(i*2)+1]!=-1)
+        nodes[i].left = nodes[(i*2)+1]
+        if((i*2)+2<arrSize && arr[(i*2)+2]!=-1)
+        nodes[i].right = nodes[(i*2)+2]
+    }
     print("Pre Order traversal of tree is ")
-    zero.preorderTraversal()
+    nodes[0].preorderTraversal()
 }
-
-//Output: Pre Order traversal of tree is 0 1 3 4 2 5 
+/*
+*Enter the size of Array:
+*7
+*Enter the array respresentaion of binary tree
+*1
+*2
+*3
+*4
+*5
+*-1
+*7
+*Pre Order traversal of tree is 1 2 4 5 3 7 
+*/
