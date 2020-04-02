8d0518cc351a4fe023ff89ef938a133432c2860a
Anushka118 <39857792+Anushka118@users.noreply.github.com>
2020-04-03 01:16:56 +0530

    Update and rename AhoCorasick.py to Aho_Corasick.py

--- Aho_Corasick.py
+++ Aho_Corasick.py
@@ -1,19 +1,16 @@
 # implementing AhoCorasick using adjacency list
+
 import collections
 
-AList = []  # declaring thr adjacency list
-
+AdjList = []  # declaring thr adjacency list
 
 def init_trie(wordList):
     createTrie()
     addKeyWords(wordList)
     setFailTrans()
 
-
 def createTrie():  # initializing a root of the trie
-
-    AList.append({'character': ' ', 'nextState': [], 'failedState': 0, 'outputList': []})
-
+    AdjList.append({'character': ' ', 'nextState': [], 'failedState': 0, 'outputList': []})
 
 # character -> the character the root node is repesenting
 # nextStates -> list of ids of child nodes of the root node
@@ -24,16 +21,13 @@ def addKeyWords(wordList):  # ading all the valid words in list of valid words
     for validWord in wordList:
         addWordList(validWord)
 
-
 def findNextState(currState, val):
     # takes a node and value and return the id of the child of that node
     # whose character matches value or else return none if not found
-    for node in AList[currState]["nextState"]:
-        if AList[node]["character"] == val:
+    for node in AdjList[currState]["nextState"]:
+        if AdjList[node]["character"] == val:
             return node
-
     return None
-
 
 # adding a keyword to the trie and makring the output as the last node
 def addWordList(keyword):
@@ -55,44 +49,40 @@ def addWordList(keyword):
         node = {'character': keyword[i], 'nextState': [], 'failedState': 0, 'outputList': []}
 
         # appending the character to the Adjacency List
-        AList.append(node)
-        AList[currState]["nextState"].append(len(AList) - 1)
-        currState = len(AList) - 1
+        AdjList.append(node)
+        AdjList[currState]["nextState"].append(len(AdjList) - 1)
+        currState = len(AdjList) - 1
     # appending te keyword to the output of the last node
-    AList[currState]["outputList"].append(keyword)
-
+    AdjList[currState]["outputList"].append(keyword)
 
 # for fail transitions, breadth first search will be done on the trie
-
 def setFailTrans():
     q = collections.deque()
     child = 0
-    for node in AList[0]["nextState"]:
+    for node in AdjList[0]["nextState"]:
         q.append(node)
-        AList[node]["failedState"] = 0
+        AdjList[node]["failedState"] = 0
 
     while q:
         r = q.popleft()
-        for child in AList[r]["nextState"]:
+        for child in AdjList[r]["nextState"]:
             q.append(child)
-            state = AList[r]["failedState"]
-            while (findNextState(state, AList[child]["character"]) is None) and (state != 0):
-                state = AList[state]["failedState"]
+            state = AdjList[r]["failedState"]
+            while (findNextState(state, AdjList[child]["character"]) is None) and (state != 0):
+                state = AdjList[state]["failedState"]
 
-            AList[child]["failedState"] = findNextState(state, AList[child]["character"])
+            AdjList[child]["failedState"] = findNextState(state, AdjList[child]["character"])
 
-            if AList[child]["failedState"] is None:
-                AList[child]["failedState"] = 0
+            if AdjList[child]["failedState"] is None:
+                AdjList[child]["failedState"] = 0
 
-            AList[child]["outputList"] = AList[child]["outputList"] + AList[AList[child]["failedState"]]["outputList"]
-
+            AdjList[child]["outputList"] = AdjList[child]["outputList"] + AdjList[AdjList[child]["failedState"]]["outputList"]
 
 def GetKeywords(words):
     currState = 0
     for i in range(len(words)):
-
         while findNextState(currState, words[i]) is None and currState != 0:
-            currState = AList[currState]["failedState"]
+            currState = AdjList[currState]["failedState"]
 
         currState = findNextState(currState, words[i])
 
@@ -100,11 +90,10 @@ def GetKeywords(words):
             currState = 0
 
         else:
-            for j in AList[currState]["outputList"]:
+            for j in AdjList[currState]["outputList"]:
                 start = i - len(j) + 1
                 end = len(j) + start -1
                 print("keyword", j, " appears from ",start , " to " ,end)
-
 
 n = int(input("enter no. of keywords to be saved: "))
 keywords = []
@@ -114,7 +103,6 @@ for i in range(n):
 init_trie(keywords)
 text = input("Enter the text: ")
 GetKeywords(text)
-
 
 """
 input
