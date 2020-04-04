73bb8131edb65f1fe60dde35075f412d8f6b1656
Anushka-Rajvanshi <60550664+Anushka-Rajvanshi@users.noreply.github.com>
2020-04-04 15:35:43 +0530

    Update chinese_remainder_theorem.c

--- Chinese_Remainder_Theorem/chinese_remainder_theorem.c
+++ Chinese_Remainder_Theorem/chinese_remainder_theorem.c
@@ -1,26 +1,37 @@
 #include <stdio.h>
 
 //function to find minimum number such that num%divisor[i] gives corresponding remainder[i] 
-void minresult( int divisor[], int rem[], int size)
+int minresult( int divisor[], int rem[], int size)
 {
-    //each number starting from 1 is checked for minimum value
-    int res = 1, i = 0;
-    do
-    {  
-        if((res%divisor[i]) == rem[i])
+    int m = 1, M[size], inv[size], i, k, count, x = 0;
+    for(i = 0; i < size; i++)
+    {
+        m = m * divisor[i];
+    }
+    for(i = 0; i < size; i++)
+    {
+        M[i] = m / divisor[i];
+    }
+    for(i = 0; i < size; i++)
+    {   
+        count = 0;
+        k = 1;
+        do
         {
- // When value of i is equal to value of size i.e. all remainder matches with given rem[] array, then res is the minimum value
-            i++ ;
-        }
-        else
-        {    //if above condition gives false for any value of i, check for next number.
-            res++ ;
-            i = 0; 
-        }
-        
-    }while(i < size);
-    printf("Answer is: %d", res); //display result.
-    
+            if(((M[i] % divisor[i]) *  k) % divisor[i] == 1)
+            {
+                inv[i] = k;
+                count++;
+            } 
+            k++; 
+        }while(count == 0);  
+    }
+    for(i = 0; i < size; i++)
+    {
+        x = x + (rem[i] * M[i] * inv[i]); //according to CRT
+    }
+    x = x % m;
+    return x; 
 }
 
 int main()
@@ -41,31 +52,13 @@ int main()
         scanf("%d", &rem[i]);
     }
     //function call to find result.
-    minresult( divisor, rem, size);
+    printf("Answer is: %d ", minresult( divisor, rem, size));
     return 0;
 }
 
-/*Sample input-output:
-
-Enter size of array:4
-Enter value of coprime divisors:2 3 5 7
-Enter value of remainders:1 2 3 4
-Answer is: 53*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+/* Sample input-output:
+Enter size of array:3
+Enter value of coprime divisors:2 3 5
+Enter value of remainders:1 2 4
+Answer is: 29
+*/
