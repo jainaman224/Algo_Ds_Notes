cfe1f65bae7a08e42ba597fb4524a409665ca839
Mrunal <44579222+mruna7@users.noreply.github.com>
2020-04-04 17:21:18 +0530

    Update Decimal_To_Binary.java

--- Number_Conversion/Java/Decimal_To_Binary.java
+++ Number_Conversion/Java/Decimal_To_Binary.java
@@ -4,34 +4,30 @@
 package algo_ds;
 
 import java.util.Scanner;
-
-/**
- * @author mrunal
- *
- */
-public class DecimalToBinary {
-       
-	static void Decimal_to_binary(int decimal)
-	{   int i = 0;
-		int binary[]=new int[100];
-		while(decimal > 0)
-		{
-			binary[i]=decimal % 2;
-			decimal=decimal / 2;
-			i++;
-		}
-		System.out.println("binary representation");
-		for(int j = i; j <= i; j--)
-			System.out.print(binary[j]);
-	}
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a decimal number");
-		int decimal=sc.nextInt();
-		Decimal_to_binary(decimal);
-	}
- }
+public class DecimalToBinary 
+{
+    static void Decimal_to_binary(int decimal)
+    {   
+         int i = 0;
+	 int binary[]=new int[100];
+	  while(decimal > 0)
+	  {
+	       binary[i]=decimal % 2;
+	       decimal=decimal / 2;
+	       i++;
+	  }
+	  System.out.println("binary representation");
+	  for(int j = i; j <= i; j--)
+	      System.out.print(binary[j]);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a decimal number");
+	int decimal=sc.nextInt();
+	Decimal_to_binary(decimal);
+    }
+}
 
 /*INPUT:4
 OUTPUT:100
