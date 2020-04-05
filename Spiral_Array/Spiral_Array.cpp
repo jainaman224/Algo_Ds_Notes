ecea1313ef054d5d507e277649f64c810de86988
animeshramasami@gmail.com <animeshramasami@gmail.com>
2020-04-05 18:47:45 +0530

    Added spiral cpp code

---
+++ Spiral_Array/Spiral_Array.cpp
@@ -0,0 +1,78 @@
+//SPIRAL PRINTING 2-D matrix
+
+#include <bits/stdc++.h>
+using namespace std;
+#define R 3
+#define C 6
+
+void spiralPrint(int m, int n, int a[R][C])
+{
+    int i, k = 0, l = 0;
+
+    /* k - starting row index
+        m - ending row index
+        l - starting column index
+        n - ending column index
+        i - iterator
+    */
+
+    while (k < m && l < n) {
+        /* Print the first row from
+               the remaining rows */
+        for (i = l; i < n; ++i) {
+            cout << a[k][i] << " ";
+        }
+        k++;
+
+        /* Print the last column
+         from the remaining columns */
+        for (i = k; i < m; ++i) {
+            cout << a[i][n - 1] << " ";
+        }
+        n--;
+
+        /* Print the last row from
+                the remaining rows */
+        if (k < m) {
+            for (i = n - 1; i >= l; --i) {
+                cout << a[m - 1][i] << " ";
+            }
+            m--;
+        }
+
+        /* Print the first column from
+                   the remaining columns */
+        if (l < n) {
+            for (i = m - 1; i >= k; --i) {
+                cout << a[i][l] << " ";
+            }
+            l++;
+        }
+    }
+}
+
+/* Driver program to test above functions */
+int main()
+{
+    int a[R][C] = { { 1, 2, 3, 4, 5, 6 },
+                    { 7, 8, 9, 10, 11, 12 },
+                    { 13, 14, 15, 16, 17, 18 } };
+
+    spiralPrint(R, C, a);
+    return 0;
+}
+
+
+/* SAMPLE I/O
+
+Input
+
+3 3
+1 2 3
+4 5 6 
+7 8 9
+
+Output
+
+1 2 3 6 9 8 7 4 5
+*/
