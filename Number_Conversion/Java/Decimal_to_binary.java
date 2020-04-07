fa7936755e753d234991310fcca801cd214abab8
Mrunal <rmrunalvartak@gmal.com>
2020-04-07 18:13:28 +0530

    commmitfinal

--- Number_Conversion/Java/Decimal_to_binary.java
+++ Number_Conversion/Java/Decimal_to_binary.java
@@ -1,30 +1,34 @@
-//JAVA program to convert Decimal integer to it's Binary form
+/**
+ * Decimal To Binary Conversion
+ */
+package algo_ds;
 
 import java.util.Scanner;
-
-public class Decimal_to_binary {
-
-    //Driver Code
-    public static void main(String[] args){
-        int n, rem;
-        String Bin = "";    //String to store the binary number
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter any decimal number:");
-        n = sc.nextInt();   //Taking the decimal input
-
-        if (n == 0){
-            Bin = "0";
-        }
-
-        //Simultaneously storing the remainder when number divided by 2 in the string in reverse order
-        while (n > 0){
-            rem = n % 2;
-            Bin = rem + Bin;
-            n = n / 2;
-        }
-        System.out.println("Binary number:"+Bin);   //output
+public class DecimalToBinary 
+{
+    static void Decimal_to_binary(int decimal)
+    {   
+         int i = 0;
+	 int binary[]=new int[100];
+	  while(decimal > 0)
+	  {
+	       binary[i]=decimal % 2;
+	       decimal=decimal / 2;
+	       i++;
+	  }
+	  System.out.println("binary representation");
+	  for(int j = i; j <= i; j--)
+	      System.out.print(binary[j]);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a decimal number");
+	int decimal=sc.nextInt();
+	Decimal_to_binary(decimal);
     }
 }
-//Input : 10 
-//Output : 1010 
 
+/*INPUT:4
+OUTPUT:100
+*/
