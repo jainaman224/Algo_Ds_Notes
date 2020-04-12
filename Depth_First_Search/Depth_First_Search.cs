35ede1eccf6674a07cd643a20024135fdf6e4d0e
Shreya Kapoor <shreyakapoor98@gmail.com>
2020-04-12 16:14:48 +0530

    corrections in DFS C#

--- Depth_First_Search/Depth_First_Search.cs
+++ Depth_First_Search/Depth_First_Search.cs
@@ -42,11 +42,10 @@ class DirectedGraph
     
   public static void Main(String[] args) 
     { 
-        DirectedGraph newgraph = new DirectedGraph(4);
-        Console.WriteLine("Number of edges to enter");
+        DirectedGraph newgraph = new DirectedGraph(4); // Number of vertices is 4
+        Console.WriteLine("Number of edges between the vertices to enter");
         int n = Convert.ToInt32(Console.ReadLine());
-        
-        for(int i=0;i<n;i++){
+        for (int i=0;i<n;i++){
           int x = Convert.ToInt32(Console.ReadLine());
           int y = Convert.ToInt32(Console.ReadLine());
           newgraph.Addition_of_Edge(x, y);
@@ -58,6 +57,13 @@ class DirectedGraph
         newgraph.Depth_First_Search(s); 
     } 
 } 
-// Sample input: 0 2 0 1 2 1 2 0 3 2 2 2
-//               3
-// Sample OP: 3 2 1 0
+// Sample input:
+// Number of edges between the vertices to enter
+// 6
+// 0 2 0 1 2 1 2 0 3 2 2 2
+// Enter vertex which is the starting point
+// 3
+
+// Sample OP:
+// Depth First Search Traversal starting from vertex 3 is :
+// 3 2 1 0
