585ec16b3b344c9a0a747df5b489df63c864dc38
Manil Dayma <manil.dayma@gmail.com>
2020-04-12 20:10:06 +0530

    Update bubble_sort.ts
    indentation updated

--- Bubble_Sort/bubble_sort.ts
+++ Bubble_Sort/bubble_sort.ts
@@ -2,30 +2,35 @@
 
 function bubble(array : number[]) : number[]
 {
- console.log("Original Array : " + array); // print original array
- console.log("Steps in bubble sorting the array : ");
+     // print original array
+     console.log("Original Array : " + array);
+     console.log("Steps in bubble sorting the array : ");
+     
+         //FOR LOOP to perform bubble sort(swapping)
+         for( let i = 0; i < array.length ; i++)
+         { 
+              for(let j = 0; j < array.length - 1; j++ ) 
+              {
+                   if(array[j] > array[j + 1])
+                   {
+                        let swap = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = swap;
+                       
+                       /* prints arrray to check 
+                        after every bubble sort step */
+                        console.log(array); 
+                   }    
+              }    
+         }
     
-     //FOR LOOP to perform bubble sort(swapping)
-     for( let i = 0; i < array.length ; i++)
-     { 
-          for(let j = 0; j < array.length - 1; j++ ) 
-          {
-               if(array[j] > array[j + 1])
-               {
-                    let swap = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = swap;
-                   // prints arrray to check after every bubble sort step 
-                   console.log(array); 
-            }
-        }
-    }
-// print bubble sorted array
-console.log(" Final Array : " + array); 
-return array;
-}
+     // print bubble sorted array
+     console.log(" Final Array : " + array); 
+     return array;
+}    
   
-// consider the array input of bubble sort (may choose any array)
+/* consider the array input of 
+bubble sort (may choose any array) */
 bubble([2, 1, 99, 8, 4, 0, 18]);
 
 // OUTPUT
