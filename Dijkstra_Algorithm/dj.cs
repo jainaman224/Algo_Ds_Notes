accbfeabe89410efe9f347e3d5bb18b37cd65953
anshu18107 <54129042+anshu18107@users.noreply.github.com>
2020-04-13 13:30:26 +0530

    Updated changes

--- Dijkstra_Algorithm/dj.cs
+++ Dijkstra_Algorithm/dj.cs
@@ -1,64 +1,62 @@
 using System;
 
-class DA{
-	static int V = 9;
-	int minDistance(int[] dist,
-					bool[] sptSet)
+class DA
+{
+	static int v = 9;
+	int minDistance(int[] dist,bool[] sptSet)
 	{
-		int min = int.MaxValue, min_index = -1;
+	    int min = int.MaxValue, min_index = -1;
+            for (int i = 0; i < v; i++)
+	        if (sptSet[i] == false && dist[i] <= min) 
+		    {
+		        min = dist[i];
+			min_index = i;
+		    }
 
-		for (int v = 0; v < V; v++)
-			if (sptSet[v] == false && dist[v] <= min) {
-				min = dist[v];
-				min_index = v;
-			}
-
-		return min_index;
+	    return min_index;
 	}
 	void printSolution(int[] dist, int n)
 	{
-		Console.Write("Vertex	 Distance "
-					+ "from Source\n");
-		for (int i = 0; i < V; i++)
-			Console.Write(i + " \t\t " + dist[i] + "\n");
+	    Console.Write("Vertex	 Distance "+ "from Source\n");
+	    for (int i = 0; i < v; i++)
+	        Console.Write(i + " \t\t " + dist[i] + "\n");
 	}
 
 	void dijkstra(int[, ] graph, int src)
 	{
-		int[] dist = new int[V];
-		bool[] sptSet = new bool[V];
+	    int[] dist = new int[v];
+	    bool[] sptSet = new bool[v];
 
-		for (int i = 0; i < V; i++) {
-			dist[i] = int.MaxValue;
-			sptSet[i] = false;
+	    for (int i = 0; i < v; i++) 
+	        {
+		    dist[i] = int.MaxValue;
+		    sptSet[i] = false;
 		}
 
-		dist[src] = 0;
-		for (int count = 0; count < V - 1; count++) {
-			int u = minDistance(dist, sptSet);
-			for (int v = 0; v < V; v++)
-
-
-				if (!sptSet[v] && graph[u, v] != 0 &&
-					dist[u] != int.MaxValue && dist[u] + graph[u, v] < dist[v])
-					dist[v] = dist[u] + graph[u, v];
+	    dist[src] = 0;
+	    for (int count = 0; count < v - 1; count++) 
+	        {
+		    int u = minDistance(dist, sptSet);
+		    for (int v = 0; v < V; v++)
+		        if (!sptSet[v] && graph[u, v] != 0 && dist[u] != int.MaxValue && dist[u] + graph[u, v] < dist[v])
+			    dist[v] = dist[u] + graph[u, v];
 		}
 	}
 
 	// Driver Code
 	public static void Main()
 	{
-		int[, ] graph = new int[, ] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-									{ 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-									{ 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-									{ 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-									{ 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-									{ 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-									{ 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-									{ 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-									{ 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
-		DA t = new DA();
-		t.dijkstra(graph, 0);
+	    int[, ] graph = new int[, ] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+					  { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+					  { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+					  { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+					  { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+					  { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+					  { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+					  { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+					  { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+	    DA t = new DA();
+	    t.dijkstra(graph, 0);
 	}
 }
 
