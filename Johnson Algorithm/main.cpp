2b8fb7fc6cb0ac13092ce5a5b839c95cbb0dafd7
Aakansha <jainaakansha28@gmail.com>
2020-04-18 17:19:06 +0530

    Done required changes

--- Johnson Algorithm/main.cpp
+++ Johnson Algorithm/main.cpp
@@ -13,40 +13,43 @@ inline int min(int a, int b)
 {
    return (a<b)?a:b;
 }
-main()
+int main()
 {
-   int vert, edge, i, j, k, c;
 
-   cout << "Enter no of vertices: ";
-   cin >> vert;
-   cout << "Enter no of edges: ";
-   cin >> edge;
-   cout << "Enter the EDGE Costs:\n";
+    int vert, edge, i, j, k, c;
+    cout << "Enter no of vertices: ";
+    cin >> vert;
+    cout << "Enter no of edges: ";
+    cin >> edge;
+    cout << "Enter the EDGE Costs:\n";
 
-   for (k = 1; k <= edge; k++) //take the input and store it into adj and cost matrix
-   {
-       cin >> i >> j >> c;
-       adj[i][j] = cost[i][j] = c;
-   }
+    //take the input and store it into adj and cost matrix
+    for (k = 1; k < edge; k++)
+    {
+        cin >> i >> j >> c;
+        adj[i][j] = cost[i][j] = c;
+    }
 
-   for (i = 1; i <= vert; i++)
-      for (j = 1; j <= vert; j++) {
-         if (adj[i][j] == 0 && i != j)
-            adj[i][j] = INF; //if there is no edge, put infinity
-      }
+    for (i = 1; i <= vert; i++)
+        for (j = 1; j <= vert; j++) {
+            if (adj[i][j] == 0 && i != j)
+                //if there is no edge, put infinity
+                adj[i][j] = INF;
+        }
 
-   for (k = 1; k <= vert; k++)
-      for (i = 1; i <= vert; i++)
-         for (j = 1; j <= vert; j++)
-            adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]); //find minimum path from i to j through k
-   cout << "Resultant adj matrix\n";
+    for (k = 1; k <= vert; k++)
+        for (i = 1; i <= vert; i++)
+            for (j = 1; j <= vert; j++)
+                //find minimum path from i to j through k
+                adj[i][j] = min(adj[i][j], adj[i][k] + adj[k][j]);
 
-   for (i = 1; i <= vert; i++) {
-      for (j = 1; j <= vert; j++) {
+    cout << "Resultant adj matrix\n";
+    for (i = 1; i <= vert; i++) {
+        for (j = 1; j <= vert; j++) {
             if (adj[i][j] != INF)
-               cout << adj[i][j] << " ";
-      }
-      cout << "\n";
+                cout << adj[i][j] << " ";
+        }
+        cout << "\n";
    }
 }
 
