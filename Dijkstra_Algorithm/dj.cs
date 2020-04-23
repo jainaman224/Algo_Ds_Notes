08578a5589d8f1802a50cc1364a784820bb6646a
anshu18107 <54129042+anshu18107@users.noreply.github.com>
2020-04-23 10:04:10 +0530

    update dj.cs

--- Dijkstra_Algorithm/dj.cs
+++ Dijkstra_Algorithm/dj.cs
@@ -10,56 +10,54 @@ class DA
 	        if (sptSet[i] == false && dist[i] <= min) 
 		    {
 		        min = dist[i];
-			    min_index = i;
+			min_index = i;
 		    }
 
 	    return min_index;
-	}
+    }
     
     
-	void printSolution(int[] dist, int n)
-	{
-	    Console.Write("Vertex	 Distance "+ "from Source\n");
-	    for (int i = 0; i < v; i++)
-	        Console.Write(i + " \t\t " + dist[i] + "\n");
-	}
+    void printSolution(int[] dist, int n)
+    {
+        Console.Write("Vertex	 Distance "+ "from Source\n");
+        for (int i = 0; i < v; i++)
+            Console.Write(i + " \t\t " + dist[i] + "\n");
+    }
 
-	void dijkstra(int[, ] graph, int src)
-	{
-	    int[] dist = new int[v];
-	    bool[] sptSet = new bool[v];
+    void dijkstra(int[, ] graph, int src)
+    {
+        int[] dist = new int[v];
+        bool[] sptSet = new bool[v];
+        for (int i = 0; i < v; i++) 
+        {
+	        dist[i] = int.MaxValue;
+	        sptSet[i] = false;
+	    }
+        dist[src] = 0;
+        for (int count = 0; count < v - 1; count++) 
+        {
+	        int u = minDistance(dist, sptSet);
+	        for (int v = 0; v < V; v++)
+	            if (!sptSet[v] && graph[u, v] != 0 && dist[u] != int.MaxValue && dist[u] + graph[u, v] < dist[v])
+		            dist[v] = dist[u] + graph[u, v];
+	    }
+}
 
-	    for (int i = 0; i < v; i++) 
-	        {
-		    dist[i] = int.MaxValue;
-		    sptSet[i] = false;
-		    }
-
-	    dist[src] = 0;
-	    for (int count = 0; count < v - 1; count++) 
-	        {
-		    int u = minDistance(dist, sptSet);
-		    for (int v = 0; v < V; v++)
-		        if (!sptSet[v] && graph[u, v] != 0 && dist[u] != int.MaxValue && dist[u] + graph[u, v] < dist[v])
-			    dist[v] = dist[u] + graph[u, v];
-		    }
-	}
-
-	// Driver Code
-	public static void Main()
-	{
-	    int[, ] graph = new int[, ] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
-					  { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
-					  { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
-					  { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
-					  { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
-					  { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
-					  { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
-					  { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
-					  { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
-	    DA t = new DA();
-	    t.dijkstra(graph, 0);
-	}
+// Driver Code
+    public static void Main()
+    {
+        int[, ] graph = new int[, ] { { 0, 4, 0, 0, 0, 0, 0, 8, 0 },
+				                      { 4, 0, 8, 0, 0, 0, 0, 11, 0 },
+				                      { 0, 8, 0, 7, 0, 4, 0, 0, 2 },
+				                      { 0, 0, 7, 0, 9, 14, 0, 0, 0 },
+				                      { 0, 0, 0, 9, 0, 10, 0, 0, 0 },
+				                      { 0, 0, 4, 14, 10, 0, 2, 0, 0 },
+				                      { 0, 0, 0, 0, 0, 2, 0, 1, 6 },
+				                      { 8, 11, 0, 0, 0, 0, 1, 0, 7 },
+				                      { 0, 0, 2, 0, 0, 0, 6, 7, 0 } };
+         DA t = new DA();
+         t.dijkstra(graph, 0);
+    }
 }
 
 
