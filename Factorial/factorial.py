10b63b58ca96009e13ce02b302a51af1e7791190
Nikhita7Chavva <54998946+Nikhita7Chavva@users.noreply.github.com>
2020-04-25 02:51:22 +0530

    Added python code

---
+++ Factorial/factorial.py
@@ -0,0 +1,21 @@
+# To take input from the user
+num = int(input("Enter a number: "))
+
+factorial = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
+
+
+
+Output:
+Enter a number: 4
+4
+The factorial of 4 is 24
