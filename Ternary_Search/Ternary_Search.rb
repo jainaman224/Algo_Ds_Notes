57fcfb19bbd6afbd8e8e3acd457fc321511cff95
Abhinav Anand <manasgmishra@gmail.com>
2020-04-30 18:33:19 +0530

    Updated Ternary_Search.rb with proper indentation

--- Ternary_Search/Ternary_Search.rb
+++ Ternary_Search/Ternary_Search.rb
@@ -1,9 +1,9 @@
 def ternarySearch(l, r, key, ar) #function to perform ternary search
-
     if (r >= l)
         #find mid1 and mid2
         mid1 = l + (r - l) / 3
         mid2 = r - (r - l) / 3
+        
         if ar[mid1] == key #check if key is equal to mid1
             mid1
         elsif ar[mid2] == key #check if key is equal to mid2
@@ -19,9 +19,10 @@ def ternarySearch(l, r, key, ar) #function to perform ternary search
         end
     end
 end
-n= gets.to_i #length of array
+
+n = gets.to_i #length of array
 arr = []
-while n>0
+while n > 0
   num = gets.chomp.to_i
   arr.push(num)
   n = n-1
