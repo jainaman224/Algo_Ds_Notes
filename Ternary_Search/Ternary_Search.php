e601f7e15ed8d2ac90176df2f8d07042c38b746e
Shweta Bhardwaj <59722795+BhardwajShweta25@users.noreply.github.com>
2020-05-12 17:23:37 +0530

    Updated Ternary_Search.php

--- Ternary_Search/Ternary_Search.php
+++ Ternary_Search/Ternary_Search.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 
     /* Ternary search is a searching technique much like binary search 
      *  but instead of dividing the given array in two parts, it divides it in 3 parts.
@@ -6,35 +6,35 @@
      */
 
     // Ternary Search function 
-    function ternary_search( $ar ,$l , $r , $x )
+    function ternary_search($ar ,$l , $r , $x)
     {
         // r >= l means there is some space to search x in ar
         if( $r >= $l )
        {
             /* Dividing the array into three parts 
-             * namely ar[0]..ar[mid1], ar[mid1]...ar[mid2], ar[mid2]...ar[length-1]
+             * namely ar[0]..ar[mid1], ar[mid1]...ar[mid2], ar[mid2]...ar[length - 1]
              */
-            $mid1=( int )( $l + ( $r - $l ) / 3 );
-            $mid2=( int )( $mid1 + ( $r - $l ) /3 );
+            $mid1 = (int)($l + ($r - $l) / 3);
+            $mid2 = (int)($mid1 + ($r - $l) / 3);
        
             // if x is present at mid1
-            if( $ar[ $mid1 ] == $x )
+            if($ar[$mid1] == $x)
                 return $mid1;
 
             // if x is present at mid2
-            if( $ar[ $mid2 ] == $x )
+            if($ar[$mid2] == $x)
                 return $mid2;
         
             // if x is present in left one-third part
-            if( $ar[ $mid1 ] > $x )
-                return ternary_search($ar, $l, $mid-1, $x);
+            if($ar[$mid1] > $x)
+                return ternary_search($ar, $l, $mid - 1, $x);
 
            // if x is present in right one third part
-            if( $ar[ $mid2 ] < $x )
-                return ternary_search($ar, $mid2+1, $r,  $x);
+            if($ar[$mid2] < $x)
+                return ternary_search($ar, $mid2 + 1, $r,  $x);
         
             // if x is present in middle one third part
-            return ternary_search($ar, $mid1+1, $mid2-1, $x);
+            return ternary_search($ar, $mid1 + 1, $mid2 - 1, $x);
        }
        else   
             return -1;
@@ -48,10 +48,10 @@
     $x = 4;
 
     /* to search x in ar, we firstly pass whole of array to the ternary_search() function
-     * i.e., (l == 0, r == count(ar)-1), where count(ar) returns the number of elements in ar
+     * i.e., (l == 0, r == count(ar) - 1), where count(ar) returns the number of elements in ar
      */
 
-    $index_x = ternary_search($ar, 0, count($ar)-1, $x);
+    $index_x = ternary_search($ar, 0, count($ar) - 1, $x);
     // index_x holds the "index" where x is found in ar or -1 if not found 
  
     echo "$index_x";
