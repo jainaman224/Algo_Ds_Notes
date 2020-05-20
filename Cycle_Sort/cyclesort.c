2250df6c5eddb5e5836c2fa5905f1c31d8e1dc5a
sejal-Srivastava <62070965+sejalshri@users.noreply.github.com>
2020-05-20 10:24:10 +0530

    Update cyclesort.c

--- Cycle_Sort/cyclesort.c
+++ Cycle_Sort/cyclesort.c
@@ -7,15 +7,18 @@ void main()
     printf("enter the number of elements required \n");
     scanf("%d", &n);
     printf("enter the elements of the array");
+    
     for (i = 0; i < n; i++)
     {
         scanf("%d", &arr[i]);
     }  
+    
     //this sorting follows comparing process,with it's right elements hence loop goes to n-2 to avoid garbage values in the output
     for (k = 0; k < n - 2; k++)
     {
         value = arr[k];
         index = k;
+        
         //loop to find number of elements less than the element in consideration 
         for (j = k + 1; j < n; j++)
         {
@@ -24,16 +27,19 @@ void main()
                 index++;
             }
         }
+        
         //if the element is at right position
         if (index == k)
         {
             continue;
         }
+        
         //skipping duplicate elements
         while (arr[index] == value)
         {
             index++;
         }
+        
         //providing the correct position to the element
         if (index != k)
         {
@@ -41,10 +47,12 @@ void main()
             value = arr[index];
             arr[index] = temp;
         }
+        
         //rotate rest of the cycle
         while (index != k)
         {
             index = k;
+            
             for (p = k + 1; p < n; p++)
             {
                 if (arr[p] < value)
@@ -52,10 +60,12 @@ void main()
                     index++;
                 }
             }
+            
             while (arr[index] == value)
             {
                 index++;
             }
+            
             if (index != k)
             {
                 temp = value;
@@ -65,12 +75,13 @@ void main()
         }
         arr[k] = value;
     }
-
     printf("the sorted array is");
+    
     for (l = 0; l < n; l++)
     {
         printf("%d\t", arr[l]);
     }
+    
 }
 
 //sample input-output
