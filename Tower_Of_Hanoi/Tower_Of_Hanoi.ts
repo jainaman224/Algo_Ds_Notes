3a90f76295076a959414a900c794adae39618087
Vyom Chandra Gallani <36289357+vyom153069@users.noreply.github.com>
2020-05-20 13:58:25 +0530

    Update Tower_Of_Hanoi.ts
    fixed output logs 
    run for 3 and 4

--- Tower_Of_Hanoi/Tower_Of_Hanoi.ts
+++ Tower_Of_Hanoi/Tower_Of_Hanoi.ts
@@ -22,261 +22,97 @@ solveHanoi(6, "Left", "Right", "Middle", (from , to) => {
     console.log(moveCount + ": Move Disk from " + from + " to " + to + ".")
 })
 
+//output for 3
+/*
+1: Move Disk from Left to Right.
+2: Move Disk from Left to Middle.
+3: Move Disk from Right to Middle.
+4: Move Disk from Left to Right.
+5: Move Disk from Middle to Left.
+6: Move Disk from Middle to Right.
+7: Move Disk from Left to Right.
+*/
+
+//output for 4
+/*
+1: Move Disk from Left to Middle.
+2: Move Disk from Left to Right.
+3: Move Disk from Middle to Right.
+4: Move Disk from Left to Middle.
+5: Move Disk from Right to Left.
+6: Move Disk from Right to Middle.
+7: Move Disk from Left to Middle.
+8: Move Disk from Left to Right.
+9: Move Disk from Middle to Right.
+10: Move Disk from Middle to Left.
+11: Move Disk from Right to Left.
+12: Move Disk from Middle to Right.
+13: Move Disk from Left to Middle.
+14: Move Disk from Left to Right.
+15: Move Disk from Middle to Right.
+*/
+
 //output for 6
 /*
-[LOG]: 1: Move from Left to Middle. 
-[LOG]: 2: Move from Left to Right. 
-[LOG]: 3: Move from Middle to Right. 
-[LOG]: 4: Move from Left to Middle. 
-[LOG]: 5: Move from Right to Left. 
-[LOG]: 6: Move from Right to Middle. 
-[LOG]: 7: Move from Left to Middle. 
-[LOG]: 8: Move from Left to Right. 
-[LOG]: 9: Move from Middle to Right. 
-[LOG]: 10: Move from Middle to Left. 
-[LOG]: 11: Move from Right to Left. 
-[LOG]: 12: Move from Middle to Right. 
-[LOG]: 13: Move from Left to Middle. 
-[LOG]: 14: Move from Left to Right. 
-[LOG]: 15: Move from Middle to Right. 
-[LOG]: 16: Move from Left to Middle. 
-[LOG]: 17: Move from Right to Left. 
-[LOG]: 18: Move from Right to Middle. 
-[LOG]: 19: Move from Left to Middle. 
-[LOG]: 20: Move from Right to Left. 
-[LOG]: 21: Move from Middle to Right. 
-[LOG]: 22: Move from Middle to Left. 
-[LOG]: 23: Move from Right to Left. 
-[LOG]: 24: Move from Right to Middle. 
-[LOG]: 25: Move from Left to Middle. 
-[LOG]: 26: Move from Left to Right. 
-[LOG]: 27: Move from Middle to Right. 
-[LOG]: 28: Move from Left to Middle. 
-[LOG]: 29: Move from Right to Left. 
-[LOG]: 30: Move from Right to Middle. 
-[LOG]: 31: Move from Left to Middle. 
-[LOG]: 32: Move from Left to Right. 
-[LOG]: 33: Move from Middle to Right. 
-[LOG]: 34: Move from Middle to Left. 
-[LOG]: 35: Move from Right to Left. 
-[LOG]: 36: Move from Middle to Right. 
-[LOG]: 37: Move from Left to Middle. 
-[LOG]: 38: Move from Left to Right. 
-[LOG]: 39: Move from Middle to Right. 
-[LOG]: 40: Move from Middle to Left. 
-[LOG]: 41: Move from Right to Left. 
-[LOG]: 42: Move from Right to Middle. 
-[LOG]: 43: Move from Left to Middle. 
-[LOG]: 44: Move from Right to Left. 
-[LOG]: 45: Move from Middle to Right. 
-[LOG]: 46: Move from Middle to Left. 
-[LOG]: 47: Move from Right to Left. 
-[LOG]: 48: Move from Middle to Right. 
-[LOG]: 49: Move from Left to Middle. 
-[LOG]: 50: Move from Left to Right. 
-[LOG]: 51: Move from Middle to Right. 
-[LOG]: 52: Move from Left to Middle. 
-[LOG]: 53: Move from Right to Left. 
-[LOG]: 54: Move from Right to Middle. 
-[LOG]: 55: Move from Left to Middle. 
-[LOG]: 56: Move from Left to Right. 
-[LOG]: 57: Move from Middle to Right. 
-[LOG]: 58: Move from Middle to Left. 
-[LOG]: 59: Move from Right to Left. 
-[LOG]: 60: Move from Middle to Right. 
-[LOG]: 61: Move from Left to Middle. 
-[LOG]: 62: Move from Left to Right. 
-[LOG]: 63: Move from Middle to Right. 
-[LOG]: 1: Move Disk from Left to Middle. 
-[LOG]: 2: Move Disk from Left to Right. 
-[LOG]: 3: Move Disk from Middle to Right. 
-[LOG]: 4: Move Disk from Left to Middle. 
-[LOG]: 5: Move Disk from Right to Left. 
-[LOG]: 6: Move Disk from Right to Middle. 
-[LOG]: 7: Move Disk from Left to Middle. 
-[LOG]: 8: Move Disk from Left to Right. 
-[LOG]: 9: Move Disk from Middle to Right. 
-[LOG]: 10: Move Disk from Middle to Left. 
-[LOG]: 11: Move Disk from Right to Left. 
-[LOG]: 12: Move Disk from Middle to Right. 
-[LOG]: 13: Move Disk from Left to Middle. 
-[LOG]: 14: Move Disk from Left to Right. 
-[LOG]: 15: Move Disk from Middle to Right. 
-[LOG]: 16: Move Disk from Left to Middle. 
-[LOG]: 17: Move Disk from Right to Left. 
-[LOG]: 18: Move Disk from Right to Middle. 
-[LOG]: 19: Move Disk from Left to Middle. 
-[LOG]: 20: Move Disk from Right to Left. 
-[LOG]: 21: Move Disk from Middle to Right. 
-[LOG]: 22: Move Disk from Middle to Left. 
-[LOG]: 23: Move Disk from Right to Left. 
-[LOG]: 24: Move Disk from Right to Middle. 
-[LOG]: 25: Move Disk from Left to Middle. 
-[LOG]: 26: Move Disk from Left to Right. 
-[LOG]: 27: Move Disk from Middle to Right. 
-[LOG]: 28: Move Disk from Left to Middle. 
-[LOG]: 29: Move Disk from Right to Left. 
-[LOG]: 30: Move Disk from Right to Middle. 
-[LOG]: 31: Move Disk from Left to Middle. 
-[LOG]: 32: Move Disk from Left to Right. 
-[LOG]: 33: Move Disk from Middle to Right. 
-[LOG]: 34: Move Disk from Middle to Left. 
-[LOG]: 35: Move Disk from Right to Left. 
-[LOG]: 36: Move Disk from Middle to Right. 
-[LOG]: 37: Move Disk from Left to Middle. 
-[LOG]: 38: Move Disk from Left to Right. 
-[LOG]: 39: Move Disk from Middle to Right. 
-[LOG]: 40: Move Disk from Middle to Left. 
-[LOG]: 41: Move Disk from Right to Left. 
-[LOG]: 42: Move Disk from Right to Middle. 
-[LOG]: 43: Move Disk from Left to Middle. 
-[LOG]: 44: Move Disk from Right to Left. 
-[LOG]: 45: Move Disk from Middle to Right. 
-[LOG]: 46: Move Disk from Middle to Left. 
-[LOG]: 47: Move Disk from Right to Left. 
-[LOG]: 48: Move Disk from Middle to Right. 
-[LOG]: 49: Move Disk from Left to Middle. 
-[LOG]: 50: Move Disk from Left to Right. 
-[LOG]: 51: Move Disk from Middle to Right. 
-[LOG]: 52: Move Disk from Left to Middle. 
-[LOG]: 53: Move Disk from Right to Left. 
-[LOG]: 54: Move Disk from Right to Middle. 
-[LOG]: 55: Move Disk from Left to Middle. 
-[LOG]: 56: Move Disk from Left to Right. 
-[LOG]: 57: Move Disk from Middle to Right. 
-[LOG]: 58: Move Disk from Middle to Left. 
-[LOG]: 59: Move Disk from Right to Left. 
-[LOG]: 60: Move Disk from Middle to Right. 
-[LOG]: 61: Move Disk from Left to Middle. 
-[LOG]: 62: Move Disk from Left to Right. 
-[LOG]: 63: Move Disk from Middle to Right. 
-[LOG]: 1: Move Disk from Left to Middle. 
-[LOG]: 2: Move Disk from Left to Right. 
-[LOG]: 3: Move Disk from Middle to Right. 
-[LOG]: 4: Move Disk from Left to Middle. 
-[LOG]: 5: Move Disk from Right to Left. 
-[LOG]: 6: Move Disk from Right to Middle. 
-[LOG]: 7: Move Disk from Left to Middle. 
-[LOG]: 8: Move Disk from Left to Right. 
-[LOG]: 9: Move Disk from Middle to Right. 
-[LOG]: 10: Move Disk from Middle to Left. 
-[LOG]: 11: Move Disk from Right to Left. 
-[LOG]: 12: Move Disk from Middle to Right. 
-[LOG]: 13: Move Disk from Left to Middle. 
-[LOG]: 14: Move Disk from Left to Right. 
-[LOG]: 15: Move Disk from Middle to Right. 
-[LOG]: 16: Move Disk from Left to Middle. 
-[LOG]: 17: Move Disk from Right to Left. 
-[LOG]: 18: Move Disk from Right to Middle. 
-[LOG]: 19: Move Disk from Left to Middle. 
-[LOG]: 20: Move Disk from Right to Left. 
-[LOG]: 21: Move Disk from Middle to Right. 
-[LOG]: 22: Move Disk from Middle to Left. 
-[LOG]: 23: Move Disk from Right to Left. 
-[LOG]: 24: Move Disk from Right to Middle. 
-[LOG]: 25: Move Disk from Left to Middle. 
-[LOG]: 26: Move Disk from Left to Right. 
-[LOG]: 27: Move Disk from Middle to Right. 
-[LOG]: 28: Move Disk from Left to Middle. 
-[LOG]: 29: Move Disk from Right to Left. 
-[LOG]: 30: Move Disk from Right to Middle. 
-[LOG]: 31: Move Disk from Left to Middle. 
-[LOG]: 32: Move Disk from Left to Right. 
-[LOG]: 33: Move Disk from Middle to Right. 
-[LOG]: 34: Move Disk from Middle to Left. 
-[LOG]: 35: Move Disk from Right to Left. 
-[LOG]: 36: Move Disk from Middle to Right. 
-[LOG]: 37: Move Disk from Left to Middle. 
-[LOG]: 38: Move Disk from Left to Right. 
-[LOG]: 39: Move Disk from Middle to Right. 
-[LOG]: 40: Move Disk from Middle to Left. 
-[LOG]: 41: Move Disk from Right to Left. 
-[LOG]: 42: Move Disk from Right to Middle. 
-[LOG]: 43: Move Disk from Left to Middle. 
-[LOG]: 44: Move Disk from Right to Left. 
-[LOG]: 45: Move Disk from Middle to Right. 
-[LOG]: 46: Move Disk from Middle to Left. 
-[LOG]: 47: Move Disk from Right to Left. 
-[LOG]: 48: Move Disk from Middle to Right. 
-[LOG]: 49: Move Disk from Left to Middle. 
-[LOG]: 50: Move Disk from Left to Right. 
-[LOG]: 51: Move Disk from Middle to Right. 
-[LOG]: 52: Move Disk from Left to Middle. 
-[LOG]: 53: Move Disk from Right to Left. 
-[LOG]: 54: Move Disk from Right to Middle. 
-[LOG]: 55: Move Disk from Left to Middle. 
-[LOG]: 56: Move Disk from Left to Right. 
-[LOG]: 57: Move Disk from Middle to Right. 
-[LOG]: 58: Move Disk from Middle to Left. 
-[LOG]: 59: Move Disk from Right to Left. 
-[LOG]: 60: Move Disk from Middle to Right. 
-[LOG]: 61: Move Disk from Left to Middle. 
-[LOG]: 62: Move Disk from Left to Right. 
-[LOG]: 63: Move Disk from Middle to Right. 
-[ERR]: Executed JavaScript Failed: 
-[ERR]: import declarations may only appear at top level of a module 
-[LOG]: 1: Move Disk from Left to Middle. 
-[LOG]: 2: Move Disk from Left to Right. 
-[LOG]: 3: Move Disk from Middle to Right. 
-[LOG]: 4: Move Disk from Left to Middle. 
-[LOG]: 5: Move Disk from Right to Left. 
-[LOG]: 6: Move Disk from Right to Middle. 
-[LOG]: 7: Move Disk from Left to Middle. 
-[LOG]: 8: Move Disk from Left to Right. 
-[LOG]: 9: Move Disk from Middle to Right. 
-[LOG]: 10: Move Disk from Middle to Left. 
-[LOG]: 11: Move Disk from Right to Left. 
-[LOG]: 12: Move Disk from Middle to Right. 
-[LOG]: 13: Move Disk from Left to Middle. 
-[LOG]: 14: Move Disk from Left to Right. 
-[LOG]: 15: Move Disk from Middle to Right. 
-[LOG]: 16: Move Disk from Left to Middle. 
-[LOG]: 17: Move Disk from Right to Left. 
-[LOG]: 18: Move Disk from Right to Middle. 
-[LOG]: 19: Move Disk from Left to Middle. 
-[LOG]: 20: Move Disk from Right to Left. 
-[LOG]: 21: Move Disk from Middle to Right. 
-[LOG]: 22: Move Disk from Middle to Left. 
-[LOG]: 23: Move Disk from Right to Left. 
-[LOG]: 24: Move Disk from Right to Middle. 
-[LOG]: 25: Move Disk from Left to Middle. 
-[LOG]: 26: Move Disk from Left to Right. 
-[LOG]: 27: Move Disk from Middle to Right. 
-[LOG]: 28: Move Disk from Left to Middle. 
-[LOG]: 29: Move Disk from Right to Left. 
-[LOG]: 30: Move Disk from Right to Middle. 
-[LOG]: 31: Move Disk from Left to Middle. 
-[LOG]: 32: Move Disk from Left to Right. 
-[LOG]: 33: Move Disk from Middle to Right. 
-[LOG]: 34: Move Disk from Middle to Left. 
-[LOG]: 35: Move Disk from Right to Left. 
-[LOG]: 36: Move Disk from Middle to Right. 
-[LOG]: 37: Move Disk from Left to Middle. 
-[LOG]: 38: Move Disk from Left to Right. 
-[LOG]: 39: Move Disk from Middle to Right. 
-[LOG]: 40: Move Disk from Middle to Left. 
-[LOG]: 41: Move Disk from Right to Left. 
-[LOG]: 42: Move Disk from Right to Middle. 
-[LOG]: 43: Move Disk from Left to Middle. 
-[LOG]: 44: Move Disk from Right to Left. 
-[LOG]: 45: Move Disk from Middle to Right. 
-[LOG]: 46: Move Disk from Middle to Left. 
-[LOG]: 47: Move Disk from Right to Left. 
-[LOG]: 48: Move Disk from Middle to Right. 
-[LOG]: 49: Move Disk from Left to Middle. 
-[LOG]: 50: Move Disk from Left to Right. 
-[LOG]: 51: Move Disk from Middle to Right. 
-[LOG]: 52: Move Disk from Left to Middle. 
-[LOG]: 53: Move Disk from Right to Left. 
-[LOG]: 54: Move Disk from Right to Middle. 
-[LOG]: 55: Move Disk from Left to Middle. 
-[LOG]: 56: Move Disk from Left to Right. 
-[LOG]: 57: Move Disk from Middle to Right. 
-[LOG]: 58: Move Disk from Middle to Left. 
-[LOG]: 59: Move Disk from Right to Left. 
-[LOG]: 60: Move Disk from Middle to Right. 
-[LOG]: 61: Move Disk from Left to Middle. 
-[LOG]: 62: Move Disk from Left to Right. 
-[LOG]: 63: Move Disk from Middle to Right. 
-Popular Documentation Pages
-*/
+1: Move Disk from Left to Middle.
+2: Move Disk from Left to Right.
+3: Move Disk from Middle to Right.
+4: Move Disk from Left to Middle.
+5: Move Disk from Right to Left.
+6: Move Disk from Right to Middle.
+7: Move Disk from Left to Middle.
+8: Move Disk from Left to Right.
+9: Move Disk from Middle to Right.
+10: Move Disk from Middle to Left.
+11: Move Disk from Right to Left.
+12: Move Disk from Middle to Right.
+13: Move Disk from Left to Middle.
+14: Move Disk from Left to Right.
+15: Move Disk from Middle to Right.
+16: Move Disk from Left to Middle.
+17: Move Disk from Right to Left.
+18: Move Disk from Right to Middle.
+19: Move Disk from Left to Middle.
+20: Move Disk from Right to Left.
+21: Move Disk from Middle to Right.
+22: Move Disk from Middle to Left.
+23: Move Disk from Right to Left.
+24: Move Disk from Right to Middle.
+25: Move Disk from Left to Middle.
+26: Move Disk from Left to Right.
+27: Move Disk from Middle to Right.
+28: Move Disk from Left to Middle.
+29: Move Disk from Right to Left.
+30: Move Disk from Right to Middle.
+31: Move Disk from Left to Middle.
+32: Move Disk from Left to Right.
+33: Move Disk from Middle to Right.
+34: Move Disk from Middle to Left.
+35: Move Disk from Right to Left.
+36: Move Disk from Middle to Right.
+37: Move Disk from Left to Middle.
+38: Move Disk from Left to Right.
+39: Move Disk from Middle to Right.
+40: Move Disk from Middle to Left.
+41: Move Disk from Right to Left.
+42: Move Disk from Right to Middle.
+43: Move Disk from Left to Middle.
+44: Move Disk from Right to Left.
+45: Move Disk from Middle to Right.
+46: Move Disk from Middle to Left.
+47: Move Disk from Right to Left.
+48: Move Disk from Middle to Right.
+49: Move Disk from Left to Middle.
+50: Move Disk from Left to Right.
+51: Move Disk from Middle to Right.
+52: Move Disk from Left to Middle.
+53: Move Disk from Right to Left.
+54: Move Disk from Right to Middle.
+55: Move Disk from Left to Middle.
+56: Move Disk from Left to Right.
+57: Move Disk from Middle to Right.
+58: Move Disk from Middle to Left.
+59: Move Disk from Right to Left.
+60: Move Disk from Middle to Right.
+61: Move Disk from Left to Middle.
+62: Move Disk from Left to Right.*/
