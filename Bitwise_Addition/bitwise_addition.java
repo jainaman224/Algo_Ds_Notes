962be272f2e9b19e8ad63a0cb8995b30c2e759b0
Tamonash Chakraborty <59210880+m-e-r-l-i-n@users.noreply.github.com>
2020-05-24 18:41:22 +0530

    Update bitwise_addition.java

--- Bitwise_Addition/bitwise_addition.java
+++ Bitwise_Addition/bitwise_addition.java
@@ -3,26 +3,28 @@ class Bitwise
 {
     public static void main(String args[])throws Exception
     {
-        BufferedReader bu=new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb=new StringBuilder();
+        BufferedReader bu = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         System.out.print("Enter two numbers: ");
-        String s[]=bu.readLine().split(" ");
-        int a=Integer.parseInt(s[0]),b=Integer.parseInt(s[1]);
+        String s[] = bu.readLine().split(" ");
+        int a = Integer.parseInt(s[0]), b = Integer.parseInt(s[1]);
         sb.append("\nusing bitwise operations,\n");
-        sb.append("\n"+a+" + "+b+" = "+bitwise(a,b));
+        sb.append("\n" + a + " + " + b + " = " + bitwise(a,b));
         System.out.print(sb);
     }
+
     static int bitwise(int a,int b)
     {
-        while(b!=0)
+        while(b != 0)
         {
-            int carry=a&b;  //common set bits of a and b
-            a=a^b;  //sum of bits of a and b where at least one bit is unset
-            b=carry<<1; //left shifting carry by 1 so that a can give the sum
+            int carry = a & b;  //common set bits of a and b
+            a = a ^ b;  //sum of bits of a and b where at least one bit is unset
+            b = carry << 1; //left shifting carry by 1 so that a can give the sum
         }
         return a;
     }
 }
+
 /*
 INPUT:
     Enter two numbers: 12 30
