d67bb0e0024d4a2826eae94df7f30030c1148268
Jitmanew Tyagi <jitmanew@gmail.com>
2020-05-25 10:52:58 +0530

    Extended eucledian algorithm in Dart

--- Extended_Euclidean_Algorithm/extended_euc_algo.dart
+++ Extended_Euclidean_Algorithm/extended_euc_algo.dart
@@ -9,26 +9,27 @@
   value of GCD(a,b) by replacing (x,y) with their respective values.
 */
 
-void main(){
-    int x = 0, y = 0, a = 98, b = 21;
+import 'dart:io';
+
+void main() {
+    int x = 0, y = 0;
+    var a = int.parse(stdin.readLineSync());
+    var b = int.parse(stdin.readLineSync());
     // function called for 98 and 21
     print(gcdFunction(a, b, x, y));
 }
 
-int gcdFunction(a,b,x,y){
-    if (a == 0){
+int gcdFunction(a, b, x, y) {
+    if (a == 0) {
         x = 0;
         y = 0;
         return b;
     }
+
     int x1 = 0, y1 = 0;
     int gcd = gcdFunction(b % a, a, x1, y1);
     x = y1 - (b / a).round() * x1;
     y = x1;
-    
+
     return gcd;
-
-    
 }
-
-
