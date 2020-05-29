c08763139bbbeb692e36418a5ec3de1f16092e91
vedangi163 <59529326+vedangi163@users.noreply.github.com>
2020-05-29 17:39:25 +0530

    Change suggested by you is done.
    Hello @asha15 @singh-shreya6 @somya-kapoor @jainaman224, change suggested by you is done. Please review my PR.

--- Radix_Sort/RadixSort.php
+++ Radix_Sort/RadixSort.php
@@ -3,46 +3,35 @@
 function countSort(&$arr, $n, $exp) 
 { 
 	// output array 
-	$output = array_fill(0, $n, 0); 
+    $output = array_fill(0, $n, 0); 
 	$count = array_fill(0, 10, 0); 
 
-	// Store count of occurrences in count[] 
+    // Store count of occurrences in count[] 
 	for ($i = 0; $i < $n; $i++) 
 		$count[ ($arr[$i] / $exp) % 10 ]++; 
 
-	/* Change count[i] so that count[i] 
-	   now contains actual position of 
-	   this digit in output[] */
+    // contains actual position of this digit in output[] 
 	for ($i = 1; $i < 10; $i++) 
 		$count[$i] += $count[$i - 1]; 
 
-	// Build the output array 
+    // Build the output array 
 	for ($i = $n - 1; $i >= 0; $i--) 
 	{ 
 		$output[$count[ ($arr[$i] / $exp) % 10 ] - 1] = $arr[$i]; 
 		$count[ ($arr[$i] / $exp) % 10 ]--; 
 	} 
 
-	/* Copy the output array to arr[], so 
-	   that arr[] now contains sorted numbers 
-	   according to current digit */ 
+    // Copy the output array to arr[]
 	for ($i = 0; $i < $n; $i++) 
 		$arr[$i] = $output[$i]; 
-} 
+   } 
 
-// The main function to that sorts arr[] 
-// of size n using Radix Sort 
+// The main function that sorts arr[]  
 function radixsort(&$arr, $n) 
 { 
-	
-	// Find the maximum number to know 
-	// number of digits 
-	$m = max($arr); 
-
-	/* Do counting sort for every digit. Note 
-	  that instead of passing digit number, 
-	  exp is passed. exp is 10^i where i is 
-	  current digit number */
+	// Find the maximum number to know number of digits
+    $m = max($arr); 
+    // Do counting sort for every digit
 	for ($exp = 1; $m / $exp > 0; $exp *= 10) 
 		countSort($arr, $n, $exp); 
 } 
@@ -51,7 +40,7 @@ function radixsort(&$arr, $n)
 function PrintArray(&$arr,$n) 
 { 
 	echo "sorted Array : ";
-	for ($i = 0; $i < $n; $i++) 
+    for ($i = 0; $i < $n; $i++) 
 		echo $arr[$i] . " "; 
 } 
 
