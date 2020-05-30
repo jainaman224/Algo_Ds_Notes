332da980988c3ac3846b8f3490bee94ec58106d9
Modassir Afzal <60973906+Moddy2024@users.noreply.github.com>
2020-05-31 04:21:41 +0530

    Floyd_Warshall_Algorithm.c

--- Floyd_Warshall_Algorithm/Floyd_Warshall_Algorithm.c
+++ Floyd_Warshall_Algorithm/Floyd_Warshall_Algorithm.c
@@ -14,49 +14,48 @@
       printf("\n The adjacency matrix is : ");
       display(adj);
      // Initialize the solution matrix same as input graph matrix. Or  we can say the initial values of shortest distances are based on shortest paths considering no intermediate vertex. 
-	 for(i=0;i<n;i++)
+     for(i=0;i<n;i++)
    {
-	 	for(j=0;j<n;j++)
-     {
-	 	 	 if(adj[i][j] == 0)
-             Q[i][j] = INFINITY;
-              else
-             Q[i][j] = adj[i][j];
-     }
+	for(j=0;j<n;j++)
+      {
+	  if(adj[i][j] == 0)
+       Q[i][j] = INFINITY;
+       else
+       Q[i][j] = adj[i][j];
+      }
    }
-	 for(k=0; k<n;k++)                   // Add all vertices one by one to the set of intermediate vertices. Before start of an iteration, we have shortest distances between all pairs of vertices such that the shortest distances consider only the vertices in set {0, 1, 2, .. k-1} as intermediate vertices. 
-                                       // After the end of an iteration, vertex no. k is added to the set of intermediate vertices and the set becomes {0, 1, 2, .. k} 
-  {
+     for(k=0; k<n;k++)           // Add all vertices one by one to the set of intermediate vertices. Before start of an iteration, we have shortest distances between all pairs of vertices such that the shortest distances consider only the vertices in set {0, 1, 2, .. k-1} as intermediate vertices. 
+     // After the end of an iteration, vertex no. k is added to the set of intermediate vertices and the set becomes {0, 1, 2, .. k} 
+     {
              // Pick all vertices as source one by one 
-	 	 for(i=0;i<n;i++)
-         {
-                // Pick all vertices as destination for the 
+         for(i=0;i<n;i++)
+           {
+             // Pick all vertices as destination for the 
             // above picked source 
 
          for(j=0;j<n;j++)               
- {
-     if(Q[i][j] <= Q[i][k] + Q[k][j])               // If vertex k is on the shortest path from 
-      Q[i][j] = Q[i][j];                            // i to j, then update the value of dist[i][j] 
-         
-     else
-     Q[i][j] = Q[i][k] + Q[k][j];
-         }
-  }
-     printf("\n\n");
-     display(Q);
-  }
+     {
+         if(Q[i][j] <= Q[i][k] + Q[k][j])               // If vertex k is on the shortest path from 
+          Q[i][j] = Q[i][j];                            // i to j, then update the value of dist[i][j] 
+         else
+         Q[i][j] = Q[i][k] + Q[k][j];
+       }
+   }
+         printf("\n\n");
+         display(Q);
+   }
 	 return 0;
- }
-     void display(int mat[n][n])
- {
-    int i, j;
-	 for(i=0;i<n;i++)
- {
-         printf("\n");
-	 	 for(j=0;j<n;j++)
+  }
+        void display(int mat[n][n])
+  {
+       int i, j;
+      for(i=0;i<n;i++)
+  {
+        printf("\n");
+       for(j=0;j<n;j++)
         printf("%d\t", mat[i][j]);
- }
- }
+   }
+  }
 /* INPUT  0       5       9999    10                                                                                                                      
           9999    0       3       9999                                                                                                                    
           9999    9999    0       1                                                                                                                       
