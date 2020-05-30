4d34cfdea5993a16d7bbd10506a29f9b95d23275
vedangi163 <59529326+vedangi163@users.noreply.github.com>
2020-05-30 11:09:07 +0530

    Radix Sort in PHP (#3006)
    * Committed changes
    
    * Change suggested by you is done.
    
    Hello @asha15 @singh-shreya6 @somya-kapoor @jainaman224, change suggested by you is done. Please review my PR.
    
    * Update RadixSort.php
    
    * Update RadixSort.php
    
    * Update RadixSort.php
    
    * Update RadixSort.php

---
+++ Radix_Sort/RadixSort.php
@@ -0,0 +1,73 @@
+<?php 
+
+function countSort(&$arr, $n, $exp) 
+{ 
+    // output array 
+    $output = array_fill(0, $n, 0); 
+    $count = array_fill(0, 10, 0); 
+
+    // Store count of occurrences in count[] 
+    for ($i = 0; $i < $n; $i++) 
+        $count[ ($arr[$i] / $exp) % 10 ]++; 
+
+    // contains actual position of this digit in output[] 
+    for ($i = 1; $i < 10; $i++) 
+        $count[$i] += $count[$i - 1]; 
+
+    // Build the output array 
+    for ($i = $n - 1; $i >= 0; $i--) 
+    { 
+        $output[$count[ ($arr[$i] / $exp) % 10 ] - 1] = $arr[$i]; 
+        $count[ ($arr[$i] / $exp) % 10 ]--; 
+    } 
+
+    // Copy the output array to arr[]
+    for ($i = 0; $i < $n; $i++) 
+        $arr[$i] = $output[$i]; 
+} 
+
+// The main function that sorts arr[]  
+function radixsort(&$arr, $n) 
+{ 
+    // Find the maximum number to know number of digits
+    $m = max($arr); 
+	
+    // Do counting sort for every digit
+    for ($exp = 1; $m / $exp > 0; $exp *= 10) 
+        countSort($arr, $n, $exp); 
+} 
+
+// A utility function to print an array 
+function PrintArray(&$arr,$n) 
+{ 
+    echo "sorted Array : ";
+    for ($i = 0; $i < $n; $i++) 
+        echo $arr[$i] . " "; 
+} 
+
+// Driver Code 
+$n = readline ("Enter size of an array : ");
+readline ("Enter the elements : ");
+for ($i = 0; $i < $n; $i++) 
+{
+    $arr[$i] = readline(); 
+}
+
+radixsort($arr, $n); 
+PrintArray($arr, $n); 
+
+/*
+OUTPUT:
+Enter size of an array : 7
+Enter the elements : 
+12
+45
+9
+85
+700
+2
+4
+Sorted Array : 2 4 9 12 45 85 700
+*/
+
+?>
