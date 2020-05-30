25bf0a7a2807a71afcb16d4ddb2b58259b88d566
KhushbooGoel01 <59544806+KhushbooGoel01@users.noreply.github.com>
2020-05-31 01:15:40 +0530

    FFT using numpy in python

---
+++ Fast_Fourier_transform/Fast_Fourier_transform_using_numpy.py
@@ -0,0 +1,5 @@
+ from numpy.fft import fft
+ from numpy import array
+ a = array([1.0, 1.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0])
+# print( ' '.join("%5.3f" % abs(f) for f in fft(a)) )
+#4.000 2.613 0.000 1.082 0.000 1.082 0.000 2.613
