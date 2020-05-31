d813a53ea9c01d293af9d4177e54227959159049
Modassir Afzal <60973906+Moddy2024@users.noreply.github.com>
2020-05-31 17:03:44 +0530

    Update Intro_Sort.cpp

--- Intro_Sort/Intro_Sort.cpp
+++ Intro_Sort/Intro_Sort.cpp
@@ -1,16 +1,14 @@
-#include<bits/stdc++.h> 
-using namespace std; 
-
-// A utility function to swap the values pointed by 
-// the two pointers 
-void swapValue(int *a, int *b) 
+ #include<bits/stdc++.h> 
+ using namespace std; 
+ // A utility function to swap the values pointed by 
+ // the two pointers 
+ void swapValue(int *a, int *b) 
 { 
        int *temp = a; 
        a = b; 
        b = temp; 
    return; 
 } 
-
 /* Function to sort an array using insertion sort*/
 void InsertionSort(int arr[], int *begin, int *end) 
 { 
@@ -18,12 +16,10 @@ void InsertionSort(int arr[], int *begin, int *end)
       // to be sorted 
       int left = begin - arr; 
       int right = end - arr; 
-
      for (int i = left+1; i <= right; i++) 
       { 
       int key = arr[i]; 
          int j = i-1; 
-
          /* Move elements of arr[0..i-1], that are 
        greater than key, to one position ahead 
        of their current position */
@@ -34,10 +30,8 @@ void InsertionSort(int arr[], int *begin, int *end)
       } 
            arr[j+1] = key; 
 } 
-
   return; 
 } 
-
     // A function to partition the array and return 
     // the partition point 
    int* Partition(int arr[], int low, int high) 
@@ -51,17 +45,14 @@ void InsertionSort(int arr[], int *begin, int *end)
         // equal to pivot 
          if (arr[j] <= pivot) 
          {
-          // increment index of smaller element 
-   i++; 
-
+          // increment index of smaller element
+                i++;
        swap(arr[i], arr[j]); 
     } 
     } 
      swap(arr[i + 1], arr[high]); 
        return (arr + i + 1); 
 } 
-
-
 // A function that find the middle of the 
 // values pointed by the pointers a, b, c 
 // and return that pointer 
@@ -85,14 +76,12 @@ int *MedianOfThree(int * a, int * b, int * c)
      if (*c <= *b && *b <= *a) 
       return (b); 
 } 
-
 // A Utility function to perform intro sort 
 void IntrosortUtil(int arr[], int * begin, 
  int * end, int depthLimit) 
   { 
     // Count the number of elements 
     int size = end - begin; 
-
     // If partition size is low then do insertion sort 
         if (size < 16) 
          { 
@@ -107,22 +96,17 @@ void IntrosortUtil(int arr[], int * begin,
        sort_heap(begin, end+1); 
          return; 
     } 
-
     // Else use a median-of-three concept to 
        // find a good pivot 
        int * pivot = MedianOfThree(begin, begin+size/2, end); 
-
         // Swap the values pointed by the two pointers 
-      swapValue(pivot, end); 
-
+      swapValue(pivot, end);
      // Perform Quick Sort 
      int * partitionPoint = Partition(arr, begin-arr, end-arr); 
      IntrosortUtil(arr, begin, partitionPoint-1, depthLimit - 1); 
      IntrosortUtil(arr, partitionPoint + 1, end, depthLimit - 1); 
-
       return; 
 } 
-
 /* Implementation of introsort*/
 void Introsort(int arr[], int *begin, int *end) 
 { 
@@ -132,7 +116,6 @@ void Introsort(int arr[], int *begin, int *end)
 
     return; 
 } 
-
 // A utility function ot print an array of size n 
 void printArray(int arr[], int n) 
 { 
@@ -140,7 +123,6 @@ for (int i=0; i < n; i++)
     printf("%d ", arr[i]); 
      printf("\n"); 
 } 
-
 // Driver program to test Introsort 
 int main() 
 { 
@@ -151,7 +133,6 @@ int main()
        // the pointer to the last element 
        Introsort(arr, arr, arr+n-1); 
        printArray(arr, n); 
-
            return(0); 
 } 
 //OUTPUT  -313 -9 -9 1 3 23 23 32 233
