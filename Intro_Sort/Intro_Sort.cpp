0a517ec20500696e47576a38c313c6481053892c
Modassir Afzal <60973906+Moddy2024@users.noreply.github.com>
2020-05-31 17:30:22 +0530

    Update Intro_Sort.cpp

--- Intro_Sort/Intro_Sort.cpp
+++ Intro_Sort/Intro_Sort.cpp
@@ -1,63 +1,63 @@
-  #include<bits/stdc++.h> 
-  using namespace std; 
-  // A utility function to swap the values pointed by 
-  // the two pointers 
-  void swapValue(int *a, int *b) 
-  { 
+    #include<bits/stdc++.h> 
+    using namespace std; 
+    // A utility function to swap the values pointed by 
+    // the two pointers 
+    void swapValue(int *a, int *b) 
+    { 
        int *temp = a; 
        a = b; 
        b = temp; 
-   return; 
-  } 
-   /* Function to sort an array using insertion sort*/
-  void InsertionSort(int arr[], int *begin, int *end) 	
-  {
-      // Get the left and the right index of the subarray 
-      // to be sorted 
-      int left  =  begin  -  arr; 
-      int right  =  end  -  arr; 
-     for (int i = left+1; i <= right; i++) 
+     return; 
+   } 
+     /* Function to sort an array using insertion sort*/
+     void InsertionSort(int arr[], int *begin, int *end) 	
+   {
+        // Get the left and the right index of the subarray 
+        // to be sorted 
+        int left  =  begin  -  arr; 
+       int right  =  end  -  arr; 
+       for (int i = left+1; i <= right; i++) 
       { 
-      int key  =  arr[i]; 
+         int key  =  arr[i]; 
          int j  =  i - 1; 
          /* Move elements of arr[0..i-1], that are 
        greater than key, to one position ahead 
        of their current position */
           while (j >= left && arr[j] > key) 
-     { 
-       arr[j+1]  =  arr[j]; 
-       j  =  j - 1; 
+      { 
+            arr[j+1]  =  arr[j]; 
+            j =  j - 1; 
       } 
            arr[j+1]  =  key; 
- } 
-  return; 
- } 
-    // A function to partition the array and return 
-    // the partition point 
-   int* Partition(int arr[], int low, int high) 
- { 
-     int pivot = arr[high];  // pivot         
-     int i  =  (low - 1); // Index of smaller element 
+   } 
+   return; 
+  } 
+       // A function to partition the array and return 
+       // the partition point 
+       int* Partition(int arr[], int low, int high) 
+  { 
+       int pivot = arr[high];  // pivot         
+       int i  =  (low - 1); // Index of smaller element 
 
-    for (int j = low; j <= high- 1; j++) 
+        for (int j = low; j <= high- 1; j++) 
       { 
          // If current element is smaller than or 
-        // equal to pivot 
+         // equal to pivot 
          if (arr[j] <= pivot) 
          {
           // increment index of smaller element
                 i++;
-       swap(arr[i], arr[j]); 
+         swap(arr[i], arr[j]); 
     } 
     } 
-     swap(arr[i + 1], arr[high]); 
+        swap(arr[i + 1], arr[high]); 
        return (arr + i + 1); 
  } 
- // A function that find the middle of the 
- // values pointed by the pointers a, b, c 
- // and return that pointer 
- int *MedianOfThree(int * a, int * b, int * c) 
- { 
+    // A function that find the middle of the 
+    // values pointed by the pointers a, b, c 
+   // and return that pointer 
+   int *MedianOfThree(int * a, int * b, int * c) 
+  { 
      if (*a < *b && *b < *c) 
         return (b); 
 
@@ -75,10 +75,10 @@
 
      if (*c <= *b && *b <= *a) 
       return (b); 
- } 
- // A Utility function to perform intro sort 
- void IntrosortUtil(int arr[], int * begin, 
-  int * end, int depthLimit) 
+  } 
+   // A Utility function to perform intro sort 
+   void IntrosortUtil(int arr[], int * begin, 
+    int * end, int depthLimit) 
   { 
     // Count the number of elements 
     int size = end - begin; 
@@ -107,17 +107,17 @@
      IntrosortUtil(arr, partitionPoint + 1, end, depthLimit - 1); 
       return; 
  } 
- /* Implementation of introsort*/
- void Introsort(int arr[], int *begin, int *end) 
- { 
+    /* Implementation of introsort*/
+    void Introsort(int arr[], int *begin, int *end) 
+  { 
    int depthLimit = 2 * log(end-begin); 
    // Perform a recursive Introsort 
     IntrosortUtil(arr, begin, end, depthLimit); 
 
     return; 
- } 
- // A utility function ot print an array of size n 
- void printArray(int arr[], int n) 
+   } 
+   // A utility function ot print an array of size n 
+   void printArray(int arr[], int n) 
  { 
    for (int i=0; i < n; i++) 
     printf("%d ", arr[i]); 
