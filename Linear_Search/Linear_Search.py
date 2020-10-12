a70e3b32f4e811d4cd3b5ea712ee318e3104ee29
Shloka gupta <chicken-biryani@users.noreply.github.com>
2020-10-12 20:55:27 +0530

    A few changes
    Would like to make more changes later.  full advantage of Python was not take so made minor changes

--- Linear_Search/Linear_Search.py
+++ Linear_Search/Linear_Search.py
@@ -1,29 +1,45 @@
-# Function for Linear Search
-def linearSearch(list, desired):
-    for i in range(0, len(list)):
-        # return positon if found
-        if(list[i] == desired):
-            return i
 
-    return -1
+#Putting it all in try except
 
-num = int(input())
-list = []
-for i in range(0, num):
-    list.append(int(input()))
-desired = int(input())
-if(linearSearch(list,desired) != -1):
-    print("Found")
-else:
-    print("Number not found")
+try:
+	num = int(input("Enter the number of integers you want in the list "))
+	list = [] 
+	#taking the number of elements wanted in the list
+	
 
 
-'''
-Input :
-num = 5
-arr = [1,4,5,6,3]
-desired = 3
+	for i in range(0, num):
+		print("Enter value of element ",i+1)
+		list.append(int(input()))
 
-Output :
-Number not found
-'''
+	#taking the elements
+
+	
+
+	Ele=int(input("Enter the element you whose position you want to find "))
+
+	#Searching the element
+
+	if Ele not in list:
+		print("The element ",Ele," is not in the list ")
+	else:
+		desired = list.index(Ele)
+		print("The position of ",Ele, "is ",i+1)
+
+except ValueError:
+	print("Enter integer only ")
+
+
+"""
+Output
+
+Enter the number of integers you want in the list 3
+Enter value of element  1
+1
+Enter value of element  2
+2
+Enter value of element  3
+3
+Enter the element you whose position you want to find 4
+The element  4  is not in the list
+"""
