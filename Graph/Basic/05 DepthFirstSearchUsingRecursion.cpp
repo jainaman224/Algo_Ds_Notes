508f33c10ff6890381c85968d3259fda2f1cca4d
Shivendra kumar jha <53100303+shivendrakrjha@users.noreply.github.com>
2020-10-15 12:03:55 +0530

    Add files via upload

---
+++ Graph/Basic/05 DepthFirstSearchUsingRecursion.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+ 
+void DFS(int u, int A[][8], int n){
+    static int visited[8] {0};
+ 
+    if (visited[u] == 0){
+        cout << u << ", " << flush;
+        visited[u] = 1;
+        for (int v=1; v<n; v++){
+            if (A[u][v] == 1 && visited[v] == 0){
+                DFS(v, A, n);
+            }
+        }
+    }
+}
+ 
+int main (){
+ 
+    int A[8][8] = {{0, 0, 0, 0, 0, 0, 0, 0},
+                   {0, 0, 1, 1, 1, 0, 0, 0},
+                   {0, 1, 0, 1, 0, 0, 0, 0},
+                   {0, 1, 1, 0, 1, 1, 0, 0},
+                   {0, 1, 0, 1, 0, 1, 0, 0},
+                   {0, 0, 0, 1, 1, 0, 1, 1},
+                   {0, 0, 0, 0, 0, 1, 0, 0},
+                   {0, 0, 0, 0, 0, 1, 0, 0}};
+ 
+    cout << "Vertex: 4 -> " << flush;
+    DFS(4, A, 8);
+    cout << endl;
+ 
+    return 0;
+}
---
+++ Graph/Basic/07 BreadthFirstSearchUsingSTLQueue.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+ 
+void BFS(int vtx, int A[][8], int n){
+    queue<int> Q;
+    int visited[8] {0};
+ 
+    // Initial
+    cout << vtx << ", " << flush;  // Visit vertex
+    visited[vtx] = 1;
+    Q.emplace(vtx);
+ 
+    // Explore
+    while (!Q.empty()){
+        int u = Q.front();  // Vertex u for exploring
+        Q.pop();
+        for (int v=1; v<=n; v++){  // Adjacent vertices of vertex u
+            if (A[u][v] == 1 && visited[v] == 0){  // Adjacent vertex and not visited
+                cout << v << ", " << flush;  // Visit vertex
+                visited[v] = 1;
+                Q.emplace(v);
+            }
+        }
+    }
+    cout << endl;
+}
+ 
+int main (){
+ 
+    int A[8][8] = {{0, 0, 0, 0, 0, 0, 0, 0},
+                   {0, 0, 1, 1, 1, 0, 0, 0},
+                   {0, 1, 0, 1, 0, 0, 0, 0},
+                   {0, 1, 1, 0, 1, 1, 0, 0},
+                   {0, 1, 0, 1, 0, 1, 0, 0},
+                   {0, 0, 0, 1, 1, 0, 1, 1},
+                   {0, 0, 0, 0, 0, 1, 0, 0},
+                   {0, 0, 0, 0, 0, 1, 0, 0}};
+ 
+    cout << "Vertex: 1 -> " << flush;
+    BFS(1, A, 8);
+ 
+    cout << "Vertex: 4 -> " << flush;
+    BFS(4, A, 8);
+ 
+ 
+    return 0;
+}
