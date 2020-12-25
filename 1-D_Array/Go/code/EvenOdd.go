2bba9a0645725848b54db1245907a0eb3ec4c3c1
Rishabh Jain <rishabh.jain.cs14@gmail.com>
2020-12-25 21:30:28 +0530

    added sample input and output

--- 1-D_Array/Go/code/EvenOdd.go
+++ 1-D_Array/Go/code/EvenOdd.go
@@ -3,18 +3,8 @@ package code
 import "fmt"
 
 // CheckEvenOrOdd checks slice elements for even and odd
-func CheckEvenOrOdd() {
-
-	var n int
-	fmt.Scanf("%d", &n)
-
-	arr := make([]int, n)
-
-	for i := 0; i < n; i++ {
-		fmt.Scanf("%d", &arr[i])
-	}
-
-	for i := 0; i < n; i++ {
+func CheckEvenOrOdd(arr []int) {
+	for i := 0; i < len(arr); i++ {
 		if arr[i]%2 == 0 {
 			fmt.Printf("%d is an even number\n", arr[i])
 		} else {
--- 1-D_Array/Go/code/FrequencyOfAnElement.go
+++ 1-D_Array/Go/code/FrequencyOfAnElement.go
@@ -3,20 +3,7 @@ package code
 import "fmt"
 
 // CheckFrequencyOfAnElement checks array for the frequency of the element
-func CheckFrequencyOfAnElement() {
-
-	var n int
-	fmt.Scanf("%d", &n)
-
-	arr := make([]int, n)
-
-	for i := 0; i < n; i++ {
-		fmt.Scanf("%d", &arr[i])
-	}
-
-	var num int
-	fmt.Scanf("%d", &num)
-
+func CheckFrequencyOfAnElement(arr []int, num int) {
 	count := 0
 	for _, v := range arr {
 		if v == num {
