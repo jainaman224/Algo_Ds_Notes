da1f012e76dea4ec99ccb0bb43faea11884edbd7
Sahil Tripathi <55251741+sahil2128@users.noreply.github.com>
2021-07-21 13:58:47 +0530

    Create Police_catch_thieves.cpp

---
+++ Police Catch Thieves/Police_catch_thieves.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+//we are returning the maximum number of thieves that can be caught 
+int policeThief(char arr[], int n, int k){
+   int caught = 0;
+   vector<int> thieves;
+   vector<int> policemen;
+   for (int i = 0; i < n; i++) { // storing the indices 
+      if (arr[i] == 'P')
+         policemen.push_back(i);
+      else if (arr[i] == 'T')
+         thieves.push_back(i);
+   }
+   int thief = 0, police = 0; // tracking lower indices of thieves 
+   while (thief < thieves.size() && police < policemen.size()) {
+     
+     // thieves can be caught 
+      if (abs(thieves[thief] - policemen[police]) <= k) {
+         caught++;
+         thief++;
+         police++;
+      }
+      else if (thieves[thief] < policemen[police])
+         thief++;
+      else
+         police++;
+   }
+   return caught;
+}
+int main(){
+   int k, n;
+   char arr2[] = {'P', 'T', 'T', 'P', 'P', 'T', 'T', 'T', 'T', 'P' };
+   k = 2;
+   n = sizeof(arr2) / sizeof(arr2[0]);
+   cout << "Maximum number of thieves that can be caught by police is :"<<policeThief(arr2, n, k);
+   return 0;
+}
